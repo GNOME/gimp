099012f211f974038803d547e49fed8a7f1f5adc
Manish Singh <yosh@src.gnome.org>
1997-12-15 03:44:55 +0000

    Configured in the flame plugin Updated the ripply-anim script (still broken) Preparation for 0.99.16 release
    -Yosh

--- plug-ins/script-fu/scripts/ripply-anim.scm
+++ plug-ins/script-fu/scripts/ripply-anim.scm
@@ -6,13 +6,22 @@
 ; of saving animations (i.e. the GIF plugin).
 ;
 
+(define (copy-layer-ripple dest-image dest-drawable source-image source-drawable)
+  (gimp-selection-all dest-image)
+  (gimp-edit-clear dest-image dest-drawable)
+  (gimp-selection-none dest-image)
+  (gimp-selection-all source-image)
+  (gimp-edit-copy source-image source-drawable)
+      (let ((floating-sel (car (gimp-edit-paste dest-image dest-drawable FALSE))))
+	(gimp-floating-sel-anchor floating-sel)))
+
 (define (script-fu-ripply-anim img drawable displacement num-frames)
   (let* ((width (car (gimp-drawable-width drawable)))
 	 (height (car (gimp-drawable-height drawable)))
 	 (ripple-image (car (gimp-image-new width height GRAY)))
 	 (ripple-layer (car (gimp-layer-new ripple-image width height GRAY_IMAGE "Ripple Texture" 100 NORMAL))))
 
-; this script generates its own displacement map
+ ; this script generates its own displacement map
 
     (gimp-image-disable-undo ripple-image)
     (gimp-palette-set-background '(127 127 127) )
@@ -22,7 +31,7 @@
     ; tile noise
     (set! rippletiled-ret (plug-in-tile 1 ripple-image ripple-layer (* width 3) (* height 3) TRUE))
     (gimp-image-enable-undo ripple-image)
-    (gimp-image-delete ripple-image)
+;    (gimp-image-delete ripple-image)
 
     (set! rippletiled-image (car rippletiled-ret))
     (set! rippletiled-layer (cadr rippletiled-ret))
@@ -44,33 +53,38 @@
     (set! xoffset (/ width num-frames))
     (set! yoffset (/ height num-frames))
 
-    (set! out-imagestack (car (gimp-image-new width height RGB)))
-    (gimp-image-disable-undo out-imagestack)
+    (let* ((out-imagestack (car (gimp-image-new width height RGB)))
+	   (this-layer (car (gimp-layer-new out-imagestack width height RGB "out-layer" 100 NORMAL))))
 
-    (while (> remaining-frames 0)
-	   (set! dup-image (car (gimp-channel-ops-duplicate rippletiled-image)))
-	   (gimp-crop dup-image width height xpos ypos)
-
-	   (set! this-layer (car (gimp-layer-copy drawable TRUE)))
-	   (gimp-image-add-layer out-imagestack this-layer 0)
-
-	   (set! dup-layer (car (gimp-image-get-active-layer dup-image)))
-	   (plug-in-displace 1 out-imagestack this-layer displacement displacement TRUE TRUE dup-layer dup-layer 2)
-
-	   (gimp-image-enable-undo dup-image)
-	   (gimp-image-delete dup-image)
-
-	   (set! remaining-frames (- remaining-frames 1))
-	   (set! xpos (+ xoffset xpos))
-	   (set! ypos (+ yoffset ypos)))
-
-    (gimp-image-enable-undo rippletiled-image)
-    (gimp-image-delete rippletiled-image)
-
-    (gimp-image-enable-undo out-imagestack)
-    (gimp-display-new out-imagestack)
-
-    (gimp-displays-flush)))
+      (gimp-image-disable-undo out-imagestack)
+      
+      (while (> remaining-frames 0)
+	     (set! dup-image (car (gimp-channel-ops-duplicate rippletiled-image)))
+	     (gimp-crop dup-image width height xpos ypos)
+	     
+	     ;	   (set! this-layer (car (gimp-layer-copy drawable TRUE)))
+	     ;	   (gimp-image-add-layer out-imagestack this-layer 0)
+	     
+	     (copy-layer-ripple out-imagestack this-layer img drawable)
+	     
+	     (set! dup-layer (car (gimp-image-get-active-layer dup-image)))
+	     (plug-in-displace 1 out-imagestack this-layer displacement displacement TRUE TRUE dup-layer dup-layer 2)
+	     
+	     (gimp-image-enable-undo dup-image)
+	     (gimp-image-delete dup-image)
+	     
+	     (set! remaining-frames (- remaining-frames 1))
+	     (set! xpos (+ xoffset xpos))
+	     (set! ypos (+ yoffset ypos)))
+      
+      (gimp-image-enable-undo rippletiled-image)
+      (gimp-image-delete rippletiled-image)
+      (gimp-image-delete ripple-image)
+      (gimp-image-enable-undo out-imagestack)
+;      (gimp-display-new out-imagestack)
+))
+  
+  (gimp-displays-flush))
 
 (script-fu-register "script-fu-ripply-anim"
 		    "<Image>/Script-Fu/Animators/Rippling"
