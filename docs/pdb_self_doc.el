69593b73ed7c1ac709c142876b9e73f79ba00901
Manish Singh <yosh@src.gnome.org>
1998-02-14 23:03:49 +0000

    Fixed up docs and autoconf interaction (part of gimp-rrh-980125-0)
    -Yosh

--- docs/pdb_self_doc.el
+++ docs/pdb_self_doc.el
@@ -7,83 +7,13 @@
 (defvar list-of-pdb-entries nil)
 (setq list-of-pdb-entries nil)
 
-(defun make-new-file(file-name)
+(defun make-new-file (file-name)
   (let ((buffer (get-buffer-create (generate-new-buffer-name file-name))))
-    (set-buffer buffer)
-    (insert
-     "\\input texinfo   @c -*-texinfo-*-
-@setfilename pdb.info
-@settitle GIMP Procedural Database Documentation
-@setchapternewpage on
-
-@ifinfo
-This file describes the GIMP procedural database.
-
-Copyright (C) 1995, 1996, 1997 by Spencer Kimball and Peter Mattis.  All rights reserved.
-
-We distribute @sc{gimp} under the terms of the GNU General Public
-License, Version 2, which we have included with this release
-in the file named @file{COPYING}, and in the ``Copying'' section of
-this manual.
-As indicated in the License,
-we provide the program
-``as is'' without warranty
-of any kind, either expressed or implied, without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-@end ifinfo
-
-@c @iftex
-@c @hyphenation{Project-Header Project-Author Project-Date Project-Version}
-@c @end iftex
-
-@titlepage
-@center @titlefont{GIMP: Procedural Database Documentation}
-@sp 2
-@center Spencer Kimball and Peter Mattis
-
-@center eXperimental Computing Facility
-@center The University of California at Berkeley
-@page
-@vskip 0pt plus 1filll
-Copyright @copyright{} 1995, 1996, 1997 Spencer Kimball and Peter Mattis.
-
-We distribute @sc{gimp} under the terms of the GNU General Public
-License, Version 2, which we have included with this release
-in the file named @file{COPYING}, and in the appendix to this manual.  As indicated in the License,
-we provide the program
-``as is'' without warranty
-of any kind, either expressed or implied, without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-@end titlepage
-
-@dircategory Elite Image Manipulation
-@direntry
-* GIMP!: (pdb).		The GIMP procedural database.
-@end direntry
-
-@node    Top, , (dir), (dir)
-
-@ifinfo
-This document is the procedural database documentation for @sc{The
-GIMP}, the GNU Image Manipulation Program. The documentation is
-automatically generated from help strings embedded in the code. It is
-intended to provide information about the functionality, arguments and
-return values for procedures in the procedural database.
-@end ifinfo
-
-@menu
-* Commands::     All commands.
-@end menu
-
-@node    Commands, , Top, Top
-@c 1
-@chapter Commands
-
-")
-    buffer))
+    (set-buffer buffer)))
 
 (defun finish-file()
-  (insert "@bye\n"))
+  ;; do nothing 
+  )
 
 (defun pdb-sort(a b)
   (string< (car a) (car b)))
@@ -160,4 +90,4 @@ return values for procedures in the procedural database.
     ))
 
 (defun make-docs-noargs ()
-  (make-docs "pdb_dump" "pdb.texi"))
+  (make-docs "pdb_dump" "pdb_dump.texi"))
