9ed940918b8a6d63a1248c589d1568034c4f80db
Manish Singh <yosh@src.gnome.org>
1998-07-17 02:19:56 +0000

    Added --cflags-noui and --libs-noui flags and appropriate sed junk to
    * gimptool.in: Added --cflags-noui and --libs-noui flags and
    appropriate sed junk to craft it. Don't call gtk-config unless
    GTK_CONFIG is set; hard code the info. Strip out redundant
    stuff from the output
    
    * libgimp/gimp.h
    * libgimp/gimpimage.c: make gimp_image_flatten return a value
    to be consistent with the PDB
    
    -Yosh

--- libgimp/gimpimage.c
+++ libgimp/gimpimage.c
@@ -239,18 +239,25 @@ gimp_image_clean_all (gint32 image_ID)
   gimp_destroy_params (return_vals, nreturn_vals);
 }
 
-void
+gint32
 gimp_image_flatten (gint32 image_ID)
 {
   GParam *return_vals;
   int nreturn_vals;
+  gint32 layer_ID;
 
   return_vals = gimp_run_procedure ("gimp_image_flatten",
 				    &nreturn_vals,
 				    PARAM_IMAGE, image_ID,
 				    PARAM_END);
 
+  layer_ID = -1;
+  if (return_vals[0].data.d_status == STATUS_SUCCESS)
+    layer_ID = return_vals[1].data.d_layer;
+
   gimp_destroy_params (return_vals, nreturn_vals);
+
+  return layer_ID;
 }
 
 void
