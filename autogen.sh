57b32958e68ddde7e1983d03862e2c40df0f7162
Manish Singh <yosh@src.gnome.org>
1998-12-15 03:13:46 +0000

    removed intl virtual modules, use them new fangled autogen.sh's
    * removed intl virtual modules, use them new fangled autogen.sh's
    
    -Yosh

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+PROJECT=GIMP
+TEST_TYPE=-d
+FILE=plug-ins
+
 DIE=0
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoconf installed to compile GIMP."
+	echo "You must have autoconf installed to compile $PROJECT."
 	echo "Download the appropriate package for your distribution,"
 	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
 	DIE=1
@@ -13,16 +22,16 @@ DIE=0
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have libtool installed to compile GIMP."
-	echo "Get ftp://alpha.gnu.org/pub/gnu/libtool-1.2b.tar.gz"
+	echo "You must have libtool installed to compile $PROJECT."
+	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.2b.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have automake installed to compile GIMP."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
 }
@@ -31,8 +40,8 @@ if test "$DIE" -eq 1; then
 	exit 1
 fi
 
-(test -d app && test -d plug-ins) || {
-	echo "You must run this script in the top-level GIMP directory"
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
 	exit 1
 }
 
@@ -41,13 +50,26 @@ if test -z "$*"; then
         echo "to pass any to it, please specify them on the $0 command line."
 fi
 
-echo processing...
+case $CC in
+*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
+esac
 
 aclocal $ACLOCAL_FLAGS
-automake
-autoconf
 
-./configure "$@"
+echo "Running gettextize...  Ignore non-fatal messages."
+# Hmm, we specify --force here, since otherwise things dont'
+# get added reliably, but we don't want to overwrite intl
+# while making dist.
+echo "no" | gettextize --copy --force
+
+# optionally feature autoheader
+(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
+
+automake $am_opt
+autoconf
+cd $ORIGDIR
+
+$srcdir/configure "$@"
 
 echo 
-echo "Now type 'make' to compile GIMP."
+echo "Now type 'make' to compile $PROJECT."
