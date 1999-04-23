7499e231b30a04a547c78c8a1889ec6a20e6c968
Asbjørn Pettersen <asbjoer@src.gnome.org>
1999-04-23 09:39:51 +0000

    sys/types.h before sys/stat.h

--- app/gimpbrushlist.c
+++ app/gimpbrushlist.c
@@ -21,14 +21,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 #ifdef HAVE_DIRENT_H
 #include <dirent.h>
 #endif
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <math.h>
 
 #include "appenv.h"
