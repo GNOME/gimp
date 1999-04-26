8253eda56a90c6e78bb83b086fc6b9fbb2e63849
Asbjørn Pettersen <asbjoer@src.gnome.org>
1999-04-26 14:13:40 +0000

    add sys/types.h

--- plug-ins/CML_explorer/CML_explorer.c
+++ plug-ins/CML_explorer/CML_explorer.c
@@ -77,6 +77,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>		/* for seed of random number */
 
--- plug-ins/FractalExplorer/FractalExplorer.c
+++ plug-ins/FractalExplorer/FractalExplorer.c
@@ -46,6 +46,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <math.h>
 #include <dirent.h>
