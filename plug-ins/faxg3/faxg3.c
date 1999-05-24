942728ab90addc0e0d1bdfa2dbabb2ad493252fc
Asbjørn Pettersen <asbjoer@src.gnome.org>
1999-05-24 19:18:03 +0000

    Add include <sys/types.h> for OS/2

--- plug-ins/faxg3/faxg3.c
+++ plug-ins/faxg3/faxg3.c
@@ -25,6 +25,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
--- plug-ins/faxg3/g3.c
+++ plug-ins/faxg3/g3.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
