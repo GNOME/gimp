199c43272d8bb89afda7aa59a90e532d9b35a745
Manish Singh <yosh@src.gnome.org>
1999-06-29 06:15:15 +0000

    CVS SUCKS SUCKS SUCKS
    -Yosh

--- plug-ins/sgi/sgi.h
+++ plug-ins/sgi/sgi.h
@@ -21,63 +21,7 @@
  *
  * Revision History:
  *
- *   $Log$
- *   Revision 1.9  1999/06/28 17:54:16  tml
- *   	* */makefile.msc: Use the DEBUG nmake variable to determine
- *   	whether to build for debugging or not.
- *
- *   	* libgimp/gimp.def: Add some missing entry points.
- *
- *   	* plug-ins/makefile.msc: Redo as to Yosh's reorg of the
- *    	sources. Add some plug-ins missing earlier. (For instance print,
- *    	which only prints to files on Win32. We still need a real Win32
- *    	print plug-in. Much code probably could be lifted from the bmp
- *    	plug-in.)
- *
- *   	* plug-ins/MapObject/arcball.c: Change Qt_ToMatrix() to void,
- *    	instead of returning the address of its parameter (dubious
- *    	practise), as its value is never used anyway.
- *
- *   	For the following changes, thanks to Hans Breuer:
- *
- *   	* plug-ins/FractalExplorer/Dialogs.h: Check for feof, not to get
- *   	into an endless loop on malformed files.
- *
- *   	* plug-ins/common/header.c: Support indexed images.
- *
- *   	* plug-ins/common/sunras.c
- *   	* plug-ins/common/xwd.c
- *   	* plug-ins/print/print.h
- *   	* plug-ins/sgi/sgi.h: Include config.h, guard inclusion of
- *    	unistd.h.
- *
- *   	* plug-ins/print/print.c: Guard for SIGBUS being undefined. Open
- *    	output file in binary mode.
- *
- *   	* po/makefile.msc: Add no.
- *
- *   Revision 1.8  1998/06/06 23:22:20  yosh
- *   * adding Lighting plugin
- *
- *   * updated despeckle, png, sgi, and sharpen
- *
- *   -Yosh
- *
- *   Revision 1.5  1998/05/17 16:01:33  mike
- *   Added <unistd.h> header file.
- *
- *   Revision 1.4  1998/04/23  17:40:49  mike
- *   Updated to support 16-bit <unsigned> image data.
- *
- *   Revision 1.3  1998/02/05  17:10:58  mike
- *   Added sgiOpenFile() function for opening an existing file pointer.
- *
- *   Revision 1.2  1997/06/18  00:55:28  mike
- *   Updated to hold length table when writing.
- *   Updated to hold current length when doing ARLE.
- *
- *   Revision 1.1  1997/06/15  03:37:19  mike
- *   Initial revision
+ *   see ChangeLog
  */
 
 #ifndef _SGI_H_
