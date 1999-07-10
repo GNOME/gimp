aa3b79d1ab5698e84e977e33871ec1b10beb1f3f
Asbjørn Pettersen <asbjoer@src.gnome.org>
1999-07-10 14:08:04 +0000

    add <float.f> or FLT_MAX,FLT_MIN

--- plug-ins/sel2path/math.c
+++ plug-ins/sel2path/math.c
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+#include <float.h>
 #include <math.h>
 #include <errno.h>
 #include <stdio.h>
