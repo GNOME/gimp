fd586862fcd41000c93ded3b3b1604a65d7192fe
jaycox <jaycox@src.gnome.org>
1999-07-28 10:45:04 +0000

    new file that shold have gone with my last commit.
    	* app/pixel_regionP.h: new file that shold have gone with my last
    	commit.

---
+++ app/pixel_regionP.h
@@ -0,0 +1,49 @@
+/* The GIMP -- an image manipulation program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __PIXEL_REGION_P_H__
+#define __PIXEL_REGION_P_H__
+
+#include <glib.h>
+#include "pixel_region.h"
+
+typedef struct _PixelRegionHolder PixelRegionHolder;
+
+struct _PixelRegionHolder
+{
+  PixelRegion *PR;
+  unsigned char *original_data;
+  int startx, starty;
+  int count;
+};
+
+
+typedef struct _PixelRegionIterator PixelRegionIterator;
+
+struct _PixelRegionIterator
+{
+  GSList *pixel_regions;
+  int dirty_tiles;
+  int region_width;
+  int region_height;
+  int portion_width;
+  int portion_height;
+  int process_count;
+};
+
+#endif  /*  __PIXEL_REGION_P_H__  */
