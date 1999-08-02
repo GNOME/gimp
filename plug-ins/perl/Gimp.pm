fac0f61eb0b003b30b64c92fc0f50a7115f875c0
Marc Lehmann <pcg@src.gnome.org>
1999-08-02 20:05:14 +0000

    see plug-ins/perl/Changes

--- plug-ins/perl/Gimp.pm
+++ plug-ins/perl/Gimp.pm
@@ -12,7 +12,7 @@ use subs qw(init end lock unlock canonicalize_color);
 require DynaLoader;
 
 @ISA=qw(DynaLoader);
-$VERSION = 1.1;
+$VERSION = 1.11;
 
 @_param = qw(
 	PARAM_BOUNDARY	PARAM_CHANNEL	PARAM_COLOR	PARAM_DISPLAY	PARAM_DRAWABLE
