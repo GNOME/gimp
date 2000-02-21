c655dfcd357b79ef184c1a92366136f650b2887f
Marc Lehmann <pcg@src.gnome.org>
2000-02-21 12:39:22 +0000

    see plug-ins/perl/Changes

--- plug-ins/perl/po/update.sh
+++ plug-ins/perl/po/update.sh
@@ -3,11 +3,5 @@
 cd ..
 test -f MANIFEST || exec echo "must be started in plug-ins/perl/po"
 
-./pxgettext `find . -name '*.pm' -o -name '*.xs' -o -path './examples/*'` Perl-Server |
-   msgmerge -w 83 po/gimp-perl.pot - >gimp-perl.pot~ &&
-   mv gimp-perl.pot~ po/gimp-perl.pot
-
-#for po in po/*.po; do
-#   msgmerge -w 83 $po po/gimp-perl.pot >$po~ && mv $po~ $po
-#done
+./pxgettext `find . -name '*.pm' -o -name '*.xs' -o -path './examples/*'` Perl-Server > po/gimp-perl.pot
 
