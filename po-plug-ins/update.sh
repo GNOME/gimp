d31c37cc66b4d5a2796e72e3cb35caf5ff7e3432
Sven Neumann <neo@src.gnome.org>
2000-05-13 21:37:03 +0000

    updated the update.sh scripts to the new version that was checked into po a few days ago
    --Sven

--- po-plug-ins/update.sh
+++ po-plug-ins/update.sh
@@ -1,8 +1,42 @@
 #!/bin/sh
 
-xgettext --default-domain=gimp-std-plugins --directory=.. \
+PACKAGE="gimp-std-plugins"
+
+if [ "x$1" = "x--help" ]; then
+
+echo Usage: ./update.sh langcode
+echo --help                  display this help and exit
+echo
+echo Examples of use:
+echo ./update.sh ----- just creates a new pot file from the source
+echo ./update.sh da -- created new pot file and updated the da.po file 
+
+elif [ "x$1" = "x" ]; then 
+
+echo "Building the $PACKAGE.pot ..."
+
+xgettext --default-domain=$PACKAGE --directory=.. \
   --add-comments --keyword=_ --keyword=N_ \
   --files-from=./POTFILES.in \
-&& test ! -f gimp-std-plugins.po \
-   || ( rm -f ./gimp-std-plugins.pot \
-    && mv gimp-std-plugins.po ./gimp-std-plugins.pot )
+&& test ! -f $PACKAGE.po \
+   || ( rm -f ./$PACKAGE.pot \
+&& mv $PACKAGE.po ./$PACKAGE.pot );
+
+else
+
+xgettext --default-domain=$PACKAGE --directory=.. \
+  --add-comments --keyword=_ --keyword=N_ \
+  --files-from=./POTFILES.in \
+&& test ! -f $PACKAGE.po \
+   || ( rm -f ./PACKAGE.pot \
+&& mv $PACKAGE.po ./$PACKAGE.pot );
+
+echo "Building the $PACKAGE.pot ..."
+echo "Now merging $1.po with $PACKAGE.pot, and creating an updated $1.po ..." 
+
+mv $1.po $1.po.old && msgmerge $1.po.old $PACKAGE.pot -o $1.po \
+&& rm $1.po.old;
+
+msgfmt --statistics $1.po
+
+fi;
--- po-script-fu/update.sh
+++ po-script-fu/update.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-xgettext --default-domain=gimp-script-fu --directory=.. \
+PACKAGE="gimp-script-fu"
+
+if [ "x$1" = "x--help" ]; then
+
+echo Usage: ./update.sh langcode
+echo --help                  display this help and exit
+echo
+echo Examples of use:
+echo ./update.sh ----- just creates a new pot file from the source
+echo ./update.sh da -- created new pot file and updated the da.po file 
+
+elif [ "x$1" = "x" ]; then 
+
+echo "Building the $PACKAGE.pot ..."
+
+xgettext --default-domain=$PACKAGE --directory=.. \
   --add-comments --keyword=_ --keyword=N_ \
   --files-from=./POTFILES.in \
 && ./script-fu-xgettext \
@@ -8,6 +23,30 @@ xgettext --default-domain=gimp-script-fu --directory=.. \
         ../plug-ins/gap/sel-to-anim-img.scm \
         ../plug-ins/webbrowser/web-browser.scm \
         >> gimp-script-fu.po \
-&& test ! -f gimp-script-fu.po \
-   || ( rm -f ./gimp-script-fu.pot \
-    && mv gimp-script-fu.po ./gimp-script-fu.pot )
+&& test ! -f $PACKAGE.po \
+   || ( rm -f ./$PACKAGE.pot \
+&& mv $PACKAGE.po ./$PACKAGE.pot );
+
+else
+
+xgettext --default-domain=$PACKAGE --directory=.. \
+  --add-comments --keyword=_ --keyword=N_ \
+  --files-from=./POTFILES.in \
+&& ./script-fu-xgettext \
+	../plug-ins/script-fu/scripts/*.scm \
+        ../plug-ins/gap/sel-to-anim-img.scm \
+        ../plug-ins/webbrowser/web-browser.scm \
+        >> gimp-script-fu.po \
+&& test ! -f $PACKAGE.po \
+   || ( rm -f ./$PACKAGE.pot \
+&& mv $PACKAGE.po ./$PACKAGE.pot );
+
+echo "Building the $PACKAGE.pot ..."
+echo "Now merging $1.po with $PACKAGE.pot, and creating an updated $1.po ..." 
+
+mv $1.po $1.po.old && msgmerge $1.po.old $PACKAGE.pot -o $1.po \
+&& rm $1.po.old;
+
+msgfmt --statistics $1.po
+
+fi;
