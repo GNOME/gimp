0f58285f40c6e81bbda886e5534f0bda5b233fd4
Piers Cornwell <piers.cornwell@bigfoot.com>
2000-08-27 20:30:11 +0000

    s/Treshold/Threshold.
    2000-08-27  Piers Cornwell  <piers.cornwell@bigfoot.com>
    
        * plug-ins/Lighting/lighting_ui.c: s/Treshold/Threshold.

--- plug-ins/Lighting/lighting_ui.c
+++ plug-ins/Lighting/lighting_ui.c
@@ -523,7 +523,7 @@ create_options_page (void)
 		      GTK_SIGNAL_FUNC (gimp_double_adjustment_update),
 		      &mapvals.pixel_treshold);
   gimp_table_attach_aligned (GTK_TABLE (table), 0, 1,
-			     _("Treshold:"), 1.0, 1.0,
+			     _("Threshold:"), 1.0, 1.0,
 			     spinbutton, 1, TRUE);
 
   gimp_help_set_help_data (spinbutton,
