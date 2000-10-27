0cc531a65d0cfa41510a36626852fbd61fa48ac4
Yuri Syrota <rasta@src.gnome.org>
2000-10-27 09:30:50 +0000

    Updated update.sh

--- po-script-fu/update.sh
+++ po-script-fu/update.sh
@@ -48,6 +48,6 @@ echo "Now merging $1.po with $PACKAGE.pot, and creating an updated $1.po ..."
 mv $1.po $1.po.old && msgmerge $1.po.old $PACKAGE.pot -o $1.po \
 && rm $1.po.old;
 
-msgfmt --statistics $1.po
+msgfmt --statistics $1.po -o /dev/null
 
 fi;
