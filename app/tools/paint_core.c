d0c7d9f8629e105ad0b48cc623f86d5274e90310
Simon Budig <simon@gimp.org>
2001-01-02 20:28:42 +0000

    use floor() before casting to gint when calculating the current brush
    2001-01-02  Simon Budig  <simon@gimp.org>
    
    	* app/paint_core.c: use floor() before casting to gint when
    	calculating the current brush coordinates. Fixes the jagged brush
    	stroke when stroking a path that leaves the image at the top or left
    	edge (bug #6043).
    
    Merged fix from HEAD.

--- app/tools/paint_core.c
+++ app/tools/paint_core.c
@@ -1068,8 +1068,8 @@ paint_core_get_paint_area (PaintCore    *paint_core,
 				   &bwidth, &bheight);
 
   /*  adjust the x and y coordinates to the upper left corner of the brush  */
-  x = (gint) paint_core->curx - (bwidth  >> 1);
-  y = (gint) paint_core->cury - (bheight >> 1);
+  x = (gint) floor (paint_core->curx) - (bwidth  >> 1);
+  y = (gint) floor (paint_core->cury) - (bheight >> 1);
 
   dwidth  = drawable_width  (drawable);
   dheight = drawable_height (drawable);
@@ -1785,8 +1785,8 @@ brush_to_canvas_tiles (PaintCore *paint_core,
 		     canvas_buf->x, canvas_buf->y,
 		     canvas_buf->width, canvas_buf->height, TRUE);
 
-  x = (gint) paint_core->curx - (brush_mask->width  >> 1);
-  y = (gint) paint_core->cury - (brush_mask->height >> 1);
+  x = (gint) floor (paint_core->curx) - (brush_mask->width  >> 1);
+  y = (gint) floor (paint_core->cury) - (brush_mask->height >> 1);
   xoff = (x < 0) ? -x : 0;
   yoff = (y < 0) ? -y : 0;
 
@@ -1814,8 +1814,8 @@ brush_to_canvas_buf (PaintCore *paint_core,
   gint xoff;
   gint yoff;
 
-  x = (gint) paint_core->curx - (brush_mask->width  >> 1);
-  y = (gint) paint_core->cury - (brush_mask->height >> 1);
+  x = (gint) floor (paint_core->curx) - (brush_mask->width  >> 1);
+  y = (gint) floor (paint_core->cury) - (brush_mask->height >> 1);
   xoff = (x < 0) ? -x : 0;
   yoff = (y < 0) ? -y : 0;
 
@@ -1858,8 +1858,8 @@ paint_to_canvas_tiles (PaintCore *paint_core,
 		     canvas_buf->x, canvas_buf->y,
 		     canvas_buf->width, canvas_buf->height, TRUE);
 
-  x = (gint) paint_core->curx - (brush_mask->width  >> 1);
-  y = (gint) paint_core->cury - (brush_mask->height >> 1);
+  x = (gint) floor (paint_core->curx) - (brush_mask->width  >> 1);
+  y = (gint) floor (paint_core->cury) - (brush_mask->height >> 1);
   xoff = (x < 0) ? -x : 0;
   yoff = (y < 0) ? -y : 0;
 
@@ -1903,8 +1903,8 @@ paint_to_canvas_buf (PaintCore *paint_core,
   gint xoff;
   gint yoff;
 
-  x = (gint) paint_core->curx - (brush_mask->width  >> 1);
-  y = (gint) paint_core->cury - (brush_mask->height >> 1);
+  x = (gint) floor (paint_core->curx) - (brush_mask->width  >> 1);
+  y = (gint) floor (paint_core->cury) - (brush_mask->height >> 1);
   xoff = (x < 0) ? -x : 0;
   yoff = (y < 0) ? -y : 0;
 
@@ -2056,8 +2056,8 @@ paint_core_color_area_with_pixmap (PaintCore            *paint_core,
    * paint_core_get_paint_area.  Ugly to have to do this here, too.
    */
 
-  ulx = (gint) paint_core->curx - (pixmap_mask->width  >> 1);
-  uly = (gint) paint_core->cury - (pixmap_mask->height >> 1);
+  ulx = (gint) floor (paint_core->curx) - (pixmap_mask->width  >> 1);
+  uly = (gint) floor (paint_core->cury) - (pixmap_mask->height >> 1);
 
   offsetx = area->x - ulx;
   offsety = area->y - uly;
