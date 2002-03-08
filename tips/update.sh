7090eca719c4336b5454be42ab1f5ac54f482761
Sven Neumann <neo@src.gnome.org>
2002-03-08 19:02:29 +0000

    Attempt to validate the XML after updating.

--- tips/update.sh
+++ tips/update.sh
@@ -34,3 +34,18 @@ echo "Building the $PACKAGE.pot, merging and updating ..."
 intltool-update --gettext-package $PACKAGE $1
 
 fi
+
+intltool-merge . gimp-tips.xml.in gimp-tips.xml -x -u -c .intltool-merge-cache
+
+
+xmllint=`which xmllint`
+if test -n "$xmllint" && test -x "$xmllint"; then
+    echo "Validating gimp-tips.xml."
+    $xmllint --noout --valid gimp-tips.xml \
+    || ( echo "*************************";
+	 echo "* gimp-tips.xml INVALID *";
+	 echo "*************************";
+	 exit 1; )
+else
+	echo "Can't find xmllint to validate gimp-tips.xml; proceed with fingers crossed...";
+fi
