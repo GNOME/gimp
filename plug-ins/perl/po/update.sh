28d5de44e23b6cea6d11ec480e71d8cd270d8570
Kwok-Koon Cheung <baddog@src.gnome.org>
2002-05-26 21:33:57 +0000

    trivial addition

--- plug-ins/perl/po/update.sh
+++ plug-ins/perl/po/update.sh
@@ -2,5 +2,8 @@
 
 test -f ../MANIFEST || exec echo "must be started in plug-ins/perl/po"
 
-make update-po
-
+if test $# -eq 1 -a "$1" = "--pot"; then
+	perl ../pxgettext `find .. -type f -print | grep '\.pm$\|\.xs$\|examples/\|Perl-Server'` > gimp-perl.pot
+else
+	make update-po
+fi
