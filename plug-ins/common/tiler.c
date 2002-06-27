ba1cbd1674645da134ac992d9667cd6c2acef547
Maurits Rijk <maurits@src.gnome.org>
2002-06-27 18:49:52 +0000

    Major performance improvement for Make Seamless filter and fix for #67686

--- plug-ins/common/tiler.c
+++ plug-ins/common/tiler.c
@@ -5,7 +5,6 @@
 
 /* TODO:
  * + better basis function
- * + optimize
  */
 
 /* History:
@@ -108,7 +107,6 @@ run (gchar      *name,
     }
   else
     {
-      /* gimp_message ("laplace: cannot operate on indexed color images"); */
       status = GIMP_PDB_EXECUTION_ERROR;
     }
 
@@ -121,7 +119,6 @@ run (gchar      *name,
   gimp_drawable_detach (drawable);
 }
 
-
 static gint
 scale (gint width,
        gint height,
@@ -133,19 +130,15 @@ scale (gint width,
   gint B = height/2-1;
   gint a, b;
 
-  if (x<width/2)
-    a = width/2-x-1; 
-  else if ((x==width/2) && (width&1))
-      return data;
+  if (x < width/2)
+    a = width/2 - x - 1; 
   else
-    a = x-width/2-(width&1);
+    a = x - width/2 - (width & 1);
 
-  if (y<height/2)
-    b = height/2-y-1; 
-  else if ((y==height/2) && (height&1))
-    return data;
+  if (y < height/2)
+    b = height/2 - y -1; 
   else
-    b = y-height/2-(height&1);
+    b = y - height/2 - (height & 1);
   
   if ((B*a<A*b) || ((B*a==A*b) && (a&1)))
     {
@@ -154,100 +147,158 @@ scale (gint width,
       if (a==A)
 	return data;
       else
-	return data-((data*(A*B-a*B))/(A*b+A*B-a*B));
+	return data - data * (A*B-a*B) /(A * b + A * B - a * B);
     }
   else
     {
       if (a==A)
 	return 0;
       else 
-	return (data*(A*B-a*B))/(A*b+A*B-a*B);
+	return data * (A * B - a * B) / (A * b + A * B - a * B);
   }
 }
 
-
 static void
-tile (GimpDrawable *drawable)
+tile_region (GimpDrawable *drawable, gboolean left,
+	     gint x1, gint y1, gint x2, gint y2)
 {
-  glong      bytes;
-  glong      val;
+  glong      width, height;
+  gint       bpp;
   gint       wodd, hodd;
-  GimpPixelRgn  srcPR, destPR;
-  gint       x1, y1, x2, y2;
-  gint       row, col, width, height, c;
-  guchar    *cur_row, *dest_cur, *dest_top, *dest_bot;
+  gint	     w, h, x, y;
+  gint	     rgn1_x, rgn2_x, off_x;
+  static gint progress = 0;
+  gint       max_progress;
+  GimpPixelRgn  src1_rgn, src2_rgn, dest1_rgn, dest2_rgn;
+  gpointer     pr;
 
-  /* Get the input */
+  bpp = gimp_drawable_bpp (drawable->drawable_id);
 
-  gimp_drawable_mask_bounds (drawable->drawable_id, &x1, &y1, &x2, &y2);
-  gimp_progress_init (_("Tiler..."));
-  
-  width = x2 - x1;
   height = y2 - y1;
-  bytes  = drawable->bpp;
-
-  /*  allocate row buffers  */
-  cur_row  = g_new (guchar, width * bytes);
-  dest_cur = g_new0 (guchar, width * bytes);
-  dest_top = g_new (guchar, width * bytes);
-  dest_bot = g_new (guchar, width * bytes);
-
-  gimp_pixel_rgn_init (&srcPR, drawable, x1, y1, width, height, FALSE, FALSE);
-  gimp_pixel_rgn_init (&destPR, drawable, x1, y1, width, height, TRUE, TRUE);
+  width = x2 - x1;
 
   wodd = width & 1;
   hodd = height & 1;
 
-  for (row = 0; row  < height; row++)
+  w = width / 2;
+  h = height / 2;
+
+  if (left) 
     {
-      gimp_pixel_rgn_set_row (&destPR, dest_cur, x1, y1 + row, width);
+      rgn1_x = x1;
+      rgn2_x = x1 + w + wodd;
+      off_x = w + wodd;
+    } 
+  else
+    {
+      rgn1_x = x1 + w + wodd;
+      rgn2_x = x1;
+      off_x = -w - wodd;
     }
 
-  for (row = 0; row < height; row++)
-    {
-      gimp_pixel_rgn_get_row (&srcPR, cur_row, x1, y1 + row, width);
-      gimp_pixel_rgn_get_row (&destPR, dest_cur, x1, y1 + row, width);
-      if (row >= height/2+hodd)
-	gimp_pixel_rgn_get_row (&destPR, dest_top,
-				x1, y1+(row-height/2-hodd), width);
-      if (row < height/2)
-	gimp_pixel_rgn_get_row (&destPR, dest_bot,
-				x1, y1+(row+height/2+hodd), width);
+  gimp_pixel_rgn_init (&src1_rgn, drawable, rgn1_x, y1, w, h, FALSE, FALSE);
+  gimp_pixel_rgn_init (&dest1_rgn, drawable, rgn1_x, y1, w, h, TRUE, TRUE);
+  gimp_pixel_rgn_init (&src2_rgn, drawable, rgn2_x, y1 + h + hodd, 
+		       w, h, FALSE, FALSE);
+  gimp_pixel_rgn_init (&dest2_rgn, drawable, rgn2_x, y1 + h + hodd, 
+		       w, h, TRUE, TRUE);
 
-      for (col = 0; col < width; col++)
+  max_progress = width * height / 2;
+
+  for (pr = gimp_pixel_rgns_register (4, &src1_rgn, &dest1_rgn, &src2_rgn,
+				      &dest2_rgn);
+       pr != NULL;
+       pr = gimp_pixel_rgns_process (pr))
+    {
+      guchar *src1  = src1_rgn.data;
+      guchar *dest1 = dest1_rgn.data;
+      guchar *src2  = src2_rgn.data;
+      guchar *dest2 = dest2_rgn.data;
+      gint row = src1_rgn.y - y1;
+
+      for (y = 0; y < src1_rgn.h; y++, row++)
 	{
-	  for (c = 0; c < bytes; c++)
+	  guchar *s1 = src1;
+	  guchar *s2 = src2;
+	  guchar *d1 = dest1;
+	  guchar *d2 = dest2;
+	  gint col = src1_rgn.x - x1;
+
+	  for (x = 0; x < src1_rgn.w; x++, col++)
 	    {
-	      val = scale (width, height, col, row, cur_row[col*bytes+c]);
+	      gint c;
 
-	      /* Main image */
-	      dest_cur[col*bytes+c] += val;
-	      /* top left */
-	      if ((col>=width/2+wodd) && (row>=height/2+hodd))
-		dest_top[(col-width/2-wodd)*bytes+c] += val;
-	      /* top right */
-	      if ((col<width/2) && (row>=height/2+hodd))
-		dest_top[(width/2+col+wodd)*bytes+c] += val;
-	      /* bottom left */
-	      if ((col>=width/2+wodd) && (row<height/2))
-		dest_bot[(col-width/2-wodd)*bytes+c] += val;
-	      /* bottom right */
-	      if ((col<width/2) && (row<height/2))
-		dest_bot[(col+width/2+wodd)*bytes+c] += val;
+	      for (c = 0; c < bpp; c++)
+		{
+		  gint val = scale (width, height, col, row, s1[c]) + 
+		     scale (width, height, col + off_x, row + h + hodd, s2[c]);
+
+		  d1[c] = d2[c] = (val > 255) ? 255 : val;
+		}
+	      s1 += bpp;
+	      s2 += bpp;
+	      d1 += bpp;
+	      d2 += bpp;
 	    }
+
+	  src1 += src1_rgn.rowstride;
+	  src2 += src2_rgn.rowstride;
+	  dest1 += dest1_rgn.rowstride;
+	  dest2 += dest2_rgn.rowstride;
 	}
-
-      gimp_pixel_rgn_set_row (&destPR, dest_cur, x1, y1+row, width);
-      if (row >= height/2+hodd)
-	gimp_pixel_rgn_set_row (&destPR, dest_top,
-				x1, y1+(row-height/2-hodd), width);
-      if (row < height/2)
-	gimp_pixel_rgn_set_row (&destPR, dest_bot,
-				x1, y1+(row+height/2+hodd), width);
-
-      if ((row % 5) == 0)
-	gimp_progress_update ((gdouble) row / (gdouble) height);
+      progress += src1_rgn.w * src1_rgn.h;
+      gimp_progress_update ((gdouble) progress / (gdouble) max_progress);
     }
+}
+
+static void
+copy_region (GimpDrawable *drawable, gint x, gint y, gint w, gint h)
+{
+  GimpPixelRgn  src_rgn, dest_rgn;
+  gpointer     pr;
+
+  gimp_pixel_rgn_init (&src_rgn, drawable, x, y, w, h, FALSE, FALSE);
+  gimp_pixel_rgn_init (&dest_rgn, drawable, x, y, w, h, TRUE, TRUE);
+
+  for (pr = gimp_pixel_rgns_register (2, &src_rgn, &dest_rgn);
+       pr != NULL;
+       pr = gimp_pixel_rgns_process (pr))
+    {
+      gint k;
+
+      for (k = 0; k < src_rgn.h; k++)
+	{
+	  memcpy (dest_rgn.data + k * dest_rgn.rowstride,
+		  src_rgn.data + k * src_rgn.rowstride,
+		  src_rgn.w * src_rgn.bpp);
+	}
+    }
+}
+
+static void
+tile (GimpDrawable *drawable)
+{
+  glong      width, height;
+  gint       x1, y1, x2, y2;
+
+  gimp_drawable_mask_bounds (drawable->drawable_id, &x1, &y1, &x2, &y2);
+  gimp_progress_init (_("Tiler..."));
+  
+  height = y2 - y1;
+  width = x2 - x1;
+
+  if (width & 1) /* Copy middle column */
+    {
+       copy_region (drawable, x1 + width / 2, y1, 1, height);
+    }
+
+  if (height & 1) /* Copy middle row */
+    {
+      copy_region (drawable, x1, y1 + height / 2, width, 1);
+    }
+
+  tile_region (drawable, TRUE, x1, y1, x2, y2);
+  tile_region (drawable, FALSE, x1, y1, x2, y2);
 
   gimp_drawable_flush (drawable);
   gimp_drawable_merge_shadow (drawable->drawable_id, TRUE);
