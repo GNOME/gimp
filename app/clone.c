f93cca77d26c6dccc077b2b48323192dd482eb45
Manish Singh <yosh@gimp.org>
2002-09-05 07:17:07 +0000

    only do the below for image clone, pattern clones don't have source
    2002-09-03  Manish Singh  <yosh@gimp.org>
    
            * app/clone.c: only do the below for image clone, pattern clones
            don't have source drawables

--- app/clone.c
+++ app/clone.c
@@ -304,7 +304,13 @@ clone_paint_func (PaintCore    *paint_core,
     {
       gint off_x, off_y;
 
-      gimp_drawable_offsets (src_drawable_, &off_x, &off_y);
+      if (clone_options->type == PATTERN_CLONE)
+	{
+	  off_x = 0;
+	  off_y = 0;
+	}
+      else
+	gimp_drawable_offsets (src_drawable_, &off_x, &off_y);
 
       /*  Find the target cursor's location onscreen  */
       gdisplay_transform_coords (src_gdisp,
