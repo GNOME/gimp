70c3ea651b521dda067a56dc754967cc13475550
Sven Neumann <sven@gimp.org>
2003-02-05 08:45:26 +0000

    app/text/gimptextlayer.c fixed includes.
    2003-02-05  Sven Neumann  <sven@gimp.org>
    
    	* app/text/gimptextlayer.c
    	* app/tools/gimptexttool.c: fixed includes.

--- app/text/gimptextlayer.c
+++ app/text/gimptextlayer.c
@@ -24,7 +24,6 @@
 #include <string.h>
 
 #include <glib-object.h>
-#include <pango/pangoft2.h>
 
 #include "text-types.h"
 
--- app/tools/gimptexttool.c
+++ app/tools/gimptexttool.c
@@ -25,6 +25,7 @@
 
 #include <gtk/gtk.h>
 
+#include "libgimpbase/gimpbase.h"
 #include "libgimpwidgets/gimpwidgets.h"
 
 #include "tools-types.h"
