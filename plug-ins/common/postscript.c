e41fcd1ea191a42c77f6d818e7bd5009e280fd8d
Manish Singh <yosh@gimp.org>
2003-03-22 01:07:10 +0000

    always quote the filename, since system/popen uses the shell (dropped in
    2003-03-21  Manish Singh  <yosh@gimp.org>
    
            * plug-ins/common/ps.c: always quote the filename, since system/popen
            uses the shell (dropped in g_shell_quote from glib2). Also, never
            quote the filename if we are reading from a parameter file (only
            in the win32 case). Fixes #108648.
    
            * tools/pdbgen/lib.pl: fix minor buglet that earlier perls silently
            ate (fix from HEAD)

--- plug-ins/common/postscript.c
+++ plug-ins/common/postscript.c
@@ -90,6 +90,14 @@ static char ident[] = "@(#) GIMP PostScript/PDF file-plugin v1.15  04-Oct-2002";
 
 #define STR_LENGTH 64
 
+#ifndef G_OS_WIN32
+#define DEFAULT_GS_PROG "gs"
+#else
+/* We want the console ghostscript application. It should be in the PATH */
+#define DEFAULT_GS_PROG "gswin32c"
+#endif
+
+
 /* Load info */
 typedef struct
 {
@@ -244,6 +252,10 @@ static void dither_grey     (guchar *grey,
 			     guchar *bw,
 			     gint    npix,
 			     gint    linecount);
+
+
+/* Misc utility from glib2 */
+static gchar *g_shell_quote (const gchar *unquoted_string);
 
 
 /* Dialog-handling */
@@ -1279,13 +1291,12 @@ ps_open (gchar            *filename,
          gint             *ury,
          gint             *is_epsf)
 {
-  char *cmd, *gs, *gs_opts, *driver, *fnbuf = NULL;
+  char *cmd, *gs, *gs_opts, *driver, *quoted_fn, *fnbuf = NULL;
   FILE *fd_popen;
   int width, height, resolution;
   int x0, y0, x1, y1;
   int offx = 0, offy = 0;
   int is_pdf, maybe_epsf = 0;
-  int blank, anf, apo;
   char TextAlphaBits[64], GraphicsAlphaBits[64], geometry[32];
   char offset[32];
 
@@ -1370,43 +1381,10 @@ ps_open (gchar            *filename,
 #endif
 
   gs = getenv ("GS_PROG");
-#ifndef G_OS_WIN32
-  if (gs == NULL) gs = "gs";
-
-  /* Escape special characters. Escaping " does not work with call of shell. */
-  /* fnbuf points to memory that should be freed. */
-  filename = fnbuf = gimp_strescape (filename, "\"");
-  blank = (strchr (filename, ' ') != NULL);
-  apo = (strchr (filename, '\'') != NULL);
-  anf = (strchr (filename, '"') != NULL);
-
-  /* Must the filename be enclosed ? */
-  /* If we have " and ' it will not work */
-  if (blank || anf || apo)
-  {
-    if (!anf) /* No " ? Enclose with " */
-    {
-      filename = g_strdup_printf ("\"%s\"", filename);
-      g_free (fnbuf);
-      fnbuf = filename;
-    }
-    else if (!apo) /* No ' ? Enclose with ' */
-    {
-      filename = g_strdup_printf ("'%s'", filename);
-      g_free (fnbuf);
-      fnbuf = filename;
-    }
-  }
-
-#else
-  /* We want the console ghostscript application. It should be in the PATH */
   if (gs == NULL)
-    gs = "gswin32c";
-  /* Quote the filename in case it contains spaces. Ignore memory leak,
-   * this is a short-lived plug-in.
-   */
-  filename = g_strdup_printf ("\"%s\"", filename);
-#endif
+    gs = DEFAULT_GS_PROG;
+
+  quoted_fn = g_shell_quote (filename);
 
   gs_opts = getenv ("GS_OPTIONS");
   if (gs_opts == NULL)
@@ -1432,11 +1410,11 @@ ps_open (gchar            *filename,
   if (!is_pdf)    /* For PDF, we cant set geometry */
     sprintf (geometry,"-g%dx%d ", width, height);
 
-  cmd = g_strdup_printf ("%s -sDEVICE=%s -r%d %s%s%s-q -dNOPAUSE %s \
--sOutputFile=%s %s-f %s %s-c quit",
+  cmd = g_strdup_printf ("%s -sDEVICE=%s -r%d %s%s%s-q -dNOPAUSE %s "
+                         "-sOutputFile=%s %s-f %s %s-c quit",
 			 gs, driver, resolution, geometry,
 			 TextAlphaBits, GraphicsAlphaBits,
-			 gs_opts, pnmfile, offset, filename,
+			 gs_opts, pnmfile, offset, quoted_fn,
                          *is_epsf ? "-c showpage " : "");
 #ifdef PS_DEBUG
   g_print ("Going to start ghostscript with:\n%s\n", cmd);
@@ -1464,7 +1442,12 @@ ps_open (gchar            *filename,
       indirfile = g_strdup_printf ("%s" G_DIR_SEPARATOR_S "i%lx",
 				   g_get_tmp_dir (), getpid ());
       indf = fopen (indirfile, "w");
-      fprintf (indf, "%s\n", cmd + strlen (gs) + 1);
+      fprintf (indf, "%s -sDEVICE=%s -r%d %s%s%s-q -dNOPAUSE %s "
+	             "-sOutputFile=%s %s-f %s %s-c quit\n",
+	       gs, driver, resolution, geometry,
+	       TextAlphaBits, GraphicsAlphaBits,
+	       gs_opts, pnmfile, offset, filename,
+	       *is_epsf ? "-c showpage " : "");
       sprintf (cmd, "%s @%s", gs, indirfile);
       fclose (indf);
     }
@@ -1474,6 +1457,7 @@ ps_open (gchar            *filename,
 #endif
   g_free (cmd);
   g_free (fnbuf);
+  g_free (quoted_fn);
 
   return (fd_popen);
 }
@@ -3042,4 +3026,45 @@ save_unit_toggle_update (GtkWidget *widget,
 	  gtk_adjustment_set_value (GTK_ADJUSTMENT (vals->adjustment[i]), value);
 	}
     }
+}
+
+/* Misc utility from glib2 */
+static gchar *
+g_shell_quote (const gchar *unquoted_string)
+{
+  /* We always use single quotes, because the algorithm is cheesier.
+   * We could use double if we felt like it, that might be more
+   * human-readable.
+   */
+
+  const gchar *p;
+  GString *dest;
+  gchar *ret;
+
+  g_return_val_if_fail (unquoted_string != NULL, NULL);
+
+  dest = g_string_new ("'");
+
+  p = unquoted_string;
+
+  /* could speed this up a lot by appending chunks of text at a
+   * time.
+   */
+  while (*p)
+    {
+      /* Replace literal ' with a close ', a \', and a open ' */
+      if (*p == '\'')
+        g_string_append (dest, "'\\''");
+      else
+        g_string_append_c (dest, *p);
+
+      ++p;
+    }
+
+  /* close the quote */
+  g_string_append_c (dest, '\'');
+
+  ret = dest->str;
+  g_string_free (dest, FALSE);
+  return ret;
 }
