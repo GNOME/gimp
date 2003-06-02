911c8a5e1bfe2a4dc0e8734dbb4b0e99172b9990
Sven Neumann <sven@gimp.org>
2003-06-02 12:31:41 +0000

    merged fix for bug #114225 from HEAD.
    2003-06-02  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/common/grid.c: merged fix for bug #114225 from HEAD.

--- plug-ins/common/grid.c
+++ plug-ins/common/grid.c
@@ -427,11 +427,11 @@ doit (gint32        image_ID,
       else 
 	gimp_pixel_rgn_get_row (&srcPR, dest, sx1, y, (sx2 - sx1));
 
-      y_offset = y - grid_cfg.voffset;
+      y_offset = y - grid_cfg.hoffset;
       while (y_offset < 0)
-	y_offset += grid_cfg.vspace;
+	y_offset += grid_cfg.hspace;
 
-      if ((y_offset + (grid_cfg.vwidth / 2)) % grid_cfg.vspace < grid_cfg.vwidth)
+      if ((y_offset + (grid_cfg.hwidth / 2)) % grid_cfg.hspace < grid_cfg.hwidth)
 	{
 	  for (x = sx1; x < sx2; x++)
 	    {
@@ -439,21 +439,21 @@ doit (gint32        image_ID,
 	    }
 	}
 
-      if ((y_offset + (grid_cfg.iwidth / 2)) % grid_cfg.vspace < grid_cfg.iwidth)
+      if ((y_offset + (grid_cfg.iwidth / 2)) % grid_cfg.hspace < grid_cfg.iwidth)
         {
 	  for (x = sx1; x < sx2; x++)
 	    {
-	      x_offset = grid_cfg.hspace + x - grid_cfg.hoffset;
+	      x_offset = grid_cfg.vspace + x - grid_cfg.voffset;
 	      while (x_offset < 0)
-		x_offset += grid_cfg.hspace;
+		x_offset += grid_cfg.vspace;
 
-              if ((x_offset % grid_cfg.hspace >= grid_cfg.ispace
+              if ((x_offset % grid_cfg.vspace >= grid_cfg.ispace
 		   && 
-		   x_offset % grid_cfg.hspace < grid_cfg.ioffset) 
+		   x_offset % grid_cfg.vspace < grid_cfg.ioffset) 
 		  || 
-		  (grid_cfg.hspace - (x_offset % grid_cfg.hspace) >= grid_cfg.ispace
+		  (grid_cfg.vspace - (x_offset % grid_cfg.vspace) >= grid_cfg.ispace
 		   && 
-		   grid_cfg.hspace - (x_offset % grid_cfg.hspace) < grid_cfg.ioffset))
+		   grid_cfg.vspace - (x_offset % grid_cfg.vspace) < grid_cfg.ioffset))
 		{
 		  pix_composite (&dest[(x-sx1) * bytes], icolor, bytes, blend, alpha);
                 }
@@ -462,24 +462,24 @@ doit (gint32        image_ID,
 
       for (x = sx1; x < sx2; x++)
         {
-	  x_offset = grid_cfg.hspace + x - grid_cfg.hoffset;
+	  x_offset = grid_cfg.vspace + x - grid_cfg.voffset;
 	  while (x_offset < 0)
-	    x_offset += grid_cfg.hspace;
+	    x_offset += grid_cfg.vspace;
 
-          if ((x_offset + (grid_cfg.hwidth / 2)) % grid_cfg.hspace < grid_cfg.hwidth)
+          if ((x_offset + (grid_cfg.vwidth / 2)) % grid_cfg.vspace < grid_cfg.vwidth)
             {
 	      pix_composite (&dest[(x-sx1) * bytes], vcolor, bytes, blend, alpha);
             }
 
-          if ((x_offset + (grid_cfg.iwidth / 2)) % grid_cfg.hspace < grid_cfg.iwidth
+          if ((x_offset + (grid_cfg.iwidth / 2)) % grid_cfg.vspace < grid_cfg.iwidth
 	      && 
-	      ((y_offset % grid_cfg.vspace >= grid_cfg.ispace
+	      ((y_offset % grid_cfg.hspace >= grid_cfg.ispace
 		&& 
-		y_offset % grid_cfg.vspace < grid_cfg.ioffset)
+		y_offset % grid_cfg.hspace < grid_cfg.ioffset)
 	       || 
-	       (grid_cfg.vspace - (y_offset % grid_cfg.vspace) >= grid_cfg.ispace
+	       (grid_cfg.hspace - (y_offset % grid_cfg.hspace) >= grid_cfg.ispace
 		&& 
-		grid_cfg.vspace - (y_offset % grid_cfg.vspace) < grid_cfg.ioffset)))
+		grid_cfg.hspace - (y_offset % grid_cfg.hspace) < grid_cfg.ioffset)))
             {
 	      pix_composite (&dest[(x-sx1) * bytes], icolor, bytes, blend, alpha);
             }
