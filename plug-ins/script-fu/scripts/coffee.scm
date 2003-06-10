7a04ec894cec3c977612241a3cc40eccb1e38e00
Michael Natterer <mitch@gimp.org>
2003-06-10 12:57:27 +0000

    added an undo group around all operations.
    2003-06-10  Michael Natterer  <mitch@gimp.org>
    
    	* plug-ins/script-fu/scripts/coffee.scm: added an undo group
    	around all operations.
    
    	* plug-ins/script-fu/scripts/distress-selection.scm: replaced
    	undo disable/enable by an undo group.
    
    	Together fixes bug #114493. Removed weird spacing and indented
    	both scripts to make them readable.

--- plug-ins/script-fu/scripts/coffee.scm
+++ plug-ins/script-fu/scripts/coffee.scm
@@ -1,6 +1,3 @@
-;
-;
-;
 ; Chris Gutteridge (cjg@ecs.soton.ac.uk)
 ; At ECS Dept, University of Southampton, England.
 
@@ -19,53 +16,66 @@
 ; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 
-(define (script-fu-coffee-stain inImage inLayer inNumber inDark)
+(define (script-fu-coffee-stain inImage
+				inLayer
+				inNumber
+				inDark)
 
    (set! old-gradient (car (gimp-gradients-get-gradient)))
    (set! theImage inImage)
    (set! theHeight (car (gimp-image-height theImage)))
    (set! theWidth (car (gimp-image-width theImage)))
    (set! theNumber inNumber)
-   (set! theSize (min theWidth theHeight) )
+   (set! theSize (min theWidth theHeight))
+
+   (gimp-undo-push-group-start theImage)
 
    (while (> theNumber 0)
-       (set! theNumber (- theNumber 1))
-       (set! theStain (car (gimp-layer-new theImage theSize theSize RGBA_IMAGE "Stain" 100
-             (if (= inDark TRUE) DARKEN-ONLY NORMAL)          )))
+	  (set! theNumber (- theNumber 1))
+	  (set! theStain (car (gimp-layer-new theImage theSize theSize
+					      RGBA_IMAGE "Stain" 100
+					      (if (= inDark TRUE)
+						  DARKEN-ONLY NORMAL))))
 
-  
-                              
-       (gimp-image-add-layer theImage theStain 0)
-       (gimp-selection-all theImage)
-       (gimp-edit-clear theStain)
-       (let ((blobSize (/ (rand (- theSize 40)) (+ (rand 3) 1)  ) ) )
+	  (gimp-image-add-layer theImage theStain 0)
+	  (gimp-selection-all theImage)
+	  (gimp-edit-clear theStain)
+
+	  (let ((blobSize (/ (rand (- theSize 40)) (+ (rand 3) 1))))
             (gimp-ellipse-select theImage
-                 (/ (- theSize blobSize) 2)
-                 (/ (- theSize blobSize) 2)
-		 blobSize blobSize REPLACE TRUE 0 FALSE)
-       )
-       (script-fu-distress-selection theImage theStain (* (+ (rand 15) 1) (+ (rand 15) 1)) (/ theSize 25) 4 2 TRUE TRUE   )
-       (gimp-gradients-set-gradient "Coffee")
-       (gimp-blend theStain CUSTOM NORMAL SHAPEBURST-DIMPLED 100 0 REPEAT-NONE FALSE 0 0 0 0 0 0)
-       (gimp-layer-set-offsets theStain (- (rand theWidth) (/ theSize 2)) (- (rand theHeight) (/ theSize 2)) theSize)
-    )
+				 (/ (- theSize blobSize) 2)
+				 (/ (- theSize blobSize) 2)
+				 blobSize blobSize REPLACE TRUE 0 FALSE))
+
+	  (script-fu-distress-selection theImage theStain
+					(* (+ (rand 15) 1) (+ (rand 15) 1))
+					(/ theSize 25) 4 2 TRUE TRUE)
+
+	  (gimp-gradients-set-gradient "Coffee")
+
+	  (gimp-blend theStain CUSTOM NORMAL SHAPEBURST-DIMPLED
+		      100 0 REPEAT-NONE FALSE 0 0 0 0 0 0)
+	  (gimp-layer-set-offsets theStain
+				  (- (rand theWidth) (/ theSize 2))
+				  (- (rand theHeight) (/ theSize 2)) theSize))
+
    (gimp-selection-none theImage)
+
+   (gimp-undo-push-group-end theImage)
+
    (gimp-gradients-set-gradient old-gradient)
-   (gimp-displays-flush)
-)
+   (gimp-displays-flush))
 
 ; Register the function with the GIMP:
 
-(script-fu-register
-    "script-fu-coffee-stain"
-    _"<Image>/Script-Fu/Decor/Coffee Stain..."
-    "Draws realistic looking coffee stains"
-    "Chris Gutteridge"
-    "1998, Chris Gutteridge / ECS dept, University of Southampton, England."
-    "25th April 1998"
-    "RGB*"
-    SF-IMAGE "The Image" 0
-    SF-DRAWABLE "The Layer" 0
-    SF-ADJUSTMENT _"Stains" '(3 1 10 1 1 0 0)
-    SF-TOGGLE _"Darken Only\n(Better, but only for Images with alot of White)" TRUE
-)
+(script-fu-register "script-fu-coffee-stain"
+		    _"<Image>/Script-Fu/Decor/Coffee Stain..."
+		    "Draws realistic looking coffee stains"
+		    "Chris Gutteridge"
+		    "1998, Chris Gutteridge / ECS dept, University of Southampton, England."
+		    "25th April 1998"
+		    "RGB*"
+		    SF-IMAGE       "The Image" 0
+		    SF-DRAWABLE    "The Layer" 0
+		    SF-ADJUSTMENT _"Stains"    '(3 1 10 1 1 0 0)
+		    SF-TOGGLE     _"Darken Only\n(Better, but only for Images with alot of White)" TRUE)
--- plug-ins/script-fu/scripts/distress-selection.scm
+++ plug-ins/script-fu/scripts/distress-selection.scm
@@ -21,85 +21,81 @@
 
 ; Define the function:
 
-(define (script-fu-distress-selection	inImage
-					inLayer
-					inThreshold
-					inSpread
-					inGranu
-					inSmooth
-                                        inSmoothH
-                                        inSmoothV
-	)
+(define (script-fu-distress-selection inImage
+				      inLayer
+				      inThreshold
+				      inSpread
+				      inGranu
+				      inSmooth
+				      inSmoothH
+				      inSmoothV)
 
-	(set! theImage inImage)
-        (set! theWidth (car (gimp-image-width inImage)))
-	(set! theHeight (car (gimp-image-height inImage)))
+  (set! theImage inImage)
+  (set! theWidth (car (gimp-image-width inImage)))
+  (set! theHeight (car (gimp-image-height inImage)))
 
-	(gimp-image-undo-disable theImage)
+  (gimp-undo-push-group-start theImage)
 
-	(set! theLayer (car (gimp-layer-new 	theImage
-						theWidth
-						theHeight
-						RGBA_IMAGE
-						"Distress Scratch Layer"
-						100
-						NORMAL
-	) ) )
+  (set! theLayer (car (gimp-layer-new theImage
+				      theWidth
+				      theHeight
+				      RGBA_IMAGE
+				      "Distress Scratch Layer"
+				      100
+				      NORMAL)))
 
-	(gimp-image-add-layer theImage theLayer 0)
-        (if (= TRUE (car (gimp-selection-is-empty theImage)))
-            ()
-	    (gimp-edit-fill theLayer BG-IMAGE-FILL)
-        )
-	(gimp-selection-invert theImage)
-        (if (= TRUE (car (gimp-selection-is-empty theImage)))
-            ()
-            (gimp-edit-clear theLayer)
-        )
-	(gimp-selection-invert theImage)
-        (gimp-selection-none inImage)
+  (gimp-image-add-layer theImage theLayer 0)
 
-	(gimp-layer-scale 	theLayer
-				(/ theWidth inGranu)
-				(/ theHeight inGranu)
-				TRUE
-	)
+  (if (= TRUE (car (gimp-selection-is-empty theImage)))
+      ()
+      (gimp-edit-fill theLayer BG-IMAGE-FILL))
 
-	(plug-in-spread 	TRUE
-				theImage
-				theLayer
-				inSpread
-				inSpread
-	)
-	(plug-in-gauss-iir TRUE theImage theLayer inSmooth inSmoothH inSmoothV)
-;	(plug-in-gauss-iir TRUE theImage theLayer 2 TRUE TRUE)
-	(gimp-layer-scale theLayer theWidth theHeight TRUE)
-	(plug-in-threshold-alpha TRUE theImage theLayer inThreshold)
-        (plug-in-gauss-iir TRUE theImage theLayer 1 TRUE TRUE)
-	(gimp-selection-layer-alpha theLayer)
-	(gimp-image-remove-layer theImage theLayer)
-;	(gimp-layer-delete theLayer)
-	(gimp-image-undo-enable theImage)
-	(gimp-displays-flush)
-)
+  (gimp-selection-invert theImage)
+
+  (if (= TRUE (car (gimp-selection-is-empty theImage)))
+      ()
+      (gimp-edit-clear theLayer))
+
+  (gimp-selection-invert theImage)
+  (gimp-selection-none inImage)
+
+  (gimp-layer-scale theLayer
+		    (/ theWidth inGranu)
+		    (/ theHeight inGranu)
+		    TRUE)
+
+  (plug-in-spread TRUE
+		  theImage
+		  theLayer
+		  inSpread
+		  inSpread)
+
+  (plug-in-gauss-iir TRUE theImage theLayer inSmooth inSmoothH inSmoothV)
+  (gimp-layer-scale theLayer theWidth theHeight TRUE)
+  (plug-in-threshold-alpha TRUE theImage theLayer inThreshold)
+  (plug-in-gauss-iir TRUE theImage theLayer 1 TRUE TRUE)
+  (gimp-selection-layer-alpha theLayer)
+  (gimp-image-remove-layer theImage theLayer)
+
+  (gimp-undo-push-group-end theImage)
+
+  (gimp-displays-flush))
 
 
 ; Register the function with the GIMP:
 
-(script-fu-register
-    "script-fu-distress-selection"
-    _"<Image>/Script-Fu/Selection/Distress Selection..."
-    "No description"
-    "Chris Gutteridge"
-    "1998, Chris Gutteridge / ECS dept, University of Southampton, England."
-    "23rd April 1998"
-    "RGB*"
-    SF-IMAGE "The Image" 0
-    SF-DRAWABLE "The Layer" 0
-    SF-ADJUSTMENT _"Threshold (Bigger 1<-->255 Smaller)" '(127 1 255 1 10 0 0)
-    SF-ADJUSTMENT _"Spread" '(8 0 1000 1 10 0 1)
-    SF-ADJUSTMENT _"Granularity (1 is Low)" '(4 1 25 1 10 0 1)
-    SF-ADJUSTMENT _"Smooth" '(2 0 150 1 10 0 1)
-    SF-TOGGLE _"Smooth Horizontally" TRUE
-    SF-TOGGLE _"Smooth Vertically" TRUE
-)
+(script-fu-register "script-fu-distress-selection"
+		    _"<Image>/Script-Fu/Selection/Distress Selection..."
+		    "No description"
+		    "Chris Gutteridge"
+		    "1998, Chris Gutteridge / ECS dept, University of Southampton, England."
+		    "23rd April 1998"
+		    "RGB*"
+		    SF-IMAGE       "The Image" 0
+		    SF-DRAWABLE    "The Layer" 0
+		    SF-ADJUSTMENT _"Threshold (Bigger 1<-->255 Smaller)" '(127 1 255 1 10 0 0)
+		    SF-ADJUSTMENT _"Spread"   '(8 0 1000 1 10 0 1)
+		    SF-ADJUSTMENT _"Granularity (1 is Low)" '(4 1 25 1 10 0 1)
+		    SF-ADJUSTMENT _"Smooth"   '(2 0 150 1 10 0 1)
+		    SF-TOGGLE     _"Smooth Horizontally" TRUE
+		    SF-TOGGLE     _"Smooth Vertically"   TRUE)
