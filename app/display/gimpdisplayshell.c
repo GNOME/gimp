3a7354ba62e8bd17b39f87d8746278ca763fabff
Michael Natterer <mitch@src.gnome.org>
2003-08-23 19:50:35 +0000

    Oops, should have gone with my last checkin.

--- app/display/gimpdisplayshell.c
+++ app/display/gimpdisplayshell.c
@@ -337,7 +337,7 @@ gimp_display_shell_init (GimpDisplayShell *shell)
                               shell);
 
   gimp_help_connect (GTK_WIDGET (shell), gimp_standard_help_func,
-		     "image/image_window.html");
+		     "image/image_window.html", NULL);
 
 }
 
