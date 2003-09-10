9506c7570e6efded0e824692eb83d4587af5836b
Sven Neumann <sven@gimp.org>
2003-09-10 14:55:54 +0000

    started to add framework for SVG parser.
    2003-09-10  Sven Neumann  <sven@gimp.org>
    
    	* app/vectors/gimpvectors-import.c: started to add framework for
    	SVG parser.

--- app/vectors/gimpvectors-import.c
+++ app/vectors/gimpvectors-import.c
@@ -18,7 +18,9 @@
 
 #include "config.h"
 
+#include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 #include <glib-object.h>
 
@@ -33,13 +35,62 @@
 #include "gimp-intl.h"
 
 
+typedef enum
+{
+  PARSER_START,
+  PARSER_IN_SVG,
+  PARSER_IN_PATH,
+  PARSER_IN_UNKNOWN
+} ParserState;
+
+
+typedef struct
+{
+  ParserState   state;
+  ParserState   last_known_state;
+  gint          unknown_depth;
+  GimpVectors  *vectors;
+} VectorsParser;
+
+
+static void  parser_start_element (GMarkupParseContext  *context,
+                                   const gchar          *element_name,
+                                   const gchar         **attribute_names,
+                                   const gchar         **attribute_values,
+                                   gpointer              user_data,
+                                   GError              **error);
+static void  parser_end_element   (GMarkupParseContext  *context,
+                                   const gchar          *element_name,
+                                   gpointer              user_data,
+                                   GError              **error);
+
+static void  parser_start_unknown (VectorsParser        *parser);
+static void  parser_end_unknown   (VectorsParser        *parser);
+
+static void  parser_path_data     (VectorsParser        *parser,
+                                   const gchar          *data);
+
+
+static const GMarkupParser markup_parser =
+{
+  parser_start_element,
+  parser_end_element,
+  NULL,  /*  characters   */
+  NULL,  /*  passthrough  */
+  NULL   /*  error        */
+};
+
+
 GimpVectors *
 gimp_vectors_import (GimpImage    *image,
                      const gchar  *filename,
                      GError      **error)
 {
-  GimpVectors *vectors;
-  FILE        *file;
+  GMarkupParseContext *context;
+  FILE                *file;
+  VectorsParser        parser;
+  gsize                bytes;
+  gchar                buf[4096];
 
   g_return_val_if_fail (GIMP_IS_IMAGE (image), NULL);
   g_return_val_if_fail (filename != NULL, NULL);
@@ -54,12 +105,122 @@ gimp_vectors_import (GimpImage    *image,
       return NULL;
     }
 
-  vectors = gimp_vectors_new (image, "Imported Path");
+  parser.state   = PARSER_START;
+  parser.vectors = gimp_vectors_new (image, _("Imported Path"));
 
-  /* FIXME:  add export implementation here */
-  g_warning ("gimp_vectors_import: unimplemented");
+  context = g_markup_parse_context_new (&markup_parser, 0, &parser, NULL);
+
+  while ((bytes = fread (buf, sizeof (gchar), sizeof (buf), file)) > 0 &&
+         g_markup_parse_context_parse (context, buf, bytes, error))
+    ;
+
+  if (error == NULL || *error == NULL)
+    g_markup_parse_context_end_parse (context, error);
 
   fclose (file);
+  g_markup_parse_context_free (context);
 
-  return vectors;
+  return parser.vectors;
+}
+
+static void
+parser_start_element (GMarkupParseContext *context,
+                      const gchar         *element_name,
+                      const gchar        **attribute_names,
+                      const gchar        **attribute_values,
+                      gpointer             user_data,
+                      GError             **error)
+{
+  VectorsParser *parser = (VectorsParser *) user_data;
+
+  switch (parser->state)
+    {
+    case PARSER_START:
+      if (strcmp (element_name, "svg") == 0)
+        parser->state = PARSER_IN_SVG;
+      else
+        parser_start_unknown (parser);
+      break;
+
+    case PARSER_IN_SVG:
+      if (strcmp (element_name, "path") == 0)
+        parser->state = PARSER_IN_PATH;
+      else
+        parser_start_unknown (parser);
+      break;
+
+    case PARSER_IN_PATH:
+    case PARSER_IN_UNKNOWN:
+      parser_start_unknown (parser);
+      break;
+    }
+
+  if (parser->state == PARSER_IN_PATH)
+    {
+      while (*attribute_names)
+        {
+          if (strcmp (*attribute_names, "d") == 0)
+            parser_path_data (parser, *attribute_values);
+
+          attribute_names++;
+          attribute_values++;
+        }
+    }
+}
+
+static void
+parser_end_element (GMarkupParseContext *context,
+                    const gchar         *element_name,
+                    gpointer             user_data,
+                    GError             **error)
+{
+  VectorsParser *parser = (VectorsParser *) user_data;
+
+  switch (parser->state)
+    {
+    case PARSER_START:
+      g_return_if_reached ();
+      break;
+
+    case PARSER_IN_SVG:
+      parser->state = PARSER_START;
+      break;
+
+    case PARSER_IN_PATH:
+      parser->state = PARSER_IN_SVG;
+      break;
+
+    case PARSER_IN_UNKNOWN:
+      parser_end_unknown (parser);
+      break;
+    }
+}
+
+static void
+parser_start_unknown (VectorsParser *parser)
+{
+  if (parser->unknown_depth == 0)
+    parser->last_known_state = parser->state;
+
+  parser->state = PARSER_IN_UNKNOWN;
+  parser->unknown_depth++;
+}
+
+static void
+parser_end_unknown (VectorsParser *parser)
+{
+  g_return_if_fail (parser->unknown_depth > 0 &&
+                    parser->state == PARSER_IN_UNKNOWN);
+
+  parser->unknown_depth--;
+
+  if (parser->unknown_depth == 0)
+    parser->state = parser->last_known_state;
+}
+
+static void
+parser_path_data (VectorsParser *parser,
+                  const gchar   *data)
+{
+  /* FIXME */
 }
