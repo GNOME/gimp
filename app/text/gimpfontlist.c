361b1f567772f102169f6683d617b2d47cdeb395
Manish Singh <yosh@gimp.org>
2004-04-16 00:56:25 +0000

    tools/Makefile.am app/Makefile.am app, tools, and user dir bumped to
    2004-04-15  Manish Singh  <yosh@gimp.org>
    
            * tools/Makefile.am
            * app/Makefile.am
            * configure.in: app, tools, and user dir bumped to version 2.1 names.
    
            * app/text/gimpfontlist.c: since we now depend on pango 1.4, we can
    	use pango_fc_font_description_from_pattern() instead of our
    	cut-n-paste function, gimp_font_list_font_desc_from_pattern().

--- app/text/gimpfontlist.c
+++ app/text/gimpfontlist.c
@@ -27,6 +27,7 @@
 
 #include <glib-object.h>
 #include <pango/pangoft2.h>
+#include <pango/pangofc-fontmap.h>
 
 #include "text-types.h"
 
@@ -222,117 +223,6 @@ gimp_font_list_load_aliases (GimpFontList *list,
     }
 }
 
-/* This is copied straight from _pango_fc_font_desc_from_pattern, minus
- * the size bits.
- * FIXME: Use pango_fc_font_description_from_pattern from 1.4 when we can.
- */
-static PangoFontDescription *
-gimp_font_list_font_desc_from_pattern (FcPattern *pattern)
-{
-  PangoFontDescription *desc;
-  PangoStyle            style;
-  PangoWeight           weight;
-  PangoStretch          stretch;
-  FcChar8              *s;
-  gint                  i;
-  FcResult              res;
-
-  desc = pango_font_description_new ();
-
-  res = FcPatternGetString (pattern, FC_FAMILY, 0, (FcChar8 **) &s);
-  g_assert (res == FcResultMatch);
-
-  pango_font_description_set_family (desc, (gchar *)s);
-
-  if (FcPatternGetInteger (pattern, FC_SLANT, 0, &i) == FcResultMatch)
-    {
-      switch (i)
-        {
-        case FC_SLANT_ROMAN:
-          style = PANGO_STYLE_NORMAL;
-          break;
-        case FC_SLANT_ITALIC:
-          style = PANGO_STYLE_ITALIC;
-          break;
-        case FC_SLANT_OBLIQUE:
-          style = PANGO_STYLE_OBLIQUE;
-          break;
-        default:
-          style = PANGO_STYLE_NORMAL;
-          break;
-        }
-    }
-  else
-    style = PANGO_STYLE_NORMAL;
-
-  pango_font_description_set_style (desc, style);
-
-  if (FcPatternGetInteger (pattern, FC_WEIGHT, 0, &i) == FcResultMatch)
-    {
-      if (i < FC_WEIGHT_LIGHT)
-        weight = PANGO_WEIGHT_ULTRALIGHT;
-      else if (i < (FC_WEIGHT_LIGHT + FC_WEIGHT_MEDIUM) / 2)
-        weight = PANGO_WEIGHT_LIGHT;
-      else if (i < (FC_WEIGHT_MEDIUM + FC_WEIGHT_DEMIBOLD) / 2)
-        weight = PANGO_WEIGHT_NORMAL;
-      else if (i < (FC_WEIGHT_DEMIBOLD + FC_WEIGHT_BOLD) / 2)
-        weight = 600;
-      else if (i < (FC_WEIGHT_BOLD + FC_WEIGHT_BLACK) / 2)
-        weight = PANGO_WEIGHT_BOLD;
-      else
-        weight = PANGO_WEIGHT_ULTRABOLD;
-    }
-  else
-    weight = PANGO_WEIGHT_NORMAL;
-
-  pango_font_description_set_weight (desc, weight);
-
-  if (FcPatternGetInteger (pattern, FC_WIDTH, 0, &i) == FcResultMatch)
-    {
-      switch (i)
-        {
-        case FC_WIDTH_NORMAL:
-          stretch = PANGO_STRETCH_NORMAL;
-          break;
-        case FC_WIDTH_ULTRACONDENSED:
-          stretch = PANGO_STRETCH_ULTRA_CONDENSED;
-          break;
-        case FC_WIDTH_EXTRACONDENSED:
-          stretch = PANGO_STRETCH_EXTRA_CONDENSED;
-          break;
-        case FC_WIDTH_CONDENSED:
-          stretch = PANGO_STRETCH_CONDENSED;
-          break;
-        case FC_WIDTH_SEMICONDENSED:
-          stretch = PANGO_STRETCH_SEMI_CONDENSED;
-          break;
-        case FC_WIDTH_SEMIEXPANDED:
-          stretch = PANGO_STRETCH_SEMI_EXPANDED;
-          break;
-        case FC_WIDTH_EXPANDED:
-          stretch = PANGO_STRETCH_EXPANDED;
-          break;
-        case FC_WIDTH_EXTRAEXPANDED:
-          stretch = PANGO_STRETCH_EXTRA_EXPANDED;
-          break;
-        case FC_WIDTH_ULTRAEXPANDED:
-          stretch = PANGO_STRETCH_ULTRA_EXPANDED;
-          break;
-        default:
-          stretch = PANGO_STRETCH_NORMAL;
-          break;
-        }
-    }
-  else
-    stretch = PANGO_STRETCH_NORMAL;
-
-  pango_font_description_set_stretch (desc, stretch);
-
-  pango_font_description_set_variant (desc, PANGO_VARIANT_NORMAL);
-
-  return desc;
-}
-
 static void
 gimp_font_list_load_names (GimpFontList *list,
                            PangoFontMap *fontmap,
@@ -358,7 +248,7 @@ gimp_font_list_load_names (GimpFontList *list,
     {
       PangoFontDescription *desc;
 
-      desc = gimp_font_list_font_desc_from_pattern (fontset->fonts[i]);
+      desc = pango_fc_font_description_from_pattern (fontset->fonts[i], FALSE);
       gimp_font_list_add_font (list, context, desc);
       pango_font_description_free (desc);
     }
