068ed6e6005fe757cd0e2c8230780170c428a386
Sven Neumann <sven@gimp.org>
2004-05-04 09:29:03 +0000

    removed frame, adjusted spacing, fixes bug #141773.
    2004-05-04  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/common/sobel.c (sobel_dialog): removed frame, adjusted
    	spacing, fixes bug #141773.

--- plug-ins/common/sobel.c
+++ plug-ins/common/sobel.c
@@ -223,7 +223,6 @@ sobel_dialog (void)
 {
   GtkWidget *dlg;
   GtkWidget *toggle;
-  GtkWidget *frame;
   GtkWidget *vbox;
   gboolean   run;
 
@@ -239,14 +238,9 @@ sobel_dialog (void)
                          NULL);
 
   /*  parameter settings  */
-  frame = gtk_frame_new ( _("Parameter Settings"));
-  gtk_container_set_border_width (GTK_CONTAINER (frame), 6);
-  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dlg)->vbox), frame, TRUE, TRUE, 0);
-  gtk_widget_show (frame);
-
-  vbox = gtk_vbox_new (FALSE, 1);
-  gtk_container_set_border_width (GTK_CONTAINER (vbox), 2);
-  gtk_container_add (GTK_CONTAINER (frame), vbox);
+  vbox = gtk_vbox_new (FALSE, 6);
+  gtk_container_set_border_width (GTK_CONTAINER (vbox), 12);
+  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dlg)->vbox), vbox, TRUE, TRUE, 0);
   gtk_widget_show (vbox);
 
   toggle = gtk_check_button_new_with_mnemonic (_("Sobel _Horizontally"));
