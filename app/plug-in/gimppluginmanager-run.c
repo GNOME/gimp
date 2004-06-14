1bdd36c9526ff8513000a96bb4074a37d7b52878
Henrik Brix Andersen <brix@gimp.org>
2004-06-14 11:58:49 +0000

    let extensions run synchronously when called via PDB. Fixes bug #140112.
    2004-06-14 Henrik Brix Andersen <brix@gimp.org>
    
    * app/plug-in/plug-in-run.c: let extensions run synchronously when
    called via PDB. Fixes bug #140112.

--- app/plug-in/gimppluginmanager-run.c
+++ app/plug-in/gimppluginmanager-run.c
@@ -73,8 +73,6 @@ plug_in_run (Gimp        *gimp,
   g_return_val_if_fail (GIMP_IS_CONTEXT (context), NULL);
   g_return_val_if_fail (proc_rec != NULL, NULL);
   g_return_val_if_fail (argc == 0 || args != NULL, NULL);
-  g_return_val_if_fail (proc_rec->proc_type != GIMP_EXTENSION ||
-                        synchronous == FALSE, NULL);
 
   if (proc_rec->proc_type == GIMP_TEMPORARY)
     {
