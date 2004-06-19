4c183e1f588b13d21a48eb5e56798d0ae367f7a0
Pedro Gimeno <pggimeno@wanadoo.es>
2004-06-19 16:18:47 +0000

    Fix typos and improve documentation. Merged from HEAD. Addresses bug
    2004-06-19  Pedro Gimeno  <pggimeno@wanadoo.es>
    
    	* tools/pdbgen/pdb/paths.pdb: Fix typos and improve documentation.
    	Merged from HEAD. Addresses bug #144267.
    
    	* app/pdb/paths_cmds.c
    	* libgimp/gimppaths_pdb.c: regenerated.

--- app/pdb/paths_cmds.c
+++ app/pdb/paths_cmds.c
@@ -120,12 +120,12 @@ static ProcArg path_list_outargs[] =
   {
     GIMP_PDB_INT32,
     "num_paths",
-    "The number of paths returned"
+    "The number of paths returned."
   },
   {
     GIMP_PDB_STRINGARRAY,
     "path_list",
-    "List of the paths belonging to this image"
+    "List of the paths belonging to this image."
   }
 };
 
@@ -182,7 +182,7 @@ static ProcArg path_get_current_inargs[] =
   {
     GIMP_PDB_IMAGE,
     "image",
-    "The ID of the image to get the current path from"
+    "The ID of the image to get the current path from."
   }
 };
 
@@ -191,7 +191,7 @@ static ProcArg path_get_current_outargs[] =
   {
     GIMP_PDB_STRING,
     "name",
-    "The name of the current path"
+    "The name of the current path."
   }
 };
 
@@ -246,12 +246,12 @@ static ProcArg path_set_current_inargs[] =
   {
     GIMP_PDB_IMAGE,
     "image",
-    "The ID of the image to list set the paths in"
+    "The ID of the image in which a path will become current."
   },
   {
     GIMP_PDB_STRING,
     "name",
-    "The name of the path to set the current path to"
+    "The name of the path to make current."
   }
 };
 
@@ -259,7 +259,7 @@ static ProcRecord path_set_current_proc =
 {
   "gimp_path_set_current",
   "Sets the current path associated with the passed image.",
-  "List the paths associated with the passed image.",
+  "Sets a named path as the current path.",
   "Andy Thomas",
   "Andy Thomas",
   "1999",
@@ -306,12 +306,12 @@ static ProcArg path_delete_inargs[] =
   {
     GIMP_PDB_IMAGE,
     "image",
-    "The ID of the image to list delete the paths from"
+    "The ID of the image to delete the path from."
   },
   {
     GIMP_PDB_STRING,
     "name",
-    "The name of the path to delete"
+    "The name of the path to delete."
   }
 };
 
@@ -412,12 +412,12 @@ static ProcArg path_get_points_inargs[] =
   {
     GIMP_PDB_IMAGE,
     "image",
-    "The ID of the image to list the paths from"
+    "The ID of the image to list the paths from."
   },
   {
     GIMP_PDB_STRING,
     "name",
-    "the name of the path whose points should be listed"
+    "The name of the path whose points should be listed."
   }
 };
 
@@ -436,12 +436,12 @@ static ProcArg path_get_points_outargs[] =
   {
     GIMP_PDB_INT32,
     "num_path_point_details",
-    "The number of points returned. Each point is made up of (x, y, pnt_type) of floats"
+    "The number of points returned. Each point is made up of (x, y, pnt_type) of floats."
   },
   {
     GIMP_PDB_FLOATARRAY,
     "points_pairs",
-    "The points in the path represented as 3 floats. The first is the x pos, next is the y pos, last is the type of the pnt. The type field is dependant on the path type. For beziers (type 1 paths) the type can either be (1.0 = BEZIER_ANCHOR, 2.0 = BEZIER_CONTROL, 3.0 = BEZIER_MOVE). Note all points are returned in pixel resolution"
+    "The points in the path represented as 3 floats. The first is the x pos, next is the y pos, last is the type of the pnt. The type field is dependant on the path type. For beziers (type 1 paths) the type can either be (1.0 = BEZIER_ANCHOR, 2.0 = BEZIER_CONTROL, 3.0 = BEZIER_MOVE). Note all points are returned in pixel resolution."
   }
 };
 
@@ -537,27 +537,27 @@ static ProcArg path_set_points_inargs[] =
   {
     GIMP_PDB_IMAGE,
     "image",
-    "The ID of the image to set the paths in"
+    "The ID of the image to set the paths in."
   },
   {
     GIMP_PDB_STRING,
     "name",
-    "The name of the path to create (if it exists then a unique name will be created - query the list of paths if you want to make sure that the name of the path you create is unique. This will be set as the current path."
+    "The name of the path to create. If it exists then a unique name will be created - query the list of paths if you want to make sure that the name of the path you create is unique. This will be set as the current path."
   },
   {
     GIMP_PDB_INT32,
     "ptype",
-    "The type of the path. Currently only one type (1 = Bezier) is supported"
+    "The type of the path. Currently only one type (1 = Bezier) is supported."
   },
   {
     GIMP_PDB_INT32,
     "num_path_points",
-    "The number of points in the path. Each point is made up of (x, y, type) of floats. Currently only the creation of bezier curves is allowed. The type parameter must be set to (1) to indicate a BEZIER type curve. For BEZIERS. Note the that points must be given in the following order... ACCACCAC ... If the path is not closed the last control point is missed off. Points consist of three control points (control/anchor/control) so for a curve that is not closed there must be at least two points passed (2 x,y pairs). If num_path_pnts % 3 = 0 then the path is assumed to be closed and the points are ACCACCACCACC."
+    "The number of elements in the array, i.e. the number of points in the path * 3. Each point is made up of (x, y, type) of floats. Currently only the creation of bezier curves is allowed. The type parameter must be set to (1) to indicate a BEZIER type curve. Note that for BEZIER curves, points must be given in the following order: ACCACCAC... If the path is not closed the last control point is missed off. Points consist of three control points (control/anchor/control) so for a curve that is not closed there must be at least two points passed (2 x,y pairs). If (num_path_points/3) % 3 = 0 then the path is assumed to be closed and the points are ACCACCACCACC."
   },
   {
     GIMP_PDB_FLOATARRAY,
     "points_pairs",
-    "The points in the path represented as 3 floats. The first is the x pos, next is the y pos, last is the type of the pnt. The type field is dependant on the path type. For beziers (type 1 paths) the type can either be (1.0 = BEZIER_ANCHOR, 2.0 = BEZIER_CONTROL, 3.0= BEZIER_MOVE). Note all points are returned in pixel resolution"
+    "The points in the path represented as 3 floats. The first is the x pos, next is the y pos, last is the type of the pnt. The type field is dependant on the path type. For beziers (type 1 paths) the type can either be (1.0 = BEZIER_ANCHOR, 2.0 = BEZIER_CONTROL, 3.0= BEZIER_MOVE). Note all points are returned in pixel resolution."
   }
 };
 
@@ -617,7 +617,7 @@ static ProcArg path_stroke_current_inargs[] =
   {
     GIMP_PDB_IMAGE,
     "image",
-    "The ID of the image which contains the path to stroke"
+    "The ID of the image which contains the path to stroke."
   }
 };
 
@@ -691,7 +691,7 @@ static ProcArg path_get_point_at_dist_inargs[] =
   {
     GIMP_PDB_FLOAT,
     "distance",
-    "The distance along the path"
+    "The distance along the path."
   }
 };
 
@@ -700,17 +700,17 @@ static ProcArg path_get_point_at_dist_outargs[] =
   {
     GIMP_PDB_INT32,
     "x_point",
-    "The x position of the point"
+    "The x position of the point."
   },
   {
     GIMP_PDB_INT32,
     "y_point",
-    "The y position of the point"
+    "The y position of the point."
   },
   {
     GIMP_PDB_FLOAT,
     "gradient",
-    "The gradient at the specified point"
+    "The gradient at the specified point."
   }
 };
 
@@ -718,7 +718,7 @@ static ProcRecord path_get_point_at_dist_proc =
 {
   "gimp_path_get_point_at_dist",
   "Get point on a path at a specified distance along the path.",
-  "This will return the x,y position of a point at a given distance along the bezier curve. The distance will the obtained by first digitizing the curve internally an then walking along the curve. For a closed curve the start of the path is the first point on the path that was created. This might not be obvious. Note the current path is used.",
+  "This will return the x,y position of a point at a given distance along the bezier curve. The distance will be obtained by first digitizing the curve internally and then walking along the curve. For a closed curve the start of the path is the first point on the path that was created. This might not be obvious. Note the current path is used.",
   "Andy Thomas",
   "Andy Thomas",
   "1999",
@@ -777,7 +777,7 @@ static ProcArg path_get_tattoo_inargs[] =
   {
     GIMP_PDB_STRING,
     "name",
-    "the name of the path whose tattoo should be obtained"
+    "The name of the path whose tattoo should be obtained."
   }
 };
 
@@ -786,7 +786,7 @@ static ProcArg path_get_tattoo_outargs[] =
   {
     GIMP_PDB_INT32,
     "tattoo",
-    "The tattoo associated with the name path"
+    "The tattoo associated with the named path."
   }
 };
 
@@ -849,12 +849,12 @@ static ProcArg path_set_tattoo_inargs[] =
   {
     GIMP_PDB_STRING,
     "name",
-    "the name of the path whose tattoo should be set"
+    "The name of the path whose tattoo should be set."
   },
   {
     GIMP_PDB_INT32,
     "tattovalue",
-    "The tattoo associated with the name path. Only values returned from 'path_get_tattoo' should be used here"
+    "The tattoo associated with the name path. Only values returned from 'path_get_tattoo' should be used here."
   }
 };
 
@@ -919,7 +919,7 @@ static ProcArg get_path_by_tattoo_inargs[] =
   {
     GIMP_PDB_INT32,
     "tattoo",
-    "The tattoo of the required path"
+    "The tattoo of the required path."
   }
 };
 
@@ -928,7 +928,7 @@ static ProcArg get_path_by_tattoo_outargs[] =
   {
     GIMP_PDB_STRING,
     "name",
-    "The name of the path with the specified tattoo"
+    "The name of the path with the specified tattoo."
   }
 };
 
@@ -936,7 +936,7 @@ static ProcRecord get_path_by_tattoo_proc =
 {
   "gimp_get_path_by_tattoo",
   "Return the name of the path with the given tattoo.",
-  "The procedure returns the name of the path in the specified image which has the passed tattoo. The tattoos are unique within the image and will be preserved across sessions and through renaming of the path. An error is returned if no path woth the specified tattoo can be found.",
+  "The procedure returns the name of the path in the specified image which has the passed tattoo. The tattoos are unique within the image and will be preserved across sessions and through renaming of the path. An error is returned if no path with the specified tattoo can be found.",
   "Andy Thomas",
   "Andy Thomas",
   "1999",
@@ -995,7 +995,7 @@ static ProcArg path_get_locked_inargs[] =
   {
     GIMP_PDB_STRING,
     "name",
-    "the name of the path whose locked status should be obtained"
+    "The name of the path whose locked status should be obtained."
   }
 };
 
@@ -1004,7 +1004,7 @@ static ProcArg path_get_locked_outargs[] =
   {
     GIMP_PDB_INT32,
     "lockstatus",
-    "The lock status associated with the name path. 0 returned if the path is not locked. 1 is returned if the path is locked"
+    "The lock status associated with the name path. 0 is returned if the path is not locked. 1 is returned if the path is locked."
   }
 };
 
@@ -1156,32 +1156,32 @@ static ProcArg path_to_selection_inargs[] =
   {
     GIMP_PDB_STRING,
     "name",
-    "The name of the path which should be made into selection"
+    "The name of the path which should be made into selection."
   },
   {
     GIMP_PDB_INT32,
     "op",
-    "The desired operation with current selection"
+    "The desired operation with current selection."
   },
   {
     GIMP_PDB_INT32,
     "antialias",
-    "Antialias selection"
+    "Antialias selection."
   },
   {
     GIMP_PDB_INT32,
     "feather",
-    "Feather selection"
+    "Feather selection."
   },
   {
     GIMP_PDB_FLOAT,
     "feather_radius_x",
-    "Feather radius x"
+    "Feather radius x."
   },
   {
     GIMP_PDB_FLOAT,
     "feather_radius_y",
-    "Feather radius y"
+    "Feather radius y."
   }
 };
 
@@ -1244,12 +1244,12 @@ static ProcArg path_import_inargs[] =
   {
     GIMP_PDB_INT32,
     "merge",
-    "Merge paths into a single vectors object"
+    "Merge paths into a single vectors object."
   },
   {
     GIMP_PDB_INT32,
     "scale",
-    "Scale the SVG to image dimensions"
+    "Scale the SVG to image dimensions."
   }
 };
 
@@ -1257,7 +1257,7 @@ static ProcRecord path_import_proc =
 {
   "gimp_path_import",
   "Import paths from an SVG file.",
-  "This procedure imports path from an SVG file. This is a temporary solution until the new vectors PDB API is in place. Don't rely on this function being available in future GIMP releases.",
+  "This procedure imports paths from an SVG file. This is a temporary solution until the new vectors PDB API is in place. Don't rely on this function being available in future GIMP releases.",
   "Sven Neumann",
   "Sven Neumann",
   "2003",
