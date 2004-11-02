e85b1480dd2106b1a1153422a1cc399a15bb8d00
Michael Natterer <mitch@gimp.org>
2004-11-02 13:54:56 +0000

    one more s/cmap/colormap/.
    2004-11-02  Michael Natterer  <mitch@gimp.org>
    
    	* libgimp/gimpimage.h: one more s/cmap/colormap/.

--- libgimp/gimpimage.h
+++ libgimp/gimpimage.h
@@ -38,7 +38,7 @@ gboolean   gimp_image_set_cmap            (gint32          image_ID,
 guchar   * gimp_image_get_colormap        (gint32          image_ID,
                                            gint           *num_colors);
 gboolean   gimp_image_set_colormap        (gint32          image_ID,
-                                           const guchar   *cmap,
+                                           const guchar   *colormap,
                                            gint            num_colors);
 
 guchar   * gimp_image_get_thumbnail_data  (gint32          image_ID,
