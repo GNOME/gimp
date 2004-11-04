04f914f1a62cd37cc99f455dfe984a898fa5099c
Michael Natterer <mitch@gimp.org>
2004-11-04 12:35:38 +0000

    #undef GTK_DISABLE_DEPRECATED.
    2004-11-04  Michael Natterer  <mitch@gimp.org>
    
    	* plug-ins/ifscompose/ifscompose.c: #undef GTK_DISABLE_DEPRECATED.

--- plug-ins/ifscompose/ifscompose.c
+++ plug-ins/ifscompose/ifscompose.c
@@ -41,6 +41,11 @@
 #include <string.h>
 #include <errno.h>
 
+#ifdef __GNUC__
+#warning GTK_DISABLE_DEPRECATED
+#endif
+#undef GTK_DISABLE_DEPRECATED
+
 #include <gtk/gtk.h>
 
 #include <libgimp/gimp.h>
