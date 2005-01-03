28251be0e962ee6c2dcd5ce3020d18e137a8ac20
Sven Neumann <neumann@jpk.com>
2005-01-03 15:40:51 +0000

    use g_free() to release memory allocated using g_malloc().
    2005-01-03  Sven Neumann  <neumann@jpk.com>
    
    	* plug-ins/ifscompose/ifscompose.c: use g_free() to release memory
    	allocated using g_malloc().

--- plug-ins/ifscompose/ifscompose.c
+++ plug-ins/ifscompose/ifscompose.c
@@ -36,7 +36,6 @@
 
 #include "config.h"
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -2420,11 +2419,11 @@ ifsfile_replace_ifsvals (IfsComposeVals  *new_ifsvals,
 {
   gdouble width  = ifsDesign->area->allocation.width;
   gdouble height = ifsDesign->area->allocation.height;
-  guint i;
+  guint   i;
 
   for (i = 0; i < ifsvals.num_elements; i++)
     aff_element_free (elements[i]);
-  free(elements);
+  g_free (elements);
 
   ifsvals = *new_ifsvals;
   elements = new_elements;
