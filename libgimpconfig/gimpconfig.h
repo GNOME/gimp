328ad06794b065e01f14da8b8377795896c2d969
William Skaggs <weskaggs@src.gnome.org>
2005-01-24 22:54:27 +0000

    Bill Skaggs  <weskaggs@primate.ucdavis.edu>
    	* libgimpconfig/Makefile.am
    	* libgimpconfig/gimpconfig.h: setting up for libgimpconfig

---
+++ libgimpconfig/gimpconfig.h
@@ -0,0 +1,35 @@
+/* LIBGIMP - The GIMP Library
+ * Copyright (C) 1995-1997 Peter Mattis and Spencer Kimball
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GIMP_CONFIG_H__
+#define __GIMP_CONFIG_H__
+
+
+#include <libgimpconfig/gimpconfig-main.h>
+#include <libgimpconfig/gimpconfig-writer.h>
+#include <libgimpconfig/gimpconfig-error.h>
+#include <libgimpconfig/gimpconfig-serialize.h>
+#include <libgimpconfig/gimpconfig-deserialize.h>
+#include <libgimpconfig/gimpconfig-utils.h>
+#include <libgimpconfig/gimpconfig-params.h>
+#include <libgimpconfig/gimpconfig-path.h>
+#include <libgimpconfig/gimpscanner.h>
+#include <libgimpconfig/gimpversion.h>
+
+#endif  /* __GIMP_CONFIG_H__ */
