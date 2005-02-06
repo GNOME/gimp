2eeefb13d246784c7b35c9c418d2792ff36ffcc5
Sven Neumann <sven@gimp.org>
2005-02-06 22:15:54 +0000

    libgimpconfig/Makefile.am libgimpconfig/gimpcolorconfig-enums.[ch] added a
    2005-02-06  Sven Neumann  <sven@gimp.org>
    
    	* libgimpconfig/Makefile.am
    	* libgimpconfig/gimpcolorconfig-enums.[ch]
    	* libgimpconfig/gimpcolorconfig.[ch]: added a first draft of a
    	color management configuration object. Not yet installed nor used
    	by anything. This is based on an older patch by Stefan Döhla.

---
+++ libgimpconfig/gimpcolorconfig-enums.c
@@ -0,0 +1,108 @@
+
+/* Generated data (by gimp-mkenums) */
+
+#include "config.h"
+#include <glib-object.h>
+#include "libgimpbase/gimpbasetypes.h"
+#include "gimpcolorconfig-enums.h"
+#include "libgimp/libgimp-intl.h"
+
+/* enumerations from "./gimpcolorconfig-enums.h" */
+GType
+gimp_color_management_mode_get_type (void)
+{
+  static const GEnumValue values[] =
+  {
+    { GIMP_COLOR_MANAGEMENT_OFF, "GIMP_COLOR_MANAGEMENT_OFF", "off" },
+    { GIMP_COLOR_MANAGEMENT_DISPLAY, "GIMP_COLOR_MANAGEMENT_DISPLAY", "display" },
+    { GIMP_COLOR_MANAGEMENT_SOFTPROOF, "GIMP_COLOR_MANAGEMENT_SOFTPROOF", "softproof" },
+    { 0, NULL, NULL }
+  };
+
+  static const GimpEnumDesc descs[] =
+  {
+    { GIMP_COLOR_MANAGEMENT_OFF, N_("No color management"), NULL },
+    { GIMP_COLOR_MANAGEMENT_DISPLAY, N_("Color managed display"), NULL },
+    { GIMP_COLOR_MANAGEMENT_SOFTPROOF, N_("Print simulation"), NULL },
+    { 0, NULL, NULL }
+  };
+
+  static GType type = 0;
+
+  if (! type)
+    {
+      type = g_enum_register_static ("GimpColorManagementMode", values);
+      gimp_type_set_translation_domain (type, GETTEXT_PACKAGE "-libgimp");
+      gimp_enum_set_value_descriptions (type, descs);
+    }
+
+  return type;
+}
+
+GType
+gimp_color_rendering_intent_get_type (void)
+{
+  static const GEnumValue values[] =
+  {
+    { GIMP_COLOR_RENDERING_INTENT_PERCEPTUAL, "GIMP_COLOR_RENDERING_INTENT_PERCEPTUAL", "perceptual" },
+    { GIMP_COLOR_RENDERING_INTENT_RELATIVE_COLORIMETRIC, "GIMP_COLOR_RENDERING_INTENT_RELATIVE_COLORIMETRIC", "relative-colorimetric" },
+    { GIMP_COLOR_RENDERING_INTENT_SATURATION, "GIMP_COLOR_RENDERING_INTENT_SATURATION", "saturation" },
+    { GIMP_COLOR_RENDERING_INTENT_ABSOLUTE_COLORIMETRIC, "GIMP_COLOR_RENDERING_INTENT_ABSOLUTE_COLORIMETRIC", "absolute-colorimetric" },
+    { 0, NULL, NULL }
+  };
+
+  static const GimpEnumDesc descs[] =
+  {
+    { GIMP_COLOR_RENDERING_INTENT_PERCEPTUAL, "GIMP_COLOR_RENDERING_INTENT_PERCEPTUAL", NULL },
+    { GIMP_COLOR_RENDERING_INTENT_RELATIVE_COLORIMETRIC, "GIMP_COLOR_RENDERING_INTENT_RELATIVE_COLORIMETRIC", NULL },
+    { GIMP_COLOR_RENDERING_INTENT_SATURATION, "GIMP_COLOR_RENDERING_INTENT_SATURATION", NULL },
+    { GIMP_COLOR_RENDERING_INTENT_ABSOLUTE_COLORIMETRIC, "GIMP_COLOR_RENDERING_INTENT_ABSOLUTE_COLORIMETRIC", NULL },
+    { 0, NULL, NULL }
+  };
+
+  static GType type = 0;
+
+  if (! type)
+    {
+      type = g_enum_register_static ("GimpColorRenderingIntent", values);
+      gimp_type_set_translation_domain (type, GETTEXT_PACKAGE "-libgimp");
+      gimp_enum_set_value_descriptions (type, descs);
+    }
+
+  return type;
+}
+
+GType
+gimp_color_file_open_behaviour_get_type (void)
+{
+  static const GEnumValue values[] =
+  {
+    { GIMP_COLOR_FILE_OPEN_ASK, "GIMP_COLOR_FILE_OPEN_ASK", "ask" },
+    { GIMP_COLOR_FILE_OPEN_LEAVE, "GIMP_COLOR_FILE_OPEN_LEAVE", "leave" },
+    { GIMP_COLOR_FILE_OPEN_CONVERT_RGB, "GIMP_COLOR_FILE_OPEN_CONVERT_RGB", "convert-rgb" },
+    { 0, NULL, NULL }
+  };
+
+  static const GimpEnumDesc descs[] =
+  {
+    { GIMP_COLOR_FILE_OPEN_ASK, N_("Ask"), NULL },
+    { GIMP_COLOR_FILE_OPEN_LEAVE, N_("Use embedded profile"), NULL },
+    { GIMP_COLOR_FILE_OPEN_CONVERT_RGB, N_("Convert to RGB workspace"), NULL },
+    { 0, NULL, NULL }
+  };
+
+  static GType type = 0;
+
+  if (! type)
+    {
+      type = g_enum_register_static ("GimpColorFileOpenBehaviour", values);
+      gimp_type_set_translation_domain (type, GETTEXT_PACKAGE "-libgimp");
+      gimp_enum_set_value_descriptions (type, descs);
+    }
+
+  return type;
+}
+
+
+/* Generated data ends here */
+
---
+++ libgimpconfig/gimpcolorconfig-enums.h
@@ -0,0 +1,70 @@
+/* LIBGIMP - The GIMP Library
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * GimpColorConfig enums
+ * Copyright (C) 2004  Stefan Döhla <stefan@doehla.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GIMP_COLOR_CONFIG_ENUMS_H__
+#define __GIMP_COLOR_CONFIG_ENUMS_H__
+
+
+#define GIMP_TYPE_COLOR_MANAGEMENT_MODE (gimp_color_management_mode_get_type ())
+
+GType gimp_color_management_mode_get_type (void) G_GNUC_CONST;
+
+typedef enum
+{
+  GIMP_COLOR_MANAGEMENT_OFF,       /*< desc="No color management"   >*/
+  GIMP_COLOR_MANAGEMENT_DISPLAY,   /*< desc="Color managed display" >*/
+  GIMP_COLOR_MANAGEMENT_SOFTPROOF  /*< desc="Print simulation"      >*/
+} GimpColorManagementMode;
+
+
+#define GIMP_TYPE_COLOR_RENDERING_INTENT \
+  (gimp_color_rendering_intent_get_type ())
+
+GType gimp_color_rendering_intent_get_type (void) G_GNUC_CONST;
+
+typedef enum
+{
+  GIMP_COLOR_RENDERING_INTENT_PERCEPTUAL,
+  /*< desc="Perceptual"            >*/
+  GIMP_COLOR_RENDERING_INTENT_RELATIVE_COLORIMETRIC,
+  /*< desc="Relative Colorimetric" >*/
+  GIMP_COLOR_RENDERING_INTENT_SATURATION,
+  /*< desc="Saturation"            >*/
+  GIMP_COLOR_RENDERING_INTENT_ABSOLUTE_COLORIMETRIC
+  /*< desc="Absolute Colorimetric" >*/
+} GimpColorRenderingIntent;
+
+
+#define GIMP_TYPE_COLOR_FILE_OPEN_BEHAVIOUR \
+  (gimp_color_file_open_behaviour_get_type ())
+
+GType gimp_color_file_open_behaviour_get_type (void) G_GNUC_CONST;
+
+typedef enum
+{
+  GIMP_COLOR_FILE_OPEN_ASK,         /*< desc="Ask"                       >*/
+  GIMP_COLOR_FILE_OPEN_LEAVE,       /*< desc="Use embedded profile"      >*/
+  GIMP_COLOR_FILE_OPEN_CONVERT_RGB  /*< desc="Convert to RGB workspace"  >*/
+} GimpColorFileOpenBehaviour;
+
+
+#endif /* GIMP_COLOR_CONFIG_ENUMS_H__ */
