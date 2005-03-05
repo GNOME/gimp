d268151bc544e66919960141b08c94c424361f3b
Manish Singh <yosh@gimp.org>
2005-03-05 08:27:32 +0000

    Update or add missing copyright and GPL comments.
    2005-03-05  Manish Singh  <yosh@gimp.org>
    
            * plug-ins/pygimp/*: Update or add missing copyright and GPL comments.

--- plug-ins/pygimp/plug-ins/gtkcons.py
+++ plug-ins/pygimp/plug-ins/gtkcons.py
@@ -3,7 +3,7 @@
 #   Interactive Python-GTK Console
 #   Copyright (C), 1998 James Henstridge <james@daa.com.au>
 #
-#    This program is free software; you can redistribute it and/or modify
+#   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 2 of the License, or
 #   (at your option) any later version.
@@ -15,7 +15,7 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 # This module implements an interactive python session in a GTK window.  To
 # start the session, use the gtk_console command.  Its specification is:
