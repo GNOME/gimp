ad47bcffb00949e62548ecb822e85a6cba54c984
William Skaggs <weskaggs@src.gnome.org>
2005-03-18 17:34:34 +0000

    Bill Skaggs  <weskaggs@primate.ucdavis.edu>
    	* app/core/gimpimage-convert.c:  when converting grayscale
    	to mono, treat the palette as gray rather than rgb, giving
    	more than tenfold speedup.  Fixes bug #170801.

--- app/core/gimpimage-convert.c
+++ app/core/gimpimage-convert.c
@@ -4057,7 +4057,6 @@ initialize_median_cut (GimpImageBaseType       type,
         }
 
       if (palette_type == GIMP_WEB_PALETTE  ||
-          palette_type == GIMP_MONO_PALETTE ||
           palette_type == GIMP_CUSTOM_PALETTE)
         {
           switch (dither_type)
