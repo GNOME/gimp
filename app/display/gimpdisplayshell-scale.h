fedce533a26968ed72aa06a8fd7365dafd9dc90b
Sven Neumann <sven@gimp.org>
2005-04-28 14:43:11 +0000

    corrected variable names.
    2005-04-28  Sven Neumann  <sven@gimp.org>
    
    	* app/display/gimpdisplayshell-scale.h: corrected variable names.

--- app/display/gimpdisplayshell-scale.h
+++ app/display/gimpdisplayshell-scale.h
@@ -28,20 +28,20 @@ void   gimp_display_shell_scale_get_fraction    (gdouble           zoom_factor,
 
 void   gimp_display_shell_scale_setup           (GimpDisplayShell *shell);
 
-void   gimp_display_shell_scale_set_dot_for_dot (GimpDisplayShell *gdisp,
+void   gimp_display_shell_scale_set_dot_for_dot (GimpDisplayShell *shell,
                                                  gboolean          dot_for_dot);
 
-void   gimp_display_shell_scale                 (GimpDisplayShell *gdisp,
+void   gimp_display_shell_scale                 (GimpDisplayShell *shell,
                                                  GimpZoomType      zoom_type,
                                                  gdouble           scale);
-void   gimp_display_shell_scale_to              (GimpDisplayShell *gdisp,
+void   gimp_display_shell_scale_to              (GimpDisplayShell *shell,
                                                  GimpZoomType      zoom_type,
                                                  gdouble           scale,
                                                  gdouble           x,
                                                  gdouble           y);
 void   gimp_display_shell_scale_fit_in          (GimpDisplayShell *shell);
 void   gimp_display_shell_scale_fit_to          (GimpDisplayShell *shell);
-void   gimp_display_shell_scale_by_values       (GimpDisplayShell *gdisp,
+void   gimp_display_shell_scale_by_values       (GimpDisplayShell *shell,
                                                  gdouble           scale,
                                                  gint              offset_x,
                                                  gint              offset_y,
