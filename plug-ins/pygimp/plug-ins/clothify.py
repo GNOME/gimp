1b49742c194aa751472e976d3d7bf1a80cb8a8ec
Manish Singh <yosh@gimp.org>
2005-04-29 04:28:44 +0000

    don't reset the passed parameters to default values. Spotted by Joao S. O.
    2005-04-28  Manish Singh  <yosh@gimp.org>
    
            * plug-ins/pygimp/plug-ins/clothify.py: don't reset the passed
            parameters to default values. Spotted by Joao S. O. Bueno Calligaris.

--- plug-ins/pygimp/plug-ins/clothify.py
+++ plug-ins/pygimp/plug-ins/clothify.py
@@ -22,8 +22,6 @@ from gimpfu import *
 
 def python_clothify(timg, tdrawable, bx=9, by=9,
                     azimuth=135, elevation=45, depth=3):
-    bx = 9 ; by = 9 ; azimuth = 135 ; elevation = 45 ; depth = 3
-
     width = tdrawable.width
     height = tdrawable.height
 
