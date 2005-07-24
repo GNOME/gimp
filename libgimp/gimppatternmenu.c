b7b799c0419ec3a32d1674515c1f955a6063268c
Michael Natterer <mitch@gimp.org>
2005-07-24 14:40:10 +0000

    libgimp/gimpbrushmenu.c put the previews into sunken instead of etched-in
    2005-07-24  Michael Natterer  <mitch@gimp.org>
    
    	* libgimp/gimpbrushmenu.c
    	* libgimp/gimppatternmenu.c: put the previews into sunken instead
    	of etched-in frames.

--- libgimp/gimppatternmenu.c
+++ libgimp/gimppatternmenu.c
@@ -133,7 +133,7 @@ gimp_pattern_select_widget_new (const gchar            *title,
   hbox = gtk_hbox_new (FALSE, 6);
 
   frame = gtk_frame_new (NULL);
-  gtk_frame_set_shadow_type (GTK_FRAME(frame), GTK_SHADOW_ETCHED_IN);
+  gtk_frame_set_shadow_type (GTK_FRAME(frame), GTK_SHADOW_IN);
   gtk_box_pack_start (GTK_BOX (hbox), frame, FALSE, FALSE, 0);
   gtk_widget_show (frame);
 
