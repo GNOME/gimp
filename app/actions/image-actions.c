48a5711ed44f2cf6a17a7823736d73e40df9df20
Akkana Peck <akkana@src.gnome.org>
2005-08-16 22:30:54 +0000

    app/actions/image-actions.c Fix formatting.
    * app/actions/image-actions.c
    * app/actions/plug-in-actions.c: Fix formatting.

--- app/actions/image-actions.c
+++ app/actions/image-actions.c
@@ -55,11 +55,11 @@ static GimpActionEntry image_actions[] =
     N_("Image Menu"), NULL, NULL, NULL,
     GIMP_HELP_IMAGE_WINDOW },
 
-  { "extensions-menu",      NULL, N_("_Xtns")      },
-  { "image-menu",           NULL, N_("_Image")     },
-  { "image-mode-menu",      NULL, N_("_Image Mode")      },
-  { "image-transform-menu", NULL, N_("_Transform") },
-  { "image-guides-menu",    NULL, N_("_Guides")    },
+  { "extensions-menu",      NULL, N_("_Xtns")       },
+  { "image-menu",           NULL, N_("_Image")      },
+  { "image-mode-menu",      NULL, N_("_Image Mode") },
+  { "image-transform-menu", NULL, N_("_Transform")  },
+  { "image-guides-menu",    NULL, N_("_Guides")     },
 
   { "image-new", GTK_STOCK_NEW,
     N_("_New..."), "<control>N", NULL,
