5340ff3d797f6862bdc626d7f7ef08beeacd4936
Sven Neumann <sven@gimp.org>
2005-10-27 15:34:58 +0000

    Made 2.2.9 release.
    2005-10-27  Sven Neumann  <sven@gimp.org>
    
            * Made 2.2.9 release.
    
            * autogen.sh: prefer newer versions of automake over older ones.

--- autogen.sh
+++ autogen.sh
@@ -122,16 +122,16 @@ fi
 
 
 echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
-if (automake-1.7 --version) < /dev/null > /dev/null 2>&1; then
-   AUTOMAKE=automake-1.7
-   ACLOCAL=aclocal-1.7
+if (automake-1.9 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.9
+   ACLOCAL=aclocal-1.9
 elif (automake-1.8 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.8
    ACLOCAL=aclocal-1.8
    AUTOMAKE_REQUIRED_VERSION=1.8.3
-elif (automake-1.9 --version) < /dev/null > /dev/null 2>&1; then
-   AUTOMAKE=automake-1.9
-   ACLOCAL=aclocal-1.9
+elif (automake-1.7 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.7
+   ACLOCAL=aclocal-1.7
 elif (automake-1.6 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.6
    ACLOCAL=aclocal-1.6
