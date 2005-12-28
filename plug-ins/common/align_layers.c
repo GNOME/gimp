fa41465e2ac035e53748f8f39ac41b87be35ecff
Sven Neumann <sven@gimp.org>
2005-12-28 21:08:26 +0000

    added "Image/Arrange" placeholder.
    2005-12-28  Sven Neumann  <sven@gimp.org>
    
            * menus/image-menu.xml.in: added "Image/Arrange" placeholder.
    
            * plug-ins/common/align_layers.c: moved to the Image menu, it
            doesn't work on a single layer.

--- plug-ins/common/align_layers.c
+++ plug-ins/common/align_layers.c
@@ -136,7 +136,7 @@ query (void)
                           G_N_ELEMENTS (args), 0,
                           args, NULL);
 
-  gimp_plugin_menu_register (PLUG_IN_PROC, "<Image>/Layer");
+  gimp_plugin_menu_register (PLUG_IN_PROC, "<Image>/Image/Arrange");
 }
 
 static void
