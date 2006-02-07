5d162ab7d11e4b64d04adf2b431141ea9768d79c
Sven Neumann <sven@gimp.org>
2006-02-07 10:47:15 +0000

    s/This program/GIMP/g
    2006-02-07  Sven Neumann  <sven@gimp.org>
    
    	* app/about.h (GIMP_LICENSE): s/This program/GIMP/g

--- app/about.h
+++ app/about.h
@@ -20,7 +20,8 @@
 #define __ABOUT_H__
 
 
-#define GIMP_ACRONYM  "GIMP"
+#define GIMP_ACRONYM \
+  _("GIMP")
 
 #define GIMP_NAME \
   _("GNU Image Manipulation Program")
@@ -30,20 +31,19 @@
     "Spencer Kimball, Peter Mattis and the GIMP Development Team")
 
 #define GIMP_LICENSE \
-  _("This program is free software; you can redistribute it and/or modify "  \
-    "it under the terms of the GNU General Public License as published by "  \
-    "the Free Software Foundation; either version 2 of the License, or "     \
-    "(at your option) any later version."                                    \
-    "\n\n"                                                                   \
-    "This program is distributed in the hope that it will be useful, "       \
-    "but WITHOUT ANY WARRANTY; without even the implied warranty of "        \
-    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. "                  \
-    "See the GNU General Public License for more details."                   \
-    "\n\n"                                                                   \
-    "You should have received a copy of the GNU General Public License "     \
-    "along with this program; if not, write to the Free Software "           \
-    "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, " \
-    "USA.")
+  _("GIMP is free software; you can redistribute it and/or modify it "   \
+    "under the terms of the GNU General Public License as published by " \
+    "the Free Software Foundation; either version 2 of the License, or " \
+    "(at your option) any later version."                                \
+    "\n\n"                                                               \
+    "GIMP is distributed in the hope that it will be useful, "           \
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of "    \
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "     \
+    "GNU General Public License for more details."                       \
+    "\n\n"                                                               \
+    "You should have received a copy of the GNU General Public License " \
+    "along with GIMP; if not, write to the Free Software Foundation, "   \
+    "Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.")
 
 
 #endif /* __ABOUT_H__ */
