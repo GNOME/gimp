d875469bd2aeae3da02014ef486dbb475a4d64d6
Sven Neumann <sven@gimp.org>
2006-03-09 10:51:27 +0000

    Merged from HEAD branch:
    2006-03-09  Sven Neumann  <sven@gimp.org>
    
    	Merged from HEAD branch:
    
    	* plug-ins/imagemap/imap_main.c (button_press): do not attempt to
    	use a NULL function pointer.

--- plug-ins/imagemap/imap_main.c
+++ plug-ins/imagemap/imap_main.c
@@ -1093,7 +1093,10 @@ preview_leave(GtkWidget *widget, GdkEventCrossing *event)
 static gboolean
 button_press(GtkWidget* widget, GdkEventButton* event, gpointer data)
 {
-   return _button_press_func(widget, event, _button_press_param);
+  if (_button_press_func)
+    return _button_press_func(widget, event, _button_press_param);
+
+  return FALSE;
 }
 
 /* A few global vars for key movement */
