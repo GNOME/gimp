ca17c590691ac6c84be598f6fafed664c854e9e2
Michael Natterer <mitch@gimp.org>
2006-05-29 12:33:42 +0000

    Merge from HEAD:
    2006-05-29  Michael Natterer  <mitch@gimp.org>
    
    	Merge from HEAD:
    
    	* app/widgets/gimpdock.c (gimp_dock_key_press_event): make sure
    	text widgets get all key events first. Fixes bug #301006.

--- app/widgets/gimpdock.c
+++ app/widgets/gimpdock.c
@@ -374,12 +374,17 @@ gimp_dock_key_press_event (GtkWidget   *widget,
                            GdkEventKey *event)
 {
   GtkWindow *window  = GTK_WINDOW (widget);
+  GtkWidget *focus   = gtk_window_get_focus (window);
   gboolean   handled = FALSE;
 
   /* we're overriding the GtkWindow implementation here to give
    * the focus widget precedence over unmodified accelerators
    * before the accelerator activation scheme.
    */
+
+  /* text widgets get all key events first */
+  if (G_UNLIKELY (GTK_IS_EDITABLE (focus) || GTK_IS_TEXT_VIEW (focus)))
+    handled = gtk_window_propagate_key_event (window, event);
 
   /* invoke control/alt accelerators */
   if (! handled && event->state & (GDK_CONTROL_MASK | GDK_MOD1_MASK))
