fa4427f83a834f8370ab683d42aa9bdcecbdcfc8
Sven Neumann <sven@gimp.org>
2006-06-05 20:36:21 +0000

    fixed documentation of gimp-selection-sharpen (bug #343911).
    2006-06-05  Sven Neumann  <sven@gimp.org>
    
    	* tools/pdbgen/pdb/selection.pdb: fixed documentation of
    	gimp-selection-sharpen (bug #343911).
    
    	* libgimp/gimpselection_pdb.c: regenerated.

--- libgimp/gimpselection_pdb.c
+++ libgimp/gimpselection_pdb.c
@@ -241,7 +241,7 @@ _gimp_selection_float (gint32 drawable_ID,
  * Invert the selection mask.
  *
  * This procedure inverts the selection mask. For every pixel in the
- * selection channel, its new value is calculated as (255 - old_value).
+ * selection channel, its new value is calculated as (255 - old-value).
  *
  * Returns: TRUE on success.
  */
@@ -271,9 +271,9 @@ gimp_selection_invert (gint32 image_ID)
  * Sharpen the selection mask.
  *
  * This procedure sharpens the selection mask. For every pixel in the
- * selection channel, if the value is &gt; 0, the new pixel is assigned
- * a value of 255. This removes any \"anti-aliasing\" that might exist
- * in the selection mask's boundary.
+ * selection channel, if the value is &gt; 127, the new pixel is
+ * assigned a value of 255. This removes any \"anti-aliasing\" that
+ * might exist in the selection mask's boundary.
  *
  * Returns: TRUE on success.
  */
