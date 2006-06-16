091c572e7ebc710f1c0f5029848ace87e11a5554
Michael Natterer <mitch@gimp.org>
2006-06-16 20:03:48 +0000

    moved variables to local scopes.
    2006-06-16  Michael Natterer  <mitch@gimp.org>
    
    	* app/core/gimpimage-convert.c (gimp_image_convert): moved
    	variables to local scopes.

--- app/core/gimpimage-convert.c
+++ app/core/gimpimage-convert.c
@@ -773,11 +773,8 @@ gimp_image_convert (GimpImage              *image,
                     GimpProgress           *progress)
 {
   QuantizeObj       *quantobj = NULL;
-  GimpLayer         *layer;
   GimpImageBaseType  old_type;
   GList             *list;
-  GimpImageType      new_layer_type;
-  TileManager       *new_tiles;
   const gchar       *undo_desc = NULL;
   gint               nth_layer, n_layers;
 
@@ -880,7 +877,7 @@ gimp_image_convert (GimpImage              *image,
                list;
                list = g_list_next (list), nth_layer++)
             {
-              layer = list->data;
+              GimpLayer *layer = list->data;
 
               if (old_type == GIMP_GRAY)
                 generate_histogram_gray (quantobj->histogram,
@@ -965,7 +962,9 @@ gimp_image_convert (GimpImage              *image,
        list;
        list = g_list_next (list), nth_layer++)
     {
-      layer = list->data;
+      GimpLayer     *layer = list->data;
+      GimpImageType  new_layer_type;
+      TileManager   *new_tiles;
 
       new_layer_type = GIMP_IMAGE_TYPE_FROM_BASE_TYPE (new_type);
 
