a584c1312d6d4c89cc0835213e467a951b1a2381
Michael Natterer <mitch@gimp.org>
2006-09-12 20:45:51 +0000

    added a parargaph of the original copyright notice that was forgotten when
    2006-09-12  Michael Natterer  <mitch@gimp.org>
    
    	* plug-ins/common/tiff.c: added a parargaph of the original
    	copyright notice that was forgotten when originally pasting it.

--- plug-ins/common/tiff.c
+++ plug-ins/common/tiff.c
@@ -20,21 +20,27 @@
  */
 
 /*
-** tifftopnm.c - converts a Tagged Image File to a portable anymap
-**
-** Derived by Jef Poskanzer from tif2ras.c, which is:
-**
-** Copyright (c) 1990 by Sun Microsystems, Inc.
-**
-** Author: Patrick J. Naughton
-** naughton@wind.sun.com
-**
-** This file is provided AS IS with no warranties of any kind.  The author
-** shall have no liability with respect to the infringement of copyrights,
-** trade secrets or any patents by this file or any part thereof.  In no
-** event will the author be liable for any lost revenue or profits or
-** other special, indirect and consequential damages.
-*/
+ * tifftopnm.c - converts a Tagged Image File to a portable anymap
+ *
+ * Derived by Jef Poskanzer from tif2ras.c, which is:
+ *
+ * Copyright (c) 1990 by Sun Microsystems, Inc.
+ *
+ * Author: Patrick J. Naughton
+ * naughton@wind.sun.com
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation.
+ *
+ * This file is provided AS IS with no warranties of any kind.  The author
+ * shall have no liability with respect to the infringement of copyrights,
+ * trade secrets or any patents by this file or any part thereof.  In no
+ * event will the author be liable for any lost revenue or profits or
+ * other special, indirect and consequential damages.
+ */
 
 #include "config.h"
 
