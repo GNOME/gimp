9da34b7e79e9d604a75800bff174b9417228a9c8
William Skaggs <weskaggs@src.gnome.org>
2006-09-15 16:11:36 +0000

    Bill Skaggs  <weskaggs@primate.ucdavis.edu>
    	* plug-ins/common/screenshot.c: when shooting a region, apply delay
    	_after_ selecting region, applying before is useless.

--- plug-ins/common/screenshot.c
+++ plug-ins/common/screenshot.c
@@ -328,7 +328,7 @@ run (const gchar      *name,
 
   if (status == GIMP_PDB_SUCCESS)
     {
-      if (shootvals.select_delay > 0)
+      if (shootvals.select_delay > 0 && shootvals.shoot_type != SHOOT_REGION)
 	shoot_delay (shootvals.select_delay);
 
       if (shootvals.shoot_type != SHOOT_ROOT && ! shootvals.window_id)
@@ -338,6 +338,9 @@ run (const gchar      *name,
           if (! shootvals.window_id)
             status = GIMP_PDB_CANCEL;
         }
+
+      if (shootvals.select_delay > 0 && shootvals.shoot_type == SHOOT_REGION)
+	shoot_delay (shootvals.select_delay);
     }
 
   if (status == GIMP_PDB_SUCCESS)
