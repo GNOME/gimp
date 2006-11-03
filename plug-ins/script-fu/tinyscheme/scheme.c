798c6b924a326a57659cab21dd64f6bbb30cf012
Sven Neumann <sven@gimp.org>
2006-11-03 09:07:43 +0000

    use g_utf8_casefold() to implement case insensitive sorting.
    2006-11-03  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/script-fu/tinyscheme/scheme.c (utf8_stricmp): use
    	g_utf8_casefold() to implement case insensitive sorting.

--- plug-ins/script-fu/tinyscheme/scheme.c
+++ plug-ins/script-fu/tinyscheme/scheme.c
@@ -79,8 +79,8 @@ static int utf8_stricmp(const char *s1, const char *s2)
   char *s1a, *s2a;
   int result;
 
-  s1a = g_utf8_strdown(s1, -1);
-  s2a = g_utf8_strdown(s2, -1);
+  s1a = g_utf8_casefold(s1, -1);
+  s2a = g_utf8_casefold(s2, -1);
 
   result = g_utf8_collate(s1a, s2a);
 
