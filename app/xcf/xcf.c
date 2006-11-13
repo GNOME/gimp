baf90e745ecb8200df5705b0a86bb51d025f2f7f
Michael Natterer <mitch@gimp.org>
2006-11-13 13:11:34 +0000

    fix g_str_has_prefix() change below. Now we can open XCF files again.
    2006-11-13  Michael Natterer  <mitch@gimp.org>
    
    	* app/xcf/xcf.c (xcf_load_invoker): fix g_str_has_prefix() change
    	below. Now we can open XCF files again. Fixes bug #374657.

--- app/xcf/xcf.c
+++ app/xcf/xcf.c
@@ -272,7 +272,7 @@ xcf_load_invoker (GimpProcedure     *procedure,
 
       info.cp += xcf_read_int8 (info.fp, (guint8 *) id, 14);
 
-      if (g_str_has_prefix (id, "gimp xcf "))
+      if (! g_str_has_prefix (id, "gimp xcf "))
         {
           success = FALSE;
         }
