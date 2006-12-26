682b7ec50dffe2ecdbeded805773876d212f6a2f
Sven Neumann <sven@gimp.org>
2006-12-26 23:27:28 +0000

    show a message in the statusbar when settings are saved.
    2006-12-27  Sven Neumann  <sven@gimp.org>
    
    	* app/tools/gimpimagemaptool.c (gimp_image_map_tool_load_save):
    	show a message in the statusbar when settings are saved.

--- app/tools/gimpimagemaptool.c
+++ app/tools/gimpimagemaptool.c
@@ -598,7 +598,17 @@ gimp_image_map_tool_load_save (GimpImageMapTool *tool,
 
   if (save)
     {
-      gimp_image_map_tool_settings_save (tool, file);
+      if (gimp_image_map_tool_settings_save (tool, file))
+        {
+          gchar *name = g_filename_display_name (filename);
+
+          gimp_message (GIMP_TOOL (tool)->tool_info->gimp,
+                        G_OBJECT (GIMP_TOOL (tool)->display),
+                        GIMP_MESSAGE_INFO,
+                        _("Settings saved to '%s'"),
+                        name);
+          g_free (name);
+        }
     }
   else if (! gimp_image_map_tool_settings_load (tool, file, &error))
     {
