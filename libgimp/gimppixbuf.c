98b060eb18e985eb239ba55a7c0893ef64ac48ca
Sven Neumann <sven@gimp.org>
2007-01-22 21:20:04 +0000

    free the thumbnail data that used to be leaked for grayscale thumbnails.
    2007-01-22  Sven Neumann  <sven@gimp.org>
    
    	* libgimp/gimppixbuf.c (gimp_pixbuf_from_data): free the 
    thumbnail
    	data that used to be leaked for grayscale thumbnails.
    
    
    svn path=/trunk/; revision=21758

--- libgimp/gimppixbuf.c
+++ libgimp/gimppixbuf.c
@@ -176,116 +176,6 @@ gimp_drawable_get_sub_thumbnail (gint32                  drawable_ID,
   return NULL;
 }
 
-static GdkPixbuf *
-gimp_pixbuf_from_data (guchar                 *data,
-                       gint                    width,
-                       gint                    height,
-                       gint                    bpp,
-                       GimpPixbufTransparency  alpha)
-{
-  GdkPixbuf *pixbuf;
-
-  switch (bpp)
-    {
-    case 1:
-      pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, width, height);
-      {
-        guchar *pixels    = gdk_pixbuf_get_pixels (pixbuf);
-        gint    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
-        gint    y;
-
-        for (y = 0; y < height; y++)
-          {
-            guchar *dest = pixels;
-            gint    x;
-
-            for (x = 0; x < width; x++, data += 1, dest += 3)
-              {
-                dest[0] = dest[1] = dest[2] = data[0];
-              }
-
-            pixels += rowstride;
-          }
-      }
-      bpp = 3;
-      break;
-
-    case 2:
-      pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, TRUE, 8, width, height);
-      {
-        guchar *pixels    = gdk_pixbuf_get_pixels (pixbuf);
-        gint    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
-        gint    y;
-
-        for (y = 0; y < height; y++)
-          {
-            guchar *dest = pixels;
-            gint    x;
-
-            for (x = 0; x < width; x++, data += 2, dest += 4)
-              {
-                dest[0] = dest[1] = dest[2] = data[0];
-                dest[3] = data[1];
-              }
-
-            pixels += rowstride;
-          }
-      }
-      bpp = 4;
-      break;
-
-    case 3:
-      pixbuf = gdk_pixbuf_new_from_data (data,
-                                         GDK_COLORSPACE_RGB, FALSE, 8,
-                                         width, height, width * bpp,
-                                         (GdkPixbufDestroyNotify) g_free, NULL);
-      break;
-
-    case 4:
-      pixbuf = gdk_pixbuf_new_from_data (data,
-                                         GDK_COLORSPACE_RGB, TRUE, 8,
-                                         width, height, width * bpp,
-                                         (GdkPixbufDestroyNotify) g_free, NULL);
-      break;
-
-    default:
-      g_return_val_if_reached (NULL);
-      return NULL;
-    }
-
-  if (bpp == 4)
-    {
-      GdkPixbuf *tmp;
-      gint       check_size  = 0;
-
-      switch (alpha)
-        {
-        case GIMP_PIXBUF_KEEP_ALPHA:
-          return pixbuf;
-
-        case GIMP_PIXBUF_SMALL_CHECKS:
-          check_size = GIMP_CHECK_SIZE_SM;
-          break;
-
-        case GIMP_PIXBUF_LARGE_CHECKS:
-          check_size = GIMP_CHECK_SIZE;
-          break;
-        }
-
-      tmp = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, width, height);
-
-      gdk_pixbuf_composite_color (pixbuf, tmp,
-                                  0, 0, width, height, 0, 0, 1.0, 1.0,
-                                  GDK_INTERP_NEAREST, 255,
-                                  0, 0, check_size, 0x66666666, 0x99999999);
-
-      g_object_unref (pixbuf);
-      pixbuf = tmp;
-    }
-
-  return pixbuf;
-}
-
 /**
  * gimp_layer_new_from_pixbuf:
  * @image_ID: The RGB image to which to add the layer.
@@ -398,4 +288,125 @@ gimp_layer_new_from_pixbuf (gint32                image_ID,
   gimp_drawable_detach (drawable);
 
   return layer;
+}
+
+
+/*
+ * The data that is passed to this function is either freed here or
+ * owned by the returned pixbuf.
+ */
+static GdkPixbuf *
+gimp_pixbuf_from_data (guchar                 *data,
+                       gint                    width,
+                       gint                    height,
+                       gint                    bpp,
+                       GimpPixbufTransparency  alpha)
+{
+  GdkPixbuf *pixbuf;
+
+  switch (bpp)
+    {
+    case 1:
+      pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, width, height);
+      {
+        guchar *src       = data;
+        guchar *pixels    = gdk_pixbuf_get_pixels (pixbuf);
+        gint    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+        gint    y;
+
+        for (y = 0; y < height; y++)
+          {
+            guchar *dest = pixels;
+            gint    x;
+
+            for (x = 0; x < width; x++, src += 1, dest += 3)
+              {
+                dest[0] = dest[1] = dest[2] = src[0];
+              }
+
+            pixels += rowstride;
+          }
+
+        g_free (data);
+      }
+      bpp = 3;
+      break;
+
+    case 2:
+      pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, TRUE, 8, width, height);
+      {
+        guchar *src       = data;
+        guchar *pixels    = gdk_pixbuf_get_pixels (pixbuf);
+        gint    rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+        gint    y;
+
+        for (y = 0; y < height; y++)
+          {
+            guchar *dest = pixels;
+            gint    x;
+
+            for (x = 0; x < width; x++, src += 2, dest += 4)
+              {
+                dest[0] = dest[1] = dest[2] = src[0];
+                dest[3] = src[1];
+              }
+
+            pixels += rowstride;
+          }
+
+        g_free (data);
+      }
+      bpp = 4;
+      break;
+
+    case 3:
+      pixbuf = gdk_pixbuf_new_from_data (data,
+                                         GDK_COLORSPACE_RGB, FALSE, 8,
+                                         width, height, width * bpp,
+                                         (GdkPixbufDestroyNotify) g_free, NULL);
+      break;
+
+    case 4:
+      pixbuf = gdk_pixbuf_new_from_data (data,
+                                         GDK_COLORSPACE_RGB, TRUE, 8,
+                                         width, height, width * bpp,
+                                         (GdkPixbufDestroyNotify) g_free, NULL);
+      break;
+
+    default:
+      g_return_val_if_reached (NULL);
+      return NULL;
+    }
+
+  if (bpp == 4)
+    {
+      GdkPixbuf *tmp;
+      gint       check_size  = 0;
+
+      switch (alpha)
+        {
+        case GIMP_PIXBUF_KEEP_ALPHA:
+          return pixbuf;
+
+        case GIMP_PIXBUF_SMALL_CHECKS:
+          check_size = GIMP_CHECK_SIZE_SM;
+          break;
+
+        case GIMP_PIXBUF_LARGE_CHECKS:
+          check_size = GIMP_CHECK_SIZE;
+          break;
+        }
+
+      tmp = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, width, height);
+
+      gdk_pixbuf_composite_color (pixbuf, tmp,
+                                  0, 0, width, height, 0, 0, 1.0, 1.0,
+                                  GDK_INTERP_NEAREST, 255,
+                                  0, 0, check_size, 0x66666666, 0x99999999);
+
+      g_object_unref (pixbuf);
+      pixbuf = tmp;
+    }
+
+  return pixbuf;
 }
