287331b36f962c8c5347dea7a9065ca226e5cc15
Sven Neumann <sven@gimp.org>
2007-02-10 15:10:01 +0000

    modules/controller_linux_input.c changed license to GPL, updated copyright
    2007-02-10  Sven Neumann  <sven@gimp.org>
    
    	* modules/controller_linux_input.c
    	* modules/controller_midi.c: changed license to GPL, updated
    	copyright and module info.
    
    
    svn path=/trunk/; revision=21889

--- modules/controller_linux_input.c
+++ modules/controller_linux_input.c
@@ -1,23 +1,23 @@
-/* LIBGIMP - The GIMP Library
- * Copyright (C) 1995-1997 Peter Mattis and Spencer Kimball
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995-1997 Spencer Kimball and Peter Mattis
  *
  * controller_linux_input.c
- * Copyright (C) 2004 Sven Neumann <sven@gimp.org>
+ * Copyright (C) 2004-2007 Sven Neumann <sven@gimp.org>
+ *                         Michael Natterer <mitch@gimp.org>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include "config.h"
@@ -169,10 +169,10 @@ static const GimpModuleInfo linux_input_info =
 {
   GIMP_MODULE_ABI_VERSION,
   N_("Linux input event controller"),
-  "Sven Neumann <sven@gimp.org>",
-  "v0.1",
-  "(c) 2004, released under the GPL",
-  "June 2004"
+  "Sven Neumann <sven@gimp.org>, Michael Natterer <mitch@gimp.org>",
+  "v0.2",
+  "(c) 2004-2007, released under the GPL",
+  "2004-2007"
 };
 
 
