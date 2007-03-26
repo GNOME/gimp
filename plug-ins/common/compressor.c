9057a3dfa4baf690863837798221a0fd8a91f689
Michael Schumacher <schumaml@cvs.gnome.org>
2007-03-26 22:43:46 +0000

    Check if the files to be read or written and the temporary files have been
    2007-03-26  Michael Schumacher  <schumaml@cvs.gnome.org>
    
    	* plug-ins/common/compressor.c (save_image, load_image): 
    	Check if the files to be read or written and the temporary files
    	have been opened. Otherwise, bail out safely instead of causing a
    	crash. Fixes bug #422444.
    
    svn path=/trunk/; revision=22175

--- plug-ins/common/compressor.c
+++ plug-ins/common/compressor.c
@@ -459,6 +459,24 @@ save_image (const Compressor *compressor,
     in  = g_fopen (tmpname, "rb");
     out = g_fopen (filename, "wb");
 
+    if (in == NULL)
+      {
+        g_message (_("Could not open '%s' for reading: %s"),
+                   gimp_filename_to_utf8 (tmpname), g_strerror (errno));
+        g_free (tmpname);
+
+        return GIMP_PDB_EXECUTION_ERROR;
+      }
+
+    if (out == NULL)
+      {
+        g_message (_("Could not open '%s' for writing: %s"),
+                   gimp_filename_to_utf8 (filename), g_strerror (errno));
+        g_free (tmpname);
+        
+        return GIMP_PDB_EXECUTION_ERROR;
+      }
+
     startupinfo.cb          = sizeof (STARTUPINFO);
     startupinfo.lpReserved  = NULL;
     startupinfo.lpDesktop   = NULL;
@@ -598,6 +616,26 @@ load_image (const Compressor  *compressor,
 
     in  = g_fopen (filename, "rb");
     out = g_fopen (tmpname, "wb");
+
+    if (in == NULL)
+      {
+        g_message (_("Could not open '%s' for reading: %s"),
+                   gimp_filename_to_utf8 (filename), g_strerror (errno));
+        g_free (tmpname);
+       
+        *status = GIMP_PDB_EXECUTION_ERROR;
+        return -1;
+      }
+
+    if (out == NULL)
+      {
+        g_message (_("Could not open '%s' for writing: %s"),
+                   gimp_filename_to_utf8 (tmpname), g_strerror (errno));
+        g_free (tmpname);
+
+        *status = GIMP_PDB_EXECUTION_ERROR;
+        return -1;
+      }
 
     startupinfo.cb          = sizeof (STARTUPINFO);
     startupinfo.lpReserved  = NULL;
