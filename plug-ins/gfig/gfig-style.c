43360ede74aab578fb43031bd151540239732a8d
Mukund Sivaraman <muks@mukund.org>
2007-05-31 10:09:06 +0000

    Specify maximum field width in sscanf() to avoid buffer overflows.
    2007-05-31  Mukund Sivaraman  <muks@mukund.org> 
    
            * plug-ins/gfig/gfig-style.c: Specify maximum field width in
            sscanf() to avoid buffer overflows.
    
    
    svn path=/trunk/; revision=22680

--- plug-ins/gfig/gfig-style.c
+++ plug-ins/gfig/gfig-style.c
@@ -213,7 +213,7 @@ gfig_load_style (Style *style,
   offset = ftell (fp);
 
   get_line (load_buf2, MAX_LOAD_LINE, fp, 0);
-  if (1 != sscanf (load_buf2, "<Style %s>", name))
+  if (1 != sscanf (load_buf2, "<Style %99s>", name))
     {
       /* no style data, copy default style and fail silently */
       gfig_style_copy (style, &gfig_context->default_style, "default style");
