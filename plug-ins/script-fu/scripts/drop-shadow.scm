6e412c1e42e6821855ae1671c766b82685d6858d
Sven Neumann <sven@gimp.org>
2007-09-24 08:00:14 +0000

    applied patch from Ulf-D. Ehlert which fixes a bug which may shrink the
    2007-09-24  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/script-fu/scripts/drop-shadow.scm: applied patch from
    	Ulf-D. Ehlert which fixes a bug which may shrink the image instead
    	of enlarging it (bug #478385).
    
    svn path=/trunk/; revision=23636

--- plug-ins/script-fu/scripts/drop-shadow.scm
+++ plug-ins/script-fu/scripts/drop-shadow.scm
@@ -93,14 +93,14 @@
                 (set! image-offset-x (- 0 (+ shadow-offset-x
                                              shadow-transl-x)))
                 (set! shadow-offset-x (- 0 shadow-transl-x))
-                (set! new-image-width (- new-image-width image-offset-x))))
+                (set! new-image-width (+ new-image-width image-offset-x))))
 
           (if (< (+ shadow-offset-y shadow-transl-y) 0)
               (begin
                 (set! image-offset-y (- 0 (+ shadow-offset-y
                                              shadow-transl-y)))
                 (set! shadow-offset-y (- 0 shadow-transl-y))
-                (set! new-image-height (- new-image-height image-offset-y))))
+                (set! new-image-height (+ new-image-height image-offset-y))))
 
           (if (> (+ (+ shadow-width shadow-offset-x) shadow-transl-x)
                  new-image-width)
@@ -127,7 +127,7 @@
                                             "Drop Shadow"
                                             shadow-opacity
                                             NORMAL-MODE)))
-   (gimp-image-add-layer image shadow-layer -1)
+    (gimp-image-add-layer image shadow-layer -1)
     (gimp-layer-set-offsets shadow-layer
                             shadow-offset-x
                             shadow-offset-y))
