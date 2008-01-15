9cffaaa8e1b6c238f9621c22569e5dae20998a69
Michael Natterer <mitch@gimp.org>
2008-01-15 14:46:11 +0000

    Merged from trunk:
    2008-01-15  Michael Natterer  <mitch@gimp.org>
    
    	Merged from trunk:
    
    	* app/tools/gimpimagemaptool.c (gimp_image_map_tool_notify_preview):
    	check for image_map_tool->image_map also when the preview is
    	switched on, not only when it's switched off.
    
    
    svn path=/branches/gimp-2-4/; revision=24619

--- app/tools/gimpimagemaptool.c
+++ app/tools/gimpimagemaptool.c
@@ -524,17 +524,17 @@ gimp_image_map_tool_notify_preview (GObject          *config,
   GimpTool            *tool    = GIMP_TOOL (image_map_tool);
   GimpImageMapOptions *options = GIMP_IMAGE_MAP_OPTIONS (config);
 
-  if (options->preview)
+  if (image_map_tool->image_map)
     {
-      gimp_tool_control_set_preserve (tool->control, TRUE);
+      if (options->preview)
+        {
+          gimp_tool_control_set_preserve (tool->control, TRUE);
 
-      gimp_image_map_tool_map (image_map_tool);
+          gimp_image_map_tool_map (image_map_tool);
 
-      gimp_tool_control_set_preserve (tool->control, FALSE);
-    }
-  else
-    {
-      if (image_map_tool->image_map)
+          gimp_tool_control_set_preserve (tool->control, FALSE);
+        }
+      else
         {
           gimp_tool_control_set_preserve (tool->control, TRUE);
 
