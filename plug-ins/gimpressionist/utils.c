2163a07f9eebdb45f436bd8835951b11e6a5bd9a
Sven Neumann <sven@gimp.org>
2008-01-28 21:08:35 +0000

    added missing copyright header.
    2008-01-28  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/gimpressionist/*.[ch]: added missing copyright 
    header.
    
    
    svn path=/trunk/; revision=24739

--- plug-ins/gimpressionist/utils.c
+++ plug-ins/gimpressionist/utils.c
@@ -1,7 +1,26 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 /*
  * utils.c - various utility routines that don't fit anywhere else. Usually
  * these routines don't affect the state of the program.
- * */
+ */
+
 #include "config.h"
 
 #include <string.h>
