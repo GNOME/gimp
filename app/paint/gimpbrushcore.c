a17e88f50abc85ee33b579b0c3c15d132a3f301f
Sven Neumann <sven@gimp.org>
2008-02-04 15:39:00 +0000

    Merged from trunk:
    2008-02-04  Sven Neumann  <sven@gimp.org>
    
    	Merged from trunk:
    
    	* app/paint/gimpbrushcore.c (gimp_brush_core_create_bound_segs):
    	changed workaround to look at the brush size instead (bug #514309).
    
    svn path=/branches/gimp-2-4/; revision=24787

--- app/paint/gimpbrushcore.c
+++ app/paint/gimpbrushcore.c
@@ -780,16 +780,15 @@ gimp_brush_core_create_bound_segs (GimpBrushCore    *core,
       BoundSeg    *boundary;
       gint         num_groups;
 
-      if (mask->width > 2 && mask->height > 2)
-        {
-          /*  smooth the mask in order to obtain a simpler boundary  */
-          pixel_region_init_temp_buf (&PR, mask,
-                                      1, 1, mask->width - 2, mask->height - 2);
-          smooth_region (&PR);
-        }
-
       pixel_region_init_temp_buf (&PR, mask,
                                   0, 0, mask->width, mask->height);
+
+      /* Large, complex brush outlines are a performance problem.
+       * Smooth the mask in order to obtain a simpler boundary.
+       */
+      if (mask->width > 32 && mask->height > 32)
+        smooth_region (&PR);
+
 
       boundary = boundary_find (&PR, BOUNDARY_WITHIN_BOUNDS,
                                 0, 0, PR.w, PR.h,
