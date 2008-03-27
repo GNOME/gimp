9fba58b39570a4290f8f0785dbd38efc3f5840e2
Sven Neumann <sven@gimp.org>
2008-03-27 19:45:41 +0000

    removed SHOW_TIPS_BLURB.
    2008-03-27  Sven Neumann  <sven@gimp.org>
    
    	* app/config/gimprc-blurbs.h: removed SHOW_TIPS_BLURB.
    
    
    svn path=/trunk/; revision=25266

--- app/config/gimprc-blurbs.h
+++ app/config/gimprc-blurbs.h
@@ -327,9 +327,6 @@ N_("When enabled, the grid is visible by default. This can also be toggled " \
 N_("When enabled, the sample points are visible by default. This can also be " \
    "toggled with the \"View->Show Sample Points\" command.")
 
-#define SHOW_TIPS_BLURB \
-N_("Enable displaying a handy GIMP tip on startup.")
-
 #define SHOW_TOOLTIPS_BLURB \
 N_("Show a tooltip when the pointer hovers over an item.")
 
