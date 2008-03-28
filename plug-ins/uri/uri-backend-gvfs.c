3f18f32cfef7dbf1a6c84830cbc6836a419440a9
Sven Neumann <sven@gimp.org>
2008-03-28 16:09:44 +0000

    put GIO check back in as we need it to initialize GIO_CFLAGS and GIO_LIBS.
    2008-03-28  Sven Neumann  <sven@gimp.org>
    
    	* configure.in: put GIO check back in as we need it to initialize
    	GIO_CFLAGS and GIO_LIBS. Renamed --without-gio to --without-gvfs.
    
    	* INSTALL: changed accordingly.
    
    	* plug-ins/uri/Makefile.am
    	* plug-ins/uri/uri-backend-gio.c
    	* plug-ins/uri/uri-backend-gvfs.c: renamed the gio backend to gvfs.
    
    svn path=/trunk/; revision=25284

--- plug-ins/uri/uri-backend-gvfs.c
+++ plug-ins/uri/uri-backend-gvfs.c
@@ -1,7 +1,7 @@
 /* GIMP - The GNU Image Manipulation Program
  * Copyright (C) 1995 Spencer Kimball and Peter Mattis
  *
- * URI plug-in, GIO backend
+ * URI plug-in, GIO/GVfs backend
  * Copyright (C) 2008  Sven Neumann <sven@gimp.org>
  *
  * This program is free software; you can redistribute it and/or modify
