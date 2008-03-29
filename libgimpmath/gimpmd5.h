540d533bef37dad9e85387789ec39617e3c4c58d
Sven Neumann <sven@gimp.org>
2008-03-29 16:56:44 +0000

    removed implementation of the MD5 algorithm and delegate to GChecksum
    2008-03-29  Sven Neumann  <sven@gimp.org>
    
    	* libgimpmath/gimpmd5.[ch]: removed implementation of the MD5
    	algorithm and delegate to GChecksum instead.
    	
    	* libgimpmath/Makefile.am
    	* libgimpmath/test-md5.c: removed test of the MD5 
    implementation.
    
    
    svn path=/trunk/; revision=25302

--- libgimpmath/gimpmd5.h
+++ libgimpmath/gimpmd5.h
@@ -2,23 +2,7 @@
  *
  * gimpmd5.h
  *
- * This code implements the MD5 message-digest algorithm.
- * The algorithm is due to Ron Rivest.  This code was
- * written by Colin Plumb in 1993, no copyright is claimed.
- * This code is in the public domain; do with it what you wish.
- *
- * Equivalent code is available from RSA Data Security, Inc.
- * This code has been tested against that, and is equivalent,
- * except that you don't need to include two pages of legalese
- * with every copy.
- *
- * GIMPified 2002 by Sven Neumann <sven@gimp.org>
- */
-
-/* parts of this file are :
- * Written March 1993 by Branko Lankester
- * Modified June 1993 by Colin Plumb for altered md5.c.
- * Modified October 1995 by Erik Troan for RPM
+ * Use of this code is deprecated! Use %GChecksum from GLib instead.
  */
 
 #ifndef __GIMP_MD5_H__
