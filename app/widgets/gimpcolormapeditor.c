af8f8ed6455f772fa223073be5c47552b600ab96
Sven Neumann <sven@gimp.org>
2008-04-05 16:15:18 +0000

    corrected fix for bug #511926.
    2008-04-05  Sven Neumann  <sven@gimp.org>
    
    	* app/widgets/gimpcolormapeditor.c (gimp_colormap_editor_draw):
    	corrected fix for bug #511926.
    
    
    svn path=/trunk/; revision=25371

--- app/widgets/gimpcolormapeditor.c
+++ app/widgets/gimpcolormapeditor.c
@@ -563,7 +563,7 @@ gimp_colormap_editor_draw (GimpColormapEditor *editor)
               row[(j * cellsize + k) * 3 + b] = image->colormap[col * 3 + b];
         }
 
-      if (j * cellsize > width)
+      if (j * cellsize < width)
         memset (row + j * cellsize * 3, 255, 3 * (width - j * cellsize));
 
       for (k = 0; k < cellsize; k++)
