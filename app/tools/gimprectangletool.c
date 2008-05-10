39291199342cea1bd17c0e7e3b80b05893069723
Martin Nordholts <martinn@svn.gnome.org>
2008-05-10 19:23:54 +0000

    Merged from trunk:
    2008-05-10  Martin Nordholts  <martinn@svn.gnome.org>
    
    	Merged from trunk:
    
    	* app/tools/gimprectangletool.c
    	(gimp_rectangle_tool_button_press): Active the tool control
    	earlier.
    	(gimp_rectangle_tool_synthesize_motion): Bail out if the tool
    	control is active, we don't want to synthesize a motion in this
    	case as it emits unwanted rectangle-changed signals.
    
    svn path=/branches/gimp-2-4/; revision=25631

--- app/tools/gimprectangletool.c
+++ app/tools/gimprectangletool.c
@@ -719,6 +719,8 @@ gimp_rectangle_tool_button_press (GimpTool        *tool,
 
   gimp_draw_tool_pause (draw_tool);
 
+  gimp_tool_control_activate (tool->control);
+
   if (display != tool->display)
     {
       if (gimp_draw_tool_is_active (draw_tool))
@@ -881,8 +883,6 @@ gimp_rectangle_tool_button_press (GimpTool        *tool,
       private->other_side_x = other_side_x;
       private->other_side_y = other_side_y;
     }
-
-  gimp_tool_control_activate (tool->control);
 
   gimp_draw_tool_resume (draw_tool);
 }
@@ -2080,6 +2080,17 @@ gimp_rectangle_tool_synthesize_motion (GimpRectangleTool *rectangle,
 {
   GimpRectangleToolPrivate *private;
   GimpRectangleFunction     old_function;
+
+  /* We don't want to synthesize motions if the tool control is active
+   * since that means the mouse button is down and the rectangle will
+   * get updated in _motion anyway. The reason we want to prevent this
+   * function from executing is that is emits the rectangle-changed
+   * signal which we don't want in the middle of a rectangle change.
+   */
+  if (gimp_tool_control_is_active (GIMP_TOOL (rectangle)->control))
+    {
+      return;
+    }
 
   private = GIMP_RECTANGLE_TOOL_GET_PRIVATE (rectangle);
 
