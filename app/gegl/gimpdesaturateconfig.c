7add4e32479aed1414d9b1630348846c6d6aceed
Michael Natterer <mitch@gimp.org>
2008-05-21 19:04:56 +0000

    use a stock_id that exists.
    2008-05-21  Michael Natterer  <mitch@gimp.org>
    
    	* app/gegl/gimpdesaturateconfig.c: use a stock_id that exists.
    
    
    svn path=/trunk/; revision=25737

--- app/gegl/gimpdesaturateconfig.c
+++ app/gegl/gimpdesaturateconfig.c
@@ -63,7 +63,10 @@ gimp_desaturate_config_class_init (GimpDesaturateConfigClass *klass)
   object_class->set_property       = gimp_desaturate_config_set_property;
   object_class->get_property       = gimp_desaturate_config_get_property;
 
-  viewable_class->default_stock_id = "gimp-tool-desaturate";
+#ifdef __GNUC__
+#warning FIXME: change string when a desaturate icon gets added
+#endif
+  viewable_class->default_stock_id = "gimp-convert-grayscale";
 
   GIMP_CONFIG_INSTALL_PROP_ENUM (object_class, PROP_MODE,
                                  "mode",
