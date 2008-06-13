aa0dd869226b9036272b1af42e88c31e74cec134
Sven Neumann <sven@gimp.org>
2008-06-13 06:14:08 +0000

    configure.in depend on the newest released babl, version 0.0.22.
    2008-06-13  Sven Neumann  <sven@gimp.org>
    
    	* configure.in
    	* app/sanity.c (BABL_REQUIRED_MICRO): depend on the newest
    	released babl, version 0.0.22.
    
    
    svn path=/trunk/; revision=25941

--- app/sanity.c
+++ app/sanity.c
@@ -260,7 +260,7 @@ sanity_check_babl (void)
 
 #define BABL_REQUIRED_MAJOR 0
 #define BABL_REQUIRED_MINOR 0
-#define BABL_REQUIRED_MICRO 21
+#define BABL_REQUIRED_MICRO 22
 
   babl_get_version (&babl_major_version,
                     &babl_minor_version,
