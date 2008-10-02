d5be34058cdcef04083cec78b78f4e2931173683
Sven Neumann <sven@gimp.org>
2008-10-02 19:08:16 +0000

    add "text" to the list of blacklisted operations.
    2008-10-02  Sven Neumann  <sven@gimp.org>
    
    	* app/tools/gimpgegltool.c 
    (gimp_gegl_tool_operation_blacklisted):
    	add "text" to the list of blacklisted operations.
    
    
    svn path=/trunk/; revision=27114

--- app/tools/gimpgegltool.c
+++ app/tools/gimpgegltool.c
@@ -226,7 +226,7 @@ gimp_gegl_tool_operation_blacklisted (const gchar *name)
 {
   static const gchar * const blacklist[] =
   {
-    "convert-format", "gimp-", "introspect", "stress"
+    "convert-format", "gimp-", "introspect", "stress", "text"
   };
   gint i;
 
