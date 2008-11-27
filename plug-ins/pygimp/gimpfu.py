50bf14ad797c44f3dff30fb25b1783f8236cc866
Sven Neumann <sven@gimp.org>
2008-11-27 10:25:59 +0000

    Merged from trunk:
    2008-11-27  Sven Neumann  <sven@gimp.org>
    
    	Merged from trunk:
    
    	Bug 562386 – PF_SLIDER and PF_SPINNER 'Step' values do not 
    change
    	consistently...
    
    	* plug-ins/pygimp/gimpfu.py (SpinnerEntry): initialize the
    	spin-button the way that gtk_spin_button_new_with_range() is
    	implemented.
    
    
    svn path=/branches/gimp-2-6/; revision=27727

--- plug-ins/pygimp/gimpfu.py
+++ plug-ins/pygimp/gimpfu.py
@@ -71,6 +71,7 @@ are installed.
 '''
 
 import string as _string
+import math
 import gimp
 import gimpcolor
 from gimpenums import *
@@ -493,10 +494,17 @@ def _interact(proc_name, start_params):
     class SpinnerEntry(gtk.SpinButton):
         # bounds is (upper, lower, step)
         def __init__(self, default=0, bounds=(0, 100, 5)):
-            self.adj = gtk.Adjustment(default, bounds[0],
-                                        bounds[1], bounds[2],
-                                        bounds[2], 0)
-            gtk.SpinButton.__init__(self, self.adj, 1, 0)
+            step = bounds[2]
+            self.adj = gtk.Adjustment(default, bounds[0], bounds[1],
+                                      step, 10 * step, 0)
+            step = bounds[2]
+            if math.fabs(step) >= 1.0 or step == 0.0:
+                digits = 0
+            else:
+                digits = abs(math.floor(math.log10(math.fabs(step))));
+            if digits > 20:
+                digits = 20
+            gtk.SpinButton.__init__(self, self.adj, step, int(digits))
 
         def get_value(self):
             try:
