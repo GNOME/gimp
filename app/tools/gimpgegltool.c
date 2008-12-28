72ef363094c33389ce776d129fad961ea559fc3b
Sven Neumann <sven@gimp.org>
2008-12-28 02:19:50 +0000

    fixed typo in GEGL version number check.
    2008-12-28  Sven Neumann  <sven@gimp.org>
    
    	* app/tools/gimpgegltool.c (gimp_param_spec_duplicate): fixed 
    typo
    	in GEGL version number check.
    
    
    svn path=/branches/gimp-2-6/; revision=27845

--- app/tools/gimpgegltool.c
+++ app/tools/gimpgegltool.c
@@ -446,7 +446,7 @@ gimp_param_spec_duplicate (GParamSpec *pspec)
     {
       GParamSpecString *spec = G_PARAM_SPEC_STRING (pspec);
 
-#if ((GEGL_MAGOR_VERSION == 0) && \
+#if ((GEGL_MAJOR_VERSION == 0) && \
      (GEGL_MINOR_VERSION == 0) && \
      (GEGL_MICRO_VERSION >= 21))
       if (GEGL_IS_PARAM_SPEC_FILE_PATH (pspec))
