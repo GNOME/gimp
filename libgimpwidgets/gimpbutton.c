ef3fbab8bced82b3f0e91f3d5587cb4fb2a90517
Michael Natterer <mitch@gimp.org>
2009-01-26 20:23:01 +0000

    chain up if a parent impl exists to be safe against future gtk versions
    2009-01-26  Michael Natterer  <mitch@gimp.org>
    
    	* libgimpwidgets/gimpbutton.c (gimp_button_clicked): chain up if a
    	parent impl exists to be safe against future gtk versions doing
    	something in GtkButton::clicked() (as happened in GTK+ trunk).
    
    
    svn path=/trunk/; revision=27958

--- libgimpwidgets/gimpbutton.c
+++ libgimpwidgets/gimpbutton.c
@@ -132,4 +132,8 @@ gimp_button_clicked (GtkButton *button)
       gimp_button_extended_clicked (GIMP_BUTTON (button),
                                     GIMP_BUTTON (button)->press_state);
     }
+  else if (GTK_BUTTON_CLASS (parent_class)->clicked)
+    {
+      GTK_BUTTON_CLASS (parent_class)->clicked (button);
+    }
 }
