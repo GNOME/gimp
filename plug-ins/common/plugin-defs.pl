27cec2c3f2fcb3a3aa728283bac52bf534c670f9
Takeshi Matsuyama <tksmashiw@gmail.com>
2009-05-27 12:04:35 +0900

    plug-ins: add X11 Mouse Cursor plug-in
    This commit adds a plug-in to read and write X11 mouse cursor files.

--- plug-ins/common/plugin-defs.pl
+++ plug-ins/common/plugin-defs.pl
@@ -77,6 +77,7 @@
     'file-tiff-save' => { ui => 1, optional => 1, libs => 'TIFF_LIBS' },
     'file-wmf' => { ui => 1, optional => 1, libs => 'WMF_LIBS', cflags => 'WMF_CFLAGS' },
     'file-xbm' => { ui => 1 },
+    'file-xmc' => { ui => 1, optional => 1, libs => 'XMC_LIBS' },
     'file-xpm' => { ui => 1, optional => 1, libs => 'XPM_LIBS' },
     'file-xwd' => {  ui => 1 },
     'film' => { ui => 1 },
