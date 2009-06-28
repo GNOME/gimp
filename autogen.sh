66cc3e5f22c61115d0c3057b4475f1a6a7508ea9
Martin Nordholts <martinn@src.gnome.org>
2009-06-28 11:02:26 +0200

    Check for automake-1.11

--- autogen.sh
+++ autogen.sh
@@ -148,6 +148,9 @@ echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
 if ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=$AUTOMAKE
    ACLOCAL=$ACLOCAL
+elif (automake-1.11 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.11
+   ACLOCAL=aclocal-1.11
 elif (automake-1.10 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.10
    ACLOCAL=aclocal-1.10
