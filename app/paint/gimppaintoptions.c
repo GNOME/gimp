2e28c79973285b00e30d7877eda7670b9917c1d4
Alexia Death <alexiadeath@gmail.com>
2009-07-22 20:55:34 +0300

    Adding back some missing enum getters and setters right this time. Gimp runns now, allbeit with warnings

--- app/paint/gimppaintoptions.c
+++ app/paint/gimppaintoptions.c
@@ -407,56 +407,70 @@ gimp_paint_options_get_property (GObject    *object,
     case PROP_HARD:
       g_value_set_boolean (value, options->hard);
       break;
+
+
+    case PROP_USE_FADE:
+      g_value_set_boolean (value, fade_options->use_fade);
+      break;
+
+    case PROP_FADE_LENGTH:
+      g_value_set_double (value, fade_options->fade_length);
+      break;
+
+    case PROP_FADE_UNIT:
+      g_value_set_int (value, fade_options->fade_unit);
+      break;
+
     case PROP_USE_JITTER:
-      jitter_options->use_jitter = g_value_get_boolean (value);
+      g_value_set_boolean (value, jitter_options->use_jitter);
       break;
 
     case PROP_JITTER_AMOUNT:
-      jitter_options->jitter_amount = g_value_get_double (value);
+      g_value_set_double (value, jitter_options->jitter_amount);
       break;
 
     case PROP_USE_GRADIENT:
-      gradient_options->use_gradient = g_value_get_boolean (value);
+      g_value_set_boolean (value, gradient_options->use_gradient);
       break;
 
     case PROP_GRADIENT_REVERSE:
-      gradient_options->gradient_reverse = g_value_get_boolean (value);
+      g_value_set_boolean (value, gradient_options->gradient_reverse);
       break;
 
     case PROP_GRADIENT_REPEAT:
-      gradient_options->gradient_repeat = g_value_get_enum (value);
+      g_value_set_enum (value, gradient_options->gradient_repeat);
       break;
 
     case PROP_GRADIENT_LENGTH:
-      gradient_options->gradient_length = g_value_get_double (value);
+      g_value_set_double (value, gradient_options->gradient_length);
       break;
 
     case PROP_GRADIENT_UNIT:
-      gradient_options->gradient_unit = g_value_get_int (value);
+      g_value_set_int (value, gradient_options->gradient_unit);
       break;
 
     case PROP_BRUSH_VIEW_TYPE:
-      options->brush_view_type = g_value_get_enum (value);
+      g_value_set_enum (value, options->brush_view_type);
       break;
 
     case PROP_BRUSH_VIEW_SIZE:
-      options->brush_view_size = g_value_get_int (value);
+      g_value_set_int (value, options->brush_view_size);
       break;
 
     case PROP_PATTERN_VIEW_TYPE:
-      options->pattern_view_type = g_value_get_enum (value);
+      g_value_set_enum (value, options->pattern_view_type);
       break;
 
     case PROP_PATTERN_VIEW_SIZE:
-      options->pattern_view_size = g_value_get_int (value);
+      g_value_set_int (value, options->pattern_view_size);
       break;
 
     case PROP_GRADIENT_VIEW_TYPE:
-      options->gradient_view_type = g_value_get_enum (value);
+      g_value_set_enum (value, options->gradient_view_type);
       break;
 
     case PROP_GRADIENT_VIEW_SIZE:
-      options->gradient_view_size = g_value_get_int (value);
+      g_value_set_int (value, options->gradient_view_size);
       break;
 
     default:
