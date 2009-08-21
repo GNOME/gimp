0a90a7570538959e46fed9f1a4d8cab6e0edc9d9
Alexia Death <alexiadeath@gmail.com>
2009-08-21 21:14:23 +0300

    Make the dynamics editor build and move the commented out mess to the end of the file.

--- app/widgets/gimpdynamicseditor.c
+++ app/widgets/gimpdynamicseditor.c
@@ -45,33 +45,6 @@
 #include "widgets/gimpdynamicseditor.h"
 #include "core/gimpdynamics.h"
 #include "core/gimpbrush.h"
-//To do:
-// discard unneeded ones.
-// needs to be fixed to gimppaintdynamics.h when that works.
-
-/*
-#include "paint/gimppaintoptions.h"
-
-#include "core/gimptoolinfo.h"
-
-#include "widgets/gimppropwidgets.h"
-#include "widgets/gimpviewablebox.h"
-#include "widgets/gimpwidgets-utils.h"
-
-#include "tools/gimpairbrushtool.h"
-#include "tools/gimpclonetool.h"
-#include "tools/gimpconvolvetool.h"
-#include "tools/gimpdodgeburntool.h"
-#include "tools/gimperasertool.h"
-#include "tools/gimphealtool.h"
-#include "tools/gimpinktool.h"
-#include "tools/gimppaintoptions-gui.h"
-#include "tools/gimppenciltool.h"
-#include "tools/gimpperspectiveclonetool.h"
-#include "tools/gimpsmudgetool.h"
-#include "tools/gimptooloptions-gui.h"
-
-*/
 
 
 /*  local function prototypes  */
@@ -87,44 +60,6 @@ static GObject * gimp_dynamics_editor_constructor (GType              type,
 
 static void   gimp_dynamics_editor_set_context    (GimpDocked         *docked,
                                                    GimpContext        *context);
-
-/*dynamics options gui*/
-/*
-static gboolean    tool_has_opacity_dynamics      (GType       tool_type);
-static gboolean    tool_has_hardness_dynamics     (GType       tool_type);
-static gboolean    tool_has_rate_dynamics         (GType       tool_type);
-static gboolean    tool_has_size_dynamics         (GType       tool_type);
-static gboolean    tool_has_color_dynamics        (GType       tool_type);
-static gboolean    tool_has_angle_dynamics        (GType       tool_type);
-static gboolean    tool_has_aspect_ratio_dynamics (GType       tool_type);
-
-static void        pressure_options_gui  (GimpPaintOptions *paint_options,
-                                          GType             tool_type,
-                                          GtkTable         *table,
-                                          gint              row,
-                                          GtkWidget        *labels[]);
-static void        velocity_options_gui  (GimpPaintOptions *paint_options,
-                                          GType             tool_type,
-                                          GtkTable         *table,
-                                          gint              row);
-static void        direction_options_gui (GimpPaintOptions *paint_options,
-                                          GType             tool_type,
-                                          GtkTable         *table,
-                                          gint              row);
-static void        tilt_options_gui      (GimpPaintOptions *paint_options,
-                                          GType             tool_type,
-                                          GtkTable         *table,
-                                          gint              row);
-static void        random_options_gui    (GimpPaintOptions *paint_options,
-                                          GType             tool_type,
-                                          GtkTable         *table,
-                                          gint              row);
-
-static void        fading_options_gui    (GimpPaintOptions *paint_options,
-                                          GType             tool_type,
-                                          GtkTable         *table,
-                                          gint              row);
-*/
 
 
 G_DEFINE_TYPE_WITH_CODE (GimpDynamicsEditor, gimp_dynamics_editor,
@@ -160,153 +95,7 @@ gimp_dynamics_editor_docked_iface_init (GimpDockedInterface *iface)
   iface->set_context = gimp_dynamics_editor_set_context;
 }
 
-/*
-To do:
-look at other init for dataeditors
-look at how to move gui codes from paintopitons to here
-*/
 
-
-static void
-gimp_dynamics_editor_init (GimpDynamicsEditor *editor)
-{
-  GimpDataEditor *data_editor = GIMP_DATA_EDITOR (editor);
-  GtkWidget      *vbox;
-  
-  GtkWidget        *table;
-  GtkWidget        *label;
-  gint              n_dynamics         = 0;
-/*  
-  GtkWidget        *dynamics_labels[7];
-  GtkWidget      *frame;
-  GtkWidget      *box;
-  gint            row = 0;
-  GtkWidget        *menu;
-  GtkWidget        *button;
-  GtkWidget        *incremental_toggle = NULL;
-  gint              table_row          = 0;
-  GType             tool_type;
-*/
-
-/*
-  //add a frame
-  frame = gtk_frame_new (NULL);
-  gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_IN);
-  gtk_box_pack_start (GTK_BOX (editor), frame, TRUE, TRUE, 0);
-  gtk_widget_show (frame);
-*/
-  vbox = gtk_vbox_new (FALSE, 6);
-  //gtk_box_pack_start (GTK_BOX (editor), vbox, TRUE, TRUE, 0);
-  gtk_box_pack_start (GTK_BOX (data_editor), vbox, TRUE, TRUE, 0);
-  gtk_widget_show (vbox);
-
-  data_editor->view = gimp_view_new_full_by_types (NULL,
-                                                   GIMP_TYPE_VIEW,
-                                                   GIMP_TYPE_DYNAMICS,
-                                                   DYNAMICS_VIEW_SIZE,
-                                                   DYNAMICS_VIEW_SIZE, 0,
-                                                   FALSE, FALSE, TRUE);
-  gtk_widget_set_size_request (data_editor->view, -1, DYNAMICS_VIEW_SIZE);
-  gimp_view_set_expand (GIMP_VIEW (data_editor->view), TRUE);
-  gtk_container_add (GTK_CONTAINER (vbox), data_editor->view);
-  gtk_widget_show (data_editor->view);
-
-  n_dynamics = 5;
-
-  if (n_dynamics > 0)
-    {
-      GtkWidget *inner_frame;
-      GtkWidget *fixed;
-      gint       i;
-      gboolean   rtl = gtk_widget_get_direction (vbox) == GTK_TEXT_DIR_RTL;
-
-      //frame = gimp_prop_expander_new (config, "dynamics-expanded",
-      //                                _("Brush Dynamics"));
-/*      gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
-      gtk_widget_show (frame);
-*/
-      inner_frame = gimp_frame_new (NULL);
-      gtk_container_add (GTK_CONTAINER (vbox), inner_frame);
-      gtk_widget_show (inner_frame);
-
-
-      table = gtk_table_new (7, n_dynamics + 2, FALSE);
-      gtk_container_add (GTK_CONTAINER (inner_frame), table);
-      gtk_widget_show (table);
-
-      label = gtk_label_new (_("Pressure:"));
-      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
-      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 1, 2,
-                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
-      gtk_widget_show (label);
-
-      label = gtk_label_new (_("Velocity:"));
-      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
-      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 2, 3,
-                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
-      gtk_widget_show (label);
-
-      label = gtk_label_new (_("Direction:"));
-      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
-      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 3, 4,
-                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
-      gtk_widget_show (label);
-
-      label = gtk_label_new (_("Tilt:"));
-      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
-      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 4, 5,
-                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
-      gtk_widget_show (label);
-
-      label = gtk_label_new (_("Random:"));
-      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
-      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 5, 6,
-                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
-      gtk_widget_show (label);
-
-      label = gtk_label_new (_("Fading - new:"));
-      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
-      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 6, 7,
-                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
-      gtk_widget_show (label);
-
-/*
-  //editor->shape_group = NULL;
-  editor->options_vbox = gtk_table_new (4, 3, FALSE);
-  gtk_table_set_row_spacings (GTK_TABLE (editor->options_vbox), 2);
-  gtk_table_set_col_spacings (GTK_TABLE (editor->options_vbox), 2);
-  gtk_box_pack_start (GTK_BOX (editor), editor->options_vbox, FALSE, FALSE, 0);
-  gtk_widget_show (editor->options_vbox);
-
-  table = gtk_table_new (3, 3, FALSE);
-  gtk_table_set_col_spacings (GTK_TABLE (table), 2);
-  gtk_table_set_row_spacings (GTK_TABLE (table), 2);
-  gtk_box_pack_start (GTK_BOX (editor->options_vbox), table, FALSE, FALSE, 0);
-  gtk_widget_show (table);
-*/
-/*
-  g_object_set_data (G_OBJECT (vbox), "set_options", table);
-
-  menu  = gimp_prop_paint_mode_menu_new (config, "paint-mode", TRUE, FALSE);
-  label = gimp_table_attach_aligned (GTK_TABLE (table), 0, table_row++,
-                                     _("Mode:"), 0.0, 0.5,
-                                     menu, 2, FALSE);
-
-//  if (tool_type == GIMP_TYPE_ERASER_TOOL     ||
-//      tool_type == GIMP_TYPE_CONVOLVE_TOOL   ||
-//      tool_type == GIMP_TYPE_DODGE_BURN_TOOL ||
-//      tool_type == GIMP_TYPE_SMUDGE_TOOL)
-    {
-      gtk_widget_set_sensitive (menu, FALSE);
-      gtk_widget_set_sensitive (label, FALSE);
-    }
-
-  gimp_prop_opacity_entry_new (config, "opacity",
-                               GTK_TABLE (table), 0, table_row++,
-                               _("Opacity:"));
-*/
-
-}
 
 static GObject *
 gimp_dynamics_editor_constructor (GType                  type,
@@ -352,10 +141,163 @@ gimp_dynamics_editor_new (GimpContext     *context,
                        "context",         context,
                        "data",            gimp_context_get_dynamics (context),
                        NULL);
- 
+
 }
 
+/*
+To do:
+look at other init for dataeditors
+look at how to move gui codes from paintopitons to here
+*/
 
+
+static void
+gimp_dynamics_editor_init (GimpDynamicsEditor *editor)
+{
+  GimpDataEditor *data_editor = GIMP_DATA_EDITOR (editor);
+  GtkWidget      *vbox;
+
+  GtkWidget        *table;
+  GtkWidget        *label;
+  gint              n_dynamics         = 0;
+
+vbox = gtk_vbox_new (FALSE, 6);
+  //gtk_box_pack_start (GTK_BOX (editor), vbox, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (data_editor), vbox, TRUE, TRUE, 0);
+  gtk_widget_show (vbox);
+
+  data_editor->view = gimp_view_new_full_by_types (NULL,
+                                                   GIMP_TYPE_VIEW,
+                                                   GIMP_TYPE_DYNAMICS,
+                                                   DYNAMICS_VIEW_SIZE,
+                                                   DYNAMICS_VIEW_SIZE, 0,
+                                                   FALSE, FALSE, TRUE);
+  gtk_widget_set_size_request (data_editor->view, -1, DYNAMICS_VIEW_SIZE);
+  gimp_view_set_expand (GIMP_VIEW (data_editor->view), TRUE);
+  gtk_container_add (GTK_CONTAINER (vbox), data_editor->view);
+  gtk_widget_show (data_editor->view);
+
+  n_dynamics = 5;
+
+  if (n_dynamics > 0)
+    {
+      GtkWidget *inner_frame;
+      GtkWidget *fixed;
+      gint       i;
+      gboolean   rtl = gtk_widget_get_direction (vbox) == GTK_TEXT_DIR_RTL;
+
+      inner_frame = gimp_frame_new (NULL);
+      gtk_container_add (GTK_CONTAINER (vbox), inner_frame);
+      gtk_widget_show (inner_frame);
+
+
+      table = gtk_table_new (7, n_dynamics + 2, FALSE);
+      gtk_container_add (GTK_CONTAINER (inner_frame), table);
+      gtk_widget_show (table);
+
+      label = gtk_label_new (_("Pressure:"));
+      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 1, 2,
+                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
+      gtk_widget_show (label);
+
+      label = gtk_label_new (_("Velocity:"));
+      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 2, 3,
+                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
+      gtk_widget_show (label);
+
+      label = gtk_label_new (_("Direction:"));
+      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 3, 4,
+                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
+      gtk_widget_show (label);
+
+      label = gtk_label_new (_("Tilt:"));
+      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 4, 5,
+                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
+      gtk_widget_show (label);
+
+      label = gtk_label_new (_("Random:"));
+      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 5, 6,
+                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
+      gtk_widget_show (label);
+
+      label = gtk_label_new (_("Fading - new:"));
+      gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+      gtk_table_attach (GTK_TABLE (table), label, 0, 1, 6, 7,
+                        GTK_SHRINK | GTK_FILL, GTK_SHRINK | GTK_FILL, 0, 0);
+      gtk_widget_show (label);
+    }
+
+}
+
+/*        SCRAPS!           */
+
+
+      //frame = gimp_prop_expander_new (config, "dynamics-expanded",
+      //                                _("Brush Dynamics"));
+/*      gtk_box_pack_start (GTK_BOX (vbox), frame, FALSE, FALSE, 0);
+      gtk_widget_show (frame);
+*/
+
+/*
+  //editor->shape_group = NULL;
+  editor->options_vbox = gtk_table_new (4, 3, FALSE);
+  gtk_table_set_row_spacings (GTK_TABLE (editor->options_vbox), 2);
+  gtk_table_set_col_spacings (GTK_TABLE (editor->options_vbox), 2);
+  gtk_box_pack_start (GTK_BOX (editor), editor->options_vbox, FALSE, FALSE, 0);
+  gtk_widget_show (editor->options_vbox);
+
+  table = gtk_table_new (3, 3, FALSE);
+  gtk_table_set_col_spacings (GTK_TABLE (table), 2);
+  gtk_table_set_row_spacings (GTK_TABLE (table), 2);
+  gtk_box_pack_start (GTK_BOX (editor->options_vbox), table, FALSE, FALSE, 0);
+  gtk_widget_show (table);
+*/
+/*
+  g_object_set_data (G_OBJECT (vbox), "set_options", table);
+
+  menu  = gimp_prop_paint_mode_menu_new (config, "paint-mode", TRUE, FALSE);
+  label = gimp_table_attach_aligned (GTK_TABLE (table), 0, table_row++,
+                                     _("Mode:"), 0.0, 0.5,
+                                     menu, 2, FALSE);
+
+//  if (tool_type == GIMP_TYPE_ERASER_TOOL     ||
+//      tool_type == GIMP_TYPE_CONVOLVE_TOOL   ||
+//      tool_type == GIMP_TYPE_DODGE_BURN_TOOL ||
+//      tool_type == GIMP_TYPE_SMUDGE_TOOL)
+    {
+      gtk_widget_set_sensitive (menu, FALSE);
+      gtk_widget_set_sensitive (label, FALSE);
+    }
+
+  gimp_prop_opacity_entry_new (config, "opacity",
+                               GTK_TABLE (table), 0, table_row++,
+                               _("Opacity:"));
+*/
+
+/*
+  GtkWidget        *dynamics_labels[7];
+  GtkWidget      *frame;
+  GtkWidget      *box;
+  gint            row = 0;
+  GtkWidget        *menu;
+  GtkWidget        *button;
+  GtkWidget        *incremental_toggle = NULL;
+  gint              table_row          = 0;
+  GType             tool_type;
+*/
+
+/*
+  //add a frame
+  frame = gtk_frame_new (NULL);
+  gtk_frame_set_shadow_type (GTK_FRAME (frame), GTK_SHADOW_IN);
+  gtk_box_pack_start (GTK_BOX (editor), frame, TRUE, TRUE, 0);
+  gtk_widget_show (frame);
+*/
 
 /*  private functions  */
 /*
@@ -871,4 +813,70 @@ gimp_paint_options_gui (GimpToolOptions *tool_options)
   GtkWidget        *dynamics_labels[7];
   GType             tool_type;
 }
+*/
+
+
+/*dynamics options gui*/
+/*
+static gboolean    tool_has_opacity_dynamics      (GType       tool_type);
+static gboolean    tool_has_hardness_dynamics     (GType       tool_type);
+static gboolean    tool_has_rate_dynamics         (GType       tool_type);
+static gboolean    tool_has_size_dynamics         (GType       tool_type);
+static gboolean    tool_has_color_dynamics        (GType       tool_type);
+static gboolean    tool_has_angle_dynamics        (GType       tool_type);
+static gboolean    tool_has_aspect_ratio_dynamics (GType       tool_type);
+
+static void        pressure_options_gui  (GimpPaintOptions *paint_options,
+                                          GType             tool_type,
+                                          GtkTable         *table,
+                                          gint              row,
+                                          GtkWidget        *labels[]);
+static void        velocity_options_gui  (GimpPaintOptions *paint_options,
+                                          GType             tool_type,
+                                          GtkTable         *table,
+                                          gint              row);
+static void        direction_options_gui (GimpPaintOptions *paint_options,
+                                          GType             tool_type,
+                                          GtkTable         *table,
+                                          gint              row);
+static void        tilt_options_gui      (GimpPaintOptions *paint_options,
+                                          GType             tool_type,
+                                          GtkTable         *table,
+                                          gint              row);
+static void        random_options_gui    (GimpPaintOptions *paint_options,
+                                          GType             tool_type,
+                                          GtkTable         *table,
+                                          gint              row);
+
+static void        fading_options_gui    (GimpPaintOptions *paint_options,
+                                          GType             tool_type,
+                                          GtkTable         *table,
+                                          gint              row);
+*/
+//To do:
+// discard unneeded ones.
+// needs to be fixed to gimppaintdynamics.h when that works.
+
+/*
+#include "paint/gimppaintoptions.h"
+
+#include "core/gimptoolinfo.h"
+
+#include "widgets/gimppropwidgets.h"
+#include "widgets/gimpviewablebox.h"
+#include "widgets/gimpwidgets-utils.h"
+
+#include "tools/gimpairbrushtool.h"
+#include "tools/gimpclonetool.h"
+#include "tools/gimpconvolvetool.h"
+#include "tools/gimpdodgeburntool.h"
+#include "tools/gimperasertool.h"
+#include "tools/gimphealtool.h"
+#include "tools/gimpinktool.h"
+#include "tools/gimppaintoptions-gui.h"
+#include "tools/gimppenciltool.h"
+#include "tools/gimpperspectiveclonetool.h"
+#include "tools/gimpsmudgetool.h"
+#include "tools/gimptooloptions-gui.h"
+
 */
