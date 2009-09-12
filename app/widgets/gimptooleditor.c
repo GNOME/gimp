65e09758ef6ab1b2f0391da8bc4870a20597afa0
Sven Neumann <sven@gimp.org>
2009-09-12 20:33:25 +0200

    Bug 595003 -  Error in string "Lower this tool Lower this tool..."
    Fix tooltips for raise/lower buttons in the GimpToolEditor widget.

--- app/widgets/gimptooleditor.c
+++ app/widgets/gimptooleditor.c
@@ -291,16 +291,16 @@ gimp_tool_editor_new (GimpContainer *container,
   /* buttons */
   priv->raise_button =
     gimp_editor_add_button (GIMP_EDITOR (tree_view), GTK_STOCK_GO_UP,
-                            _("Raise this tool Raise this tool to the top"),
-                            NULL,
+                            _("Raise this tool"),
+                            _("Raise this tool to the top"),
                             G_CALLBACK (gimp_tool_editor_raise_clicked),
                             G_CALLBACK (gimp_tool_editor_raise_extend_clicked),
                             tool_editor);
 
   priv->lower_button =
     gimp_editor_add_button (GIMP_EDITOR (tree_view), GTK_STOCK_GO_DOWN,
-                            _("Lower this tool Lower this tool to the bottom"),
-                            NULL,
+                            _("Lower this tool"),
+                            _("Lower this tool to the bottom"),
                             G_CALLBACK (gimp_tool_editor_lower_clicked),
                             G_CALLBACK (gimp_tool_editor_lower_extend_clicked),
                             tool_editor);
