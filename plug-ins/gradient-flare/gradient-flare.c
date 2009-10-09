2e1caf6be8568893020f7ebe50823602312dea31
Michael Natterer <mitch@gimp.org>
2009-10-09 18:16:54 +0200

    Use gtk_widget_get_window() instead of widget->window

--- plug-ins/gradient-flare/gradient-flare.c
+++ plug-ins/gradient-flare/gradient-flare.c
@@ -2541,7 +2541,7 @@ dlg_preview_realize (GtkWidget *widget)
   GdkDisplay *display = gtk_widget_get_display (widget);
   GdkCursor  *cursor  = gdk_cursor_new_for_display (display, GDK_CROSSHAIR);
 
-  gdk_window_set_cursor (widget->window, cursor);
+  gdk_window_set_cursor (gtk_widget_get_window (widget), cursor);
   gdk_cursor_unref (cursor);
 }
 
