c06ed13a144787469e9945e19355ba736a6c0893
Alexia Death <alexiadeath@gmail.com>
2010-08-15 02:27:09 +0300

    core: Add function to test if image map is busy

--- app/core/gimpimagemap.h
+++ app/core/gimpimagemap.h
@@ -62,4 +62,7 @@ void           gimp_image_map_clear        (GimpImageMap          *image_map);
 void           gimp_image_map_abort        (GimpImageMap          *image_map);
 
 
+gboolean       gimp_image_map_is_busy      (GimpImageMap        *image_map);
+
+
 #endif /* __GIMP_IMAGE_MAP_H__ */
