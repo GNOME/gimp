360af98d5c58b9b8cf0f60a53fc9ae1f4138a6d7
Michael Natterer <mitch@gimp.org>
2010-10-15 10:03:55 +0200

    libgimpwidgets: remove deprecated files from the build

--- libgimpwidgets/gimpwidgets.h
+++ libgimpwidgets/gimpwidgets.h
@@ -65,7 +65,6 @@
 #include <libgimpwidgets/gimppageselector.h>
 #include <libgimpwidgets/gimppatheditor.h>
 #include <libgimpwidgets/gimppickbutton.h>
-#include <libgimpwidgets/gimppixmap.h>
 #include <libgimpwidgets/gimppreview.h>
 #include <libgimpwidgets/gimppreviewarea.h>
 #include <libgimpwidgets/gimppropwidgets.h>
@@ -76,14 +75,11 @@
 #include <libgimpwidgets/gimpsizeentry.h>
 #include <libgimpwidgets/gimpstringcombobox.h>
 #include <libgimpwidgets/gimpunitcombobox.h>
-#include <libgimpwidgets/gimpunitmenu.h>
 #include <libgimpwidgets/gimpunitstore.h>
 #include <libgimpwidgets/gimpwidgets-error.h>
 #include <libgimpwidgets/gimpwidgetsutils.h>
 #include <libgimpwidgets/gimpzoommodel.h>
-
 #include <libgimpwidgets/gimp3migration.h>
-#include <libgimpwidgets/gimpoldwidgets.h>
 
 #undef __GIMP_WIDGETS_H_INSIDE__
 
