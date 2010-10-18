349a401cc5adecf63fe2542ae6305db95cdb9826
Mukund Sivaraman <muks@banu.com>
2010-10-19 00:24:32 +0530

    file-jp2-load: Elaborate comment

--- plug-ins/common/file-jp2-load.c
+++ plug-ins/common/file-jp2-load.c
@@ -268,7 +268,8 @@ load_image (const gchar  *filename,
         }
       components[3] = jas_image_getcmptbytype (image, JAS_IMAGE_CT_OPACITY);
 
-      /* ImageMagick seems to write out the 'matte' component (number 3) */
+      /* ImageMagick seems to write out the 'matte' component type
+	 (number 3) */
       if (components[3] == -1)
         components[3] = jas_image_getcmptbytype (image, 3);
 
