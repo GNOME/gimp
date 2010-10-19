f8ef0c71c2c12ebdb496c54702d26662b1d9611c
Michael Natterer <mitch@gimp.org>
2010-10-19 20:24:43 +0200

    app: can't set the scale combo entry's properties in init() any longer
    Set them in constructed() instead.

--- app/display/gimpscalecombobox.c
+++ app/display/gimpscalecombobox.c
@@ -146,7 +146,6 @@ gimp_scale_combo_box_constructed (GObject *object)
   entry = gtk_bin_get_child (GTK_BIN (combo_box));
 
   g_object_set (entry,
-                "has-entry",          TRUE,
                 "xalign",             1.0,
                 "width-chars",        5,
                 "truncate-multiline", TRUE,
