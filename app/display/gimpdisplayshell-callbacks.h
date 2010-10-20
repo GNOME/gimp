dee620c58cadc07a0efa583b1eb5404aed01fba6
Michael Natterer <mitch@gimp.org>
2010-10-20 13:24:12 +0200

    app: didn't completely commit the display shell expose() -> draw() change

--- app/display/gimpdisplayshell-callbacks.h
+++ app/display/gimpdisplayshell-callbacks.h
@@ -24,8 +24,8 @@ void       gimp_display_shell_canvas_realize          (GtkWidget        *widget,
 void       gimp_display_shell_canvas_size_allocate    (GtkWidget        *widget,
                                                        GtkAllocation    *alloc,
                                                        GimpDisplayShell *shell);
-gboolean   gimp_display_shell_canvas_expose           (GtkWidget        *widget,
-                                                       GdkEventExpose   *eevent,
+gboolean   gimp_display_shell_canvas_draw             (GtkWidget        *widget,
+                                                       cairo_t          *cr,
                                                        GimpDisplayShell *shell);
 
 gboolean   gimp_display_shell_origin_button_press     (GtkWidget        *widget,
