5a28ed7198fd05c6b2eb9da9ad9a484b1c9845e7
Michael Natterer <mitch@gimp.org>
2010-10-21 13:35:09 +0200

    app: forgot to remove a cairo_destroy() in draw()

--- app/widgets/gimphistogramview.c
+++ app/widgets/gimphistogramview.c
@@ -325,10 +325,7 @@ gimp_histogram_view_draw (GtkWidget *widget,
   cairo_stroke (cr);
 
   if (! view->histogram && ! view->bg_histogram)
-    {
-      cairo_destroy (cr);
-      return FALSE;
-    }
+    return FALSE;
 
   x1 = CLAMP (MIN (view->start, view->end), 0, view->n_bins - 1);
   x2 = CLAMP (MAX (view->start, view->end), 0, view->n_bins - 1);
