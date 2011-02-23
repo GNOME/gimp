944ba6a99bfacb90d09ce1f37314103e395ccedc
Michael Natterer <mitch@gimp.org>
2011-02-23 08:57:00 +0100

    app: remove gimp_rgb_get,set_gdk_color()
    and inline the few lines into their only users.

--- app/widgets/gimptexttag.c
+++ app/widgets/gimptexttag.c
@@ -28,7 +28,6 @@
 #include "widgets-types.h"
 
 #include "gimptexttag.h"
-#include "gimpwidgets-utils.h"
 
 
 gint
@@ -87,14 +86,18 @@ gimp_text_tag_get_fg_color (GtkTextTag *tag,
 {
   GdkColor *gdk_color;
   gboolean  set;
+  guchar    r, g, b;
 
   g_object_get (tag,
                 "foreground-set",             &set,
                 GIMP_TEXT_PROP_NAME_FG_COLOR, &gdk_color,
                 NULL);
 
-  if (set)
-    gimp_rgb_set_gdk_color (color, gdk_color);
+  r = gdk_color->red   >> 8;
+  g = gdk_color->green >> 8;
+  b = gdk_color->blue  >> 8;
+
+  gimp_rgb_set_uchar (color, r, g, b);
 
   gdk_color_free (gdk_color);
 
@@ -107,14 +110,18 @@ gimp_text_tag_get_bg_color (GtkTextTag *tag,
 {
   GdkColor *gdk_color;
   gboolean  set;
+  guchar    r, g, b;
 
   g_object_get (tag,
                 "background-set",             &set,
                 GIMP_TEXT_PROP_NAME_BG_COLOR, &gdk_color,
                 NULL);
 
-  if (set)
-    gimp_rgb_set_gdk_color (color, gdk_color);
+  r = gdk_color->red   >> 8;
+  g = gdk_color->green >> 8;
+  b = gdk_color->blue  >> 8;
+
+  gimp_rgb_set_uchar (color, r, g, b);
 
   gdk_color_free (gdk_color);
 
