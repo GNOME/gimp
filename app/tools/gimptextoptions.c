75548264d08b1cff2bef978a94bd05f24037fdbf
Michael Natterer <mitch@gimp.org>
2011-03-14 12:58:34 +0100

    app: set the text option's unit menu to "plural"
    It gets changed to "abbreviation" for the permanently displayed
    string automatically.

--- app/tools/gimptextoptions.c
+++ app/tools/gimptextoptions.c
@@ -483,7 +483,7 @@ gimp_text_options_gui (GimpToolOptions *tool_options)
   gtk_widget_show (table);
 
   entry = gimp_prop_size_entry_new (config,
-                                    "font-size", FALSE, "font-size-unit", "%a",
+                                    "font-size", FALSE, "font-size-unit", "%p",
                                     GIMP_SIZE_ENTRY_UPDATE_SIZE, 72.0);
   gimp_table_attach_aligned (GTK_TABLE (table), 0, row++,
                              _("Size:"), 0.0, 0.5,
