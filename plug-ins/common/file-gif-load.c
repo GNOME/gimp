b1a3de761362db982c0ddfaff60ab4a3c4267f32
Nils Philippsen <nils@redhat.com>
2011-08-04 12:47:44 +0200

    file-gif-load: ensure return value of LZWReadByte() is <= 255

--- plug-ins/common/file-gif-load.c
+++ plug-ins/common/file-gif-load.c
@@ -759,11 +759,11 @@ LZWReadByte (FILE *fd,
         }
       while (firstcode == clear_code);
 
-      return firstcode;
+      return firstcode & 255;
     }
 
   if (sp > stack)
-    return *--sp;
+    return (*--sp) & 255;
 
   while ((code = GetCode (fd, code_size, FALSE)) >= 0)
     {
@@ -786,7 +786,7 @@ LZWReadByte (FILE *fd,
           sp            = stack;
           firstcode     = oldcode = GetCode (fd, code_size, FALSE);
 
-          return firstcode;
+          return firstcode & 255;
         }
       else if (code == end_code)
         {
@@ -842,10 +842,10 @@ LZWReadByte (FILE *fd,
       oldcode = incode;
 
       if (sp > stack)
-        return *--sp;
+        return (*--sp) & 255;
     }
 
-  return code;
+  return code & 255;
 }
 
 static gint32
