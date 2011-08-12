0f58d779c1c8c16e1166883d78cfb54e4ed47c77
Martin Nordholts <martinn@src.gnome.org>
2011-08-12 20:18:45 +0200

    pdb: Untabify image.pdb

--- app/pdb/image-cmds.c
+++ app/pdb/image-cmds.c
@@ -2272,7 +2272,7 @@ image_set_resolution_invoker (GimpProcedure      *procedure,
           yresolution < GIMP_MIN_RESOLUTION || yresolution > GIMP_MAX_RESOLUTION)
         {
           g_set_error_literal (error, GIMP_PDB_ERROR, GIMP_PDB_INVALID_ARGUMENT,
-                                  _("Image resolution is out of bounds, "
+                               _("Image resolution is out of bounds, "
                                  "using the default resolution instead."));
           success = FALSE;
         }
