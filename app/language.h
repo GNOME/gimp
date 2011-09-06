e4204e453ac1fa9b692d4d8fa345b808d9076e10
Mukund Sivaraman <muks@banu.com>
2011-09-06 23:12:36 +0530

    app: Move language specific code into language.[ch]

---
+++ app/language.h
@@ -0,0 +1,29 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LANGUAGE_H__
+#define __LANGUAGE_H__
+
+#ifndef GIMP_APP_GLUE_COMPILATION
+#error You must not #include "language.h" from a subdir
+#endif
+
+
+void language_init (const gchar *language);
+
+
+#endif /* __LANGUAGE_H__ */
