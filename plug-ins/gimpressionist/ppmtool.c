7d465f0a629def1986799f4659da07bef5523c2d
Mukund Sivaraman <muks@banu.com>
2011-10-03 18:53:32 +0530

    gimpressionist: Remove dead assignment (and variable)

--- plug-ins/gimpressionist/ppmtool.c
+++ plug-ins/gimpressionist/ppmtool.c
@@ -410,12 +410,11 @@ free_rotate (ppm_t *p, double amount)
 {
   int    x, y;
   double nx, ny;
-  double R, a;
+  double R;
   ppm_t  tmp = {0, 0, NULL};
   double f = amount * G_PI * 2 / 360.0;
   int    rowstride = p->width * 3;
 
-  a = p->width / (float)p->height;
   R = p->width < p->height ? p->width / 2 : p->height / 2;
 
   ppm_new (&tmp, p->width, p->height);
