3f62941c78b28379e52d67b3d60bd014da0e0b37
Mukund Sivaraman <muks@banu.com>
2011-10-11 09:56:46 +0530

    color-rotate: Delete dead variables and assignments

--- plug-ins/color-rotate/color-rotate-callbacks.c
+++ plug-ins/color-rotate/color-rotate-callbacks.c
@@ -407,12 +407,6 @@ rcm_motion_notify_event (GtkWidget      *widget,
 			 RcmCircle      *circle)
 {
   gfloat  clicked_angle, delta;
-  gfloat *alpha, *beta;
-  gint    cw_ccw;
-
-  alpha  = &(circle->angle->alpha);
-  beta   = &(circle->angle->beta);
-  cw_ccw = circle->angle->cw_ccw;
 
   clicked_angle = angle_mod_2PI (arctg (CENTER - event->y, event->x - CENTER));
 
