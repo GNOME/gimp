817d516da49918555dc1088c08443cc5edef03f9
Michael Natterer <mitch@gimp.org>
2012-03-30 10:13:23 +0200

    Depend on Babl 0.1.8

--- app/sanity.c
+++ app/sanity.c
@@ -351,7 +351,7 @@ sanity_check_babl (void)
 
 #define BABL_REQUIRED_MAJOR 0
 #define BABL_REQUIRED_MINOR 1
-#define BABL_REQUIRED_MICRO 6
+#define BABL_REQUIRED_MICRO 8
 
   babl_get_version (&babl_major_version,
                     &babl_minor_version,
