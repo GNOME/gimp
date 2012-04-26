8b028ee8ce9790e2d09c761c2f5234af8fa5b2fe
Øyvind Kolås <pippin@gimp.org>
2012-04-26 15:00:05 +0200

    depend on babl>=0.1.11

--- app/sanity.c
+++ app/sanity.c
@@ -351,7 +351,7 @@ sanity_check_babl (void)
 
 #define BABL_REQUIRED_MAJOR 0
 #define BABL_REQUIRED_MINOR 1
-#define BABL_REQUIRED_MICRO 10
+#define BABL_REQUIRED_MICRO 11
 
   babl_get_version (&babl_major_version,
                     &babl_minor_version,
