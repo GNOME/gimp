6888304f4a0d775fcc30a2bcf1ca35bc966a83ac
Michael Natterer <mitch@gimp.org>
2012-05-03 00:49:11 +0200

    configure: require GLib 2.32.0 for the new G_DEPRECATED macros

--- app/sanity.c
+++ app/sanity.c
@@ -130,8 +130,8 @@ static gchar *
 sanity_check_glib (void)
 {
 #define GLIB_REQUIRED_MAJOR 2
-#define GLIB_REQUIRED_MINOR 30
-#define GLIB_REQUIRED_MICRO 2
+#define GLIB_REQUIRED_MINOR 32
+#define GLIB_REQUIRED_MICRO 0
 
   const gchar *mismatch = glib_check_version (GLIB_REQUIRED_MAJOR,
                                               GLIB_REQUIRED_MINOR,
