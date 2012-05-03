0c4c095a57b95296d3d72f3fe9d832beea048f5a
Michael Natterer <mitch@gimp.org>
2012-05-03 15:02:03 +0200

    plug-ins: hide colorify from the menus, we have the colorize tool

--- plug-ins/common/colorify.c
+++ plug-ins/common/colorify.c
@@ -110,8 +110,6 @@ query (void)
                           GIMP_PLUGIN,
                           G_N_ELEMENTS (args), 0,
                           args, NULL);
-
-  gimp_plugin_menu_register (PLUG_IN_PROC, "<Image>/Colors/Modify");
 }
 
 static void
