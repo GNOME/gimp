a56ae5a82d1621f1a99a6051b62378491be43d1f
Michael Muré <mure.michael@gmail.com>
2012-05-06 16:42:55 +0900

    fix autogen.sh to work with automake 1.12

--- autogen.sh
+++ autogen.sh
@@ -157,6 +157,9 @@ echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
 if ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=$AUTOMAKE
    ACLOCAL=$ACLOCAL
+elif (automake-1.12 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.12
+   ACLOCAL=aclocal-1.12
 elif (automake-1.11 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.11
    ACLOCAL=aclocal-1.11
