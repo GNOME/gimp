37a884a31b45a6e1594d8e2f2eebdfa6ee66329c
Clayton Walker <blender3dartist@gmail.com>
2012-05-23 14:45:56 +0200

    Update osx build files and README

--- build/osx/launcher.sh
+++ build/osx/launcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Stuff the Mac dev of gedit wrote.
 if test "x$GTK_DEBUG_LAUNCHER" != x; then
 	set -x
 fi
@@ -13,12 +12,10 @@ else
 	EXEC=exec
 fi
 
-# Where we get all of our paths from.
+# Where is GIMP.app located
 name=$(basename "$0")
-echo $name
 
 dirn=$(dirname "$0")
-echo $dirn
 
 bundle=$(cd "$dirn/../../" && pwd)
 bundle_contents="$bundle"/Contents
@@ -29,12 +26,11 @@ bundle_data="$bundle_res"/share
 bundle_etc="$bundle_res"/etc
 
 export PATH="$bundle_bin:$PATH"
-echo $PATH
 
-# This is a comprimize between clashing with system libs and no libs.
+# This is a compromise between clashing with system libs and no libs; it may no longer be needed
 export DYLD_FALLBACK_LIBRARY_PATH="$bundle_lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
-# The fontconfig file needs to be explicitely defined
+# The fontconfig file needs to be explicitly defined
 export FONTCONFIG_FILE="$bundle_etc/fonts/fonts.conf"
 
 # Gdk needs it's own specifications
@@ -43,11 +39,11 @@ export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
 # Fix the theme engine paths
 export GTK_PATH="$bundle_lib/gtk-2.0/2.10.0"
 
-# These may or may not be necesary, no harm was observed in the commenting out of these paths
+# Backup paths
 # export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
 # export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
 
-# Strip out the argument added by the OS.
+# Strip out arguments added by the OS.
 if [ x`echo "x$1" | sed -e "s/^x-psn_.*//"` == x ]; then
 	shift 1
 fi
