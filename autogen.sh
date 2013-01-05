495b41416b381678bdc94a85e4a89b0a4c970c92
Michael Henning <drawoc@darkrefraction.com>
2013-01-04 19:48:23 -0500

    autogen.sh: also look for automake 1.13

--- autogen.sh
+++ autogen.sh
@@ -157,6 +157,9 @@ echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
 if ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=$AUTOMAKE
    ACLOCAL=$ACLOCAL
+elif (automake-1.13 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.13
+   ACLOCAL=aclocal-1.13
 elif (automake-1.12 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.12
    ACLOCAL=aclocal-1.12
