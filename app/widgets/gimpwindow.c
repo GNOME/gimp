53d083c844f0ad81f88d6e64493160c319e519ef
Michael Natterer <mitch@gimp.org>
2013-04-09 15:41:20 +0200

    Bug 675549 - image region does not have focus unless clicked on
    We cannot simply randomy move the focus from e.g. a text entry back to
    the canvas. Instead introduce global handling of "Escape" and a
    "primary_focus_widget" that is always set the the image window's
    active canvas. When Escape is pressed, move the focus to that primary
    focus widget, or beep if it is already there. Text widgets still get
    the key events before that logic and can consume the Escape.
    (cherry picked from commit 5880685472d78cb07c0339b221e6b83e92179310)

--- app/widgets/gimpwindow.c
+++ app/widgets/gimpwindow.c
@@ -20,6 +20,7 @@
 #include "config.h"
 
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 
 #include "libgimpwidgets/gimpwidgets.h"
 
@@ -33,16 +34,22 @@
 #include "gimp-log.h"
 
 
+static void      gimp_window_dispose         (GObject     *object);
 static gboolean  gimp_window_key_press_event (GtkWidget   *widget,
                                               GdkEventKey *kevent);
 
 G_DEFINE_TYPE (GimpWindow, gimp_window, GTK_TYPE_WINDOW)
 
+#define parent_class gimp_window_parent_class
+
 
 static void
 gimp_window_class_init (GimpWindowClass *klass)
 {
+  GObjectClass   *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->dispose         = gimp_window_dispose;
 
   widget_class->key_press_event = gimp_window_key_press_event;
 }
@@ -52,15 +59,24 @@ gimp_window_init (GimpWindow *window)
 {
 }
 
+static void
+gimp_window_dispose (GObject *object)
+{
+  gimp_window_set_primary_focus_widget (GIMP_WINDOW (object), NULL);
+
+  G_OBJECT_CLASS (parent_class)->dispose (object);
+}
+
 static gboolean
 gimp_window_key_press_event (GtkWidget   *widget,
                              GdkEventKey *event)
 {
-  GtkWindow       *window = GTK_WINDOW (widget);
-  GtkWidget       *focus  = gtk_window_get_focus (window);
+  GimpWindow      *gimp_window = GIMP_WINDOW (widget);
+  GtkWindow       *window      = GTK_WINDOW (widget);
+  GtkWidget       *focus       = gtk_window_get_focus (window);
   GdkModifierType  accel_mods;
   gboolean         enable_mnemonics;
-  gboolean         handled = FALSE;
+  gboolean         handled     = FALSE;
 
   /* we're overriding the GtkWindow implementation here to give
    * the focus widget precedence over unmodified accelerators
@@ -77,6 +93,16 @@ gimp_window_key_press_event (GtkWidget   *widget,
       if (handled)
         GIMP_LOG (KEY_EVENTS,
                   "handled by gtk_window_propagate_key_event(text_widget)");
+    }
+
+  if (event->keyval == GDK_KEY_Escape && gimp_window->primary_focus_widget)
+    {
+      if (focus != gimp_window->primary_focus_widget)
+        gtk_widget_grab_focus (gimp_window->primary_focus_widget);
+      else
+        gtk_widget_error_bell (widget);
+
+      return TRUE;
     }
 
   accel_mods =
@@ -135,4 +161,32 @@ gimp_window_key_press_event (GtkWidget   *widget,
     }
 
   return handled;
+}
+
+void
+gimp_window_set_primary_focus_widget (GimpWindow *window,
+                                      GtkWidget  *primary_focus)
+{
+  g_return_if_fail (GIMP_IS_WINDOW (window));
+  g_return_if_fail (GTK_IS_WIDGET (primary_focus));
+  g_return_if_fail (gtk_widget_get_toplevel (primary_focus) ==
+                    GTK_WIDGET (window));
+
+  if (window->primary_focus_widget)
+    g_object_remove_weak_pointer (G_OBJECT (window->primary_focus_widget),
+                                  (gpointer) &window->primary_focus_widget);
+
+  window->primary_focus_widget = primary_focus;
+
+  if (window->primary_focus_widget)
+    g_object_add_weak_pointer (G_OBJECT (window->primary_focus_widget),
+                               (gpointer) &window->primary_focus_widget);
+}
+
+GtkWidget *
+gimp_window_get_primary_focus_widget (GimpWindow *window)
+{
+  g_return_val_if_fail (GIMP_IS_WINDOW (window), NULL);
+
+  return window->primary_focus_widget;
 }
