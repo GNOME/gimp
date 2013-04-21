GNOME/gimp

commit 7392679be90eecb54cce6dfa151602a314a31e35

Bug 593167 - Support Windows in gimp_get_default_unit()
Function gimp_get_default_unit ported for Windows using standard GetLocaleInfo() function (from windows.h).
(cherry picked from commit 194ef70d7446a627c274135c54498d3092c9b356)

app/core/gimp-utils.c
@@ -347,7 +347,7 @@ gimp_get_default_language (const gchar *category)
 GimpUnit
 gimp_get_default_unit (void)
 {
-#ifdef HAVE__NL_MEASUREMENT_MEASUREMENT
+#if defined (HAVE__NL_MEASUREMENT_MEASUREMENT)
   const gchar *measurement = nl_langinfo (_NL_MEASUREMENT_MEASUREMENT);
 
   switch (*((guchar *) measurement))
@@ -357,6 +357,27 @@ gimp_get_default_unit (void)
 
     case 2: /* imperial */
       return GIMP_UNIT_INCH;
+    }
+
+#elif defined (G_OS_WIN32)
+  DWORD measurement;
+  int   ret;
+
+  ret = GetLocaleInfo(LOCALE_USER_DEFAULT,
+                      LOCALE_IMEASURE | LOCALE_RETURN_NUMBER,
+                      (LPTSTR)&measurement,
+                      sizeof(measurement) / sizeof(TCHAR) );
+
+  if (ret != 0) /* GetLocaleInfo succeeded */
+    {
+    switch ((guint) measurement)
+      {
+      case 0: /* metric */
+        return GIMP_UNIT_MM;
+
+      case 1: /* imperial */
+        return GIMP_UNIT_INCH;
+      }
     }
 #endif
 

commit 4055ccbe9cb3053c2c8bedabaa31df982fa1b752

app: fix broken build on windows due to not #including windows.h
This was broken after 7392679be90eecb54cce6dfa151602a314a31e35
The corresponding commit on master did not break the build because
windows.h is already included there.

app/core/gimp-utils.c
@@ -26,6 +26,12 @@
 #include <langinfo.h>
 #endif
 
+#include <glib.h>
+
+#ifdef G_OS_WIN32
+#include <windows.h>
+#endif
+
 #include <cairo.h>
 #include <glib-object.h>
 #include <gobject/gvaluecollector.h>