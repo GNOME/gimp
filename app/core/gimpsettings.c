f9b7979af0f198aea2ea9d4780e6e6a869bc9257
Michael Natterer <mitch@gimp.org>
2013-05-25 16:48:38 +0200

    app: somehow the new app/core/gimpsettings.[ch] didn't get committed

---
+++ app/core/gimpsettings.c
@@ -0,0 +1,167 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * gimpsettings.c
+ * Copyright (C) 2008 Michael Natterer <mitch@gimp.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <string.h>
+
+#include <gegl.h>
+
+#include "libgimpconfig/gimpconfig.h"
+
+#include "core-types.h"
+
+#include "gimpsettings.h"
+
+
+enum
+{
+  PROP_0,
+  PROP_TIME
+};
+
+
+static void   gimp_settings_get_property (GObject      *object,
+                                          guint         property_id,
+                                          GValue       *value,
+                                          GParamSpec   *pspec);
+static void   gimp_settings_set_property (GObject      *object,
+                                          guint         property_id,
+                                          const GValue *value,
+                                          GParamSpec   *pspec);
+
+
+G_DEFINE_TYPE (GimpSettings, gimp_settings, GIMP_TYPE_VIEWABLE)
+
+#define parent_class gimp_settings_parent_class
+
+
+static void
+gimp_settings_class_init (GimpSettingsClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->set_property = gimp_settings_set_property;
+  object_class->get_property = gimp_settings_get_property;
+
+  GIMP_CONFIG_INSTALL_PROP_UINT (object_class, PROP_TIME,
+                                 "time",
+                                 "Time of settings creation",
+                                 0, G_MAXUINT, 0, 0);
+}
+
+static void
+gimp_settings_init (GimpSettings *config)
+{
+}
+
+static void
+gimp_settings_get_property (GObject    *object,
+                            guint       property_id,
+                            GValue     *value,
+                            GParamSpec *pspec)
+{
+  GimpSettings *config = GIMP_SETTINGS (object);
+
+  switch (property_id)
+    {
+    case PROP_TIME:
+      g_value_set_uint (value, config->time);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+    }
+}
+
+static void
+gimp_settings_set_property (GObject      *object,
+                            guint         property_id,
+                            const GValue *value,
+                            GParamSpec   *pspec)
+{
+  GimpSettings *config = GIMP_SETTINGS (object);
+
+  switch (property_id)
+    {
+    case PROP_TIME:
+      config->time = g_value_get_uint (value);
+
+      if (config->time > 0)
+        {
+          time_t     t;
+          struct tm  tm;
+          gchar      buf[64];
+          gchar     *name;
+
+          t = config->time;
+          tm = *localtime (&t);
+          strftime (buf, sizeof (buf), "%Y-%m-%d %H:%M:%S", &tm);
+
+          name = g_locale_to_utf8 (buf, -1, NULL, NULL, NULL);
+          gimp_object_set_name (GIMP_OBJECT (config), name);
+          g_free (name);
+        }
+      break;
+
+   default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+    }
+}
+
+
+/*  public functions  */
+
+gint
+gimp_settings_compare (GimpSettings *a,
+                       GimpSettings *b)
+{
+  const gchar *name_a = gimp_object_get_name (a);
+  const gchar *name_b = gimp_object_get_name (b);
+
+  if (a->time > 0 && b->time > 0)
+    {
+      return - strcmp (name_a, name_b);
+    }
+  else if (a->time > 0)
+    {
+      return -1;
+    }
+  else if (b->time > 0)
+    {
+      return 1;
+    }
+  else if (name_a && name_b)
+    {
+      return strcmp (name_a, name_b);
+    }
+  else if (name_a)
+    {
+      return 1;
+    }
+  else if (name_b)
+    {
+      return -1;
+    }
+
+  return 0;
+}
---
+++ app/core/gimpsettings.h
@@ -0,0 +1,57 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * gimpsettings.h
+ * Copyright (C) 2008 Michael Natterer <mitch@gimp.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GIMP_SETTINGS_H__
+#define __GIMP_SETTINGS_H__
+
+
+#include "gimpviewable.h"
+
+
+#define GIMP_TYPE_SETTINGS            (gimp_settings_get_type ())
+#define GIMP_SETTINGS(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_SETTINGS, GimpSettings))
+#define GIMP_SETTINGS_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  GIMP_TYPE_SETTINGS, GimpSettingsClass))
+#define GIMP_IS_SETTINGS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIMP_TYPE_SETTINGS))
+#define GIMP_IS_SETTINGS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GIMP_TYPE_SETTINGS))
+#define GIMP_SETTINGS_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  GIMP_TYPE_SETTINGS, GimpSettingsClass))
+
+
+typedef struct _GimpSettingsClass GimpSettingsClass;
+
+struct _GimpSettings
+{
+  GimpViewable  parent_instance;
+
+  guint         time;
+};
+
+struct _GimpSettingsClass
+{
+  GimpViewableClass  parent_class;
+};
+
+
+GType   gimp_settings_get_type (void) G_GNUC_CONST;
+
+gint    gimp_settings_compare  (GimpSettings *a,
+                                GimpSettings *b);
+
+
+#endif /* __GIMP_SETTINGS_H__ */
