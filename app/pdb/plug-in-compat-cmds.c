a77d9223df873f9ecd996e5c12c807a59f73fdd6
Michael Natterer <mitch@gimp.org>
2013-05-27 02:34:04 +0200

    plug-ins, pdb: remove shift plug-in and add a PDB compat procedure for it

--- app/pdb/plug-in-compat-cmds.c
+++ app/pdb/plug-in-compat-cmds.c
@@ -646,6 +646,50 @@ plug_in_semiflatten_invoker (GimpProcedure         *procedure,
 }
 
 static GimpValueArray *
+plug_in_shift_invoker (GimpProcedure         *procedure,
+                       Gimp                  *gimp,
+                       GimpContext           *context,
+                       GimpProgress          *progress,
+                       const GimpValueArray  *args,
+                       GError               **error)
+{
+  gboolean success = TRUE;
+  GimpDrawable *drawable;
+  gint32 shift_amount;
+  gint32 orientation;
+
+  drawable = gimp_value_get_drawable (gimp_value_array_index (args, 2), gimp);
+  shift_amount = g_value_get_int (gimp_value_array_index (args, 3));
+  orientation = g_value_get_int (gimp_value_array_index (args, 4));
+
+  if (success)
+    {
+      if (gimp_pdb_item_is_attached (GIMP_ITEM (drawable), NULL,
+                                     GIMP_PDB_ITEM_CONTENT, error) &&
+          gimp_pdb_item_is_not_group (GIMP_ITEM (drawable), error))
+        {
+          GeglNode *node =
+            gegl_node_new_child (NULL,
+                                 "operation", "gegl:shift",
+                                 "shift",     shift_amount / 2,
+                                 "direction", orientation ? 0 : 1,
+                                 NULL);
+
+          gimp_drawable_apply_operation (drawable, progress,
+                                         C_("undo-type", "Shift"),
+                                         node);
+
+          g_object_unref (node);
+        }
+      else
+        success = FALSE;
+    }
+
+  return gimp_procedure_get_return_values (procedure, success,
+                                           error ? *error : NULL);
+}
+
+static GimpValueArray *
 plug_in_threshold_alpha_invoker (GimpProcedure         *procedure,
                                  Gimp                  *gimp,
                                  GimpContext           *context,
@@ -1310,6 +1354,54 @@ register_plug_in_compat_procs (GimpPDB *pdb)
                                                             "Input drawable",
                                                             pdb->gimp, FALSE,
                                                             GIMP_PARAM_READWRITE));
+  gimp_pdb_register_procedure (pdb, procedure);
+  g_object_unref (procedure);
+
+  /*
+   * gimp-plug-in-shift
+   */
+  procedure = gimp_procedure_new (plug_in_shift_invoker);
+  gimp_object_set_static_name (GIMP_OBJECT (procedure),
+                               "plug-in-shift");
+  gimp_procedure_set_static_strings (procedure,
+                                     "plug-in-shift",
+                                     "Shift each row or column of pixels by a random amount",
+                                     "Shifts the pixels of the specified drawable. Each row or column will be displaced a random value of pixels.",
+                                     "Compatibility procedure. Please see 'gegl:shift' for credits.",
+                                     "Compatibility procedure. Please see 'gegl:shift' for credits.",
+                                     "2013",
+                                     NULL);
+  gimp_procedure_add_argument (procedure,
+                               g_param_spec_enum ("run-mode",
+                                                  "run mode",
+                                                  "The run mode",
+                                                  GIMP_TYPE_RUN_MODE,
+                                                  GIMP_RUN_INTERACTIVE,
+                                                  GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_image_id ("image",
+                                                         "image",
+                                                         "Input image (unused)",
+                                                         pdb->gimp, FALSE,
+                                                         GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_drawable_id ("drawable",
+                                                            "drawable",
+                                                            "Input drawable",
+                                                            pdb->gimp, FALSE,
+                                                            GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("shift-amount",
+                                                      "shift amount",
+                                                      "Shift amount",
+                                                      0, 200, 0,
+                                                      GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("orientation",
+                                                      "orientation",
+                                                      "Orientation { ORIENTATION-VERTICAL (0), ORIENTATION-HORIZONTAL (1) }",
+                                                      0, 1, 0,
+                                                      GIMP_PARAM_READWRITE));
   gimp_pdb_register_procedure (pdb, procedure);
   g_object_unref (procedure);
 
