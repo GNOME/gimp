2f1e2e1c1778872e1a23e3ffd6d7c2b85b82a913
Ajay Ramanathan <ajay.010293@gmail.com>
2013-09-15 10:39:24 +0530

    app: polygon selection tool

--- app/tools/gimpselectbyshapetool.c
+++ app/tools/gimpselectbyshapetool.c
@@ -754,7 +754,7 @@ gimp_select_by_shape_tool_real_select (GimpSelectByShapeTool *rect_sel_tool,
                                    options->feather_radius,
                                    TRUE);*/
         
-        gimp_channel_select_polygon  (channel,
+      gimp_channel_select_polygon  (channel,
                                    NULL,
                                    6,
                                    array1,
