3838a8cebbfee4f44ab4bd618029ea0b9942e850
Jehan <jehan@girinstud.io>
2013-09-21 18:17:34 +1200

    modules: small tabs replacement into spaces.

--- modules/display-filter-lcms.c
+++ modules/display-filter-lcms.c
@@ -210,10 +210,10 @@ cdisplay_lcms_profile_get_info (cmsHPROFILE   profile,
         }
 
       if (! *name)
-	{
-	  /* a color profile without a name */
-	  *name = g_strdup (_("(unnamed profile)"));
-	}
+        {
+          /* a color profile without a name */
+          *name = g_strdup (_("(unnamed profile)"));
+        }
 
       descSize = cmsGetProfileInfoASCII (profile, cmsInfoManufacturer,
                                          "en", "US", NULL, 0);
