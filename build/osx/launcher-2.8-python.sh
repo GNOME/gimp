43f7961c1798efa067f9a33aacea94ba96bf4ad6
Clayton Walker <clayton.m.walker@gmail.com>
2013-11-17 17:37:17 -0700

    osx: Update build files, prep for 2.8.8 dmg

--- build/osx/launcher-2.8-python.sh
+++ build/osx/launcher-2.8-python.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+echo "Setting up environment..."
+
 if test "x$GTK_DEBUG_LAUNCHER" != x; then
-set -x
+    set -x
 fi
 
 if test "x$GTK_DEBUG_GDB" != x; then
-EXEC="gdb --args"
+    EXEC="gdb --args"
 else
-EXEC=exec
+    EXEC=exec
 fi
 
 name=`basename "$0"`
@@ -36,13 +38,20 @@ export PATH="$bundle_contents/MacOS:$bundle_bin:$PATH"
 export GTK2_RC_FILES="$bundle_etc/gtk-2.0/gtkrc"
 export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
 export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
+export GDK_PIXBUF_MODULEDIR="$bundle_lib/gdk-pixbuf-2.0/2.10.0/loaders"
+#export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
+
 export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
+export PANGO_SYSCONFDIR="$bundle_etc"
+export PANGO_LIBDIR="$bundle_lib"
 
 # Specify fonconfig configuration file
 export FONTCONFIG_FILE="$bundle_etc/fonts/fonts.conf"
 
 # Include gegl path
 export GEGL_PATH="$bundle_lib/gegl-0.2"
+
+echo "Enabling internal python..."
 
 # Set up python
 export PYTHONHOME="$bundle_res"
@@ -55,26 +64,30 @@ PYTHONPATH="$bundle_lib/pygtk/2.0:$PYTHONPATH"
 PYTHONPATH="$bundle_lib/gimp/2.0/python:$PYTHONPATH"
 export PYTHONPATH
 
+#echo "Activating dbus..."
+
 # Launch dbus if needed
-dbusenv="$TMPDIR/gimp-$USER.dbus"
+#dbusenv="$TMPDIR/gimp-$USER.dbus"
 
-if [ -f "$dbusenv" ]; then
-    source "$dbusenv"
-fi
+#if [ -f "$dbusenv" ]; then
+#    source "$dbusenv"
+#fi
 
-if [ -z "$DBUS_SESSION_BUS_PID" ] || ! ps -p "$DBUS_SESSION_BUS_PID" >/dev/null; then
-    "$bundle_bin/dbus-launch" --config-file "$bundle_etc/dbus-1/session.conf" > "$dbusenv"
-    source "$dbusenv"
-fi
+#if [ -z "$DBUS_SESSION_BUS_PID" ] || ! ps -p "$DBUS_SESSION_BUS_PID" >/dev/null; then
+#    "$bundle_bin/dbus-launch" --config-file "$bundle_etc/dbus-1/session.conf" > "$dbusenv"
+#    source "$dbusenv"
+#fi
 
-export DBUS_SESSION_BUS_PID
-export DBUS_SESSION_BUS_ADDRESS
+#export DBUS_SESSION_BUS_PID
+#export DBUS_SESSION_BUS_ADDRESS
 
 # Specify ghostscript directories
 # export GS_RESOURCE_DIR="$bundle_res/share/ghostscript/9.06/Resource"
 # export GS_ICC_PROFILES="$bundle_res/share/ghostscript/9.06/iccprofiles/"
 # export GS_LIB="$GS_RESOURCE_DIR/Init:$GS_RESOURCE_DIR:$GS_RESOURCE_DIR/Font:$bundle_res/share/ghostscript/fonts:$bundle_res/share/fonts/urw-fonts:$GS_ICC_PROFILES"
 # export GS_FONTPATH="$bundle_res/share/ghostscript/fonts:$bundle_res/share/fonts/urw-fonts:~/Library/Fonts:/Library/Fonts:/System/Library/Fonts"
+
+echo "Locale black magic..."
 
 APP=name
 I18NDIR="$bundle_data/locale"
@@ -208,4 +221,10 @@ if /bin/expr "x$1" : '^x-psn_' > /dev/null; then
 shift 1
 fi
 
+echo "Launching GIMP..."
 $EXEC "$bundle_contents/MacOS/$name-bin" "$@" $EXTRA_ARGS
+
+#"$bundle_contents/MacOS/$name-bin" "$@" $EXTRA_ARGS
+
+#echo "Cleaning up..."
+#killall dbus-daemon
