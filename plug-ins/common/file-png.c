GNOME/gimp

commit 7c493f76f836095adad08205fdfd4522e05e0bd4

Bug 721135 - Unable to export to .png - error: known incorrect sRGB profile
Some libpng errors can safely be marked as nonfatal, which is much
better than simply failing in those cases.

Thanks to John Bowler for pointing out this solution.

plug-ins/common/file-png.c
@@ -828,6 +828,9 @@ load_image (const gchar  *filename,
       return image;
     }
 
+  /* Change some libpng errors to warnings (e.g. bug 721135) */
+  png_set_benign_errors (pp, TRUE);
+
   /*
    * Open the file and initialize the PNG read "engine"...
    */
@@ -1446,6 +1449,9 @@ save_image (const gchar  *filename,
                    gimp_filename_to_utf8 (filename));
       return FALSE;
     }
+
+  /* Change some libpng errors to warnings (e.g. bug 721135) */
+  png_set_benign_errors (pp, TRUE);
 
   /*
    * Open the file and initialize the PNG write "engine"...

commit d296a2c08a00f643177c7bacc0166d60417f579a

plug-ins: Fix build with libpng versions < 1.4.
This was broken in 7c493f76f836095adad08205fdfd4522e05e0bd4.

plug-ins/common/file-png.c
@@ -828,8 +828,10 @@ load_image (const gchar  *filename,
       return image;
     }
 
+#ifdef PNG_BENIGN_ERRORS_SUPPORTED
   /* Change some libpng errors to warnings (e.g. bug 721135) */
   png_set_benign_errors (pp, TRUE);
+#endif
 
   /*
    * Open the file and initialize the PNG read "engine"...
@@ -1450,8 +1452,10 @@ save_image (const gchar  *filename,
       return FALSE;
     }
 
+#ifdef PNG_BENIGN_ERRORS_SUPPORTED
   /* Change some libpng errors to warnings (e.g. bug 721135) */
   png_set_benign_errors (pp, TRUE);
+#endif
 
   /*
    * Open the file and initialize the PNG write "engine"...