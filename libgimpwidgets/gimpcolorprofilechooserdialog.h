f6ee56288436d51426b0f875339a3d7f52d80500
Michael Natterer <mitch@gimp.org>
2014-03-23 23:59:40 +0100

    libgimpwidgets: add padding to GimpColorProfileChooserDialogClass

--- libgimpwidgets/gimpcolorprofilechooserdialog.h
+++ libgimpwidgets/gimpcolorprofilechooserdialog.h
@@ -46,6 +46,12 @@ struct _GimpColorProfileChooserDialog
 struct _GimpColorProfileChooserDialogClass
 {
   GtkFileChooserDialogClass  parent_class;
+
+  /* Padding for future expansion */
+  void (* _gimp_reserved1) (void);
+  void (* _gimp_reserved2) (void);
+  void (* _gimp_reserved3) (void);
+  void (* _gimp_reserved4) (void);
 };
 
 
