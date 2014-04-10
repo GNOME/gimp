48b01cf29729da6849d7cdfa23ff0d77b9d3702e
Michael Natterer <mitch@gimp.org>
2014-04-10 14:08:47 +0200

    Bug 727343 - gimp_plug_in_handle_tile_get fails for 64-bit depth images
    libgimp/gimp.c: forgot to double the shared memory size for 64 bit
    images.

--- libgimp/gimp.c
+++ libgimp/gimp.c
@@ -119,7 +119,7 @@
  **/
 
 
-#define TILE_MAP_SIZE (_tile_width * _tile_height * 16)
+#define TILE_MAP_SIZE (_tile_width * _tile_height * 32)
 
 #define ERRMSG_SHM_FAILED "Could not attach to gimp shared memory segment"
 
