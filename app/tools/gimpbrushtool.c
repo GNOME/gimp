6e726f248ff39b8fa3b1947728b0df5fe352f635
Michael Natterer <mitch@gimp.org>
2014-04-19 19:53:40 +0200

    app: use the right aspect and angle actions for GimpBrushTool

--- app/tools/gimpbrushtool.c
+++ app/tools/gimpbrushtool.c
@@ -101,12 +101,12 @@ gimp_brush_tool_init (GimpBrushTool *brush_tool)
 {
   GimpTool *tool = GIMP_TOOL (brush_tool);
 
-  gimp_tool_control_set_action_value_2 (tool->control,
-                                        "tools/tools-paint-brush-size-set");
+  gimp_tool_control_set_action_value_2  (tool->control,
+                                         "tools/tools-paint-brush-size-set");
   gimp_tool_control_set_action_value_3  (tool->control,
-                                         "context/context-brush-aspect-set");
+                                         "tools/tools-paint-brush-aspect-ratio-set");
   gimp_tool_control_set_action_value_4  (tool->control,
-                                         "context/context-brush-angle-set");
+                                         "tools/tools-paint-brush-angle-set");
   gimp_tool_control_set_action_object_1 (tool->control,
                                          "context/context-brush-select-set");
 }
