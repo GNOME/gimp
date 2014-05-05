26738b9ff3491724c29d391bc4eb9b0bfcf2f05e
Massimo Valentini <mvalentini@src.gnome.org>
2014-05-05 08:23:02 +0200

    Bug 722487: Something in the attached .gimp-2.8 directory makes...
    GIMP fail on start
    
    Make gimp_tool_preset_load() robust against completely failed
    deserializing.

--- app/core/gimptoolpreset-load.c
+++ app/core/gimptoolpreset-load.c
@@ -20,6 +20,7 @@
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <gegl.h>
 
+#include "libgimpbase/gimpbase.h"
 #include "libgimpconfig/gimpconfig.h"
 
 #include "core-types.h"
@@ -27,6 +28,8 @@
 #include "gimpcontext.h"
 #include "gimptoolpreset.h"
 #include "gimptoolpreset-load.h"
+
+#include "gimp-intl.h"
 
 
 GList *
@@ -49,7 +52,16 @@ gimp_tool_preset_load (GimpContext  *context,
                                     filename,
                                     NULL, error))
     {
-      return g_list_prepend (NULL, tool_preset);
+      if (GIMP_IS_CONTEXT (tool_preset->tool_options))
+        {
+          return g_list_prepend (NULL, tool_preset);
+        }
+      else
+        {
+          g_set_error (error, GIMP_CONFIG_ERROR, GIMP_CONFIG_ERROR_PARSE,
+                       _("Error while parsing '%s'"),
+                       gimp_filename_to_utf8 (filename));
+        }
     }
 
   g_object_unref (tool_preset);
