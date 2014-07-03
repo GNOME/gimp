77b6af2da864f7a4ee07e1ee7ce223bb668f78df
Michael Natterer <mitch@gimp.org>
2014-07-03 13:22:10 +0200

    app: implement gimp_curve_load() and _save() in GIO
    They are currently unused, but implementing them was a copy/paste
    no brainer.

--- app/core/gimpcurve-save.c
+++ app/core/gimpcurve-save.c
@@ -17,51 +17,27 @@
 
 #include "config.h"
 
-#include <errno.h>
-
 #include <gdk-pixbuf/gdk-pixbuf.h>
-#include <glib/gstdio.h>
 #include <gegl.h>
 
-#include "libgimpbase/gimpbase.h"
+#include "libgimpconfig/gimpconfig.h"
 
 #include "core-types.h"
 
 #include "gimpcurve.h"
 #include "gimpcurve-save.h"
 
-#include "gimp-intl.h"
-
 
 gboolean
 gimp_curve_save (GimpData  *data,
                  GError   **error)
 {
-  /* GimpCurve *curve; */
-  gchar     *path;
-  FILE      *file;
-
   g_return_val_if_fail (GIMP_IS_CURVE (data), FALSE);
   g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
 
-  /* curve = GIMP_CURVE (data); */
-
-  path = g_file_get_path (gimp_data_get_file (data));
-  file = g_fopen (path, "wb");
-  g_free (path);
-
-  if (! file)
-    {
-      g_set_error (error, GIMP_DATA_ERROR, GIMP_DATA_ERROR_OPEN,
-                   _("Could not open '%s' for writing: %s"),
-                   gimp_file_get_utf8_name (gimp_data_get_file (data)),
-                   g_strerror (errno));
-      return FALSE;
-    }
-
-  /* FIXME: write curve */
-
-  fclose (file);
-
-  return TRUE;
+  return gimp_config_serialize_to_gfile (GIMP_CONFIG (data),
+                                         gimp_data_get_file (data),
+                                         "GIMP curve file",
+                                         "end of GIMP curve file",
+                                         NULL, error);
 }
