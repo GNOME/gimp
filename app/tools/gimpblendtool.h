c298485ba478e28b3aac150d3e6cafb649bb0e1d
Michael Henning <drawoc@darkrefraction.com>
2014-10-18 19:05:56 -0400

    Revert "app: Refactor GimpBlendTool to not use update methods."
    This reverts commit 8a2a9775b97a5423d0a000a1054462328efda5ab.
    
    Conflicts:
    	app/tools/gimpblendtool.c

--- app/tools/gimpblendtool.h
+++ app/tools/gimpblendtool.h
@@ -60,6 +60,12 @@ struct _GimpBlendTool
   gdouble         mouse_x;    /*  pointer x coord   */
   gdouble         mouse_y;    /*  pointer y coord   */
 
+  GimpCanvasItem *line;
+  GimpCanvasItem *start_handle_circle;
+  GimpCanvasItem *start_handle_cross;
+  GimpCanvasItem *end_handle_circle;
+  GimpCanvasItem *end_handle_cross;
+
   GeglNode       *graph;
   GeglNode       *render_node;
   GimpImageMap   *image_map;
