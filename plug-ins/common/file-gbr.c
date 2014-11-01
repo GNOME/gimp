51c35d4c78afd7f3f1cd784a7be1360bbc01d85c
Michael Natterer <mitch@gimp.org>
2014-11-01 15:00:08 +0100

    Bug 739486 - Enter doesn't confirm "export image as brush"
    Set the "activates-default" property on the spinbutton and entry in
    the file-gbr-save dialog.

--- plug-ins/common/file-gbr.c
+++ plug-ins/common/file-gbr.c
@@ -786,6 +786,7 @@ save_dialog (void)
   adj = (GtkAdjustment *) gtk_adjustment_new (info.spacing, 1, 1000, 1, 10, 0);
   spinbutton = gtk_spin_button_new (adj, 1.0, 0);
   gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (spinbutton), TRUE);
+  gtk_entry_set_activates_default (GTK_ENTRY (spinbutton), TRUE);
   gimp_table_attach_aligned (GTK_TABLE (table), 0, 0,
                              _("Spacing:"), 1.0, 0.5,
                              spinbutton, 1, TRUE);
@@ -797,6 +798,7 @@ save_dialog (void)
   entry = gtk_entry_new ();
   gtk_widget_set_size_request (entry, 200, -1);
   gtk_entry_set_text (GTK_ENTRY (entry), info.description);
+  gtk_entry_set_activates_default (GTK_ENTRY (entry), TRUE);
   gimp_table_attach_aligned (GTK_TABLE (table), 0, 1,
                              _("Description:"), 1.0, 0.5,
                              entry, 1, FALSE);
