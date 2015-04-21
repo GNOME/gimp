957471604fca40b5265a5fc23a23592d1a2fa554
Michael Natterer <mitch@gimp.org>
2015-04-21 21:55:46 +0200

    icons, app, libgimpwidgets: use GResource for the compiled-in icons
    And a small improvement in cursors/Makefile.am

--- app/core/gimpviewable.c
+++ app/core/gimpviewable.c
@@ -38,7 +38,7 @@
 #include "gimptempbuf.h"
 #include "gimpviewable.h"
 
-#include "icons/gimp-core-pixbufs.h"
+#include "icons/gimp-core-pixbufs.c"
 
 
 enum
@@ -1089,6 +1089,7 @@ gimp_viewable_get_dummy_pixbuf (GimpViewable  *viewable,
 {
   GdkPixbuf *icon;
   GdkPixbuf *pixbuf;
+  GError    *error = NULL;
   gdouble    ratio;
   gint       w, h;
 
@@ -1096,9 +1097,14 @@ gimp_viewable_get_dummy_pixbuf (GimpViewable  *viewable,
   g_return_val_if_fail (width  > 0, NULL);
   g_return_val_if_fail (height > 0, NULL);
 
-  icon = gdk_pixbuf_new_from_inline (-1, gimp_question_64, FALSE, NULL);
-
-  g_return_val_if_fail (icon != NULL, NULL);
+  icon = gdk_pixbuf_new_from_resource ("/org/gimp/icons/64/gimp-question.png",
+                                       &error);
+  if (! icon)
+    {
+      g_critical ("Failed to create icon image: %s", error->message);
+      g_clear_error (&error);
+      return NULL;
+    }
 
   w = gdk_pixbuf_get_width (icon);
   h = gdk_pixbuf_get_height (icon);
