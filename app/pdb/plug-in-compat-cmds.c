769bc933d4dfb2fa0e2b215ec9384822a6549acc
Thomas Manni <thomas.manni@free.fr>
2015-06-11 01:26:49 +0200

    plug-ins, pdb: remove the wind plug-in and add a PDB compat proc

--- app/pdb/plug-in-compat-cmds.c
+++ app/pdb/plug-in-compat-cmds.c
@@ -3575,6 +3575,58 @@ plug_in_whirl_pinch_invoker (GimpProcedure         *procedure,
                                            error ? *error : NULL);
 }
 
+static GimpValueArray *
+plug_in_wind_invoker (GimpProcedure         *procedure,
+                      Gimp                  *gimp,
+                      GimpContext           *context,
+                      GimpProgress          *progress,
+                      const GimpValueArray  *args,
+                      GError               **error)
+{
+  gboolean success = TRUE;
+  GimpDrawable *drawable;
+  gint32 threshold;
+  gint32 direction;
+  gint32 strength;
+  gint32 algorithm;
+  gint32 edge;
+
+  drawable = gimp_value_get_drawable (gimp_value_array_index (args, 2), gimp);
+  threshold = g_value_get_int (gimp_value_array_index (args, 3));
+  direction = g_value_get_int (gimp_value_array_index (args, 4));
+  strength = g_value_get_int (gimp_value_array_index (args, 5));
+  algorithm = g_value_get_int (gimp_value_array_index (args, 6));
+  edge = g_value_get_int (gimp_value_array_index (args, 7));
+
+  if (success)
+    {
+      if (gimp_pdb_item_is_attached (GIMP_ITEM (drawable), NULL,
+                                     GIMP_PDB_ITEM_CONTENT, error) &&
+          gimp_pdb_item_is_not_group (GIMP_ITEM (drawable), error))
+        {
+          GeglNode *node =
+            gegl_node_new_child (NULL,
+                                 "operation", "gegl:wind",
+                                 "threshold", threshold,
+                                 "direction", direction,
+                                 "strength",  strength,
+                                 "style",     algorithm,
+                                 "edge",      edge,
+                                 NULL);
+
+          gimp_drawable_apply_operation (drawable, progress,
+                                         C_("undo-type", "Wind"),
+                                         node);
+          g_object_unref (node);
+        }
+      else
+        success = FALSE;
+    }
+
+  return gimp_procedure_get_return_values (procedure, success,
+                                           error ? *error : NULL);
+}
+
 void
 register_plug_in_compat_procs (GimpPDB *pdb)
 {
@@ -7125,6 +7177,72 @@ register_plug_in_compat_procs (GimpPDB *pdb)
                                                     "Radius (1.0 is the largest circle that fits in the image, and 2.0 goes all the way to the corners)",
                                                     0, 2, 0,
                                                     GIMP_PARAM_READWRITE));
+  gimp_pdb_register_procedure (pdb, procedure);
+  g_object_unref (procedure);
+
+  /*
+   * gimp-plug-in-wind
+   */
+  procedure = gimp_procedure_new (plug_in_wind_invoker);
+  gimp_object_set_static_name (GIMP_OBJECT (procedure),
+                               "plug-in-wind");
+  gimp_procedure_set_static_strings (procedure,
+                                     "plug-in-wind",
+                                     "Smear image to give windblown effect",
+                                     "Renders a wind effect.",
+                                     "Compatibility procedure. Please see 'gegl:wind' for credits.",
+                                     "Compatibility procedure. Please see 'gegl:wind' for credits.",
+                                     "2015",
+                                     NULL);
+  gimp_procedure_add_argument (procedure,
+                               g_param_spec_enum ("run-mode",
+                                                  "run mode",
+                                                  "The run mode",
+                                                  GIMP_TYPE_RUN_MODE,
+                                                  GIMP_RUN_INTERACTIVE,
+                                                  GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_image_id ("image",
+                                                         "image",
+                                                         "Input image (unused)",
+                                                         pdb->gimp, FALSE,
+                                                         GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_drawable_id ("drawable",
+                                                            "drawable",
+                                                            "Input drawable",
+                                                            pdb->gimp, FALSE,
+                                                            GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("threshold",
+                                                      "threshold",
+                                                      "Controls where blending will be done",
+                                                      0, 50, 0,
+                                                      GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("direction",
+                                                      "direction",
+                                                      "Wind direction { 0:left, 1:right, 2:top, 3:bottom }",
+                                                      0, 3, 0,
+                                                      GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("strength",
+                                                      "strength",
+                                                      "Controls the extent of the blending",
+                                                      1, 100, 1,
+                                                      GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("algorithm",
+                                                      "algorithm",
+                                                      "Algorithm { WIND (0), BLAST (1) }",
+                                                      0, 1, 0,
+                                                      GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("edge",
+                                                      "edge",
+                                                      "Affected edge { BOTH (0), LEADING (1), TRAILING (2) }",
+                                                      0, 2, 0,
+                                                      GIMP_PARAM_READWRITE));
   gimp_pdb_register_procedure (pdb, procedure);
   g_object_unref (procedure);
 }
