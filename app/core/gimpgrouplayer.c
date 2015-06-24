b12ecc71957ec9cfcead2d0757088a1111ae3065
Michael Natterer <mitch@gimp.org>
2015-06-24 19:40:54 +0200

    app: small fix for the previous patch in gimp_group_layer_update_size()

--- app/core/gimpgrouplayer.c
+++ app/core/gimpgrouplayer.c
@@ -1210,7 +1210,7 @@ gimp_group_layer_update_size (GimpGroupLayer *group)
                                        x, y, width, height);
 
           /*  see comment in gimp_group_layer_stack_update() below  */
-          gimp_pickable_flush (GIMP_PICKABLE (GET_PRIVATE (group)->projection));
+          gimp_pickable_flush (GIMP_PICKABLE (private->projection));
         }
     }
 }
