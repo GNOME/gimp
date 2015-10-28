455a91819bf3ec7b5c73055b57db0b55bc04bd65
Andrew Worsley <amworsley@gmail.com>
2015-10-28 21:38:55 +1100

    plug-ins: add back some menu entries for plugins
    So we see what we have. Need some other way of dealing with this stuff...

--- plug-ins/gimpressionist/gimp.c
+++ plug-ins/gimpressionist/gimp.c
@@ -96,6 +96,8 @@ query (void)
                           GIMP_PLUGIN,
                           G_N_ELEMENTS (args), 0,
                           args, NULL);
+
+  gimp_plugin_menu_register (PLUG_IN_PROC, "<Image>/Filters/Artistic");
 }
 
 static void
