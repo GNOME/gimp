e9dd77e072d2947c8b80b5570e0337d8603a0fca
Alexandre Prokoudine <alexandre.prokoudine@gmail.com>
2015-12-10 03:21:07 +0300

    Fix conflicting menu accelerators as per C R's request

--- app/actions/image-actions.c
+++ app/actions/image-actions.c
@@ -57,9 +57,9 @@ static const GimpActionEntry image_actions[] =
 
   { "image-menu",                  NULL, NC_("image-action", "_Image")     },
   { "image-mode-menu",             NULL, NC_("image-action", "_Mode")      },
-  { "image-precision-menu",        NULL, NC_("image-action", "_Precision") },
+  { "image-precision-menu",        NULL, NC_("image-action", "Pr_ecision") },
   { "image-color-management-menu", NULL, NC_("image-action",
-                                             "_Color Management")          },
+                                             "Color Ma_nagement")          },
   { "image-transform-menu",        NULL, NC_("image-action", "_Transform") },
   { "image-guides-menu",           NULL, NC_("image-action", "_Guides")    },
 
