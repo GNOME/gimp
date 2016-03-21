e3f3f5d2bd5e8a02e410d7b1bfdf5073581b9d8c
Michael Natterer <mitch@gimp.org>
2016-03-21 09:19:06 +0100

    app: fix calculation for action values which can wrap around
    such as the brush angle action.
    
    (cherry picked from commit c0fab2402a895d644e430a811ebe92ae87e27b9e)

--- app/actions/actions.c
+++ app/actions/actions.c
@@ -529,7 +529,7 @@ action_select_value (GimpActionSelectType  select_type,
         value = max - (min - value);
 
       while (value > max)
-        value = min + (max - value);
+        value = min + (value - max);
     }
   else
     {
