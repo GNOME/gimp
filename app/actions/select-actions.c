47a9c2a54b83487ad4a3110055c7280631ac7d25
Michael Natterer <mitch@gimp.org>
2016-04-10 16:34:14 +0200

    app: s/Remove holes/Remove Holes/, menu item labels are capitalized

--- app/actions/select-actions.c
+++ app/actions/select-actions.c
@@ -102,7 +102,7 @@ static const GimpActionEntry select_actions[] =
     GIMP_HELP_SELECTION_BORDER },
 
   { "select-flood", NULL,
-    NC_("select-action", "Re_move holes"), NULL,
+    NC_("select-action", "Re_move Holes"), NULL,
     NC_("select-action", "Remove holes from the selection"),
     G_CALLBACK (select_flood_cmd_callback),
     GIMP_HELP_SELECTION_FLOOD },
