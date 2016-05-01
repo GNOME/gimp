ed80db30c950d2f6ef72a26404e896bfe2fa0687
Hartmut Kuhse <hk_priv@gmx.de>
2016-05-01 19:02:05 +0200

    plug-ins: convert win-snap to screenshot works as win-snap, lots of optimizing work to be done.

---
+++ plug-ins/screenshot/resource.h
@@ -0,0 +1,26 @@
+/* {{NO_DEPENDENCIES}}
+ Microsoft Developer Studio generated include file.
+ Used by snappy.rc
+*/
+#define IDM_CAPTURE        100
+#define IDM_EXIT           101
+
+#define IDC_SELECT                      102
+#define IDD_SELECT                      103
+#define IDC_TEXT                        1000
+#define IDC_GROUP                       1001
+#define IDM_CAPTUREFULL                 40003
+#define IDM_HOOK                        40004
+#define IDM_UNHOOK                      40005
+
+/* Next default values for new objects */
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
--- plug-ins/screenshot/screenshot-win32.h
+++ plug-ins/screenshot/screenshot-win32.h
@@ -21,6 +21,14 @@
 
 #ifdef G_OS_WIN32
 
+#define IDC_STATIC -1
+
+#define IDS_APP_TITLE       500
+#define IDS_DISPLAYCHANGED  501
+#define IDS_VER_INFO_LANG   502
+#define IDS_VERSION_ERROR   503
+#define IDS_NO_HELP         504
+
 gboolean               screenshot_win32_available        (void);
 
 ScreenshotCapabilities screenshot_win32_get_capabilities (void);
