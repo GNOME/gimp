eba2d2d88b422b8b30fac07dadd62ac82a8e8231
Michael Natterer <mitch@gimp.org>
2016-05-21 00:01:10 +0200

    plug-ins: rename file-darktable format names to "Raw Foo"
    so they are ordered together in the file dialogs. Also rename the
    procedures to "file-farktable-foo".

--- plug-ins/file-darktable/file-formats.h
+++ plug-ins/file-darktable/file-formats.h
@@ -34,13 +34,16 @@ struct _FileFormat
   const gchar *load_help;
 };
 
-/* some magic numbers taken from http://www.garykessler.net/library/file_sigs.html
- * see also http://fileformats.archiveteam.org/wiki/Cameras_and_Digital_Image_Sensors
+/* some magic numbers taken from
+ * http://www.garykessler.net/library/file_sigs.html
+ *
+ * see also
+ * http://fileformats.archiveteam.org/wiki/Cameras_and_Digital_Image_Sensors
  */
 static const FileFormat file_formats[] =
 {
   {
-    N_("Canon raw"),
+    N_("Raw Canon"),
     "image/x-canon-cr2,image/x-canon-crw,image/tiff", // FIXME: only one mime type
     "cr2,crw,tif,tiff",
     "0,string,II*\\0\\020\\0\\0\\0CR,"             // cr2
@@ -49,273 +52,272 @@ static const FileFormat file_formats[] =
     "0,string,MM\\0*\\0\\0\\021\\064\\0\\04,"      // tiff
     "0,string,II*\\0\\0\\03\\0\\0\\0377\\01",      // tiff
 
-    "file-raw-canon-load",
+    "file-darktable-canon-load",
     "Load files in the Canon raw formats via darktable",
     "This plug-in loads files in Canon's raw formats by calling darktable."
   },
 
   {
-    N_("Nikon raw"),
+    N_("Raw Nikon"),
     "image/x-nikon-nef,image/x-nikon-nrw", // FIXME: only one mime type
     "nef,nrw",
     NULL,
 
-    "file-raw-nikon-load",
+    "file-darktable-nikon-load",
     "Load files in the Nikon raw formats via darktable",
     "This plug-in loads files in Nikon's raw formats by calling darktable."
   },
 
   {
-    N_("Hasselblad raw"),
+    N_("Raw Hasselblad"),
     "image/x-hasselblad-3fr,image/x-hasselblad-fff", // FIXME: only one mime type
     "3fr,fff",
     NULL,
 
-    "file-raw-hasselblad-load",
+    "file-darktable-hasselblad-load",
     "Load files in the Hasselblad raw formats via darktable",
     "This plug-in loads files in Hasselblad's raw formats by calling darktable."
   },
 
   {
-    N_("Sony raw"),
+    N_("Raw Sony"),
     "image/x-sony-arw,image/x-sony-srf,image/x-sony-sr2", // FIXME: only one mime type
     "arw,srf,sr2",
     NULL,
 
-    "file-raw-sony-load",
+    "file-darktable-sony-load",
     "Load files in the Sony raw formats via darktable",
     "This plug-in loads files in Sony's raw formats by calling darktable."
   },
 
   {
-    N_("Casio BAY raw"),
+    N_("Raw Casio BAY"),
     "image/x-casio-bay",
     "bay",
     NULL,
 
-    "file-raw-bay-load",
+    "file-darktable-bay-load",
     "Load files in the BAY raw format via darktable",
     "This plug-in loads files in Casio's raw BAY format by calling darktable."
   },
 
   {
-    N_("Phantom Software CINE raw"),
+    N_("Raw Phantom Software CINE"),
     "", // FIXME: find a mime type
     "cine,cin",
     NULL,
 
-    "file-raw-cine-load",
+    "file-darktable-cine-load",
     "Load files in the CINE raw format via darktable",
     "This plug-in loads files in Phantom Software's raw CINE format by calling darktable."
   },
 
   {
-    N_("Sinar raw"),
+    N_("Raw Sinar"),
     "", // FIXME: find a mime type
     "cs1,ia,sti",
     NULL,
 
-    "file-raw-sinar-load",
+    "file-darktable-sinar-load",
     "Load files in the Sinar raw formats via darktable",
     "This plug-in loads files in Sinar's raw formats by calling darktable."
   },
 
   {
-    N_("Kodak raw"),
+    N_("Raw Kodak"),
     "image/x-kodak-dc2,image/x-kodak-dcr,image/x-kodak-kdc,image/x-kodak-k25,image/x-kodak-kc2,image/tiff", // FIXME: only one mime type
     "dc2,dcr,kdc,k25,kc2,tif,tiff",
     "0,string,MM\\0*\\0\\0\\021\\0166\\0\\04,"    // tiff
     "0,string,II*\\0\\0\\03\\0\\0\\0174\\01",     // tiff
 
-    "file-raw-kodak-load",
+    "file-darktable-kodak-load",
     "Load files in the Kodak raw formats via darktable",
     "This plug-in loads files in Kodak's raw formats by calling darktable."
   },
 
   {
-    N_("Adobe DNG Digital Negative raw"),
+    N_("Raw Adobe DNG Digital Negative"),
     "image/x-adobe-dng",
     "dng",
     NULL,
 
-    "file-raw-dng-load",
+    "file-darktable-dng-load",
     "Load files in the DNG raw format via darktable",
     "This plug-in loads files in the Adobe Digital Negative DNG format by calling darktable."
   },
 
   {
-    N_("Epson ERF raw"),
+    N_("Raw Epson ERF"),
     "image/x-epson-erf",
     "erf",
     NULL,
 
-    "file-raw-erf-load",
+    "file-darktable-erf-load",
     "Load files in the ERF raw format via darktable",
     "This plug-in loads files in Epson's raw ERF format by calling darktable."
   },
 
   {
-    N_("Phase One raw"),
+    N_("Raw Phase One"),
     "image/x-phaseone-cap,image/x-phaseone-iiq", // FIXME: only one mime type
     "cap,iiq",
     NULL,
 
-    "file-raw-phaseone-load",
+    "file-darktable-phaseone-load",
     "Load files in the Phase One raw formats via darktable",
     "This plug-in loads files in Phase One's raw formats by calling darktable."
   },
 
   {
-    N_("Minolta raw"),
+    N_("Raw Minolta"),
     "image/x-minolta-mdc,image/x-minolta-mrw", // FIXME: only one mime type
     "mdc,mrw",
     NULL,
 
-    "file-raw-minolta-load",
+    "file-darktable-minolta-load",
     "Load files in the Minolta raw formats via darktable",
     "This plug-in loads files in Minolta's raw formats by calling darktable."
   },
 
   {
-    N_("Mamiya MEF raw"),
+    N_("Raw Mamiya MEF"),
     "image/x-mamiya-mef",
     "mef", NULL,
 
-    "file-raw-mef-load",
+    "file-darktable-mef-load",
     "Load files in the MEF raw format via darktable",
     "This plug-in loads files in Mamiya's raw MEF format by calling darktable."
   },
 
   {
-    N_("Leaf MOS raw"),
+    N_("Raw Leaf MOS"),
     "image/x-leaf-mos",
     "mos",
     NULL,
 
-    "file-raw-mos-load",
+    "file-darktable-mos-load",
     "Load files in the MOS raw format via darktable",
     "This plug-in loads files in Leaf's raw MOS format by calling darktable."
   },
 
   {
-    N_("Olympus ORF raw"),
+    N_("Raw Olympus ORF"),
     "image/x-olympus-orf",
     "orf",
     "0,string,IIRO,0,string,MMOR,0,string,IIRS",
 
-    "file-raw-orf-load",
+    "file-darktable-orf-load",
     "Load files in the ORF raw format via darktable",
     "This plug-in loads files in Olympus' raw ORF format by calling darktable."
   },
 
   {
-    N_("Pentax PEF raw"),
+    N_("Raw Pentax PEF"),
     "image/x-pentax-pef,image/x-pentax-raw", // FIXME: only one mime type
     "pef,raw",
     NULL,
 
-    "file-raw-pef-load",
+    "file-darktable-pef-load",
     "Load files in the PEF raw format via darktable",
     "This plug-in loads files in Pentax' raw PEF format by calling darktable."
   },
 
   {
-    N_("Logitech PXN raw"),
+    N_("Raw Logitech PXN"),
     "image/x-pxn", // FIXME: is that the correct mime type?
     "pxn",
     NULL,
 
-    "file-raw-pxn-load",
+    "file-darktable-pxn-load",
     "Load files in the PXN raw format via darktable",
     "This plug-in loads files in Logitech's raw PXN format by calling darktable."
   },
 
   {
-    N_("Apple QuickTake QTK raw"),
+    N_("Raw Apple QuickTake QTK"),
     "", // FIXME: find a mime type
     "qtk",
     NULL,
 
-    "file-raw-qtk-load",
+    "file-darktable-qtk-load",
     "Load files in the QTK raw format via darktable",
     "This plug-in loads files in Apple's QuickTake QTK raw format by calling darktable."
   },
 
   {
-    N_("Fujifilm RAF raw"),
+    N_("Raw Fujifilm RAF"),
     "image/x-fuji-raf",
     "raf",
     "0,string,FUJIFILMCCD-RAW",
 
-    "file-raw-raf-load",
+    "file-darktable-raf-load",
     "Load files in the RAF raw format via darktable",
     "This plug-in loads files in Fujifilm's raw RAF format by calling darktable."
   },
 
   {
-    N_("Panasonic raw"),
+    N_("Raw Panasonic"),
     "image/x-panasonic-raw,image/x-panasonic-rw2", // FIXME: only one mime type
     "raw,rw2",
     "0,string,IIU\\0",
 
-    "file-raw-panasonic-load",
+    "file-darktable-panasonic-load",
     "Load files in the Panasonic raw formats via darktable",
     "This plug-in loads files in Panasonic's raw formats by calling darktable."
   },
 
   {
-    N_("Digital Foto Maker RDC raw"),
+    N_("Raw Digital Foto Maker RDC"),
     "", // FIXME: find a mime type
     "rdc",
     NULL,
 
-    "file-raw-rdc-load",
+    "file-darktable-rdc-load",
     "Load files in the RDC raw format via darktable",
     "This plug-in loads files in Digital Foto Maker's raw RDC format by calling darktable."
   },
 
   {
-    N_("Leica RWL raw"),
+    N_("Raw Leica RWL"),
     "image/x-leica-rwl",
     "rwl",
     NULL,
 
-    "file-raw-rwl-load",
+    "file-darktable-rwl-load",
     "Load files in the RWL raw format via darktable",
     "This plug-in loads files in Leica's raw RWL format by calling darktable."
   },
 
   {
-    N_("Samsung SRW raw"),
+    N_("Raw Samsung SRW"),
     "image/x-samsung-srw",
     "srw",
     NULL,
 
-    "file-raw-srw-load",
+    "file-darktable-srw-load",
     "Load files in the SRW raw format via darktable",
     "This plug-in loads files in Samsung's raw SRW format by calling darktable."
   },
 
   {
-    N_("Sigma X3F raw"),
+    N_("Raw Sigma X3F"),
     "image/x-sigma-x3f",
     "x3f",
     "0,string,FOVb",
 
-    "file-raw-x3f-load",
+    "file-darktable-x3f-load",
     "Load files in the X3F raw format via darktable",
     "This plug-in loads files in Sigma's raw X3F format by calling darktable."
   },
 
   {
-    N_("Arriflex ARI raw"),
+    N_("Raw Arriflex ARI"),
     "",
     "ari",
     NULL,
 
-    "file-raw-ari-load",
+    "file-darktable-ari-load",
     "Load files in the ARI raw format via darktable",
     "This plug-in loads files in Arriflex' raw ARI format by calling darktable."
   }
-
 };
