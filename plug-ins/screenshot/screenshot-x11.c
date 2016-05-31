f67100f5549e000a4e4bf7a3b0a9a3c8e367dcdf
Michael Natterer <mitch@gimp.org>
2016-05-31 22:29:49 +0200

    plug-ins: attach the default comment to all screenshots
    not only to screenshots made with the X11 backend.

--- plug-ins/screenshot/screenshot-x11.c
+++ plug-ins/screenshot/screenshot-x11.c
@@ -407,7 +407,6 @@ create_image (cairo_surface_t *surface,
   gint32     image;
   gint32     layer;
   gdouble    xres, yres;
-  gchar     *comment;
   gint       width, height;
 
   gimp_progress_init (_("Importing screenshot"));
@@ -420,20 +419,6 @@ create_image (cairo_surface_t *surface,
 
   gimp_get_monitor_resolution (&xres, &yres);
   gimp_image_set_resolution (image, xres, yres);
-
-  comment = gimp_get_default_comment ();
-  if (comment)
-    {
-      GimpParasite *parasite;
-
-      parasite = gimp_parasite_new ("gimp-comment", GIMP_PARASITE_PERSISTENT,
-                                    strlen (comment) + 1, comment);
-
-      gimp_image_attach_parasite (image, parasite);
-      gimp_parasite_free (parasite);
-
-      g_free (comment);
-    }
 
   layer = gimp_layer_new_from_surface (image,
                                        name ? name : _("Screenshot"),
