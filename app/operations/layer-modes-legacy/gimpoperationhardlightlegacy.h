f45fc97eba624f0ffb7f1c50ec292d07c1db741e
Øyvind Kolås <pippin@gimp.org>
2017-01-11 04:54:36 +0100

    app: add missing op .h files

---
+++ app/operations/layer-modes-legacy/gimpoperationhardlightlegacy.h
@@ -0,0 +1,61 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * gimpoperationhardlightmode.h
+ * Copyright (C) 2008 Michael Natterer <mitch@gimp.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GIMP_OPERATION_HARDLIGHT_LEGACY_H__
+#define __GIMP_OPERATION_HARDLIGHT_LEGACY_H__
+
+
+#include "../gimpoperationpointlayermode.h"
+
+
+#define GIMP_TYPE_OPERATION_HARDLIGHT_LEGACY            (gimp_operation_hardlight_legacy_get_type ())
+#define GIMP_OPERATION_HARDLIGHT_LEGACY(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_OPERATION_HARDLIGHT_LEGACY, GimpOperationHardlightLegacy))
+#define GIMP_OPERATION_HARDLIGHT_LEGACY_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  GIMP_TYPE_OPERATION_HARDLIGHT_LEGACY, GimpOperationHardlightLegacyClass))
+#define GIMP_IS_OPERATION_HARDLIGHT_LEGACY(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIMP_TYPE_OPERATION_HARDLIGHT_LEGACY))
+#define GIMP_IS_OPERATION_HARDLIGHT_LEGACY_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GIMP_TYPE_OPERATION_HARDLIGHT_LEGACY))
+#define GIMP_OPERATION_HARDLIGHT_LEGACY_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  GIMP_TYPE_OPERATION_HARDLIGHT_LEGACY, GimpOperationHardlightLegacyClass))
+
+
+typedef struct _GimpOperationHardlightLegacy      GimpOperationHardlightLegacy;
+typedef struct _GimpOperationHardlightLegacyClass GimpOperationHardlightLegacyClass;
+
+struct _GimpOperationHardlightLegacy
+{
+  GimpOperationPointLayerMode  parent_instance;
+};
+
+struct _GimpOperationHardlightLegacyClass
+{
+  GimpOperationPointLayerModeClass  parent_class;
+};
+
+
+GType   gimp_operation_hardlight_legacy_get_type (void) G_GNUC_CONST;
+
+gboolean gimp_operation_hardlight_legacy_process_pixels (gfloat              *in,
+                                                         gfloat              *layer,
+                                                         gfloat              *mask,
+                                                         gfloat              *out,
+                                                         gfloat               opacity,
+                                                         glong                samples,
+                                                         const GeglRectangle *roi,
+                                                         gint                 level);
+
+#endif /* __GIMP_OPERATION_HARDLIGHT_LEGACY_H__ */
---
+++ app/operations/layer-modes/gimpoperationsoftlight.h
@@ -0,0 +1,61 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * gimpoperationsoftlightmode.h
+ * Copyright (C) 2008 Michael Natterer <mitch@gimp.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GIMP_OPERATION_SOFTLIGHT_H__
+#define __GIMP_OPERATION_SOFTLIGHT_H__
+
+
+#include "../gimpoperationpointlayermode.h"
+
+
+#define GIMP_TYPE_OPERATION_SOFTLIGHT            (gimp_operation_softlight_get_type ())
+#define GIMP_OPERATION_SOFTLIGHT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_OPERATION_SOFTLIGHT_MODE, GimpOperationSoftlight))
+#define GIMP_OPERATION_SOFTLIGHT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  GIMP_TYPE_OPERATION_SOFTLIGHT_MODE, GimpOperationSoftlightClass))
+#define GIMP_IS_OPERATION_SOFTLIGHT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIMP_TYPE_OPERATION_SOFTLIGHT_MODE))
+#define GIMP_IS_OPERATION_SOFTLIGHT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GIMP_TYPE_OPERATION_SOFTLIGHT_MODE))
+#define GIMP_OPERATION_SOFTLIGHT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  GIMP_TYPE_OPERATION_SOFTLIGHT_MODE, GimpOperationSoftlightClass))
+
+
+typedef struct _GimpOperationSoftlight      GimpOperationSoftlight;
+typedef struct _GimpOperationSoftlightClass GimpOperationSoftlightClass;
+
+struct _GimpOperationSoftlight
+{
+  GimpOperationPointLayerMode  parent_instance;
+};
+
+struct _GimpOperationSoftlightClass
+{
+  GimpOperationPointLayerModeClass  parent_class;
+};
+
+
+GType   gimp_operation_softlight_get_type (void) G_GNUC_CONST;
+
+gboolean gimp_operation_softlight_process_pixels (gfloat              *in,
+                                                  gfloat              *layer,
+                                                  gfloat              *mask,
+                                                  gfloat              *out,
+                                                  gfloat               opacity,
+                                                  glong                samples,
+                                                  const GeglRectangle *roi,
+                                                  gint                 level);
+
+#endif /* __GIMP_OPERATION_SOFTLIGHT_H__ */
