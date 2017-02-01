a18dcb94eb799602631c49621075f845c509993d
Michael Natterer <mitch@gimp.org>
2017-02-01 23:41:54 +0100

    configure.ac: require babl >= 0.1.24

--- app/sanity.c
+++ app/sanity.c
@@ -453,7 +453,7 @@ sanity_check_babl (void)
 
 #define BABL_REQUIRED_MAJOR 0
 #define BABL_REQUIRED_MINOR 1
-#define BABL_REQUIRED_MICRO 18
+#define BABL_REQUIRED_MICRO 24
 
   babl_get_version (&babl_major_version,
                     &babl_minor_version,
