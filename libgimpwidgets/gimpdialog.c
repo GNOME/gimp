GNOME/gimp

commit a94030052521e481cfe996044947c13b4e5c0276

libgimpwidgets: make GimpDialog's help_id settable after construction

libgimpwidgets/gimpdialog.c
@@ -129,8 +129,7 @@ gimp_dialog_class_init (GimpDialogClass *klass)
   g_object_class_install_property (object_class, PROP_HELP_ID,
                                    g_param_spec_string ("help-id", NULL, NULL,
                                                         NULL,
-                                                        GIMP_PARAM_READWRITE |
-                                                        G_PARAM_CONSTRUCT_ONLY));
+                                                        GIMP_PARAM_READWRITE));
 
   /**
    * GimpDialog:parent:
@@ -234,7 +233,9 @@ gimp_dialog_set_property (GObject      *object,
       break;
 
     case PROP_HELP_ID:
+      g_free (private->help_id);
       private->help_id = g_value_dup_string (value);
+      gimp_help_set_help_data (GTK_WIDGET (object), NULL, private->help_id);
       break;
 
     case PROP_PARENT:

commit b119fc995a00e9e029ded11bbf19389477983659

libgimpwidgets: show help buttons in dialogs again
GimpDialog's "help-id" property didn't have G_PARAM_CONSTRUCT.

Broken since commit a94030052521e481cfe996044947c13b4e5c0276.

libgimpwidgets/gimpdialog.c
@@ -131,7 +131,8 @@ gimp_dialog_class_init (GimpDialogClass *klass)
   g_object_class_install_property (object_class, PROP_HELP_ID,
                                    g_param_spec_string ("help-id", NULL, NULL,
                                                         NULL,
-                                                        GIMP_PARAM_READWRITE));
+                                                        GIMP_PARAM_READWRITE |
+                                                        G_PARAM_CONSTRUCT));
 
   /**
    * GimpDialog:parent: