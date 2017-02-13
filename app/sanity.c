92f102478f071c84d3f423056bfbc108fead1e0c
Michael Natterer <mitch@gimp.org>
2017-02-13 23:50:22 +0100

    configure.ac: require GEGL >= 0.3.12

--- app/sanity.c
+++ app/sanity.c
@@ -491,7 +491,7 @@ sanity_check_gegl (void)
 
 #define GEGL_REQUIRED_MAJOR 0
 #define GEGL_REQUIRED_MINOR 3
-#define GEGL_REQUIRED_MICRO 10
+#define GEGL_REQUIRED_MICRO 12
 
   gegl_get_version (&gegl_major_version,
                     &gegl_minor_version,
