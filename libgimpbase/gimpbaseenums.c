d955b8c5b479b0c8f21c5a430ea1bcccde19ec6d
Michael Natterer <mitch@gimp.org>
2017-05-10 11:45:34 +0200

    libgimpbase: change labels of GimpSelectCriterion values
    so they are consistent with layer mode labels.

--- libgimpbase/gimpbaseenums.c
+++ libgimpbase/gimpbaseenums.c
@@ -1593,13 +1593,13 @@ gimp_select_criterion_get_type (void)
     { GIMP_SELECT_CRITERION_R, NC_("select-criterion", "Red"), NULL },
     { GIMP_SELECT_CRITERION_G, NC_("select-criterion", "Green"), NULL },
     { GIMP_SELECT_CRITERION_B, NC_("select-criterion", "Blue"), NULL },
-    { GIMP_SELECT_CRITERION_H, NC_("select-criterion", "Hue"), NULL },
-    { GIMP_SELECT_CRITERION_S, NC_("select-criterion", "Saturation"), NULL },
-    { GIMP_SELECT_CRITERION_V, NC_("select-criterion", "Value"), NULL },
+    { GIMP_SELECT_CRITERION_H, NC_("select-criterion", "Hue (HSV)"), NULL },
+    { GIMP_SELECT_CRITERION_S, NC_("select-criterion", "Saturation (HSV)"), NULL },
+    { GIMP_SELECT_CRITERION_V, NC_("select-criterion", "Value (HSV)"), NULL },
     { GIMP_SELECT_CRITERION_A, NC_("select-criterion", "Alpha"), NULL },
-    { GIMP_SELECT_CRITERION_LCH_L, NC_("select-criterion", "LCH Lightness"), NULL },
-    { GIMP_SELECT_CRITERION_LCH_C, NC_("select-criterion", "LCH Chroma"), NULL },
-    { GIMP_SELECT_CRITERION_LCH_H, NC_("select-criterion", "LCH Hue"), NULL },
+    { GIMP_SELECT_CRITERION_LCH_L, NC_("select-criterion", "Lightness (LCH)"), NULL },
+    { GIMP_SELECT_CRITERION_LCH_C, NC_("select-criterion", "Chroma (LCH)"), NULL },
+    { GIMP_SELECT_CRITERION_LCH_H, NC_("select-criterion", "Hue (LCH)"), NULL },
     { 0, NULL, NULL }
   };
 
--- libgimpbase/gimpbaseenums.h
+++ libgimpbase/gimpbaseenums.h
@@ -736,17 +736,17 @@ GType gimp_select_criterion_get_type (void) G_GNUC_CONST;
 
 typedef enum
 {
-  GIMP_SELECT_CRITERION_COMPOSITE,  /*< desc="Composite"     >*/
-  GIMP_SELECT_CRITERION_R,          /*< desc="Red"           >*/
-  GIMP_SELECT_CRITERION_G,          /*< desc="Green"         >*/
-  GIMP_SELECT_CRITERION_B,          /*< desc="Blue"          >*/
-  GIMP_SELECT_CRITERION_H,          /*< desc="Hue"           >*/
-  GIMP_SELECT_CRITERION_S,          /*< desc="Saturation"    >*/
-  GIMP_SELECT_CRITERION_V,          /*< desc="Value"         >*/
-  GIMP_SELECT_CRITERION_A,          /*< desc="Alpha"         >*/
-  GIMP_SELECT_CRITERION_LCH_L,      /*< desc="LCH Lightness" >*/
-  GIMP_SELECT_CRITERION_LCH_C,      /*< desc="LCH Chroma"    >*/
-  GIMP_SELECT_CRITERION_LCH_H,      /*< desc="LCH Hue"       >*/
+  GIMP_SELECT_CRITERION_COMPOSITE,  /*< desc="Composite"        >*/
+  GIMP_SELECT_CRITERION_R,          /*< desc="Red"              >*/
+  GIMP_SELECT_CRITERION_G,          /*< desc="Green"            >*/
+  GIMP_SELECT_CRITERION_B,          /*< desc="Blue"             >*/
+  GIMP_SELECT_CRITERION_H,          /*< desc="Hue (HSV)"        >*/
+  GIMP_SELECT_CRITERION_S,          /*< desc="Saturation (HSV)" >*/
+  GIMP_SELECT_CRITERION_V,          /*< desc="Value (HSV)"      >*/
+  GIMP_SELECT_CRITERION_A,          /*< desc="Alpha"            >*/
+  GIMP_SELECT_CRITERION_LCH_L,      /*< desc="Lightness (LCH)"  >*/
+  GIMP_SELECT_CRITERION_LCH_C,      /*< desc="Chroma (LCH)"     >*/
+  GIMP_SELECT_CRITERION_LCH_H,      /*< desc="Hue (LCH)"        >*/
 } GimpSelectCriterion;
 
 
