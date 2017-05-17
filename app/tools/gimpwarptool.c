5be79bc8c461899f77409a6eec28b1e2f0f9ac0d
Ell <ell_se@yahoo.com>
2017-05-17 12:02:40 -0400

    app: in warp tool, compress motion events
    We stroke the last-reported coords in a timeout proc, so there's
    no real need for exact motion notification.

--- app/tools/gimpwarptool.c
+++ app/tools/gimpwarptool.c
@@ -177,7 +177,6 @@ gimp_warp_tool_init (GimpWarpTool *self)
   GimpTool *tool = GIMP_TOOL (self);
 
   gimp_tool_control_set_preserve        (tool->control, FALSE);
-  gimp_tool_control_set_motion_mode     (tool->control, GIMP_MOTION_MODE_EXACT);
   gimp_tool_control_set_dirty_mask      (tool->control,
                                          GIMP_DIRTY_IMAGE           |
                                          GIMP_DIRTY_DRAWABLE        |
