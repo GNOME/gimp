GNOME/gimp

commit 1ce7fc6c77ea7980ee8e468db7260ffcd15e24d2

Bug 747407 - Update use of gimp-desaturate PDB.
gimp-desaturate and gimp-brightness-contrast usage
replaced with gimp-drawable-* based calls.

gimp-drawable-desaturate calls updated to use
desaturate mode (DESATURATE-LIGHTNESS) where it
was not specified.

gimp-drawble-brightness-contrast values scaled from
previous -127/+127 range to current -0.5/0.5 range.

plug-ins/script-fu/scripts/circuit.scm
@@ -3,16 +3,9 @@
 ;
 ; Circuit board effect
 ; Copyright (c) 1997 Adrian Likins
-; aklikins@eos.ncsu.ed
 ;
-;  Genrates what looks a little like the back of an old circuit board.
-;  Looks even better when gradmapped with a suitable gradient.
-;
-; This script doesn't handle or color combos well. ie, black/black
-;  doesn't work..
-;  The effect seems to work best on odd shaped selections because of some
-; limitations in the maze codes selection handling ablity
-;
+; Generates what looks a little like the back of an old circuit board.
+; Looks even better when gradient-mapp'ed with a suitable gradient.
 ;
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
@@ -107,7 +100,7 @@
     (plug-in-oilify RUN-NONINTERACTIVE image active-layer mask-size 0)
     (plug-in-edge RUN-NONINTERACTIVE image active-layer 2 1 0)
     (if (= type RGBA-IMAGE)
-      (gimp-desaturate active-layer))
+      (gimp-desaturate-drawable active-layer DESATURATE-LIGHTNESS))
 
     (if (and
          (= remove-bg TRUE)

plug-ins/script-fu/scripts/burn-in-anim.scm
@@ -137,7 +137,7 @@
                   (begin
                     ;--- add some brightness to whole text
                     (if (= fadeout TRUE)
-                        (gimp-brightness-contrast bl-layer 100 0)
+                        (gimp-drawable-brightness-contrast bl-layer 0.787 0)
                     )
 
           ;--- blend glow color inside the letters

plug-ins/script-fu/scripts/fuzzyborder.scm
@@ -110,8 +110,8 @@
                                    (car (gimp-layer-copy theLayer FALSE)) 0 -1)
           (gimp-layer-scale theLayer
                             (- theWidth inSize) (- theHeight inSize) TRUE)
-          (gimp-desaturate theLayer)
-          (gimp-brightness-contrast theLayer 127 127)
+          (gimp-drawable-desaturate theLayer DESATURATE-LIGHTNESS)
+          (gimp-drawable-brightness-contrast theLayer 0.5 0.5)
           (gimp-invert theLayer)
           (gimp-layer-resize theLayer
                              theWidth

plug-ins/script-fu/scripts/old-photo.scm
@@ -48,9 +48,9 @@
   (set! theLayer (car (gimp-image-flatten theImage)))
 
   (if (= inSepia TRUE)
-      (begin (gimp-desaturate theLayer)
-             (gimp-brightness-contrast theLayer -20 -40)
-             (gimp-color-balance theLayer 0 TRUE 30 0 -30)
+      (begin (gimp-drawable-desaturate theLayer DESATURATE-LIGHTNESS)
+             (gimp-drawable-brightness-contrast theLayer -0.078125 -0.15625)
+             (gimp-drawable-color-balance theLayer TRANSFER-SHADOWS TRUE 30 0 -30)
       )
   )
   (set! theWidth (car (gimp-image-width theImage)))

commit 8412192f859d565ed90088e473185134da6ff72d

Bug 783386 - Circuit filter broken
Fix an error in the circuit filter introduced in
1ce7fc6c77ea7980ee8e468db7260ffcd15e24d2.

It's gimp-drawable-desaturate not gimp-desaturate-drawable.

plug-ins/script-fu/scripts/circuit.scm
@@ -100,7 +100,7 @@
     (plug-in-oilify RUN-NONINTERACTIVE image active-layer mask-size 0)
     (plug-in-edge RUN-NONINTERACTIVE image active-layer 2 1 0)
     (if (= type RGBA-IMAGE)
-      (gimp-desaturate-drawable active-layer DESATURATE-LIGHTNESS))
+      (gimp-drawable-desaturate active-layer DESATURATE-LIGHTNESS))
 
     (if (and
          (= remove-bg TRUE)