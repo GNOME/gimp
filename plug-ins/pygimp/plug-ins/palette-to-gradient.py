2265701268ce4aea6e7ea9cbe91d40671c3ef47d
Félix Piédallu <felix@piedallu.me>
2018-01-10 13:38:36 +0100

    Fix Python files: * Prefer python2 to python that may point on python3 on modern installs. * Fix indent/spaces consistency.

--- plug-ins/pygimp/plug-ins/palette-to-gradient.py
+++ plug-ins/pygimp/plug-ins/palette-to-gradient.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
