5e3d4cf1315b99e446aebaf49979aae8a9a9802f
Jehan <jehan@girinstud.io>
2018-01-11 00:44:46 +0100

    libgimp: update gimp_image_metadata_save_prepare() description.
    The returned flags should not be called "suggested" flags anymore.
    Having metadata available in the work image does not mean we want them
    exported absolutely, which can be a security risk, especially for the
    metadata which are there from the imported image.

--- libgimp/gimpimagemetadata.c
+++ libgimp/gimpimagemetadata.c
@@ -202,16 +202,19 @@ gimp_image_metadata_load_finish (gint32                 image_ID,
  * gimp_image_metadata_save_prepare:
  * @image_ID:        The image
  * @mime_type:       The saved file's mime-type
- * @suggested_flags: Suggested default values for the @flags passed to
+ * @available_flags: Value of @flags which would save all available
+ *                   metadata in @image_ID when passed to
  *                   gimp_image_metadata_save_finish()
  *
  * Gets the image metadata for saving it using
  * gimp_image_metadata_save_finish().
  *
- * The @suggested_flags are determined from what kind of metadata
+ * The @available_flags are determined from what kind of metadata
  * (Exif, XMP, ...) is actually present in the image. The suggested
  * value for GIMP_METADATA_SAVE_THUMBNAIL is determined by whether
  * there was a thumbnail in the previously imported image.
+ * It is up to the calling application to update @available_flags in
+ * order to remove any unwanted metadata before exporting.
  *
  * Returns: The image's metadata, prepared for saving.
  *
