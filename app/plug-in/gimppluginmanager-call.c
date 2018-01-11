68b12a380a2624d37e7d1e5ef5674536a77dc5a8
Jehan <jehan@girinstud.io>
2018-01-11 05:14:07 +0100

    app, libgimp, libgimpbase: new gimp_export_(exif|xmp|iptc)() functions.
    These allows plug-ins to know the preferences regarding metadata.

--- app/plug-in/gimppluginmanager-call.c
+++ app/plug-in/gimppluginmanager-call.c
@@ -206,9 +206,9 @@ gimp_plug_in_manager_call_run (GimpPlugInManager   *manager,
                                  gui_config->show_help_button);
       config.use_cpu_accel    = manager->gimp->use_cpu_accel;
       config.use_opencl       = gegl_config->use_opencl;
-      config.gimp_reserved_6  = 0;
-      config.gimp_reserved_7  = 0;
-      config.gimp_reserved_8  = 0;
+      config.export_exif      = core_config->export_metadata_exif;
+      config.export_xmp       = core_config->export_metadata_xmp;
+      config.export_iptc      = core_config->export_metadata_iptc;
       config.install_cmap     = FALSE;
       config.show_tooltips    = gui_config->show_tooltips;
       config.min_colors       = 144;
