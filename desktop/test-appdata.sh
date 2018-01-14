458e313000802282d24b4145310afc0d1cece4d2
Jehan <jehan@girinstud.io>
2018-01-14 03:40:16 +0100

    Rename gimp.appdata.xml.in to org.gimp.GIMP.appdata.xml.in.in.
    I completely forgot to rename the appstream file according to the new
    ID. While doing so, I also make it a .in.in file, with initial
    processing by the autotools. Indeed I need @GIMP_COMMAND@ to be replaced
    by AC_CONFIG_FILES().
    Finally I fix a badly closed XML tag (which reminds me I should always
    test a commit, even when it's a simple non-C 1-liner change!).

--- desktop/test-appdata.sh
+++ desktop/test-appdata.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-appstream-util validate-strict ${GIMP_TESTING_ABS_TOP_SRCDIR}/desktop/gimp.appdata.xml.in
+appstream-util validate-strict ${GIMP_TESTING_ABS_TOP_SRCDIR}/desktop/org.gimp.GIMP.appdata.xml.in.in
 appstream-util validate-strict ${GIMP_TESTING_ABS_TOP_SRCDIR}/desktop/gimp-data-extras.metainfo.xml.in
