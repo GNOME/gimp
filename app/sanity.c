7d5188cae65d4fdde2b8cebb5446472793fced4f
Øyvind Kolås <pippin@gimp.org>
2018-01-17 16:06:10 +0100

    configure: depend on babl 0.1.40 or newer

--- app/sanity.c
+++ app/sanity.c
@@ -480,7 +480,7 @@ sanity_check_babl (void)
 
 #define BABL_REQUIRED_MAJOR 0
 #define BABL_REQUIRED_MINOR 1
-#define BABL_REQUIRED_MICRO 38
+#define BABL_REQUIRED_MICRO 40
 
   babl_get_version (&babl_major_version,
                     &babl_minor_version,
