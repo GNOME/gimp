7dfe81f13d34135da8cec7465cf5480e429833fd
Ell <ell_se@yahoo.com>
2018-01-28 15:50:21 -0500

    configure.ac: require GEGL >= 0.3.29

--- app/sanity.c
+++ app/sanity.c
@@ -518,7 +518,7 @@ sanity_check_gegl (void)
 
 #define GEGL_REQUIRED_MAJOR 0
 #define GEGL_REQUIRED_MINOR 3
-#define GEGL_REQUIRED_MICRO 28
+#define GEGL_REQUIRED_MICRO 29
 
   gegl_get_version (&gegl_major_version,
                     &gegl_minor_version,
