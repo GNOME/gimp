fc8303dd0a0efab10409303519c5605437dff14e
Félix Piédallu <felix@piedallu.me>
2018-01-30 17:27:43 +0100

    (source modifs) Fix: Rename macros as it conflicts with Mingw headers. * DATADIR -> GIMPDATADIR * SYSCONFDIR -> GIMPSYSCONFDIR * DATADIR -> SYSDATADIR (tools/)

--- libgimpbase/gimputils.c
+++ libgimpbase/gimputils.c
@@ -38,14 +38,8 @@
 #include <glib/gprintf.h>
 
 #if defined(G_OS_WIN32)
-
-/* This is a hack for Windows known directory support.
- * DATADIR (autotools-generated constant) is a type defined in objidl.h
- * so we must #undef it before including shlobj.h in order to avoid a
- * name clash. */
-#undef DATADIR
-#include <windows.h>
-#include <shlobj.h>
+# include <windows.h>
+# include <shlobj.h>
 
 #else /* G_OS_WIN32 */
 
