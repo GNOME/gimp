848b89afc277a7fa31228f9a6fce248e1c756f4c
Jehan <jehan@girinstud.io>
2018-03-22 13:19:44 +0100

    autogen: add support for recent automake 1.16.

--- autogen.sh
+++ autogen.sh
@@ -157,6 +157,9 @@ printf "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
 if ($AUTOMAKE --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=$AUTOMAKE
    ACLOCAL=$ACLOCAL
+elif (automake-1.16 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.16
+   ACLOCAL=aclocal-1.16
 elif (automake-1.15 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.15
    ACLOCAL=aclocal-1.15
