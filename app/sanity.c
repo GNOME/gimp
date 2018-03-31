a3071d26207608263fd07f65a176f0dad10f0342
Michael Natterer <mitch@gimp.org>
2018-03-31 18:39:53 +0200

    configure.ac: require babl >= 0.1.45 and GEGL => 0.3.31
    and set gimp_version to "2.10.0-RC1-git" so we can distinguish it
    from the RC1 tarball.

--- app/sanity.c
+++ app/sanity.c
@@ -480,7 +480,7 @@ sanity_check_babl (void)
 
 #define BABL_REQUIRED_MAJOR 0
 #define BABL_REQUIRED_MINOR 1
-#define BABL_REQUIRED_MICRO 44
+#define BABL_REQUIRED_MICRO 45
 
   babl_get_version (&babl_major_version,
                     &babl_minor_version,
@@ -518,7 +518,7 @@ sanity_check_gegl (void)
 
 #define GEGL_REQUIRED_MAJOR 0
 #define GEGL_REQUIRED_MINOR 3
-#define GEGL_REQUIRED_MICRO 30
+#define GEGL_REQUIRED_MICRO 31
 
   gegl_get_version (&gegl_major_version,
                     &gegl_minor_version,
