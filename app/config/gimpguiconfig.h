cf21dcbe374475ef9f9f19cf491a1be39b9c5dd8
Ville Pätsi <drc@gimp.org>
2018-04-05 18:24:44 +0300

    Rework and rename the default Dark theme

--- app/config/gimpguiconfig.h
+++ app/config/gimpguiconfig.h
@@ -24,7 +24,7 @@
 #include "config/gimpdisplayconfig.h"
 
 
-#define GIMP_CONFIG_DEFAULT_THEME          "03-Dark"
+#define GIMP_CONFIG_DEFAULT_THEME          "Dark"
 #define GIMP_CONFIG_DEFAULT_ICON_THEME     "Symbolic"
 
 
