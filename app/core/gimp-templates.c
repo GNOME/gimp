421c9fc51a25de283cfff4554ac29c7c833f5745
Michael Natterer <mitch@gimp.org>
2018-04-11 11:31:02 +0200

    Bug 795050 - ‘strncpy’ output truncated before terminating nul
    Use memcpy() because we really dont' want to copy the terminating nul,
    but we don't want a warning either.

--- app/core/gimp-templates.c
+++ app/core/gimp-templates.c
@@ -140,7 +140,7 @@ gimp_templates_migrate_get_child_by_name (GimpContainer *container,
 
           if (dpi)
             {
-              strncpy (dpi, "ppi", 4);
+              memcpy (dpi, "ppi", 3);
 
               g_print ("replaced: %s\n", str1);
 
