721ce91d75e574657a8ba57d6a37e98783e36059
Michael Natterer <mitch@gimp.org>
2018-04-13 23:43:27 +0200

    Bug 795230 - Rename Blend tool and provide PDB compatibility
    Redo the part of commits 4f2e078ccb7be72c3af4641781f23b7d0c27f25e
    and 5f6dfc7617cb8d50173707ad748fd172107e06f6, reverted by commit
    e30a32d56e123a9e221102cc2707fd697f1d7945.

--- app/core/gimpdrawable-gradient.h
+++ app/core/gimpdrawable-gradient.h
@@ -15,35 +15,36 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef  __GIMP_DRAWABLE_BLEND_H__
-#define  __GIMP_DRAWABLE_BLEND_H__
+#ifndef  __GIMP_DRAWABLE_GRADIENT_H__
+#define  __GIMP_DRAWABLE_GRADIENT_H__
 
 
-void   gimp_drawable_blend (GimpDrawable       *drawable,
-                            GimpContext        *context,
-                            GimpGradient       *gradient,
-                            GeglDistanceMetric  metric,
-                            GimpLayerMode       paint_mode,
-                            GimpGradientType    gradient_type,
-                            gdouble             opacity,
-                            gdouble             offset,
-                            GimpRepeatMode      repeat,
-                            gboolean            reverse,
-                            gboolean            supersample,
-                            gint                max_depth,
-                            gdouble             threshold,
-                            gboolean            dither,
-                            gdouble             startx,
-                            gdouble             starty,
-                            gdouble             endx,
-                            gdouble             endy,
-                            GimpProgress       *progress);
+void   gimp_drawable_gradient (GimpDrawable                *drawable,
+                               GimpContext                 *context,
+                               GimpGradient                *gradient,
+                               GeglDistanceMetric           metric,
+                               GimpLayerMode                paint_mode,
+                               GimpGradientType             gradient_type,
+                               gdouble                      opacity,
+                               gdouble                      offset,
+                               GimpRepeatMode               repeat,
+                               gboolean                     reverse,
+                               GimpGradientBlendColorSpace  blend_color_space,
+                               gboolean                     supersample,
+                               gint                         max_depth,
+                               gdouble                      threshold,
+                               gboolean                     dither,
+                               gdouble                      startx,
+                               gdouble                      starty,
+                               gdouble                      endx,
+                               gdouble                      endy,
+                               GimpProgress                *progress);
 
 GeglBuffer *
-gimp_drawable_blend_shapeburst_distmap (GimpDrawable        *drawable,
-                                        GeglDistanceMetric   metric,
-                                        const GeglRectangle *region,
-                                        GimpProgress        *progress);
+gimp_drawable_gradient_shapeburst_distmap (GimpDrawable        *drawable,
+                                           GeglDistanceMetric   metric,
+                                           const GeglRectangle *region,
+                                           GimpProgress        *progress);
 
 
-#endif /* __GIMP_DRAWABLE_BLEND_H__ */
+#endif /* __GIMP_DRAWABLE_GRADIENT_H__ */
