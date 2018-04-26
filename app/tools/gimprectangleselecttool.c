a44e1500fa77dd38ca85b774c6a4a0181654d4ba
Michael Natterer <mitch@gimp.org>
2018-04-26 13:00:18 +0200

    app: fix rectangle select tool cursor after committing/halting
    gimp_rectangle_select_tool_cursor_update(): always set a cursor and
    cursor modifier even if no GimpToolRectangle widget exists, so we are
    not stuck with the last set cursor after committing or halting the
    tool.

--- app/tools/gimprectangleselecttool.c
+++ app/tools/gimprectangleselecttool.c
@@ -465,10 +465,10 @@ gimp_rectangle_select_tool_cursor_update (GimpTool         *tool,
     {
       gimp_tool_widget_get_cursor (private->widget, coords, state,
                                    &cursor, NULL, &modifier);
-
-      gimp_tool_control_set_cursor          (tool->control, cursor);
-      gimp_tool_control_set_cursor_modifier (tool->control, modifier);
     }
+
+  gimp_tool_control_set_cursor          (tool->control, cursor);
+  gimp_tool_control_set_cursor_modifier (tool->control, modifier);
 
   /* override the previous if shift or ctrl are down */
   if (state & (gimp_get_extend_selection_mask () |
