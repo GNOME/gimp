f9ae3ae014b72a2edfa59b3544ed3a819bfd26e0
Ell <ell_se@yahoo.com>
2018-05-25 20:21:24 -0400

    Issue #1490 - Crash when flattening an XCF file
    The default stack size for new threads on MacOS is 512 KiB, making
    our 512 KiB limit for stack-allocated buffers in
    gimp_operation_layer_mode_real_process() too high.  Lower it to
    256 KiB.
    
    (cherry picked from commit 367399e5c018b379cf69a234c5027659180ee676)

--- app/operations/layer-modes/gimpoperationlayermode.c
+++ app/operations/layer-modes/gimpoperationlayermode.c
@@ -37,7 +37,7 @@
 /* the maximum number of samples to process in one go.  used to limit
  * the size of the buffers we allocate on the stack.
  */
-#define GIMP_COMPOSITE_BLEND_MAX_SAMPLES ((1 << 19) /* 0.5 MiB */  /      \
+#define GIMP_COMPOSITE_BLEND_MAX_SAMPLES ((1 << 18) /* 256 KiB */  /      \
                                           16 /* bytes per pixel */ /      \
                                           2  /* max number of buffers */)
 
