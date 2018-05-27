ddc9daceca0c85b801165ff624f165ebecc751bc
Ell <ell_se@yahoo.com>
2018-05-27 13:02:25 -0400

    app: add "independent" parameter to gimp_parallel_run_async()
    Add a boolean "independent" parameter to gimp_parallel_run_async().
    When FALSE, the passed function is run in the shared async thread
    pool; when TRUE, the passed function is run in an independent
    thread.
    
    Generally, async operations should run in the async pool, however,
    it might be desirable to run long-standing operations, especially
    ones that can't be canceled, in independent threads.  This avoids
    stalling quicker operations, and shutdown.
    
    Adapt the rest of the code for the change.  In particular,
    initialize the font cache in an independent thread.
    
    (cherry picked from commit ad8add6808da4d88b91a98813ebce053dd0a1c34)

--- app/core/gimphistogram.c
+++ app/core/gimphistogram.c
@@ -348,6 +348,7 @@ gimp_histogram_calculate_async (GimpHistogram       *histogram,
     }
 
   histogram->priv->calculate_async = gimp_parallel_run_async (
+    FALSE,
     (GimpParallelRunAsyncFunc) gimp_histogram_calculate_internal,
     context);
 
