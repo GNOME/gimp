081867de1ece79216704b7b367fe60a0454cace7
Ell <ell_se@yahoo.com>
2018-05-29 16:32:07 -0400

    app: fix gui_wait() cleanup
    (cherry picked from commit 5a66d6cc74bf25911c09d89e0b70782e5815c85f)

--- app/gui/gui-vtable.c
+++ app/gui/gui-vtable.c
@@ -471,9 +471,9 @@ gui_wait_input_async (GimpAsync  *async,
 }
 
 static gboolean
-gui_wait (Gimp           *gimp,
-          GimpWaitable   *waitable,
-          const gchar    *message)
+gui_wait (Gimp         *gimp,
+          GimpWaitable *waitable,
+          const gchar  *message)
 {
   GimpProcedure  *procedure;
   GimpValueArray *args;
@@ -559,7 +559,12 @@ gui_wait (Gimp           *gimp,
   /* signal completion to the plug-in */
   close (output_pipe[1]);
 
-  g_clear_pointer (&input_async, gimp_waitable_wait);
+  if (input_async)
+    {
+      gimp_waitable_wait (GIMP_WAITABLE (input_async));
+
+      g_object_unref (input_async);
+    }
 
   close (input_pipe[0]);
 
