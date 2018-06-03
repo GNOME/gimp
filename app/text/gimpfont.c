6b5fd2794327a5b3361e5e49f3ab831ee12d15c8
Michael Natterer <mitch@gimp.org>
2018-06-03 23:13:55 +0200

    app: call the standard font "Standard" not "Sans-serif"
    Sans-serif is also an actual font from the list and the standard font
    is being used when there is really no font at all.

--- app/text/gimpfont.c
+++ app/text/gimpfont.c
@@ -334,7 +334,7 @@ gimp_font_get_standard (void)
   if (! standard_font)
     {
       standard_font = g_object_new (GIMP_TYPE_FONT,
-                                    "name", "Sans-serif",
+                                    "name", "Standard",
                                     NULL);
 
       gimp_data_clean (standard_font);
