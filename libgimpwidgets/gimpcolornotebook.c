93025adf02102854f6157050277ec996e539b64e
Michael Natterer <mitch@gimp.org>
2018-06-08 19:44:29 +0200

    Issue #1435 - Add tooltips to GimpColorNotebook's tabs
    (cherry picked from commit 8d6f023b41190375664fcfe79115e9d44200366d)

--- libgimpwidgets/gimpcolornotebook.c
+++ libgimpwidgets/gimpcolornotebook.c
@@ -33,6 +33,7 @@
 
 #include "gimpcolornotebook.h"
 #include "gimpcolorscales.h"
+#include "gimphelpui.h"
 #include "gimpwidgetsmarshal.h"
 #include "gimp3migration.h"
 
@@ -216,6 +217,7 @@ gimp_color_notebook_style_set (GtkWidget *widget,
 
       image = gtk_image_new_from_icon_name (selector_class->icon_name,
                                             icon_size);
+      gimp_help_set_help_data (image, gettext (selector_class->name), NULL);
 
       gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook->notebook),
                                   GTK_WIDGET (list->data),
@@ -460,6 +462,7 @@ gimp_color_notebook_add_page (GimpColorNotebook *notebook,
 
   image = gtk_image_new_from_icon_name (selector_class->icon_name,
                                         DEFAULT_TAB_ICON_SIZE);
+  gimp_help_set_help_data (image, gettext (selector_class->name), NULL);
 
   gtk_notebook_append_page_menu (GTK_NOTEBOOK (notebook->notebook),
                                  page, image, menu_widget);
