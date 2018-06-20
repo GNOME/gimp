7715dbba5f89a2b3d2e7676c66df6d9da9a3b772
Ell <ell_se@yahoo.com>
2018-06-20 14:34:10 -0400

    app: add gimp_ascii_strtoi() and gimp_ascii_strotod()
    ... which are similar to g_ascii_strtoll() (except that
    gimp_ascii_strtoi returns a gint, and not a gint64), and
    g_ascii_strtod(), however, they make error checking simpler, by
    returning a boolean value, indicating whether the conversion was
    successful (taking both conversion and range errors into account),
    and return the actual value through a pointer.
    
    (cherry picked from commit 3301c0616328fad7752aea4f26eed7b25bb66116)

--- app/core/gimp-utils.h
+++ app/core/gimp-utils.h
@@ -88,6 +88,14 @@ gchar      * gimp_data_input_stream_read_line_always (GDataInputStream  *stream,
                                                       GCancellable      *cancellable,
                                                       GError           **error);
 
+gboolean     gimp_ascii_strtoi                     (const gchar     *nptr,
+                                                    gchar          **endptr,
+                                                    gint             base,
+                                                    gint            *result);
+gboolean     gimp_ascii_strtod                     (const gchar     *nptr,
+                                                    gchar          **endptr,
+                                                    gdouble         *result);
+
 GimpImage  * gimp_create_image_from_buffer         (Gimp            *gimp,
                                                     GeglBuffer      *buffer,
                                                     const gchar     *image_name);
