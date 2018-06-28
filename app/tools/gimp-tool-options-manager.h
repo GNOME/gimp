d03b31780bdf29af9cfef3b03d15f616a8c191ff
Michael Natterer <mitch@gimp.org>
2018-06-28 21:41:32 +0200

    app: add gimp-tool-options-manager.[ch]
    The way we currently manage tool options, and particularly copy things
    around for "global_brush", "global_pattern" etc. sucks, is spread
    across files, happens only on tool change, is thus buggy and leads to
    all sorts of small inconsistencies.
    
    This new manager will replace all of that stuff, and it does it in one
    place, and will keep the user context, the global paint options, and
    all tool options connected permanently, and only connect/disconnect
    things when settings change, so everything is always in a consistent
    state.
    
    Pushed for review only, nothing is used yet.
    
    (cherry picked from commit ef952f29262587a1a643dbbce407e47913b7f789)

---
+++ app/tools/gimp-tool-options-manager.h
@@ -0,0 +1,29 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * gimp-tool-options-manager.h
+ * Copyright (C) 2018 Michael Natterer <mitch@gimp.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GIMP_TOOL_OPTIONS_MANAGER_H__
+#define __GIMP_TOOL_OPTIONS_MANAGER_H__
+
+
+void   gimp_tool_options_manager_init (Gimp *gimp);
+void   gimp_tool_options_manager_exit (Gimp *gimp);
+
+
+#endif  /*  __GIMP_TOOL_OPTIONS_MANAGER_H__  */
