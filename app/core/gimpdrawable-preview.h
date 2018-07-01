f993de75fe0c03574cfd1fd04af3d3956ea4c439
Ell <ell_se@yahoo.com>
2018-07-01 10:37:11 -0400

    app: add gimp_drawable_get_sub_preview_async()
    ... which is an asynchronous version of
    gimp_drawable_get_sub_preview().
    
    We currently support async preview generation for drawables whose
    buffer isn't backed by a GimpTileHandlerValidate tile handler
    (i.e., anything other than group layers), since preview generation
    fir such drawables may involve processing the corresponding graph,
    which isn't thread-safe.
    
    When the GIMP_NO_ASYNC_DRAWABLE_PREVIEWS environment variable is
    defined, all drawable previews are synchronously generated.
    
    (cherry picked from commit d79e3fbd6f80721f4297db4a198a702e627e7e86)

--- app/core/gimpdrawable-preview.h
+++ app/core/gimpdrawable-preview.h
@@ -22,34 +22,42 @@
 /*
  *  virtual functions of GimpDrawable -- don't call directly
  */
-GimpTempBuf * gimp_drawable_get_new_preview    (GimpViewable *viewable,
-                                                GimpContext  *context,
-                                                gint          width,
-                                                gint          height);
-GdkPixbuf   * gimp_drawable_get_new_pixbuf     (GimpViewable *viewable,
-                                                GimpContext  *context,
-                                                gint          width,
-                                                gint          height);
+GimpTempBuf * gimp_drawable_get_new_preview       (GimpViewable *viewable,
+                                                   GimpContext  *context,
+                                                   gint          width,
+                                                   gint          height);
+GdkPixbuf   * gimp_drawable_get_new_pixbuf        (GimpViewable *viewable,
+                                                   GimpContext  *context,
+                                                   gint          width,
+                                                   gint          height);
 
 /*
  *  normal functions (no virtuals)
  */
-const Babl  * gimp_drawable_get_preview_format (GimpDrawable *drawable);
+const Babl  * gimp_drawable_get_preview_format    (GimpDrawable *drawable);
 
-GimpTempBuf * gimp_drawable_get_sub_preview    (GimpDrawable *drawable,
-                                                gint          src_x,
-                                                gint          src_y,
-                                                gint          src_width,
-                                                gint          src_height,
-                                                gint          dest_width,
-                                                gint          dest_height);
-GdkPixbuf   * gimp_drawable_get_sub_pixbuf     (GimpDrawable *drawable,
-                                                gint          src_x,
-                                                gint          src_y,
-                                                gint          src_width,
-                                                gint          src_height,
-                                                gint          dest_width,
-                                                gint          dest_height);
+GimpTempBuf * gimp_drawable_get_sub_preview       (GimpDrawable *drawable,
+                                                   gint          src_x,
+                                                   gint          src_y,
+                                                   gint          src_width,
+                                                   gint          src_height,
+                                                   gint          dest_width,
+                                                   gint          dest_height);
+GdkPixbuf   * gimp_drawable_get_sub_pixbuf        (GimpDrawable *drawable,
+                                                   gint          src_x,
+                                                   gint          src_y,
+                                                   gint          src_width,
+                                                   gint          src_height,
+                                                   gint          dest_width,
+                                                   gint          dest_height);
+
+GimpAsync   * gimp_drawable_get_sub_preview_async (GimpDrawable *drawable,
+                                                   gint          src_x,
+                                                   gint          src_y,
+                                                   gint          src_width,
+                                                   gint          src_height,
+                                                   gint          dest_width,
+                                                   gint          dest_height);
 
 
 #endif /* __GIMP_DRAWABLE__PREVIEW_H__ */
