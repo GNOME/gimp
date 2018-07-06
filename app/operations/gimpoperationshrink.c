739d72c562ff3c39c97b748e3aa9113b80e03bcb
Øyvind Kolås <pippin@gimp.org>
2018-07-06 14:51:45 +0200

    app: gimp:shrink propagate bable space

--- app/operations/gimpoperationshrink.c
+++ app/operations/gimpoperationshrink.c
@@ -184,8 +184,9 @@ gimp_operation_shrink_set_property (GObject      *object,
 static void
 gimp_operation_shrink_prepare (GeglOperation *operation)
 {
-  gegl_operation_set_format (operation, "input",  babl_format ("Y float"));
-  gegl_operation_set_format (operation, "output", babl_format ("Y float"));
+  const Babl *space = gegl_operation_get_source_space (operation, "input");
+  gegl_operation_set_format (operation, "input",  babl_format_with_space ("Y float", space));
+  gegl_operation_set_format (operation, "output", babl_format_with_space ("Y float", space));
 }
 
 static GeglRectangle
