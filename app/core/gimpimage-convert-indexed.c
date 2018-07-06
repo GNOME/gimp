49ca383fa4571fab35d370d471d18694836e0e27
Ell <ell_se@yahoo.com>
2018-07-06 13:43:26 -0400

    app, pdb: use gimp_babl_is_valid() in more places
    Use gimp_babl_is_valid(), added in the previous commit, to validate
    image-type/precision combinations in various functions.

--- app/core/gimpimage-convert-indexed.c
+++ app/core/gimpimage-convert-indexed.c
@@ -143,6 +143,7 @@
 
 #include "core-types.h"
 
+#include "gegl/gimp-babl.h"
 #include "gegl/gimp-gegl-utils.h"
 
 #include "gimp.h"
@@ -767,6 +768,9 @@ gimp_image_convert_indexed (GimpImage               *image,
 
   g_return_val_if_fail (GIMP_IS_IMAGE (image), FALSE);
   g_return_val_if_fail (gimp_image_get_base_type (image) != GIMP_INDEXED, FALSE);
+  g_return_val_if_fail (gimp_babl_is_valid (GIMP_INDEXED,
+                                            gimp_image_get_precision (image)),
+                        FALSE);
   g_return_val_if_fail (custom_palette == NULL ||
                         GIMP_IS_PALETTE (custom_palette), FALSE);
   g_return_val_if_fail (custom_palette == NULL ||
--- app/core/gimpimage-convert-type.c
+++ app/core/gimpimage-convert-type.c
@@ -58,6 +58,9 @@ gimp_image_convert_type (GimpImage          *image,
   g_return_val_if_fail (GIMP_IS_IMAGE (image), FALSE);
   g_return_val_if_fail (new_type != gimp_image_get_base_type (image), FALSE);
   g_return_val_if_fail (new_type != GIMP_INDEXED, FALSE);
+  g_return_val_if_fail (gimp_babl_is_valid (new_type,
+                                            gimp_image_get_precision (image)),
+                        FALSE);
   g_return_val_if_fail (dest_profile == NULL || GIMP_IS_COLOR_PROFILE (dest_profile),
                         FALSE);
   g_return_val_if_fail (progress == NULL || GIMP_IS_PROGRESS (progress), FALSE);
