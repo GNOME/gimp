379d65e056186b3ebbc41a42213077644ba19685
Ell <ell_se@yahoo.com>
2018-08-03 13:40:07 -0400

    app: stop idle projection rendering when flushing group layers
    In gimp_group_layer_flush(), stop any idle rendering, initiated
    when a new buffer is allocated, before flushing the group's
    pickable.  Otherwise, the idle rendering is finished synchronously,
    which unnecessarily introduces a noticeable lag.
    
    (cherry picked from commit a4957c7c762e3fa66edea09a6e0c25a3b8035124)

--- app/core/gimpgrouplayer.c
+++ app/core/gimpgrouplayer.c
@@ -1775,6 +1775,14 @@ gimp_group_layer_flush (GimpGroupLayer *group)
     }
   else
     {
+      /* make sure we have a buffer, and stop any idle rendering, which is
+       * initiated when a new buffer is allocated.  the call to
+       * gimp_pickable_flush() below causes any pending idle rendering to
+       * finish synchronously, so this needs to happen before.
+       */
+      gimp_pickable_get_buffer (GIMP_PICKABLE (private->projection));
+      gimp_projection_stop_rendering (private->projection);
+
       /*  flush the pickable not the projectable because flushing the
        *  pickable will finish all invalidation on the projection so it
        *  can be used as source (note that it will still be constructed
