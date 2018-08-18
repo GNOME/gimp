7ed34fd9207077ec5c9c09adfc4498979c3a479d
Ell <ell_se@yahoo.com>
2018-08-17 22:53:15 -0400

    app: show horizontal scrollbar in dashboard when necessary
    (cherry picked from commit 8313a40fb583c6b580707fc9edf0fdfa45577192)

--- app/widgets/gimpdashboard.c
+++ app/widgets/gimpdashboard.c
@@ -869,7 +869,7 @@ gimp_dashboard_init (GimpDashboard *dashboard)
   /* scrolled window */
   scrolled_window = gtk_scrolled_window_new (NULL, NULL);
   gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
-                                  GTK_POLICY_NEVER,
+                                  GTK_POLICY_AUTOMATIC,
                                   GTK_POLICY_AUTOMATIC);
   gtk_container_add (GTK_CONTAINER (box), scrolled_window);
   gtk_widget_show (scrolled_window);
