64b00b5c7fdf80c0fbbbd72beba15bc1b34d5543
Jehan <jehan@girinstud.io>
2018-09-16 19:15:30 +0200

    plug-ins: add the "msf1" brand for HEIF files.
    After Dirk Farin had another look in the specs, it turns out that "mif1"
    is actually allowed as major brand for HEIF. Also adding "msf1" which is
    the equivalent for image sequences.

--- plug-ins/common/file-heif.c
+++ plug-ins/common/file-heif.c
@@ -121,9 +121,7 @@ query (void)
   gimp_register_file_handler_mime (LOAD_PROC, "image/heif");
   gimp_register_file_handler_uri (LOAD_PROC);
   /* HEIF is an ISOBMFF format whose "brand" (the value after "ftyp")
-   * can be of various values. I added the "mif1" brand as I saw some
-   * HEIF files with this value, and it loaded fine (though it may not
-   * be valid theoretically, according to libheif developers).
+   * can be of various values.
    * See also: https://gitlab.gnome.org/GNOME/gimp/issues/2209
    */
   gimp_register_magic_load_handler (LOAD_PROC,
@@ -132,7 +130,8 @@ query (void)
                                     "4,string,ftypheic,4,string,ftypheix,"
                                     "4,string,ftyphevc,4,string,ftypheim,"
                                     "4,string,ftypheis,4,string,ftyphevm,"
-                                    "4,string,ftyphevs,4,string,ftypmif1");
+                                    "4,string,ftyphevs,4,string,ftypmif1,"
+                                    "4,string,ftypmsf1");
 
   gimp_install_procedure (SAVE_PROC,
                           _("Exports HEIF images"),
