9bdaec3a498305c0186df22b436b3368b940aa2a
Ell <ell_se@yahoo.com>
2018-12-10 08:00:32 -0500

    app: add gimp_tools_blink_lock_box()
    Add gimp_tools_blink_lock_box() utility function, in a new
    gimptools-utils.c file, which takes a GimpItem, and blinks the
    GimpItemTreeView lock-box of the corresponding dockable.  This can
    be used to hint that the item's lock toggles are preventing it from
    being edited.

---
+++ app/tools/gimptools-utils.h
@@ -0,0 +1,26 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995-2001 Spencer Kimball, Peter Mattis and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GIMP_TOOLS_UTILS_H__
+#define __GIMP_TOOLS_UTILS_H__
+
+
+void   gimp_tools_blink_lock_box (Gimp     *gimp,
+                                  GimpItem *item);
+
+
+#endif  /* __GIMP_TOOLS_UTILS_H__ */
