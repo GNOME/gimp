ace9b12205b5a5f418a62b916a3ebace236b2a37
Ell <ell_se@yahoo.com>
2018-12-10 07:51:39 -0500

    app: in the paint tools, blink mode box when the current mode is invalid
    In the paint tools, when the current paint mode is invalid, i.e.,
    when it requires an alpha channel, but the the current drawable has
    no alpha channel, or its alpha channel is locked, blink the paint-
    mode box widget in the tool options, in addition to showing an
    error message in the status bar, to hint at the source of the
    error.
    
    (cherry picked from commit 464bf1b0a9ccbc64064a63dec4727371b1109b20)

--- app/tools/gimppaintoptions-gui.c
+++ app/tools/gimppaintoptions-gui.c
@@ -114,6 +114,9 @@ gimp_paint_options_gui (GimpToolOptions *tool_options)
   gtk_box_pack_start (GTK_BOX (vbox), menu, FALSE, FALSE, 0);
   gtk_widget_show (menu);
 
+  g_object_set_data (G_OBJECT (vbox),
+                     "gimp-paint-options-gui-paint-mode-box", menu);
+
   if (tool_type == GIMP_TYPE_ERASER_TOOL     ||
       tool_type == GIMP_TYPE_CONVOLVE_TOOL   ||
       tool_type == GIMP_TYPE_DODGE_BURN_TOOL ||
@@ -284,6 +287,13 @@ gimp_paint_options_gui (GimpToolOptions *tool_options)
     }
 
   return vbox;
+}
+
+GtkWidget *
+gimp_paint_options_gui_get_paint_mode_box (GtkWidget *options_gui)
+{
+  return g_object_get_data (G_OBJECT (options_gui),
+                            "gimp-paint-options-gui-paint-mode-box");
 }
 
 
