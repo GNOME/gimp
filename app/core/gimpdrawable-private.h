95393722ccd70c9741b4b5fea57d5cd175b9cd83
Ell <ell_se@yahoo.com>
2018-12-28 02:19:02 -0500

    Revert "Bug 796090 - (wrong) true-color preview of GEGL filter ops, ..."
    We now perform the conversion of filter output to the drawable
    format as part of the individual filter nodes (see the last few
    commits), so there's no need for another conversion after the
    filter stack.
    
    This reverts commit d6e0ca505440596e81593cf9ec0fac4f0d9e2ba8.

--- app/core/gimpdrawable-private.h
+++ app/core/gimpdrawable-private.h
@@ -28,7 +28,6 @@ struct _GimpDrawablePrivate
   GeglNode         *source_node;
   GeglNode         *buffer_source_node;
   GimpContainer    *filter_stack;
-  GeglNode         *convert_format;
 
   GimpLayer        *floating_selection;
   GimpFilter       *fs_filter;
