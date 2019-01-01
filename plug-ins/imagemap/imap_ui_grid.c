192bc9536c01770734a8c30e191c5ff93160ac83
Ell <ell_se@yahoo.com>
2019-01-01 07:28:36 -0500

    plug-ins: in image-map, fix spin-scale adjustment
    Fix deprecated use of non-zero page size for a spin-button
    GtkAdjustment, and increase the page increment.

--- plug-ins/imagemap/imap_ui_grid.c
+++ plug-ins/imagemap/imap_ui_grid.c
@@ -47,7 +47,7 @@ create_spin_button_in_grid (GtkWidget *grid,
                             gint       min,
                             gint       max)
 {
-   GtkAdjustment *adj = gtk_adjustment_new (value, min, max, 1, 1, 1);
+   GtkAdjustment *adj = gtk_adjustment_new (value, min, max, 1, 10, 0);
    GtkWidget *button = gtk_spin_button_new (adj, 1, 0);
 
    gtk_spin_button_set_numeric (GTK_SPIN_BUTTON (button), TRUE);
