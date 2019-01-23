18bcb415733d0771bff347b58e48b37da75e9690
Jehan <jehan@girinstud.io>
2019-01-23 23:42:17 +0100

    desktop: check that no remaining TODOs are in appdata file.
    Before each release, we prepare now a <release> tag. Unfortunately some
    details are not always well known for sure in advance, in particular the
    date of release (plans are made to be broken!), and also the release
    news URL (since the date is in the URL). So I usually leave them as TODO
    and until now, I never forgot to update them just before release. But
    there is always the risk of forgetting.
    Now the test for the appdata files will also look for remaining "TODO"
    in the file if and only if the micro version is even (which means it's a
    release). So we won't ever forget to update the file as long as we run
    `make check` on the last commit (which we always do obviously).
    
    (cherry picked from commit c0fed5afd6d1bfae9852f6d1694a426cb80d9557)

--- desktop/test-appdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# TODO: use validate-strict when the last errors for a strict validation
-# are fixed.
-appstream-util validate-relax ${GIMP_TESTING_ABS_TOP_BUILDDIR}/desktop/org.gimp.GIMP.appdata.xml && \
-appstream-util validate-relax ${GIMP_TESTING_ABS_TOP_BUILDDIR}/desktop/gimp-data-extras.metainfo.xml
