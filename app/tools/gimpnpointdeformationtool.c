158705e4ef7be28bff83f51df48985b8f4ea6718
Ell <ell_se@yahoo.com>
2019-01-31 04:37:56 -0500

    app: fix cursor precision of various tools
    Fix the cursor precision of the cage-transform, foreground-select,
    n-point deformation, and warp-transform tools.

--- app/tools/gimpnpointdeformationtool.c
+++ app/tools/gimpnpointdeformationtool.c
@@ -188,6 +188,8 @@ gimp_n_point_deformation_tool_init (GimpNPointDeformationTool *npd_tool)
 {
   GimpTool *tool = GIMP_TOOL (npd_tool);
 
+  gimp_tool_control_set_precision            (tool->control,
+                                              GIMP_CURSOR_PRECISION_SUBPIXEL);
   gimp_tool_control_set_tool_cursor          (tool->control,
                                               GIMP_TOOL_CURSOR_PERSPECTIVE);
   gimp_tool_control_set_preserve             (tool->control, FALSE);
