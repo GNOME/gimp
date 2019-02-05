c9f47356699c140c926ea1fa4c5fa9a1b79a3d69
Ell <ell_se@yahoo.com>
2019-02-05 16:58:24 -0500

    libgimp: avoid libgimp tile-cache in the plug-in tile backend
    In GimpTileBackendPlugin, avoid storing read/written tiles in the
    libgimp tile-cache, since caching is already done by GEGL.
    
    (cherry picked from commit 47dcd4b93a82750435fae70004d39eeb069fb0ac)

--- libgimp/gimptile.c
+++ libgimp/gimptile.c
@@ -100,16 +100,25 @@ gimp_tile_ref_zero (GimpTile *tile)
 }
 
 void
-_gimp_tile_ref_noinit (GimpTile *tile)
+_gimp_tile_ref_nocache (GimpTile *tile,
+                        gboolean  init)
 {
   g_return_if_fail (tile != NULL);
 
   tile->ref_count++;
 
   if (tile->ref_count == 1)
-    tile->data = g_new (guchar, tile->ewidth * tile->eheight * tile->bpp);
-
-  gimp_tile_cache_insert (tile);
+    {
+      if (init)
+        {
+          gimp_tile_get (tile);
+          tile->dirty = FALSE;
+        }
+      else
+        {
+          tile->data = g_new (guchar, tile->ewidth * tile->eheight * tile->bpp);
+        }
+    }
 }
 
 void
--- libgimp/gimptile.h
+++ libgimp/gimptile.h
@@ -62,7 +62,8 @@ void    gimp_tile_cache_ntiles (gulong     ntiles);
 
 /*  private function  */
 
-G_GNUC_INTERNAL void _gimp_tile_ref_noinit           (GimpTile     *tile);
+G_GNUC_INTERNAL void _gimp_tile_ref_nocache          (GimpTile     *tile,
+                                                      gboolean      init);
 
 G_GNUC_INTERNAL void _gimp_tile_cache_flush_drawable (GimpDrawable *drawable);
 
