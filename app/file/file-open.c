4774bd3b489c298ceda21974c28e1f1d46a839d0
Jehan <jehan@girinstud.io>
2019-02-11 15:17:36 +0100

    app: s/plug-In/plug-in in a GUI-visible string.
    (cherry picked from commit a6aa14bca28a96b9558d01150f1172bc7e26bf12)

--- app/file/file-open.c
+++ app/file/file-open.c
@@ -265,7 +265,7 @@ file_open_image (Gimp                *gimp,
     {
       if (error && ! *error)
         g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
-                     _("%s plug-In could not open image"),
+                     _("%s plug-in could not open image"),
                      gimp_procedure_get_label (GIMP_PROCEDURE (file_proc)));
     }
 
