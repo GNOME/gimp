410ffc435ea0e04908e277fc46ce5fbbc41a836a
Michael Natterer <mitch@gimp.org>
2019-02-11 18:35:07 +0100

    app: move file-pat-load from the file-pat plug-in to the core

--- app/file-data/file-data.c
+++ app/file-data/file-data.c
@@ -32,6 +32,7 @@
 
 #include "file-data.h"
 #include "file-data-gbr.h"
+#include "file-data-pat.h"
 
 #include "gimp-intl.h"
 
@@ -75,6 +76,68 @@ file_data_init (Gimp *gimp)
                                      "Tim Newsome, Jens Lautenbacher, "
                                      "Sven Neumann, Michael Natterer",
                                      "1995-2019",
+                                     NULL);
+
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_int32 ("dummy-param",
+                                                      "Dummy Param",
+                                                      "Dummy parameter",
+                                                      G_MININT32, G_MAXINT32, 0,
+                                                      GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_string ("uri",
+                                                       "URI",
+                                                       "The URI of the file "
+                                                       "to load",
+                                                       TRUE, FALSE, TRUE,
+                                                       NULL,
+                                                       GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_string ("raw-uri",
+                                                       "Raw URI",
+                                                       "The URI of the file "
+                                                       "to load",
+                                                       TRUE, FALSE, TRUE,
+                                                       NULL,
+                                                       GIMP_PARAM_READWRITE));
+
+  gimp_procedure_add_return_value (procedure,
+                                   gimp_param_spec_image_id ("image",
+                                                             "Image",
+                                                             "Output image",
+                                                             gimp, FALSE,
+                                                             GIMP_PARAM_READWRITE));
+
+  gimp_plug_in_manager_add_procedure (gimp->plug_in_manager, proc);
+  g_object_unref (procedure);
+
+  /*  file-pat-load  */
+  file = g_file_new_for_path ("file-pat-load");
+  procedure = gimp_plug_in_procedure_new (GIMP_PLUGIN, file);
+  g_object_unref (file);
+
+  procedure->proc_type    = GIMP_INTERNAL;
+  procedure->marshal_func = file_pat_load_invoker;
+
+  proc = GIMP_PLUG_IN_PROCEDURE (procedure);
+  proc->menu_label = g_strdup (N_("GIMP pattern"));
+  gimp_plug_in_procedure_set_icon (proc, GIMP_ICON_TYPE_ICON_NAME,
+                                   (const guint8 *) "gimp-pattern",
+                                   strlen ("gimp-pattern") + 1);
+  gimp_plug_in_procedure_set_image_types (proc, NULL);
+  gimp_plug_in_procedure_set_file_proc (proc, "pat", "",
+                                        "20,string,GPAT");
+  gimp_plug_in_procedure_set_mime_types (proc, "image/gimp-x-pat");
+  gimp_plug_in_procedure_set_handles_uri (proc);
+
+  gimp_object_set_static_name (GIMP_OBJECT (procedure), "file-pat-load");
+  gimp_procedure_set_static_strings (procedure,
+                                     "file-pat-load",
+                                     "Loads GIMP patterns",
+                                     "Loads GIMP patterns",
+                                     "Tim Newsome, Michael Natterer",
+                                     "Tim Newsome, Michael Natterer",
+                                     "1997-2019",
                                      NULL);
 
   gimp_procedure_add_argument (procedure,
