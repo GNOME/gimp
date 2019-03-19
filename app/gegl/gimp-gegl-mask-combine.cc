5198d3c32dbd86855131aa7e7811c99df539c34d
Ell <ell_se@yahoo.com>
2019-03-19 16:11:34 -0400

    app: convert gimp-gegl-mask-combine to C++
    ... in preparation for next commits.

--- app/gegl/gimp-gegl-mask-combine.cc
+++ app/gegl/gimp-gegl-mask-combine.cc
@@ -25,6 +25,9 @@
 #include "libgimpbase/gimpbase.h"
 #include "libgimpmath/gimpmath.h"
 
+extern "C"
+{
+
 #include "gimp-gegl-types.h"
 
 #include "gimp-babl.h"
@@ -210,7 +213,7 @@ gimp_gegl_mask_combine_ellipse_rect (GeglBuffer     *mask,
 
   while (gegl_buffer_iterator_next (iter))
     {
-      gfloat *data = iter->items[0].data;
+      gfloat *data = (gfloat *) iter->items[0].data;
       gint    py;
 
       for (py = roi->y;
@@ -443,8 +446,8 @@ gimp_gegl_mask_combine_buffer (GeglBuffer     *mask,
     case GIMP_CHANNEL_OP_REPLACE:
       while (gegl_buffer_iterator_next (iter))
         {
-          gfloat       *mask_data   = iter->items[0].data;
-          const gfloat *add_on_data = iter->items[1].data;
+          gfloat       *mask_data   = (gfloat       *) iter->items[0].data;
+          const gfloat *add_on_data = (const gfloat *) iter->items[1].data;
           gint          count       = iter->length;
 
           while (count--)
@@ -462,8 +465,8 @@ gimp_gegl_mask_combine_buffer (GeglBuffer     *mask,
     case GIMP_CHANNEL_OP_SUBTRACT:
       while (gegl_buffer_iterator_next (iter))
         {
-          gfloat       *mask_data   = iter->items[0].data;
-          const gfloat *add_on_data = iter->items[1].data;
+          gfloat       *mask_data   = (gfloat       *) iter->items[0].data;
+          const gfloat *add_on_data = (const gfloat *) iter->items[1].data;
           gint          count       = iter->length;
 
           while (count--)
@@ -482,8 +485,8 @@ gimp_gegl_mask_combine_buffer (GeglBuffer     *mask,
     case GIMP_CHANNEL_OP_INTERSECT:
       while (gegl_buffer_iterator_next (iter))
         {
-          gfloat       *mask_data   = iter->items[0].data;
-          const gfloat *add_on_data = iter->items[1].data;
+          gfloat       *mask_data   = (gfloat       *) iter->items[0].data;
+          const gfloat *add_on_data = (const gfloat *) iter->items[1].data;
           gint          count       = iter->length;
 
           while (count--)
@@ -503,3 +506,5 @@ gimp_gegl_mask_combine_buffer (GeglBuffer     *mask,
 
   return TRUE;
 }
+
+} /* extern "C" */
