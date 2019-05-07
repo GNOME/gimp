GNOME/gimp

commit 3bf2a3c16603f434521cf94c1e2fd7d745a18794

app: don't dereference NULL image pointer
windows_menu_display_query_tooltip(): bail out if "image" is
NULL. Can't happen currently but did happen temporarily while hacking
on related code. Better safe than sorry.

app/menus/windows-menu.c
@@ -415,6 +415,9 @@ windows_menu_display_query_tooltip (GtkWidget  *widget,
   gint       width;
   gint       height;
 
+  if (! image)
+    return;
+
   text = gtk_widget_get_tooltip_text (widget);
   gtk_tooltip_set_text (tooltip, text);
   g_free (text);

commit 90bc9e10f14644a2bf6c0071c0762d5f34f0957d

Issue #835: warnings building with Clang.
Fix one warning and one error (introduced in commit 3bf2a3c1660).

app/actions/dockable-commands.c
@@ -191,7 +191,7 @@ dockable_toggle_view_cmd_callback (GtkAction *action,
                       gtk_widget_show (new_dockable);
 
                       gtk_container_remove (GTK_CONTAINER (dockbook),
-                                            dockable);
+                                            GTK_WIDGET (dockable));
 
                       gtk_notebook_set_current_page (GTK_NOTEBOOK (dockbook),
                                                      page_num);

app/menus/windows-menu.c
@@ -416,7 +416,7 @@ windows_menu_display_query_tooltip (GtkWidget  *widget,
   gint       height;
 
   if (! image)
-    return;
+    return FALSE;
 
   text = gtk_widget_get_tooltip_text (widget);
   gtk_tooltip_set_text (tooltip, text);