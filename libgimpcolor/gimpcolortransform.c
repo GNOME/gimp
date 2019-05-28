890b554230fda231689c3f791f051877e7e948b5
Michael Natterer <mitch@gimp.org>
2019-05-28 17:37:08 +0200

    libgimpcolor: better error messages in GimpColorProfile and GimpColorTransform

--- libgimpcolor/gimpcolortransform.c
+++ libgimpcolor/gimpcolortransform.c
@@ -217,7 +217,7 @@ gimp_color_transform_new (GimpColorProfile         *src_profile,
                                                     &error);
   if (! priv->src_format)
     {
-      g_printerr ("%s: error making format: %s\n",
+      g_printerr ("%s: error making src format: %s\n",
                   G_STRFUNC, error->message);
       g_clear_error (&error);
     }
@@ -228,7 +228,7 @@ gimp_color_transform_new (GimpColorProfile         *src_profile,
                                                      &error);
   if (! priv->dest_format)
     {
-      g_printerr ("%s: error making format: %s\n",
+      g_printerr ("%s: error making dest format: %s\n",
                   G_STRFUNC, error->message);
       g_clear_error (&error);
     }
