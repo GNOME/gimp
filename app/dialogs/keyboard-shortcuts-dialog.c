04b69e2494ac0f54e71d9fef84ffdab45a0cfa7d
Michael Natterer <mitch@gimp.org>
2019-05-29 16:12:21 +0200

    Issue #263 - Add a "Save Keyboard Shortcuts Now" button...
    ...to the Configure Keyboard Shortcuts dialog
    
    Add the button, based on an old patch from Sven Neumann, and make the
    buttons in the keyboard shortcuts and input devices dialogs look and
    behave the same.

--- app/dialogs/keyboard-shortcuts-dialog.c
+++ app/dialogs/keyboard-shortcuts-dialog.c
@@ -30,10 +30,24 @@
 #include "widgets/gimphelp-ids.h"
 #include "widgets/gimpuimanager.h"
 
+#include "menus/menus.h"
+
 #include "keyboard-shortcuts-dialog.h"
 
 #include "gimp-intl.h"
 
+
+#define RESPONSE_SAVE 1
+
+
+/*  local function prototypes  */
+
+static void   keyboard_shortcuts_dialog_response (GtkWidget *dialog,
+                                                  gint       response,
+                                                  Gimp      *gimp);
+
+
+/*  public functions  */
 
 GtkWidget *
 keyboard_shortcuts_dialog_new (Gimp *gimp)
@@ -52,13 +66,14 @@ keyboard_shortcuts_dialog_new (Gimp *gimp)
                             gimp_standard_help_func,
                             GIMP_HELP_KEYBOARD_SHORTCUTS,
 
-                            _("_Close"), GTK_RESPONSE_OK,
+                            _("_Save"),  RESPONSE_SAVE,
+                            _("_Close"), GTK_RESPONSE_CLOSE,
 
                             NULL);
 
   g_signal_connect (dialog, "response",
-                    G_CALLBACK (gtk_widget_destroy),
-                    NULL);
+                    G_CALLBACK (keyboard_shortcuts_dialog_response),
+                    gimp);
 
   vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
   gtk_container_set_border_width (GTK_CONTAINER (vbox), 12);
@@ -84,4 +99,24 @@ keyboard_shortcuts_dialog_new (Gimp *gimp)
   gtk_widget_show (button);
 
   return dialog;
+}
+
+
+/*  private functions  */
+
+static void
+keyboard_shortcuts_dialog_response (GtkWidget *dialog,
+                                    gint       response,
+                                    Gimp      *gimp)
+{
+  switch (response)
+    {
+    case RESPONSE_SAVE:
+      menus_save (gimp, TRUE);
+      break;
+
+    default:
+      gtk_widget_destroy (dialog);
+      break;
+    }
 }
