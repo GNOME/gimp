c8e6fd2e42f8ef6cd0e851349b6736ed9182f6d5
lillolollo <4179-lillolollo@users.noreply.gitlab.gnome.org>
2019-06-30 13:37:07 +0000

    Eliminate CRITICAL as https://gitlab.gnome.org/GNOME/gimp/issues/3093#note_543428

--- app/dialogs/file-open-location-dialog.c
+++ app/dialogs/file-open-location-dialog.c
@@ -228,7 +228,9 @@ file_open_location_response (GtkDialog *dialog,
               g_clear_error (&error);
             }
 
-          g_object_unref (file);
+         if (file != NULL) {
+             g_object_unref(file);
+            }
 
           if (image != NULL)
             {
