23f373e9163987bc67afb56f7cdaf8b36b42d623
Michael Natterer <mitch@gimp.org>
2019-07-19 19:01:51 +0200

    plug-ins: remove filter-pack without any replacement

--- plug-ins/common/plugin-defs.pl
+++ plug-ins/common/plugin-defs.pl
@@ -52,7 +52,6 @@
     'file-xpm' => { ui => 1, gegl => 1, optional => 1, libs => 'XPM_LIBS' },
     'file-xwd' => {  ui => 1, gegl => 1 },
     'film' => { ui => 1, gegl => 1 },
-    'filter-pack' => { ui => 1 },
     'goat-exercise' => { gegl => 1 },
     'gradient-map' => { gegl => 1 },
     'grid' => { ui => 1, gegl => 1 },
