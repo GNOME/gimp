2c8006896d3422a871717d4d405382ce17fd9434
Ell <ell_se@yahoo.com>
2019-08-01 23:41:30 +0300

    app: in GimpFilterTool, add "clipping" option
    In GimpFilterTool, add a new "clipping" option, which can be either
    Adjust or Clip.  This option is controllable for layers, when
    there's no selection mask.
    
    When set to Adjust, which is used by default, the filter's output
    may extent past the drawable's boundary, and the drawable will be
    resized to match the output when committed.
    
    (cherry picked from commit eaec9443c33f459ad6164af2704ad9f15a8c08a3)

--- app/tools/gimpfilteroptions.h
+++ app/tools/gimpfilteroptions.h
@@ -34,16 +34,17 @@ typedef struct _GimpFilterOptionsClass GimpFilterOptionsClass;
 
 struct _GimpFilterOptions
 {
-  GimpColorOptions    parent_instance;
+  GimpColorOptions     parent_instance;
 
-  gboolean            preview;
-  gboolean            preview_split;
-  GimpAlignmentType   preview_alignment;
-  gdouble             preview_position;
-  gboolean            controller;
-  GimpFilterRegion    region;
-  gboolean            color_managed;
-  gboolean            gamma_hack;
+  gboolean             preview;
+  gboolean             preview_split;
+  GimpAlignmentType    preview_alignment;
+  gdouble              preview_position;
+  gboolean             controller;
+  GimpTransformResize  clip;
+  GimpFilterRegion     region;
+  gboolean             color_managed;
+  gboolean             gamma_hack;
 };
 
 struct _GimpFilterOptionsClass
