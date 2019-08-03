6910fc73e11a60c74fdc0e1cc5d2a8194edfeecf
Michael Natterer <mitch@gimp.org>
2019-08-03 17:29:34 +0200

    app: better error reporting in gimp_pdb_dialog_run_callback()
    Display the error returned from trying to run the temp procedure.

--- app/widgets/gimppdbdialog.c
+++ app/widgets/gimppdbdialog.c
@@ -274,12 +274,18 @@ gimp_pdb_dialog_run_callback (GimpPdbDialog *dialog,
           if (g_value_get_enum (gimp_value_array_index (return_vals, 0)) !=
               GIMP_PDB_SUCCESS)
             {
+              const gchar *message;
+
+              if (error && error->message)
+                message = error->message;
+              else
+                message = _("The corresponding plug-in may have crashed.");
+
               gimp_message (dialog->context->gimp, G_OBJECT (dialog),
                             GIMP_MESSAGE_ERROR,
-                            _("Unable to run %s callback. "
-                              "The corresponding plug-in may have "
-                              "crashed."),
-                            g_type_name (G_TYPE_FROM_INSTANCE (dialog)));
+                            _("Unable to run %s callback.\n%s"),
+                            g_type_name (G_TYPE_FROM_INSTANCE (dialog)),
+                            message);
             }
           else if (error)
             {
