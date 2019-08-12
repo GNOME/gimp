5ccf9d645a7e35da46d71257ee931155aae69aed
Jehan <jehan@girinstud.io>
2019-08-13 00:42:10 +0200

    libgimp: update non-generated API to allow old and new API.
    Same as previous commit: by default the new API will be used. But if a
    plug-in builds with GIMP_DEPRECATED_REPLACE_NEW_API macro, then the same
    function names will call the old API with ids.

--- libgimp/gimplayer.h
+++ libgimp/gimplayer.h
@@ -30,6 +30,11 @@ G_BEGIN_DECLS
 /* For information look into the C source or the html documentation */
 
 
+gint32   gimp_layer_copy               (gint32           layer_ID);
+
+
+#ifndef GIMP_DEPRECATED_REPLACE_NEW_API
+
 gint32   gimp_layer_new                (GimpImage       *image,
                                         const gchar     *name,
                                         gint             width,
@@ -37,7 +42,6 @@ gint32   gimp_layer_new                (GimpImage       *image,
                                         GimpImageType    type,
                                         gdouble          opacity,
                                         GimpLayerMode    mode);
-gint32   gimp_layer_copy               (gint32           layer_ID);
 
 gint32   gimp_layer_new_from_pixbuf    (GimpImage       *image,
                                         const gchar     *name,
@@ -51,6 +55,36 @@ gint32   gimp_layer_new_from_surface   (GimpImage       *image,
                                         cairo_surface_t *surface,
                                         gdouble          progress_start,
                                         gdouble          progress_end);
+
+#else /* GIMP_DEPRECATED_REPLACE_NEW_API */
+
+#define gimp_layer_new              gimp_layer_new_deprecated
+#define gimp_layer_new_from_pixbuf  gimp_layer_new_from_pixbuf_deprecated
+#define gimp_layer_new_from_surface gimp_layer_new_from_surface_deprecated
+
+#endif /* GIMP_DEPRECATED_REPLACE_NEW_API */
+
+
+gint32   gimp_layer_new_deprecated              (gint32           image_id,
+                                                 const gchar     *name,
+                                                 gint             width,
+                                                 gint             height,
+                                                 GimpImageType    type,
+                                                 gdouble          opacity,
+                                                 GimpLayerMode    mode);
+
+gint32   gimp_layer_new_from_pixbuf_deprecated  (gint32           image_id,
+                                                 const gchar     *name,
+                                                 GdkPixbuf       *pixbuf,
+                                                 gdouble          opacity,
+                                                 GimpLayerMode    mode,
+                                                 gdouble          progress_start,
+                                                 gdouble          progress_end);
+gint32   gimp_layer_new_from_surface_deprecated (gint32           image_id,
+                                                 const gchar     *name,
+                                                 cairo_surface_t *surface,
+                                                 gdouble          progress_start,
+                                                 gdouble          progress_end);
 
 
 G_END_DECLS
