baa5e82eaac57c901365927de7089977ccef7f5b
Jehan <jehan@girinstud.io>
2019-08-14 16:32:06 +0200

    plug-ins: port file-compressor to GimpImage/GimpDrawable.

--- plug-ins/common/plugin-defs.pl
+++ plug-ins/common/plugin-defs.pl
@@ -19,7 +19,7 @@
     'file-aa' => { ui => 1, gegl => 1, optional => 1, libs => 'AA_LIBS', old_api => 1 },
     'file-cel' => { ui => 1, gegl => 1, old_api => 1 },
     'file-csource' => { ui => 1, gegl => 1, old_api => 1 },
-    'file-compressor' => { gio => 1, libdep => 'Z:BZIP2:LZMA', cflags => 'LZMA_CFLAGS', old_api => 1 },
+    'file-compressor' => { gio => 1, libdep => 'Z:BZIP2:LZMA', cflags => 'LZMA_CFLAGS' },
     'file-desktop-link' => { gio => 1 },
     'file-dicom' => { ui => 1, gegl => 1, cflags => '-fno-strict-aliasing', old_api => 1 },
     'file-gbr' => { ui => 1, gegl => 1 },
