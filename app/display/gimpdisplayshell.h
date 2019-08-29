3b743a4d9d92c63acfbd2e5d9ea289f372c64ae9
blackle <isabelle@blackle-mori.com>
2019-08-29 02:06:57 -0400

    Revert "app: fix drawing of the empty canvas wilber"
    This reverts commit 462f61794ecd523a36d7ef9bb2adbd868e7c890c.

--- app/display/gimpdisplayshell.h
+++ app/display/gimpdisplayshell.h
@@ -228,9 +228,6 @@ struct _GimpDisplayShell
   GimpMotionBuffer  *motion_buffer;
 
   GQueue            *zoom_focus_pointer_queue;
-
-  gboolean           blink;
-  guint              blink_timeout_id;
 };
 
 struct _GimpDisplayShellClass
