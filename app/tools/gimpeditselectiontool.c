862821b98fc61eaecf4b906a38ee6cfcfb9f94f0
Ell <ell_se@yahoo.com>
2019-10-02 18:33:27 +0300

    app: fix CRITICAL when translating selected content
    Fix a CRITICAL when creating a floating selection by Alt-dragging a
    selection using one of the selection tools.
    
    (cherry picked from commit 1e9bf288ba958713ba051886428c77ce8d903357)

--- app/tools/gimpeditselectiontool.c
+++ app/tools/gimpeditselectiontool.c
@@ -596,6 +596,8 @@ gimp_edit_selection_tool_update_motion (GimpEditSelectionTool *edit_select,
 
           edit_select->live_items = g_list_prepend (NULL, active_item);
 
+          gimp_viewable_preview_freeze (GIMP_VIEWABLE (active_item));
+
           gimp_item_start_transform (active_item, TRUE);
 
           /*  fallthru  */
