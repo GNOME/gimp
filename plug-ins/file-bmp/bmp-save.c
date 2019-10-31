0a55c44705beb16d60f90ce6ed3192b6790ad67f
Sebastian Rasmussen <sebras@gmail.com>
2019-10-31 16:41:36 +0100

    plug-ins: When exporting BMP with color space info, always include color masks.
    This fixes issue #4155.
    
    (cherry picked from commit 9727a38977c0880dc54a7bd6615c8448fd477492)

--- plug-ins/file-bmp/bmp-save.c
+++ plug-ins/file-bmp/bmp-save.c
@@ -346,9 +346,15 @@ save_image (const gchar  *filename,
     SpZeile = ((gint) (((Spcols * BitsPerPixel) / 8) / 4) + 1) * 4;
 
   if (! BMPSaveData.dont_write_color_space_data)
-    color_space_size = 68;
+    {
+      /* Always include color mask for BITMAPV5HEADER, see #4155. */
+      mask_info_size = 16;
+      color_space_size = 68;
+    }
   else
-    color_space_size = 0;
+    {
+      color_space_size = 0;
+    }
 
   bitmap_file_head.bfSize    = (0x36 + MapSize + (rows * SpZeile) +
                                 mask_info_size + color_space_size);
