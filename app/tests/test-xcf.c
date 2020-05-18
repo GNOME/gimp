GNOME/gimp

commit a7c59277fb6c10969043c0e5558c726039ba985b

app: quick on-canvas copy-paste multi-layer aware.

app/core/gimpselection.c
@@ -35,6 +35,7 @@
 #include "gimpdrawable-private.h"
 #include "gimperror.h"
 #include "gimpimage.h"
+#include "gimpimage-new.h"
 #include "gimpimage-undo.h"
 #include "gimpimage-undo-push.h"
 #include "gimplayer.h"
@@ -808,7 +809,7 @@ gimp_selection_extract (GimpSelection *selection,
 
 GimpLayer *
 gimp_selection_float (GimpSelection *selection,
-                      GimpDrawable  *drawable,
+                      GList         *drawables,
                       GimpContext   *context,
                       gboolean       cut_image,
                       gint           off_x,
@@ -817,22 +818,41 @@ gimp_selection_float (GimpSelection *selection,
 {
   GimpImage        *image;
   GimpLayer        *layer;
+  GimpPickable     *pickable;
   GeglBuffer       *buffer;
   GimpColorProfile *profile;
+  GimpImage        *temp_image = NULL;
+  const Babl       *format     = NULL;
+  GList            *iter;
   gint              x1, y1;
   gint              x2, y2;
 
   g_return_val_if_fail (GIMP_IS_SELECTION (selection), NULL);
-  g_return_val_if_fail (GIMP_IS_DRAWABLE (drawable), NULL);
-  g_return_val_if_fail (gimp_item_is_attached (GIMP_ITEM (drawable)), NULL);
   g_return_val_if_fail (GIMP_IS_CONTEXT (context), NULL);
   g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+  for (iter = drawables; iter; iter = iter->next)
+    {
+      g_return_val_if_fail (GIMP_IS_DRAWABLE (iter->data), NULL);
+      g_return_val_if_fail (gimp_item_is_attached (iter->data), NULL);
+
+      if (! format)
+        format = gimp_drawable_get_format_with_alpha (iter->data);
+      else
+        g_return_val_if_fail (format == gimp_drawable_get_format_with_alpha (iter->data),
+                              NULL);
+    }
 
   image = gimp_item_get_image (GIMP_ITEM (selection));
 
   /*  Make sure there is a region to float...  */
-  if (! gimp_item_mask_bounds (GIMP_ITEM (drawable), &x1, &y1, &x2, &y2) ||
-      (x1 == x2 || y1 == y2))
+  for (iter = drawables; iter; iter = iter->next)
+    {
+      if (gimp_item_mask_bounds (iter->data, &x1, &y1, &x2, &y2) &&
+          x1 != x2 && y1 != y2)
+        break;
+    }
+  if (iter == NULL)
     {
       g_set_error_literal (error, GIMP_ERROR, GIMP_FAILED,
                            _("Cannot float selection because the selected "
@@ -844,22 +864,31 @@ gimp_selection_float (GimpSelection *selection,
   gimp_image_undo_group_start (image, GIMP_UNDO_GROUP_FS_FLOAT,
                                C_("undo-type", "Float Selection"));
 
+  if (g_list_length (drawables) > 1)
+    {
+      temp_image = gimp_image_new_from_drawables (image->gimp, drawables, TRUE);
+      pickable = GIMP_PICKABLE (temp_image);
+    }
+  else
+    {
+      pickable = GIMP_PICKABLE (drawables->data);
+    }
+
   /*  Cut or copy the selected region  */
-  buffer = gimp_selection_extract (selection, GIMP_PICKABLE (drawable), context,
+  buffer = gimp_selection_extract (selection, pickable, context,
                                    cut_image, FALSE, TRUE,
                                    &x1, &y1, NULL);
 
-  profile = gimp_color_managed_get_color_profile (GIMP_COLOR_MANAGED (drawable));
+  profile = gimp_color_managed_get_color_profile (GIMP_COLOR_MANAGED (pickable));
 
   /*  Clear the selection  */
   gimp_channel_clear (GIMP_CHANNEL (selection), NULL, TRUE);
 
-  /* Create a new layer from the buffer, using the drawable's type
+  /* Create a new layer from the buffer, using the drawables' type
    *  because it may be different from the image's type if we cut from
    *  a channel or layer mask
    */
-  layer = gimp_layer_new_from_gegl_buffer (buffer, image,
-                                           gimp_drawable_get_format_with_alpha (drawable),
+  layer = gimp_layer_new_from_gegl_buffer (buffer, image, format,
                                            _("Floated Layer"),
                                            GIMP_OPACITY_OPAQUE,
                                            gimp_image_get_default_new_layer_mode (image),
@@ -872,13 +901,16 @@ gimp_selection_float (GimpSelection *selection,
   g_object_unref (buffer);
 
   /*  Add the floating layer to the image  */
-  floating_sel_attach (layer, drawable);
+  floating_sel_attach (layer, drawables->data);
 
   /*  End an undo group  */
   gimp_image_undo_group_end (image);
 
   /*  invalidate the image's boundary variables  */
   GIMP_CHANNEL (selection)->boundary_known = FALSE;
+
+  if (temp_image)
+    g_object_unref (temp_image);
 
   return layer;
 }

app/core/gimpselection.h
@@ -65,7 +65,7 @@ GeglBuffer  * gimp_selection_extract  (GimpSelection *selection,
                                        GError       **error);
 
 GimpLayer   * gimp_selection_float    (GimpSelection *selection,
-                                       GimpDrawable  *drawable,
+                                       GList         *drawables,
                                        GimpContext   *context,
                                        gboolean       cut_image,
                                        gint           off_x,

app/tools/gimpeditselectiontool.c
@@ -595,7 +595,7 @@ gimp_edit_selection_tool_update_motion (GimpEditSelectionTool *edit_select,
         case GIMP_TRANSLATE_MODE_MASK_TO_LAYER:
         case GIMP_TRANSLATE_MODE_MASK_COPY_TO_LAYER:
           if (! gimp_selection_float (GIMP_SELECTION (gimp_image_get_mask (image)),
-                                      GIMP_DRAWABLE (selected_items->data),
+                                      selected_items,
                                       gimp_get_user_context (display->gimp),
                                       edit_select->edit_mode ==
                                       GIMP_TRANSLATE_MODE_MASK_TO_LAYER,

app/tools/gimpselectiontool.c
@@ -225,7 +225,7 @@ gimp_selection_tool_oper_update (GimpTool         *tool,
   GimpSelectionTool    *selection_tool = GIMP_SELECTION_TOOL (tool);
   GimpSelectionOptions *options        = GIMP_SELECTION_TOOL_GET_OPTIONS (tool);
   GimpImage            *image;
-  GimpDrawable         *drawable;
+  GList                *drawables;
   GimpLayer            *layer;
   GimpLayer            *floating_sel;
   GdkModifierType       extend_mask;
@@ -235,7 +235,7 @@ gimp_selection_tool_oper_update (GimpTool         *tool,
   gboolean              move_floating_sel = FALSE;
 
   image        = gimp_display_get_image (display);
-  drawable     = gimp_image_get_active_drawable (image);
+  drawables    = gimp_image_get_selected_drawables (image);
   layer        = gimp_image_pick_layer (image, coords->x, coords->y, NULL);
   floating_sel = gimp_image_get_floating_selection (image);
 
@@ -244,19 +244,29 @@ gimp_selection_tool_oper_update (GimpTool         *tool,
 
   have_selection = gimp_selection_tool_have_selection (selection_tool, display);
 
-  if (drawable)
+  if (drawables)
     {
       if (floating_sel)
         {
           if (layer == floating_sel)
             move_floating_sel = TRUE;
         }
-      else if (have_selection &&
-               gimp_item_mask_intersect (GIMP_ITEM (drawable),
-                                         NULL, NULL, NULL, NULL))
+      else if (have_selection)
         {
-          move_layer = TRUE;
+          GList *iter;
+
+          for (iter = drawables; iter; iter = iter->next)
+            {
+              if (gimp_item_mask_intersect (GIMP_ITEM (iter->data),
+                                            NULL, NULL, NULL, NULL))
+                {
+                  move_layer = TRUE;
+                  break;
+                }
+            }
         }
+
+      g_list_free (drawables);
     }
 
   selection_tool->function = SELECTION_SELECT;
@@ -509,9 +519,8 @@ gimp_selection_tool_check (GimpSelectionTool  *sel_tool,
                            GimpDisplay        *display,
                            GError            **error)
 {
-  GimpSelectionOptions *options  = GIMP_SELECTION_TOOL_GET_OPTIONS (sel_tool);
-  GimpImage            *image    = gimp_display_get_image (display);
-  GimpDrawable         *drawable = gimp_image_get_active_drawable (image);
+  GimpSelectionOptions *options   = GIMP_SELECTION_TOOL_GET_OPTIONS (sel_tool);
+  GimpImage            *image     = gimp_display_get_image (display);
 
   switch (sel_tool->function)
     {
@@ -548,22 +557,34 @@ gimp_selection_tool_check (GimpSelectionTool  *sel_tool,
 
     case SELECTION_MOVE:
     case SELECTION_MOVE_COPY:
-      if (gimp_viewable_get_children (GIMP_VIEWABLE (drawable)))
         {
-          g_set_error (error, GIMP_ERROR, GIMP_FAILED,
-                       _("Cannot modify the pixels of layer groups."));
+          GList *drawables = gimp_image_get_selected_drawables (image);
+          GList *iter;
 
-          return FALSE;
-        }
-      else if (gimp_item_is_content_locked (GIMP_ITEM (drawable)))
-        {
-          g_set_error (error, GIMP_ERROR, GIMP_FAILED,
-                       _("The active layer's pixels are locked."));
+          for (iter = drawables; iter; iter = iter->next)
+            {
+              if (gimp_viewable_get_children (iter->data))
+                {
+                  g_set_error (error, GIMP_ERROR, GIMP_FAILED,
+                               _("Cannot modify the pixels of layer groups."));
 
-          if (error)
-            gimp_tools_blink_lock_box (display->gimp, GIMP_ITEM (drawable));
+                  g_list_free (drawables);
+                  return FALSE;
+                }
+              else if (gimp_item_is_content_locked (iter->data))
+                {
+                  g_set_error (error, GIMP_ERROR, GIMP_FAILED,
+                               _("The active layer's pixels are locked."));
 
-          return FALSE;
+                  if (error)
+                    gimp_tools_blink_lock_box (display->gimp, iter->data);
+
+                  g_list_free (drawables);
+                  return FALSE;
+                }
+            }
+
+          g_list_free (drawables);
         }
       break;
 

commit 2956873740501973bb8637a3741ad61bfe20f4b3

app, libgimp, pdb: fix gimp_selection_float() usage.
This fixes bugs introduced in commit a7c59277fb where I obviously didn't
properly checked all the places where gimp_selection_float() was used
after its parameters changed.

app/actions/select-commands.c
@@ -114,12 +114,14 @@ select_float_cmd_callback (GimpAction *action,
 {
   GimpImage *image;
   GtkWidget *widget;
+  GList     *drawables;
   GError    *error = NULL;
   return_if_no_image (image, data);
   return_if_no_widget (widget, data);
 
+  drawables = gimp_image_get_selected_drawables (image);
   if (gimp_selection_float (GIMP_SELECTION (gimp_image_get_mask (image)),
-                            gimp_image_get_active_drawable (image),
+                            drawables,
                             action_data_get_context (data),
                             TRUE, 0, 0, &error))
     {
@@ -132,6 +134,7 @@ select_float_cmd_callback (GimpAction *action,
                             error->message);
       g_clear_error (&error);
     }
+  g_list_free (drawables);
 }
 
 void

app/pdb/selection-cmds.c
@@ -30,7 +30,6 @@
 #include "pdb-types.h"
 
 #include "core/gimpchannel.h"
-#include "core/gimpdrawable.h"
 #include "core/gimpimage.h"
 #include "core/gimplayer.h"
 #include "core/gimpparamspecs.h"
@@ -196,31 +195,59 @@ selection_float_invoker (GimpProcedure         *procedure,
 {
   gboolean success = TRUE;
   GimpValueArray *return_vals;
-  GimpDrawable *drawable;
+  gint num_drawables;
+  const GimpItem **drawables;
   gint offx;
   gint offy;
   GimpLayer *layer = NULL;
 
-  drawable = g_value_get_object (gimp_value_array_index (args, 0));
-  offx = g_value_get_int (gimp_value_array_index (args, 1));
-  offy = g_value_get_int (gimp_value_array_index (args, 2));
+  num_drawables = g_value_get_int (gimp_value_array_index (args, 0));
+  drawables = (const GimpItem **) gimp_value_get_object_array (gimp_value_array_index (args, 1));
+  offx = g_value_get_int (gimp_value_array_index (args, 2));
+  offy = g_value_get_int (gimp_value_array_index (args, 3));
 
   if (success)
     {
-      if (gimp_pdb_item_is_attached (GIMP_ITEM (drawable), NULL,
-                                     GIMP_PDB_ITEM_CONTENT, error) &&
-          gimp_pdb_item_is_not_group (GIMP_ITEM (drawable), error))
+      GimpImage *image = NULL;
+      gint       i;
+
+      if (num_drawables < 1)
         {
-          GimpImage *image = gimp_item_get_image (GIMP_ITEM (drawable));
+          success = FALSE;
+        }
+      else
+        {
+          for (i = 0; i < num_drawables; i++)
+            {
+              if (! gimp_pdb_item_is_attached (GIMP_ITEM (drawables[i]), NULL,
+                                               GIMP_PDB_ITEM_CONTENT, error) ||
+                  gimp_pdb_item_is_group (GIMP_ITEM (drawables[i]), error)   ||
+                  (image && image != gimp_item_get_image (GIMP_ITEM (drawables[i]))))
+                {
+                  success = FALSE;
+                  break;
+                }
+              else
+                {
+                  image = gimp_item_get_image (GIMP_ITEM (drawables[i]));
+                }
+            }
+        }
+
+      if (success)
+        {
+          GList *drawable_list = NULL;
+
+          for (i = 0; i < num_drawables; i++)
+            drawable_list = g_list_prepend (drawable_list, (gpointer) drawables[i]);
 
           layer = gimp_selection_float (GIMP_SELECTION (gimp_image_get_mask (image)),
-                                        drawable, context, TRUE, offx, offy,
+                                        drawable_list, context, TRUE, offx, offy,
                                         error);
+          g_list_free (drawable_list);
           if (! layer)
             success = FALSE;
         }
-      else
-        success = FALSE;
     }
 
   return_vals = gimp_procedure_get_return_values (procedure, success,
@@ -665,11 +692,17 @@ register_selection_procs (GimpPDB *pdb)
                                          "Spencer Kimball & Peter Mattis",
                                          "1995-1996");
   gimp_procedure_add_argument (procedure,
-                               gimp_param_spec_drawable ("drawable",
-                                                         "drawable",
-                                                         "The drawable from which to float selection",
-                                                         FALSE,
-                                                         GIMP_PARAM_READWRITE));
+                               g_param_spec_int ("num-drawables",
+                                                 "num drawables",
+                                                 "The number of drawables",
+                                                 1, G_MAXINT32, 1,
+                                                 GIMP_PARAM_READWRITE));
+  gimp_procedure_add_argument (procedure,
+                               gimp_param_spec_object_array ("drawables",
+                                                             "drawables",
+                                                             "The drawables from which to float selection",
+                                                             GIMP_TYPE_ITEM,
+                                                             GIMP_PARAM_READWRITE | GIMP_PARAM_NO_VALIDATE));
   gimp_procedure_add_argument (procedure,
                                g_param_spec_int ("offx",
                                                  "offx",

libgimp/gimpselection.c
@@ -73,10 +73,12 @@ gimp_selection_get_by_id (gint32 selection_id)
 
 /**
  * gimp_selection_float:
- * @image:    ignored
- * @drawable: The drawable from which to float selection.
- * @offx:     x offset for translation.
- * @offy:     y offset for translation.
+ * @image:       ignored
+ * @n_drawables: Size of @drawables.
+ * @drawables:   (array length=n_drawables): The drawables from which to
+ *               float selection.
+ * @offx:        x offset for translation.
+ * @offy:        y offset for translation.
  *
  * Float the selection from the specified drawable with initial offsets
  * as specified.
@@ -90,12 +92,14 @@ gimp_selection_get_by_id (gint32 selection_id)
  * Returns: (transfer none): The floated layer.
  */
 GimpLayer *
-gimp_selection_float (GimpImage    *image,
-                      GimpDrawable *drawable,
-                      gint          offx,
-                      gint          offy)
+gimp_selection_float (GimpImage     *image,
+                      gint           n_drawables,
+                      GimpDrawable **drawables,
+                      gint           offx,
+                      gint           offy)
 {
-  return _gimp_selection_float (drawable,
+  return _gimp_selection_float (n_drawables,
+                                (const GimpItem **) drawables,
                                 offx,
                                 offy);
 }

libgimp/gimpselection.h
@@ -69,7 +69,8 @@ GType           gimp_selection_get_type (void) G_GNUC_CONST;
 GimpSelection * gimp_selection_get_by_id (gint32        selection_id);
 
 GimpLayer     * gimp_selection_float     (GimpImage    *image,
-                                          GimpDrawable *drawable,
+                                          gint           n_drawables,
+                                          GimpDrawable **drawables,
                                           gint          offx,
                                           gint          offy);
 

libgimp/gimpselection_pdb.c
@@ -220,7 +220,8 @@ gimp_selection_translate (GimpImage *image,
 
 /**
  * _gimp_selection_float:
- * @drawable: The drawable from which to float selection.
+ * @num_drawables: The number of drawables.
+ * @drawables: (array length=num_drawables) (element-type GimpItem): The drawables from which to float selection.
  * @offx: x offset for translation.
  * @offy: y offset for translation.
  *
@@ -236,19 +237,22 @@ gimp_selection_translate (GimpImage *image,
  * Returns: (transfer none): The floated layer.
  **/
 GimpLayer *
-_gimp_selection_float (GimpDrawable *drawable,
-                       gint          offx,
-                       gint          offy)
+_gimp_selection_float (gint             num_drawables,
+                       const GimpItem **drawables,
+                       gint             offx,
+                       gint             offy)
 {
   GimpValueArray *args;
   GimpValueArray *return_vals;
   GimpLayer *layer = NULL;
 
   args = gimp_value_array_new_from_types (NULL,
-                                          GIMP_TYPE_DRAWABLE, drawable,
+                                          G_TYPE_INT, num_drawables,
+                                          GIMP_TYPE_OBJECT_ARRAY, NULL,
                                           G_TYPE_INT, offx,
                                           G_TYPE_INT, offy,
                                           G_TYPE_NONE);
+  gimp_value_set_object_array (gimp_value_array_index (args, 1), GIMP_TYPE_ITEM, (GObject **) drawables, num_drawables);
 
   return_vals = gimp_pdb_run_procedure_array (gimp_get_pdb (),
                                               "gimp-selection-float",

libgimp/gimpselection_pdb.h
@@ -32,36 +32,37 @@ G_BEGIN_DECLS
 /* For information look into the C source or the html documentation */
 
 
-gboolean                   gimp_selection_bounds    (GimpImage    *image,
-                                                     gboolean     *non_empty,
-                                                     gint         *x1,
-                                                     gint         *y1,
-                                                     gint         *x2,
-                                                     gint         *y2);
-gint                       gimp_selection_value     (GimpImage    *image,
-                                                     gint          x,
-                                                     gint          y);
-gboolean                   gimp_selection_is_empty  (GimpImage    *image);
-gboolean                   gimp_selection_translate (GimpImage    *image,
-                                                     gint          offx,
-                                                     gint          offy);
-G_GNUC_INTERNAL GimpLayer* _gimp_selection_float    (GimpDrawable *drawable,
-                                                     gint          offx,
-                                                     gint          offy);
-gboolean                   gimp_selection_invert    (GimpImage    *image);
-gboolean                   gimp_selection_sharpen   (GimpImage    *image);
-gboolean                   gimp_selection_all       (GimpImage    *image);
-gboolean                   gimp_selection_none      (GimpImage    *image);
-gboolean                   gimp_selection_feather   (GimpImage    *image,
-                                                     gdouble       radius);
-gboolean                   gimp_selection_border    (GimpImage    *image,
-                                                     gint          radius);
-gboolean                   gimp_selection_grow      (GimpImage    *image,
-                                                     gint          steps);
-gboolean                   gimp_selection_shrink    (GimpImage    *image,
-                                                     gint          steps);
-gboolean                   gimp_selection_flood     (GimpImage    *image);
-GimpChannel*               gimp_selection_save      (GimpImage    *image);
+gboolean                   gimp_selection_bounds    (GimpImage       *image,
+                                                     gboolean        *non_empty,
+                                                     gint            *x1,
+                                                     gint            *y1,
+                                                     gint            *x2,
+                                                     gint            *y2);
+gint                       gimp_selection_value     (GimpImage       *image,
+                                                     gint             x,
+                                                     gint             y);
+gboolean                   gimp_selection_is_empty  (GimpImage       *image);
+gboolean                   gimp_selection_translate (GimpImage       *image,
+                                                     gint             offx,
+                                                     gint             offy);
+G_GNUC_INTERNAL GimpLayer* _gimp_selection_float    (gint             num_drawables,
+                                                     const GimpItem **drawables,
+                                                     gint             offx,
+                                                     gint             offy);
+gboolean                   gimp_selection_invert    (GimpImage       *image);
+gboolean                   gimp_selection_sharpen   (GimpImage       *image);
+gboolean                   gimp_selection_all       (GimpImage       *image);
+gboolean                   gimp_selection_none      (GimpImage       *image);
+gboolean                   gimp_selection_feather   (GimpImage       *image,
+                                                     gdouble          radius);
+gboolean                   gimp_selection_border    (GimpImage       *image,
+                                                     gint             radius);
+gboolean                   gimp_selection_grow      (GimpImage       *image,
+                                                     gint             steps);
+gboolean                   gimp_selection_shrink    (GimpImage       *image,
+                                                     gint             steps);
+gboolean                   gimp_selection_flood     (GimpImage       *image);
+GimpChannel*               gimp_selection_save      (GimpImage       *image);
 
 
 G_END_DECLS

pdb/groups/selection.pdb
@@ -181,8 +181,12 @@ HELP
     $lib_private = 1;
 
     @inargs = (
-	{ name => 'drawable', type => 'drawable',
-	  desc => 'The drawable from which to float selection' },
+        { name => 'drawables', type => 'itemarray',
+	  desc => 'The drawables from which to float selection',
+          no_validate => 1,
+          array => { name => 'num_drawables',
+                     type => '1 <= int32',
+                     desc => "The number of drawables" } },
 	{ name => 'offx', type => 'int32',
 	  desc => 'x offset for translation' },
 	{ name => 'offy', type => 'int32',
@@ -197,20 +201,46 @@ HELP
     %invoke = (
 	code => <<'CODE'
 {
-  if (gimp_pdb_item_is_attached (GIMP_ITEM (drawable), NULL,
-                                 GIMP_PDB_ITEM_CONTENT, error) &&
-      gimp_pdb_item_is_not_group (GIMP_ITEM (drawable), error))
+  GimpImage *image = NULL;
+  gint       i;
+
+  if (num_drawables < 1)
     {
-      GimpImage *image = gimp_item_get_image (GIMP_ITEM (drawable));
+      success = FALSE;
+    }
+  else
+    {
+      for (i = 0; i < num_drawables; i++)
+        {
+          if (! gimp_pdb_item_is_attached (GIMP_ITEM (drawables[i]), NULL,
+                                           GIMP_PDB_ITEM_CONTENT, error) ||
+              gimp_pdb_item_is_group (GIMP_ITEM (drawables[i]), error)   ||
+              (image && image != gimp_item_get_image (GIMP_ITEM (drawables[i]))))
+            {
+              success = FALSE;
+              break;
+            }
+          else
+            {
+              image = gimp_item_get_image (GIMP_ITEM (drawables[i]));
+            }
+        }
+    }
+
+  if (success)
+    {
+      GList *drawable_list = NULL;
+
+      for (i = 0; i < num_drawables; i++)
+        drawable_list = g_list_prepend (drawable_list, (gpointer) drawables[i]);
 
       layer = gimp_selection_float (GIMP_SELECTION (gimp_image_get_mask (image)),
-                                    drawable, context, TRUE, offx, offy,
+                                    drawable_list, context, TRUE, offx, offy,
                                     error);
+      g_list_free (drawable_list);
       if (! layer)
         success = FALSE;
     }
-  else
-    success = FALSE;
 }
 CODE
     );

app/tests/test-xcf.c
@@ -571,14 +571,19 @@ gimp_create_mainimage (Gimp     *gimp,
    */
   if (with_unusual_stuff)
     {
+      GList *drawables;
+
+      drawables = gimp_image_get_selected_drawables (image);
+
       /* Floating selection */
       gimp_selection_float (GIMP_SELECTION (gimp_image_get_mask (image)),
-                            gimp_image_get_active_drawable (image),
+                            drawables,
                             gimp_get_user_context (gimp),
                             TRUE /*cut_image*/,
                             0 /*off_x*/,
                             0 /*off_y*/,
                             NULL /*error*/);
+      g_list_free (drawables);
     }
 
   /* Adds stuff like layer groups */