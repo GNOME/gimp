6d0d7b6353f2cd3aeb9bdcf6c8f9dc6c557f73d6
Ben Rogalski <ben@benrogalski.com>
2020-05-24 16:26:50 -0400

    Custom transparency checkerboard colors

--- app/core/gimpimage.c
+++ app/core/gimpimage.c
@@ -928,6 +928,12 @@ gimp_image_constructed (GObject *object)
   g_signal_connect_object (config, "notify::transparency-size",
                            G_CALLBACK (gimp_item_stack_invalidate_previews),
                            private->layers->container, G_CONNECT_SWAPPED);
+  g_signal_connect_object (config, "notify::transparency-custom-color1",
+                           G_CALLBACK (gimp_item_stack_invalidate_previews),
+                           private->layers->container, G_CONNECT_SWAPPED);
+  g_signal_connect_object (config, "notify::transparency-custom-color2",
+                           G_CALLBACK (gimp_item_stack_invalidate_previews),
+                           private->layers->container, G_CONNECT_SWAPPED);
   g_signal_connect_object (config, "notify::layer-previews",
                            G_CALLBACK (gimp_viewable_size_changed),
                            image, G_CONNECT_SWAPPED);
