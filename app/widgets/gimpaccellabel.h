2259ad5fcc25a7429d45538d71e8c5b91b6e0328
Ell <ell_se@yahoo.com>
2020-06-01 18:38:57 +0300

    app: add GimpAccelLabel
    Add a new GimpAccelLabel widget, which shows an accelerator label
    for a given GimpAction.  Unlike GtkAccelLabel, GimpAccelLabel
    doesn't show a user-provided label in addition to that.
    
    Note that the size request of GtkAccelLabel doesn't include the
    accelerator part, which is desirable in some contexts.
    GimpAccelLabel doesn't suffer from that.

---
+++ app/widgets/gimpaccellabel.h
@@ -0,0 +1,58 @@
+/* GIMP - The GNU Image Manipulation Program
+ * Copyright (C) 1995 Spencer Kimball and Peter Mattis
+ *
+ * gimpaccellabel.h
+ * Copyright (C) 2020 Ell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GIMP_ACCEL_LABEL_H__
+#define __GIMP_ACCEL_LABEL_H__
+
+
+#define GIMP_TYPE_ACCEL_LABEL            (gimp_accel_label_get_type ())
+#define GIMP_ACCEL_LABEL(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_ACCEL_LABEL, GimpAccelLabel))
+#define GIMP_ACCEL_LABEL_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GIMP_TYPE_ACCEL_LABEL, GimpAccelLabelClass))
+#define GIMP_IS_ACCEL_LABEL(obj)         (G_TYPE_CHECK_INSTANCE_TYPE (obj, GIMP_TYPE_ACCEL_LABEL))
+#define GIMP_IS_ACCEL_LABEL_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GIMP_TYPE_ACCEL_LABEL))
+#define GIMP_ACCEL_LABEL_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GIMP_TYPE_ACCEL_LABEL, GimpAccelLabelClass))
+
+
+typedef struct _GimpAccelLabelPrivate GimpAccelLabelPrivate;
+typedef struct _GimpAccelLabelClass   GimpAccelLabelClass;
+
+struct _GimpAccelLabel
+{
+  GtkLabel               parent_instance;
+
+  GimpAccelLabelPrivate *priv;
+};
+
+struct _GimpAccelLabelClass
+{
+  GtkLabelClass  parent_class;
+};
+
+
+GType        gimp_accel_label_get_type   (void) G_GNUC_CONST;
+
+GtkWidget  * gimp_accel_label_new        (GimpAction     *action);
+
+void         gimp_accel_label_set_action (GimpAccelLabel *accel_label,
+                                          GimpAction     *action);
+GimpAction * gimp_accel_label_get_action (GimpAccelLabel *accel_label);
+
+
+#endif /* __GIMP_ACCEL_LABEL_H__ */
