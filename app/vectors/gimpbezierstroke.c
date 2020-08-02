7f2d52a575df0016e574c5ee4a0cdfb8c4ad8431
Liam Quin <liam@fromoldbooks.org>
2020-08-02 22:08:34 +0200

    Issue #4328: Distorted path of an open/imported circle from an svg file.
    Undoing the apparent mistake (revert 27e78849a2) and changing the 2nd
    occurrence instead.
    
    (cherry picked from commit be0e63aa1cf87839abcb37b4f37ce60da1433d2b)

--- app/vectors/gimpbezierstroke.c
+++ app/vectors/gimpbezierstroke.c
@@ -2259,7 +2259,7 @@ gimp_bezier_stroke_new_ellipse (const GimpCoords *center,
   gimp_coords_mix (1.0, center, 1.0, &dx, &p1);
   stroke = gimp_bezier_stroke_new_moveto (&p1);
 
-  handle = g_queue_peek_tail (stroke->anchors);
+  handle = g_queue_peek_head (stroke->anchors);
   gimp_coords_mix (1.0,    &p1, -circlemagic, &dy, &handle->position);
 
   gimp_coords_mix (1.0,    &p1,  circlemagic, &dy, &p1);
@@ -2277,7 +2277,7 @@ gimp_bezier_stroke_new_ellipse (const GimpCoords *center,
   gimp_coords_mix (1.0,    &p3, -circlemagic, &dx, &p2);
   gimp_bezier_stroke_cubicto (stroke, &p1, &p2, &p3);
 
-  handle = g_queue_peek_head (stroke->anchors);
+  handle = g_queue_peek_tail (stroke->anchors);
   gimp_coords_mix (1.0,    &p3,  circlemagic, &dx, &handle->position);
 
   gimp_stroke_close (stroke);
