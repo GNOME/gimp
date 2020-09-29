8f8f7e497a54319059f6e092b0d58a2ebe05131e
Jehan <jehan@girinstud.io>
2020-09-29 19:38:23 +0200

    gitlab, build: Win32 distribution jobs for our CI.
    The main purpose of these jobs is to only package the strict necessary
    for a working GIMP under Windows, i.e. getting rid of all unnecessary
    executables, and inspecting binary dependencies recursively to only
    package used DLLs.
    
    The dll_link.py script is taken from Siril codebase (see commit a86e82a8
    on Siril repository, by FlorianBen). This was a very nice idea, and
    makes for much smaller test archive (Siril is also GPLv3 so licensing is
    ok for the reuse, also anyway it's just a small independent build
    script).
    Moreover having it as a separate job allows to have artifacts with only
    the finale distribution (artifacts on the build job also have the build
    directory and the whole prefix, which we want to keep in order to debug
    when needed).
    
    Hopefully I am not missing anything. Siril seems to package more, like
    various gdk-pixbuf-*.exe, gspawn-*.exe and gdbus.exe. I am wondering if
    these are actually necessary. I could run GIMP fine without these in
    quick tests, but I guess I'll have to investigate a bit more to figure
    this out. That's what nightly builds are for, after all, so hopefully
    people will report if we miss some runtime dependencies.

---
+++ build/windows/crossbuild-gitlab-ci/dll_link.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+################################################################################
+# Small python script to retrieve DLL depencies with objdump
+################################################################################
+
+################################################################################
+# Usage example
+#
+# python3 dll_link.py /path/to/run.exe /winenv/ /path/install
+#
+# In this case, the DLL depencies for executable run.exe will be extracted and
+# copied into /path/install/bin folder. To copy the DLL, the root path to
+# Windows environnement should be passed, here /winenv/.
+
+import sys
+import os
+import subprocess
+import re
+import string
+import shutil
+from pathlib import Path
+
+################################################################################
+# Global variables
+
+# Sets for executable and system DLL
+dll_siril_set = set()
+dll_sys_set = set()
+
+# Install prefix
+prefix = ''
+
+# Windows environement root
+basedir = ''
+
+# Common paths
+binary_dir = '/bin/'
+lib_dir = '/lib/'
+etc_dir = '/etc/'
+share_dir = '/share/'
+
+################################################################################
+# Functions
+
+# Main function
+def main():
+  global basedir
+  global prefix
+
+  if len(sys.argv) < 4:
+    exit(1)
+
+  filename = sys.argv[1]
+  basedir = sys.argv[2]
+  prefix = sys.argv[3]
+
+  recursive(filename)
+  copy_dll(dll_siril_set-dll_sys_set)
+
+# List DLL of an executable file in a recursive way
+def recursive(filename):
+  # Check if DLL exist in /bin folder, if true extract depencies too.
+  if os.path.exists(filename):
+    result = subprocess.run(
+        ['x86_64-w64-mingw32-objdump', '-p', filename], stdout=subprocess.PIPE)
+    out = result.stdout.decode('utf-8')
+    # Parse lines with DLL Name instead of lib*.dll directly
+    items = re.findall(r"DLL Name: \S+.dll", out, re.MULTILINE)
+    for x in items:
+      x = x.split(' ')[2]
+      l = len(dll_siril_set)
+      dll_siril_set.add(x)
+      if len(dll_siril_set) > l:
+        new_dll = basedir + binary_dir + x
+        recursive(new_dll)
+  # Otherwise, it is a system DLL
+  else:
+    dll_sys_set.add(os.path.basename(filename))
+
+# Copy a DLL set into the /prefix/bin directory
+def copy_dll(dll_list):
+  for file in dll_list:
+    full_file_name = os.path.join(basedir + binary_dir, file)
+    if os.path.isfile(full_file_name):
+      shutil.copy(full_file_name, prefix+binary_dir)
+
+
+if __name__ == "__main__":
+  main()
--- build/windows/crossbuild-gitlab-ci/gimp-wrapper.cmd
+++ build/windows/crossbuild-gitlab-ci/gimp-wrapper.cmd
@@ -1,3 +1,3 @@
-glib-compile-schemas.exe ..\share\glib-2.0\schemas
-gdk-pixbuf-query-loaders.exe --update-cache
-gimp-2.99.exe
+bin\glib-compile-schemas.exe share\glib-2.0\schemas
+bin\gdk-pixbuf-query-loaders.exe --update-cache
+bin\gimp-2.99.exe
