f336f86f604302fefa17187783ce2a6a8ff0b240
Jacob Boerema <jgboerema@gmail.com>
2020-10-24 18:31:31 -0400

    plug-ins: use g_message instead of g_printerr when there is data loss.
    After discussion with Jehan we concluded that it is better to report
    these data loss messages to the user using g_message.

--- plug-ins/file-tiff/file-tiff-load.c
+++ plug-ins/file-tiff/file-tiff-load.c
@@ -1608,7 +1608,7 @@ load_contiguous (TIFF        *tif,
           else if (TIFFReadScanline (tif, buffer, y, 0) == -1)
             {
               /* Error reading scanline, stop loading */
-              g_printerr ("Reading scanline failed. Image may be corrupt at line %d.\n", y);
+              g_message (_("Reading scanline failed. Image may be corrupt at line %d."), y);
               g_free (buffer);
               g_free (bw_buffer);
               return;
@@ -1783,7 +1783,7 @@ load_separate (TIFF        *tif,
                   else if (TIFFReadScanline (tif, buffer, y, compindex) == -1)
                     {
                       /* Error reading scanline, stop loading */
-                      g_printerr ("Reading scanline failed. Image may be corrupt at line %d.\n", y);
+                      g_message (_("Reading scanline failed. Image may be corrupt at line %d."), y);
                       g_free (buffer);
                       g_free (bw_buffer);
                       return;
