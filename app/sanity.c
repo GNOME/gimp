2cae9b9acf9da98c4c9990819ffbd5aabe23017e
Jehan <jehan@girinstud.io>
2020-12-14 19:53:38 +0100

    app: make "gegl:introspect" an optional operation dependency.
    Check at runtime for the operation availability and set the "Show Image
    Graph" action active depending on this check.
    
    This goes with discussions to make this operation optional with a
    runtime check for the tool `dot`.
    See: https://gitlab.gnome.org/GNOME/gegl/-/merge_requests/84

--- app/sanity.c
+++ app/sanity.c
@@ -650,7 +650,6 @@ sanity_check_gegl_ops (void)
     "gegl:hue-chroma",
     "gegl:illusion",
     "gegl:image-gradient",
-    "gegl:introspect",
     "gegl:invert-gamma",
     "gegl:invert-linear",
     "gegl:lens-blur",
