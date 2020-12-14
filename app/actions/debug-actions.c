f83fd22c4b8701ffc4ce14383e5e22756a4bce04
Jehan <jehan@girinstud.io>
2020-12-14 19:53:38 +0100

    app: make "gegl:introspect" an optional operation dependency.
    Check at runtime for the operation availability and set the "Show Image
    Graph" action active depending on this check.
    
    This goes with discussions to make this operation optional with a
    runtime check for the tool `dot`.
    See: https://gitlab.gnome.org/GNOME/gegl/-/merge_requests/84

--- app/actions/debug-actions.c
+++ app/actions/debug-actions.c
@@ -98,4 +98,10 @@ void
 debug_actions_update (GimpActionGroup *group,
                       gpointer         data)
 {
+#define SET_SENSITIVE(action,condition) \
+        gimp_action_group_set_action_sensitive (group, action, (condition) != 0)
+
+  SET_SENSITIVE ("debug-show-image-graph", gegl_has_operation ("gegl:introspect"));
+
+#undef SET_SENSITIVE
 }
