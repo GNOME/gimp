0d519a606f4a896743296ee554d410215997a562
Thomas Manni <thomas.manni@free.fr>
2021-01-31 13:09:15 +0100

    app: fix config properties / spin buttons mapping for Convolution Matrix filter
    The filter's output is now correct, and rotating/flipping the matrix works as
    expected.

--- app/propgui/gimppropgui-convolution-matrix.c
+++ app/propgui/gimppropgui-convolution-matrix.c
@@ -42,13 +42,13 @@ convolution_matrix_prop_name (gint x,
                               gint y)
 {
   static const gchar * const prop_names[5][5] = {
-    {"a1", "a2", "a3", "a4", "a5"},
-    {"b1", "b2", "b3", "b4", "b5"},
-    {"c1", "c2", "c3", "c4", "c5"},
-    {"d1", "d2", "d3", "d4", "d5"},
-    {"e1", "e2", "e3", "e4", "e5"}};
+    {"a1", "b1", "c1", "d1", "e1"},
+    {"a2", "b2", "c2", "d2", "e2"},
+    {"a3", "b3", "c3", "d3", "e3"},
+    {"a4", "b4", "c4", "d4", "e4"},
+    {"a5", "b5", "c5", "d5", "e5"}};
 
-  return prop_names[x][y];
+  return prop_names[y][x];
 }
 
 static void
@@ -178,7 +178,7 @@ _gimp_prop_gui_new_convolution_matrix (GObject                  *config,
                                             convolution_matrix_prop_name (x, y),
                                             1.0, 10.0, 2);
           gtk_entry_set_width_chars (GTK_ENTRY (spin), 8);
-          gtk_grid_attach (GTK_GRID (grid), spin, y, x, 1, 1);
+          gtk_grid_attach (GTK_GRID (grid), spin, x, y, 1, 1);
         }
     }
 
