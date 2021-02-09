dda15cb0e89c9ae9671ccb2f68ea948db203ecb2
Jehan <jehan@girinstud.io>
2021-02-09 02:39:37 +0100

    app: improve item lock management with gimp_item_tree_view_add_lock().
    Use this for the alpha lock brought by GimpLayerTreeView.
    Also use the new gimp_widget_blink_rect() to only blink the appropriate
    cell when a lock is preventing you from doing something.

--- app/tools/gimptools-utils.c
+++ app/tools/gimptools-utils.c
@@ -30,6 +30,7 @@
 
 #include "vectors/gimpvectors.h"
 
+#include "widgets/gimpcontainerview.h"
 #include "widgets/gimpdialogfactory.h"
 #include "widgets/gimpitemtreeview.h"
 #include "widgets/gimpwidgets-utils.h"
@@ -48,6 +49,10 @@ gimp_tools_blink_lock_box (Gimp     *gimp,
   GimpItemTreeView *view;
   GdkMonitor       *monitor;
   const gchar      *identifier;
+
+  GtkTreeIter      *iter;
+  GtkTreePath      *path;
+  GdkRectangle      rect;
 
   g_return_if_fail (GIMP_IS_GIMP (gimp));
   g_return_if_fail (GIMP_IS_ITEM (item));
@@ -75,5 +80,21 @@ gimp_tools_blink_lock_box (Gimp     *gimp,
 
   view = GIMP_ITEM_TREE_VIEW (gtk_bin_get_child (GTK_BIN (dockable)));
 
-  gimp_widget_blink (gimp_item_tree_view_get_lock_box (view));
+  /* Find the item in the tree view. */
+  iter = gimp_container_view_lookup (GIMP_CONTAINER_VIEW (view),
+                                     (GimpViewable *) item);
+  path = gtk_tree_model_get_path (GIMP_CONTAINER_TREE_VIEW (view)->model, iter);
+
+  /* Scroll dockable to make sure the cell is showing. */
+  gtk_tree_view_scroll_to_cell (GIMP_CONTAINER_TREE_VIEW (view)->view, path,
+                                gtk_tree_view_get_column (GIMP_CONTAINER_TREE_VIEW (view)->view, 1),
+                                FALSE, 0.0, 0.0);
+
+  /* Now blink the lock cell of the specified item. */
+  gtk_tree_view_get_cell_area (GIMP_CONTAINER_TREE_VIEW (view)->view, path,
+                               gtk_tree_view_get_column (GIMP_CONTAINER_TREE_VIEW (view)->view, 1),
+                               &rect);
+  gimp_widget_blink_rect (GTK_WIDGET (GIMP_CONTAINER_TREE_VIEW (view)->view), &rect);
+
+  gtk_tree_path_free (path);
 }
