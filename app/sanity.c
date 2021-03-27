95900ae6f196a57d64ccc997bf6d3710aba0438f
Øyvind Kolås <pippin@gimp.org>
2021-03-27 20:24:18 +0100

    configure,meson,app: depend on GEGL-0.4.30

--- app/sanity.c
+++ app/sanity.c
@@ -518,7 +518,7 @@ sanity_check_gegl (void)
 
 #define GEGL_REQUIRED_MAJOR 0
 #define GEGL_REQUIRED_MINOR 4
-#define GEGL_REQUIRED_MICRO 28
+#define GEGL_REQUIRED_MICRO 30
 
   gegl_get_version (&gegl_major_version,
                     &gegl_minor_version,
