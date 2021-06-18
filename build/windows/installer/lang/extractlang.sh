a6c44a7191877fb5d0f4da85f21c86bdfc513035
Jehan <jehan@girinstud.io>
2021-06-18 20:22:04 +0200

    build: complete Windows installer lang list in meson and fix iconv…
    … conversion when some characters are not convertible.
    
    Currently failed conversion ended up in incomplete .isl files (autotools
    build) or would break the build (meson build). Ideally we should use a
    target encoding which contains all source characters, but we use the
    encoding as set upstream in the LanguageCodePage for the given language
    file.
    I'm not sure if maybe we can mix the encoding by adding our own
    LanguageCodePage at the top of our generated <code>.setup.isl, then we
    could just use UTF-8 for any language. It will have to be tested.
    
    For now let's just discard non-convertible characters, assuming there
    shouldn't be too many of these. It's the lesser evil in this situation.

--- build/windows/installer/lang/extractlang.sh
+++ build/windows/installer/lang/extractlang.sh
@@ -8,4 +8,9 @@ prefix=$(echo "$prefix" | sed 's/[][]/\\\0/g')
 
 #echo to stdout
 sed '/^\w\+'"$prefix"'=/{s/\(.\)'"$prefix"'/\1/;n};/^\w.*=/d' "$infile" \
-| iconv -f UTF-8 -t "$encoding"
+| iconv -c -f UTF-8 -t "$encoding"
+
+# TODO: currently we silently discard non-convertible characters with -c
+# option on iconv. Eventually we would want to just use UTF-8 for all
+# language files (ideally), instead of folloding the LanguageCodePage of
+# the main .isl file as provided in the issrc repository.
