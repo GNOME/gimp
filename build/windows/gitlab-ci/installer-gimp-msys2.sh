GNOME/gimp

commit de9a171a191cd07d51eee44199116c7d87489b2b

gitlab-ci: generate SHA* checksums for generated Windows installers.

.gitlab-ci.yml
@@ -690,6 +690,10 @@ win-installer-nightly:
   script:
     - C:\msys64\usr\bin\pacman --noconfirm -Syyuu
     - C:\msys64\usr\bin\bash -lc "bash -x ./build/windows/gitlab-ci/installer-gimp-msys2.sh > installer.log 2>&1"
+    - cd build/windows/installer/_Output
+    - FILENAME=`ls gimp-*.exe` &&
+      sha256sum gimp-*.exe > ${FILENAME}.SHA256SUMS &&
+      sha512sum gimp-*.exe > ${FILENAME}.SHA512SUMS
   needs: ["packaging-win32-native", "packaging-win64-native"]
 
 sources:

commit 34748af7a7dcf94ad868c2acf6edb221d9732397

gitlab-ci, build: compute the checksums of installer in bash script.
I completely forgot that since the installer is built on a Windows
runner, I cannot expect a standard Linux shell syntax.
As a consequence, commit de9a171a19 broke the "win-installer-nightly"
job with the following error:

> The token '&&' is not a valid statement separator in this version.

Rather than trying to find the equivalent command to run on powershell
or whatever, let's just compute the checksum at the end of our installer
creation script.

.gitlab-ci.yml
@@ -690,10 +690,6 @@ win-installer-nightly:
   script:
     - C:\msys64\usr\bin\pacman --noconfirm -Syyuu
     - C:\msys64\usr\bin\bash -lc "bash -x ./build/windows/gitlab-ci/installer-gimp-msys2.sh > installer.log 2>&1"
-    - cd build/windows/installer/_Output
-    - FILENAME=`ls gimp-*.exe` &&
-      sha256sum gimp-*.exe > ${FILENAME}.SHA256SUMS &&
-      sha512sum gimp-*.exe > ${FILENAME}.SHA512SUMS
   needs: ["packaging-win32-native", "packaging-win64-native"]
 
 sources:

build/windows/gitlab-ci/installer-gimp-msys2.sh
@@ -39,6 +39,10 @@ cd build/windows/installer
 
 # Test if the installer was created and return success/failure.
 if [ -f "_Output/gimp-${MAJOR_VERSION}.${MINOR_VERSION}.${MICRO_VERSION}-setup.exe" ]; then
+  cd _Output/
+  INSTALLER="gimp-${MAJOR_VERSION}.${MINOR_VERSION}.${MICRO_VERSION}-setup.exe"
+  sha256sum $INSTALLER > ${INSTALLER}.SHA256SUMS
+  sha512sum $INSTALLER > ${INSTALLER}.SHA512SUMS
   exit 0
 else
   exit 1