3e1b783adbbf65c592d03b19cd153958be61ee31
Jehan <jehan@girinstud.io>
2021-12-20 15:05:34 +0100

    build: drop codepage conversion, use UTF-8 for Win install lang files.
    Rewriting commit f8cdec1883e by Jernej Simončič for the development
    code (which has a slightly different list of languages).
    
    Also adding the meson version of this change.
    
    Since now InnoSetup fully supports UTF-8 isl (as long as they have a
    BOM), let's stop converting translations. Then we also avoid all
    conversion errors and get a simpler/more robust build process.

--- build/windows/installer/lang/extractlang.sh
+++ build/windows/installer/lang/extractlang.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 prefix=$1
-encoding=$2
-infile=$3
+infile=$2
 
 #replace [] with \[\]
 prefix=$(echo "$prefix" | sed 's/[][]/\\\0/g')
 
-#echo to stdout
-sed '/^\w\+'"$prefix"'=/{s/\(.\)'"$prefix"'/\1/;n};/^\w.*=/d' "$infile" \
-| iconv -c -f UTF-8 -t "$encoding"
-
-# TODO: currently we silently discard non-convertible characters with -c
-# option on iconv. Eventually we would want to just use UTF-8 for all
-# language files (ideally), instead of folloding the LanguageCodePage of
-# the main .isl file as provided in the issrc repository.
+# InnoSetup now supports UTF-8 for all languages, but it requires a BOM
+# at the start of the file.
+echo -ne "\\xEF\\xBB\\xBF";
+sed '/^\w\+'"$prefix"'=/{s/\(.\)'"$prefix"'/\1/;n};/^\w.*=/d' "$infile"
