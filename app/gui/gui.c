b889de3ff6982564947dc2798f2565b718f5f2d5
Jehan <jehan@girinstud.io>
2022-02-17 23:42:57 +0100

    app: remove early macOS optimizations which are now counter-productive.
    Basically disabling commit 4f9b7373e6f8.
    After some new patches for GTK3 I wrote, and removing the settings on
    NSViewUsesAutomaticLayerBackingStores, Lukas reported that it works like
    never before, faster than 2.10 even. Note that this could only be tested
    on Monterey, nobody tested on Big Sur with this specific combination
    yet.
    
    In any case, we decided to remove this settings and add the new GTK3
    patches.
    See: https://gitlab.gnome.org/Infrastructure/gimp-macos-build/-/merge_requests/86#note_1384727

--- app/gui/gui.c
+++ app/gui/gui.c
@@ -86,7 +86,6 @@
 #ifdef GDK_WINDOWING_QUARTZ
 #import <AppKit/AppKit.h>
 #include <gtkosxapplication.h>
-#include <gdk/gdkquartz.h>
 
 /* Forward declare since we are building against old SDKs. */
 #if !defined(MAC_OS_X_VERSION_10_12) || \
@@ -274,18 +273,6 @@ gui_init (Gimp        *gimp,
    */
   if ([NSWindow respondsToSelector:@selector(setAllowsAutomaticWindowTabbing:)])
     [NSWindow setAllowsAutomaticWindowTabbing:NO];
-
-  /* MacOS 11 (Big Sur) has added a new, dynamic "accent" as default.
-   * This uses a 10-bit colorspace so every GIMP drawing operation
-   * has the additional cost of an 8-bit (ARGB) to 10-bit conversion.
-   * Let's disable this mode to regain the lost performance.
-   */
-  if (gdk_quartz_osx_version () >= GDK_OSX_BIGSUR)
-    {
-      NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-      [userDefaults setBool: NO forKey:@"NSViewUsesAutomaticLayerBackingStores"];
-    }
-
 #endif /* GDK_WINDOWING_QUARTZ */
 
   gimp_dnd_init (gimp);
