e1c3e7d85be69f1c7241db678de1ed1fc2451706
Jehan <jehan@girinstud.io>
2022-03-04 15:41:53 +0100

    app: use new gimp_tools_blink_widget() to blink source scrolldown.
    When the bucket fill can't work because the line art source is not
    right, let's blink the associated settings for people to know where to
    look at.

--- app/tools/gimpbucketfilltool.c
+++ app/tools/gimpbucketfilltool.c
@@ -632,6 +632,8 @@ gimp_bucket_fill_tool_button_press (GimpTool            *tool,
     {
       gimp_tool_message_literal (tool, display,
                                  _("No valid line art source selected."));
+      gimp_tools_blink_widget (display->gimp,
+                               "gimp-tool-options", "line-art-source");
       return;
     }
 
