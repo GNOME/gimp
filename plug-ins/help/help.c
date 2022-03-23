GNOME/gimp

commit 8eb7f6df9e903b6b544f4d949e97c67f915f8fe7

Remove GimpStringArray in favor of GStrv
GLib has a specific type of NULL-terminated string arrays:
`G_TYPE_STRV`, which is the `GType` of `char**` aka `GStrv`.

By using this type, we can avoid having a `GimpStringArray` which is a
bit cumbersome to use for both the C API, as well as bindings. By using
`GStrv`, we allow other languages to pass on string lists as they are
used to, while the bindings will make sure to do the right thing.

In the end, it makes the API a little bit simpler for everyone, and
reduces confusion for people who are used to working with string arrays
in other C/GLib based code (and not having 2 different types to denote
the same thing).

Related: https://gitlab.gnome.org/GNOME/gimp/-/issues/5919

plug-ins/help/help.c
@@ -135,27 +135,15 @@ help_create_procedure (GimpPlugIn  *plug_in,
                                       "Sven Neumann, Michael Natterer & Henrik Brix Andersen",
                                       "1999-2008");
 
-      GIMP_PROC_ARG_INT (procedure, "num-domain-names",
-                         "Num Domain Names",
-                         "Num domain names",
-                         0, G_MAXINT, 0,
-                         G_PARAM_READWRITE);
+      GIMP_PROC_ARG_STRV (procedure, "domain-names",
+                          "Domain Names",
+                          "Domain names",
+                          G_PARAM_READWRITE);
 
-      GIMP_PROC_ARG_STRING_ARRAY (procedure, "domain-names",
-                                  "Domain Names",
-                                  "Domain names",
-                                  G_PARAM_READWRITE);
-
-      GIMP_PROC_ARG_INT (procedure, "num-domain-uris",
-                         "Num Domain URIs",
-                         "Num domain URIs",
-                         0, G_MAXINT, 0,
-                         G_PARAM_READWRITE);
-
-      GIMP_PROC_ARG_STRING_ARRAY (procedure, "domain-uris",
-                                  "Domain URIs",
-                                  "Domain URIs",
-                                  G_PARAM_READWRITE);
+      GIMP_PROC_ARG_STRV (procedure, "domain-uris",
+                          "Domain URIs",
+                          "Domain URIs",
+                          G_PARAM_READWRITE);
     }
 
   return procedure;
@@ -170,10 +158,8 @@ help_run (GimpProcedure        *procedure,
 
   INIT_I18N ();
 
-  if (! gimp_help_init (GIMP_VALUES_GET_INT          (args, 0),
-                        GIMP_VALUES_GET_STRING_ARRAY (args, 1),
-                        GIMP_VALUES_GET_INT          (args, 2),
-                        GIMP_VALUES_GET_STRING_ARRAY (args, 3)))
+  if (! gimp_help_init (GIMP_VALUES_GET_STRV (args, 1),
+                        GIMP_VALUES_GET_STRV (args, 2)))
     {
       status = GIMP_PDB_CALLING_ERROR;
     }

app/core/gimp-memsize.c
@@ -230,24 +230,17 @@ gimp_g_value_get_memsize (GValue *value)
             memsize += sizeof (GimpArray) +
                        (array->static_data ? 0 : array->length);
         }
-      else if (GIMP_VALUE_HOLDS_STRING_ARRAY (value))
+      else if (G_VALUE_HOLDS (value, G_TYPE_STRV))
         {
-          GimpArray *array = g_value_get_boxed (value);
+          char **array = g_value_get_boxed (value);
 
           if (array)
             {
-              memsize += sizeof (GimpArray);
+              guint length = g_strv_length (array);
 
-              if (! array->static_data)
-                {
-                  gchar **tmp = (gchar **) array->data;
-                  gint    i;
-
-                  memsize += array->length * sizeof (gchar *);
-
-                  for (i = 0; i < array->length; i++)
-                    memsize += gimp_string_get_memsize (tmp[i]);
-                }
+              memsize += length * sizeof (gchar *);
+              for (gint i = 0; i < length; i++)
+                memsize += gimp_string_get_memsize (array[i]);
             }
         }
       else

app/core/gimp-parasites.c
@@ -79,18 +79,17 @@ list_func (const gchar    *key,
 }
 
 gchar **
-gimp_parasite_list (Gimp *gimp,
-                    gint *count)
+gimp_parasite_list (Gimp *gimp)
 {
+  gint    count;
   gchar **list;
   gchar **current;
 
   g_return_val_if_fail (GIMP_IS_GIMP (gimp), NULL);
-  g_return_val_if_fail (count != NULL, NULL);
 
-  *count = gimp_parasite_list_length (gimp->parasites);
+  count = gimp_parasite_list_length (gimp->parasites);
 
-  list = current = g_new (gchar *, *count);
+  list = current = g_new0 (gchar *, count + 1);
 
   gimp_parasite_list_foreach (gimp->parasites, (GHFunc) list_func, &current);
 

app/core/gimp-parasites.h
@@ -29,8 +29,7 @@ void                  gimp_parasite_detach       (Gimp               *gimp,
                                                   const gchar        *name);
 const GimpParasite  * gimp_parasite_find         (Gimp               *gimp,
                                                   const gchar        *name);
-gchar              ** gimp_parasite_list         (Gimp               *gimp,
-                                                  gint               *count);
+gchar              ** gimp_parasite_list         (Gimp               *gimp);
 
 void                  gimp_parasite_shift_parent (GimpParasite       *parasite);
 

app/core/gimpcontainer-filter.c
@@ -141,23 +141,21 @@ gimp_container_filter_by_name (GimpContainer  *container,
 
 gchar **
 gimp_container_get_filtered_name_array (GimpContainer *container,
-                                        const gchar   *regexp,
-                                        gint          *length)
+                                        const gchar   *regexp)
 {
   GimpContainer *weak;
   GError        *error = NULL;
 
   g_return_val_if_fail (GIMP_IS_CONTAINER (container), NULL);
-  g_return_val_if_fail (length != NULL, NULL);
 
   if (regexp == NULL || strlen (regexp) == 0)
-    return (gimp_container_get_name_array (container, length));
+    return (gimp_container_get_name_array (container));
 
   weak = gimp_container_filter_by_name (container, regexp, &error);
 
   if (weak)
     {
-      gchar **retval = gimp_container_get_name_array (weak, length);
+      gchar **retval = gimp_container_get_name_array (weak);
 
       g_object_unref (weak);
 
@@ -168,7 +166,6 @@ gimp_container_get_filtered_name_array (GimpContainer *container,
       g_warning ("%s", error->message);
       g_error_free (error);
 
-      *length = 0;
       return NULL;
     }
 }

app/core/gimpcontainer-filter.h
@@ -31,8 +31,7 @@ GimpContainer * gimp_container_filter_by_name (GimpContainer        *container,
 
 gchar        ** gimp_container_get_filtered_name_array
                                               (GimpContainer        *container,
-                                               const gchar          *regexp,
-                                               gint                 *length);
+                                               const gchar          *regexp);
 
 
 #endif  /* __GIMP_CONTAINER_FILTER_H__ */

app/core/gimpcontainer.c
@@ -996,20 +996,19 @@ gimp_container_get_name_array_foreach_func (GimpObject   *object,
 }
 
 gchar **
-gimp_container_get_name_array (GimpContainer *container,
-                               gint          *length)
+gimp_container_get_name_array (GimpContainer *container)
 {
   gchar **names;
   gchar **iter;
+  gint length;
 
   g_return_val_if_fail (GIMP_IS_CONTAINER (container), NULL);
-  g_return_val_if_fail (length != NULL, NULL);
 
-  *length = gimp_container_get_n_children (container);
-  if (*length == 0)
+  length = gimp_container_get_n_children (container);
+  if (length == 0)
     return NULL;
 
-  names = iter = g_new (gchar *, *length);
+  names = iter = g_new0 (gchar *, length + 1);
 
   gimp_container_foreach (container,
                           (GFunc) gimp_container_get_name_array_foreach_func,

app/core/gimpcontainer.h
@@ -129,8 +129,7 @@ gint         gimp_container_get_child_index    (GimpContainer           *contain
 GimpObject * gimp_container_get_neighbor_of    (GimpContainer           *container,
                                                 GimpObject              *object);
 
-gchar     ** gimp_container_get_name_array     (GimpContainer           *container,
-                                                gint                    *length);
+gchar     ** gimp_container_get_name_array     (GimpContainer           *container);
 
 GQuark       gimp_container_add_handler        (GimpContainer           *container,
                                                 const gchar             *signame,

commit cb47a720551a4ff15c7579c654fde570beb91859

Fix: unable to open online help
Recently it was not possibly in master to open online help (F1).

This is also mentioned in issue #7915. However, on macOS there are likely
also other problems, which is why I'm hesitant to close that issue with
this fix.

(help.exe:57964): LibGimpBase-CRITICAL **: 15:24:38.792:
gimp_value_array_index: assertion 'index < value_array->n_values' failed

(help.exe:57964): GLib-GObject-CRITICAL **: 15:24:38.792: g_value_get_boxed:
assertion 'G_VALUE_HOLDS_BOXED (value)' failed

This was most likely caused by 8eb7f6df9e903b6b544f4d949e97c67f915f8fe7

Changing this to use args 0 and 1 instead of 1 and 2 fixes the problem.

plug-ins/help/help.c
@@ -158,8 +158,8 @@ help_run (GimpProcedure        *procedure,
 
   INIT_I18N ();
 
-  if (! gimp_help_init (GIMP_VALUES_GET_STRV (args, 1),
-                        GIMP_VALUES_GET_STRV (args, 2)))
+  if (! gimp_help_init (GIMP_VALUES_GET_STRV (args, 0),
+                        GIMP_VALUES_GET_STRV (args, 1)))
     {
       status = GIMP_PDB_CALLING_ERROR;
     }