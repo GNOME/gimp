fc5dfa1f7589242b612dae59c4f22019fd47f1ae
Nikc <nikcdc@gmail.com>
2022-03-26 20:11:49 +0000

    pdb: Updating the parameter range for brightness

--- app/pdb/drawable-color-cmds.c
+++ app/pdb/drawable-color-cmds.c
@@ -749,7 +749,7 @@ register_drawable_color_procs (GimpPDB *pdb)
   gimp_procedure_set_static_strings (procedure,
                                      "gimp-drawable-brightness-contrast",
                                      "Modify brightness/contrast in the specified drawable.",
-                                     "This procedures allows the brightness and contrast of the specified drawable to be modified. Both 'brightness' and 'contrast' parameters are defined between -0.5 and 0.5.",
+                                     "This procedures allows the brightness and contrast of the specified drawable to be modified. Both 'brightness' and 'contrast' parameters are defined between -1.0 and 1.0.",
                                      "Spencer Kimball & Peter Mattis",
                                      "Spencer Kimball & Peter Mattis",
                                      "1997",
@@ -764,13 +764,13 @@ register_drawable_color_procs (GimpPDB *pdb)
                                g_param_spec_double ("brightness",
                                                     "brightness",
                                                     "Brightness adjustment",
-                                                    -0.5, 0.5, -0.5,
+                                                    -1.0, 1.0, -1.0,
                                                     GIMP_PARAM_READWRITE));
   gimp_procedure_add_argument (procedure,
                                g_param_spec_double ("contrast",
                                                     "contrast",
                                                     "Contrast adjustment",
-                                                    -0.5, 0.5, -0.5,
+                                                    -1.0, 1.0, -1.0,
                                                     GIMP_PARAM_READWRITE));
   gimp_pdb_register_procedure (pdb, procedure);
   g_object_unref (procedure);
