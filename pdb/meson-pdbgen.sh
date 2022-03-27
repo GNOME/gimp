79615541d93ad7480d4e9507c2d831425de783cc
Jehan <jehan@girinstud.io>
2022-03-27 22:07:32 +0200

    Issue #4291: meson: pdbgen not working.
    The build now successfully build the PDB files into the source folder
    itself. Unfortunately it seems I can't get meson dependencies to work
    properly, once more! I added a "sources" argument to the relevant
    library() or static_library() but it still uses old versions to build
    these. E.g. if I add an error on purpose to a pdb file, the next build
    still passes, yet the second-next fails (as it should have before).
    
    Note that I even tested a declare_dependency() with just the "sources"
    arguments, because it says "sources to add to targets (or generated
    header files that should be built before sources including them are
    built)" (so I assume it means that it should be trigger a rebuild,
    otherwise it's useless) but it's just not working. I'll investigate
    more.
    
    Still going with this for now, because at least generating the PDB
    source was a big miss until now. But we should

---
+++ pdb/meson-pdbgen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PERL="$1"
+top_srcdir="$2"
+top_builddir="$3"
+
+# Environment for the pdbgen.pl file.
+destdir=`cd "$top_srcdir" && pwd`
+export destdir
+builddir=`cd "$top_builddir" && pwd`
+export BUILD builddir
+
+cd "$top_srcdir"/pdb
+$PERL pdbgen.pl app lib
+echo `date` > $top_builddir/pdb/stamp-pdbgen
