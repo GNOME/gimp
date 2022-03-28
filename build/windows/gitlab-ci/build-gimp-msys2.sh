32498a72d638b6da50dd948a5ed4c1d9451c06f4
Jehan <jehan@girinstud.io>
2022-03-28 18:02:17 +0200

    build: also install intltool.
    Seems that many tools got moved into different packages these last few
    days at MSYS2!

--- build/windows/gitlab-ci/build-gimp-msys2.sh
+++ build/windows/gitlab-ci/build-gimp-msys2.sh
@@ -26,6 +26,7 @@ pacman --noconfirm -S --needed \
     mingw-w64-$MSYS2_ARCH-toolchain \
     mingw-w64-$MSYS2_ARCH-autotools \
     mingw-w64-$MSYS2_ARCH-ccache \
+    mingw-w64-$MSYS2_ARCH-intltool \
     \
     mingw-w64-$MSYS2_ARCH-appstream-glib \
     mingw-w64-$MSYS2_ARCH-atk \
