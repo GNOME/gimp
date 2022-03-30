6ae69f5e84b36300e89a01e08c913329fcf95a9d
Jehan <jehan@girinstud.io>
2022-03-31 00:14:51 +0200

    build: do not build file-mng for 32-bit Windows.
    We didn't need to do this on the autotools build, simply because the
    configure step is much more elaborated there, and was checking for the
    header file as well as well as a working mng_create() API. But since
    libmng was broken, the test failed, so we didn't need to disable it.
    
    By the way, we should check when the `.pc` file was added, because if it
    was after the required version, then the meson test is very wrong. It
    should not have been different from the autotools file.

--- build/windows/gitlab-ci/build-gimp-msys2.sh
+++ build/windows/gitlab-ci/build-gimp-msys2.sh
@@ -6,7 +6,8 @@ if [[ "$MSYSTEM" == "MINGW32" ]]; then
     export ARTIFACTS_SUFFIX="-w32"
     export MSYS2_ARCH="i686"
     export MSYS2_PREFIX="/c/msys64/mingw32"
-    export GIMP_OPTIONS="-Dvala-plugins=disabled"
+    # 32-bit libmng MSYS2 package is broken: https://github.com/msys2/MINGW-packages/issues/11136
+    export GIMP_OPTIONS="-Dvala-plugins=disabled -Dmng=disabled"
 else
     export ARTIFACTS_SUFFIX="-w64"
     export MSYS2_ARCH="x86_64"
