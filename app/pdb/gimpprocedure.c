GNOME/gimp

commit 8eb7f6df9e903b6b544f4d949e97c67f915f8fe7

Remove GimpStringArray in favor of GStrv
GLib has a specific type of NULL-terminated string arrays:
`G_TYPE_STRV`, which is the `GType` of `char**` aka `GStrv`.

By using this type, we can avoid having a `GimpStringArray` which is a
bit cumbersome to use for both the C API, as well as bindings. By using
`GStrv`, we allow other languages to pass on string lists as they are
used to, while the bindings will make sure to do the right thing.

In the end, it makes the API a little bit simpler for everyone, and
reduces confusion for people who are used to working with string arrays
in other C/GLib based code (and not having 2 different types to denote
the same thing).

Related: https://gitlab.gnome.org/GNOME/gimp/-/issues/5919

app/pdb/gimpprocedure.c
@@ -986,7 +986,8 @@ gimp_procedure_validate_args (GimpProcedure  *procedure,
 
           /*  UTT-8 validate all strings  */
           if (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_PARAM_STRING ||
-              G_PARAM_SPEC_TYPE (pspec) == GIMP_TYPE_PARAM_STRING_ARRAY)
+              (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_BOXED &&
+               G_PARAM_SPEC_VALUE_TYPE (pspec) == G_TYPE_STRV))
             {
               gboolean valid = TRUE;
 

libgimp/gimpprocedure.c
@@ -2072,7 +2072,8 @@ gimp_procedure_validate_args (GimpProcedure   *procedure,
 
           /*  UTT-8 validate all strings  */
           if (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_PARAM_STRING ||
-              G_PARAM_SPEC_TYPE (pspec) == GIMP_TYPE_PARAM_STRING_ARRAY)
+              (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_BOXED &&
+               G_PARAM_SPEC_VALUE_TYPE (pspec) == G_TYPE_STRV))
             {
               gboolean valid = TRUE;
 
@@ -2085,17 +2086,11 @@ gimp_procedure_validate_args (GimpProcedure   *procedure,
                 }
               else
                 {
-                  const GimpArray *array = g_value_get_boxed (arg);
+                  const char **strings = g_value_get_boxed (arg);
 
-                  if (array)
-                    {
-                      const gchar **strings = (const gchar **) array->data;
-                      gint          i;
-
-                      for (i = 0; i < array->length && valid; i++)
-                        if (strings[i])
-                          valid = g_utf8_validate (strings[i], -1, NULL);
-                    }
+                  for (const char **sp = strings; sp && *sp && valid; sp++)
+                    if (*sp)
+                      valid = g_utf8_validate (*sp, -1, NULL);
                 }
 
               if (! valid)

app/core/gimp-memsize.c
@@ -230,24 +230,17 @@ gimp_g_value_get_memsize (GValue *value)
             memsize += sizeof (GimpArray) +
                        (array->static_data ? 0 : array->length);
         }
-      else if (GIMP_VALUE_HOLDS_STRING_ARRAY (value))
+      else if (G_VALUE_HOLDS (value, G_TYPE_STRV))
         {
-          GimpArray *array = g_value_get_boxed (value);
+          char **array = g_value_get_boxed (value);
 
           if (array)
             {
-              memsize += sizeof (GimpArray);
+              guint length = g_strv_length (array);
 
-              if (! array->static_data)
-                {
-                  gchar **tmp = (gchar **) array->data;
-                  gint    i;
-
-                  memsize += array->length * sizeof (gchar *);
-
-                  for (i = 0; i < array->length; i++)
-                    memsize += gimp_string_get_memsize (tmp[i]);
-                }
+              memsize += length * sizeof (gchar *);
+              for (gint i = 0; i < length; i++)
+                memsize += gimp_string_get_memsize (array[i]);
             }
         }
       else

app/core/gimp-parasites.c
@@ -79,18 +79,17 @@ list_func (const gchar    *key,
 }
 
 gchar **
-gimp_parasite_list (Gimp *gimp,
-                    gint *count)
+gimp_parasite_list (Gimp *gimp)
 {
+  gint    count;
   gchar **list;
   gchar **current;
 
   g_return_val_if_fail (GIMP_IS_GIMP (gimp), NULL);
-  g_return_val_if_fail (count != NULL, NULL);
 
-  *count = gimp_parasite_list_length (gimp->parasites);
+  count = gimp_parasite_list_length (gimp->parasites);
 
-  list = current = g_new (gchar *, *count);
+  list = current = g_new0 (gchar *, count + 1);
 
   gimp_parasite_list_foreach (gimp->parasites, (GHFunc) list_func, &current);
 

app/core/gimp-parasites.h
@@ -29,8 +29,7 @@ void                  gimp_parasite_detach       (Gimp               *gimp,
                                                   const gchar        *name);
 const GimpParasite  * gimp_parasite_find         (Gimp               *gimp,
                                                   const gchar        *name);
-gchar              ** gimp_parasite_list         (Gimp               *gimp,
-                                                  gint               *count);
+gchar              ** gimp_parasite_list         (Gimp               *gimp);
 
 void                  gimp_parasite_shift_parent (GimpParasite       *parasite);
 

app/core/gimpcontainer-filter.c
@@ -141,23 +141,21 @@ gimp_container_filter_by_name (GimpContainer  *container,
 
 gchar **
 gimp_container_get_filtered_name_array (GimpContainer *container,
-                                        const gchar   *regexp,
-                                        gint          *length)
+                                        const gchar   *regexp)
 {
   GimpContainer *weak;
   GError        *error = NULL;
 
   g_return_val_if_fail (GIMP_IS_CONTAINER (container), NULL);
-  g_return_val_if_fail (length != NULL, NULL);
 
   if (regexp == NULL || strlen (regexp) == 0)
-    return (gimp_container_get_name_array (container, length));
+    return (gimp_container_get_name_array (container));
 
   weak = gimp_container_filter_by_name (container, regexp, &error);
 
   if (weak)
     {
-      gchar **retval = gimp_container_get_name_array (weak, length);
+      gchar **retval = gimp_container_get_name_array (weak);
 
       g_object_unref (weak);
 
@@ -168,7 +166,6 @@ gimp_container_get_filtered_name_array (GimpContainer *container,
       g_warning ("%s", error->message);
       g_error_free (error);
 
-      *length = 0;
       return NULL;
     }
 }

app/core/gimpcontainer-filter.h
@@ -31,8 +31,7 @@ GimpContainer * gimp_container_filter_by_name (GimpContainer        *container,
 
 gchar        ** gimp_container_get_filtered_name_array
                                               (GimpContainer        *container,
-                                               const gchar          *regexp,
-                                               gint                 *length);
+                                               const gchar          *regexp);
 
 
 #endif  /* __GIMP_CONTAINER_FILTER_H__ */

app/core/gimpcontainer.c
@@ -996,20 +996,19 @@ gimp_container_get_name_array_foreach_func (GimpObject   *object,
 }
 
 gchar **
-gimp_container_get_name_array (GimpContainer *container,
-                               gint          *length)
+gimp_container_get_name_array (GimpContainer *container)
 {
   gchar **names;
   gchar **iter;
+  gint length;
 
   g_return_val_if_fail (GIMP_IS_CONTAINER (container), NULL);
-  g_return_val_if_fail (length != NULL, NULL);
 
-  *length = gimp_container_get_n_children (container);
-  if (*length == 0)
+  length = gimp_container_get_n_children (container);
+  if (length == 0)
     return NULL;
 
-  names = iter = g_new (gchar *, *length);
+  names = iter = g_new0 (gchar *, length + 1);
 
   gimp_container_foreach (container,
                           (GFunc) gimp_container_get_name_array_foreach_func,

commit 2d192ae80438566bc602eaecafea62952a764692

app: fix GStrv argument validation in PDB calls.
Bugs introduced in commit 8eb7f6df9e9:

- The type test was wrong.
- The UTF-8 validation test was also wrong since it was still working on
  a GimpArray even though the data was now a GStrv.

Also I stop at the first invalid UTF-8 string element, otherwise later
valid strings may hide previous invalid ones.

This bug was mostly invisible since we don't have any core PDB API with
GStrv parameter so far, only GStrv return value.

app/pdb/gimpprocedure.c
@@ -986,7 +986,7 @@ gimp_procedure_validate_args (GimpProcedure  *procedure,
 
           /*  UTT-8 validate all strings  */
           if (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_PARAM_STRING ||
-              (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_BOXED &&
+              (G_PARAM_SPEC_TYPE (pspec) == G_TYPE_PARAM_BOXED &&
                G_PARAM_SPEC_VALUE_TYPE (pspec) == G_TYPE_STRV))
             {
               gboolean valid = TRUE;
@@ -1000,16 +1000,18 @@ gimp_procedure_validate_args (GimpProcedure  *procedure,
                 }
               else
                 {
-                  const GimpArray *array = g_value_get_boxed (arg);
+                  const gchar **array = g_value_get_boxed (arg);
 
                   if (array)
                     {
-                      const gchar **strings = (const gchar **) array->data;
-                      gint          i;
+                      gint i;
 
-                      for (i = 0; i < array->length && valid; i++)
-                        if (strings[i])
-                          valid = g_utf8_validate (strings[i], -1, NULL);
+                      for (i = 0; array[i]; i++)
+                        {
+                          valid = g_utf8_validate (array[i], -1, NULL);
+                          if (! valid)
+                            break;
+                        }
                     }
                 }
 