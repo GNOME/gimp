24886f91b1e55ab2f79327a00bb53729ddfde7b9
Jacob Boerema <jgboerema@gmail.com>
2022-08-24 13:58:27 -0400

    plug-ins: fix #8508 Print exporting to PSD seems to export as cm ...
    and import as inches
    
    When GIMP's display unit was mm, our PSD export converted the X and Y
    resolution as if a conversion from mm to inch was needed.
    
    However, PSD's resolution is always measured in pixels per inch, so a
    conversion is never necessary.
    Removing the conversion, resolves the issue.

--- plug-ins/file-psd/psd-save.c
+++ plug-ins/file-psd/psd-save.c
@@ -834,11 +834,6 @@ save_resources (GOutputStream  *output,
 
     if (g_unit == GIMP_UNIT_MM)
       {
-        gdouble factor = gimp_unit_get_factor (g_unit) / 10.0;
-
-        xres /= factor;
-        yres /= factor;
-
         psd_unit = PSD_UNIT_CM;
       }
     else
@@ -846,6 +841,8 @@ save_resources (GOutputStream  *output,
         psd_unit = PSD_UNIT_INCH;
       }
 
+    /* Don't convert resolution based on g_unit which is a display unit.
+     * PSD resolution is always in pixels/inch. */
     xres_fix = xres * 65536.0 + .5; /* Convert to 16.16 fixed point */
     yres_fix = yres * 65536.0 + .5; /* Convert to 16.16 fixed point */
 
