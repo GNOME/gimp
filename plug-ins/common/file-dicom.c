543f9e37e69b4e57b5e44092542c95bb2052f047
Daniel Novomeský <dnovomesky@gmail.com>
2022-11-28 18:04:57 +0100

    plug-ins: fix return value
    -1 should be returned in case of error, not NULL.
    It caused incompatible pointer to integer conversion with
    clang version 15.0.3

--- plug-ins/common/file-dicom.c
+++ plug-ins/common/file-dicom.c
@@ -602,7 +602,7 @@ load_image (const gchar  *filename,
                                (gchar *) value, samples_per_pixel);
                   g_free (dicominfo);
                   fclose (DICOM);
-                  return NULL;
+                  return -1;
                 }
 
               break;
