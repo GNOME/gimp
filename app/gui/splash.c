a4541d8a7aaff8b5dac5056acce4390373b4245d
Alx Sa <cmyk.student@gmail.com>
2023-05-20 16:20:00 +0000

    gui: Fix upper text scaling for small images
    Resolves #5742.
    From fa0a0212, the splash's upper text was scaled to PANGO_SCALE_MEDIUM
    both when the image width was 2x or less than 2x. This was likely a
    copy/paste issue, as the lower text code scaling does not duplicate.
    This patch fixes it by changing the else condition to scale to
    PANGO_SCALE_SMALL.
    Note that this change is unlikely to be seen as it requires a very small
    splash screen image to reach the else condition.

--- app/gui/splash.c
+++ app/gui/splash.c
@@ -216,7 +216,7 @@ splash_create (Gimp         *gimp,
   else if (splash->width > 2 * ink.width)
     gimp_pango_layout_set_scale (splash->upper, PANGO_SCALE_MEDIUM);
   else
-    gimp_pango_layout_set_scale (splash->upper, PANGO_SCALE_MEDIUM);
+    gimp_pango_layout_set_scale (splash->upper, PANGO_SCALE_SMALL);
 
   splash->lower = gtk_widget_create_pango_layout (splash->area,
                                                   MEASURE_LOWER);
