63076ba342123faca9bf09d008cf4cda3565b62d
Jehan <jehan@girinstud.io>
2023-08-12 21:09:30 +0200

    devel-docs: add URLs to babl and GEGL gi-docgen documentation.
    Note that these links are not valid yet. For now, documentations are only in
    testing:
    * babl: https://testing.developer.gimp.org/api/babl/
    * GEGL: https://testing.developer.gimp.org/api/gegl/
    
    The main links will appear next time we merge testing into the main developer
    website, or at the next release of GIMP.

--- devel-docs/reference/gimp/urlmap.js
+++ devel-docs/reference/gimp/urlmap.js
@@ -1,9 +1,11 @@
 // A map between namespaces and base URLs for their online gi-docgen documentation
 baseURLs = [
-  [ 'GLib', 'https://docs.gtk.org/glib/' ],
-  [ 'GObject', 'https://docs.gtk.org/gobject/' ],
-  [ 'Gdk', 'https://docs.gtk.org/gdk3/' ],
+  [ 'Babl',      'https://developer.gimp.org/api/babl/' ],
+  [ 'Gegl',      'https://developer.gimp.org/api/gegl/' ],
+  [ 'GLib',      'https://docs.gtk.org/glib/' ],
+  [ 'GObject',   'https://docs.gtk.org/gobject/' ],
+  [ 'Gdk',       'https://docs.gtk.org/gdk3/' ],
   [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
-  [ 'Gio', 'https://docs.gtk.org/gio/' ],
-  [ 'Gtk', 'https://docs.gtk.org/gtk3/' ],
+  [ 'Gio',       'https://docs.gtk.org/gio/' ],
+  [ 'Gtk',       'https://docs.gtk.org/gtk3/' ],
 ]
