GNOME/gimp

commit 3e101922d8e64a41978b17e3a2a525cb104edb60

widgets: Prevent Toolbox autoscroll bug
In Preferences > Toolbox, clicking on an item below the
initial scroll window view causes it to jump to the top
automatically. This patch prevents this by setting the
clicked index in the GtkTreeView before grabbing focus.

app/widgets/gimpcontainertreeview.c
@@ -1268,9 +1268,6 @@ gimp_container_tree_view_button (GtkWidget             *widget,
 
   tree_view->priv->dnd_renderer = NULL;
 
-  if (bevent->type != GDK_BUTTON_RELEASE && ! gtk_widget_has_focus (widget))
-    gtk_widget_grab_focus (widget);
-
   if (gtk_tree_view_get_path_at_pos (GTK_TREE_VIEW (widget),
                                      bevent->x, bevent->y,
                                      &path, &column, NULL, NULL))
@@ -1284,6 +1281,13 @@ gimp_container_tree_view_button (GtkWidget             *widget,
       gboolean                  handled = TRUE;
       gboolean                  multisel_mode;
       GdkModifierType           modifiers = (bevent->state & gimp_get_all_modifiers_mask ());
+
+      /* Confirm the path is set before grabbing focus, as it can cause
+       * the list to auto-scroll to the top on first click otherwise
+       */
+      gtk_tree_view_set_cursor (GTK_TREE_VIEW (widget), path, NULL, FALSE);
+      if (bevent->type != GDK_BUTTON_RELEASE && ! gtk_widget_has_focus (widget))
+        gtk_widget_grab_focus (widget);
 
       multisel_mode = (gtk_tree_selection_get_mode (tree_view->priv->selection)
                        == GTK_SELECTION_MULTIPLE);

commit 1e39acbac0ec0d6ebf9092598348ef86a6514503

widgets: Fix multi-select bug caused by 3e101922
(Diagnosed by @Jehan)
The call to gtk_tree_view_set_cursor () causes the selection to revert to
single selection mode; this prevents users from multi-selecting options
in lists that support it.
To fix this while preventing the original bug fixed in 3e101922, the call to
gtk_widget_grab_focus () is made via g_signal_connect_after ().

app/widgets/gimpcontainertreeview.c
@@ -705,6 +705,9 @@ gimp_container_tree_view_set_container (GimpContainerView *view,
       g_signal_connect (tree_view->view, "button-press-event",
                         G_CALLBACK (gimp_container_tree_view_button),
                         tree_view);
+      g_signal_connect_after (tree_view->view, "button-press-event",
+                              G_CALLBACK (gtk_widget_grab_focus),
+                              tree_view);
       g_signal_connect (tree_view->view, "button-release-event",
                         G_CALLBACK (gimp_container_tree_view_button),
                         tree_view);
@@ -1281,13 +1284,6 @@ gimp_container_tree_view_button (GtkWidget             *widget,
       gboolean                  handled = TRUE;
       gboolean                  multisel_mode;
       GdkModifierType           modifiers = (bevent->state & gimp_get_all_modifiers_mask ());
-
-      /* Confirm the path is set before grabbing focus, as it can cause
-       * the list to auto-scroll to the top on first click otherwise
-       */
-      gtk_tree_view_set_cursor (GTK_TREE_VIEW (widget), path, NULL, FALSE);
-      if (bevent->type != GDK_BUTTON_RELEASE && ! gtk_widget_has_focus (widget))
-        gtk_widget_grab_focus (widget);
 
       multisel_mode = (gtk_tree_selection_get_mode (tree_view->priv->selection)
                        == GTK_SELECTION_MULTIPLE);