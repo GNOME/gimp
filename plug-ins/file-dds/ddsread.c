GNOME/gimp

commit 7db71cd0b6e36c454aa0d2d3efeec7e636db4dbc

plug-ins: Fix DDS vulnerability (ZDI-CAN-22093)
Resolves #10069

Currently, the DDS header information for the width, height, and bytes per scan line
are read in and assumed to be correct. As these values are used for memory allocation
and reading, it would be good to verify they do not exceed the file size.

This patch adds a condition after the header is read in to verify those values. If they exceed
the file size (mins an offset), the file is not read in and an error message is shown.

plug-ins/file-dds/ddsread.c
@@ -109,6 +109,7 @@ read_dds (gchar    *filename,
   guchar *pixels;
   gchar *tmp;
   FILE *fp;
+  gsize file_size;
   dds_header_t hdr;
   dds_header_dx10_t dx10hdr;
   dds_load_info_t d;
@@ -129,6 +130,10 @@ read_dds (gchar    *filename,
       g_message ("Error opening file.\n");
       return GIMP_PDB_EXECUTION_ERROR;
     }
+
+  fseek (fp, 0L, SEEK_END);
+  file_size = ftell (fp);
+  fseek (fp, 0, SEEK_SET);
 
   if (strrchr (filename, '/'))
     tmp = g_strdup_printf ("Loading %s:", strrchr (filename, '/') + 1);
@@ -184,6 +189,16 @@ read_dds (gchar    *filename,
         {
           hdr.pitch_or_linsize = hdr.height * hdr.width * (hdr.pixelfmt.bpp >> 3);
         }
+    }
+
+  /* verify header information is accurate */
+  if (hdr.depth < 1                                       ||
+      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))) ||
+      (((guint64) hdr.height * hdr.width * hdr.depth) > (file_size - sizeof (hdr))))
+    {
+      fclose (fp);
+      g_message ("Invalid or corrupted DDS header\n");
+      return GIMP_PDB_EXECUTION_ERROR;
     }
 
   if (hdr.pixelfmt.flags & DDPF_FOURCC)

commit e92f279c97282a2b20dca0d923db7465f2057703

plug-ins: Fix DDS import regression from 7db71cd0
@Wormnest pointed out that compressed files are likely smaller than
width * height * bps, so our check to prevent ZDI-CAN-22093
also caught valid files.
The size check is removed from load_image () and moved to load_layer ()
before the two fread() functions, as we know exactly how much we'll
try to read at that point.
(Backport of 8faad92e)

plug-ins/file-dds/ddsread.c
@@ -191,16 +191,6 @@ read_dds (gchar    *filename,
         }
     }
 
-  /* verify header information is accurate */
-  if (hdr.depth < 1                                       ||
-      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))) ||
-      (((guint64) hdr.height * hdr.width * hdr.depth) > (file_size - sizeof (hdr))))
-    {
-      fclose (fp);
-      g_message ("Invalid or corrupted DDS header\n");
-      return GIMP_PDB_EXECUTION_ERROR;
-    }
-
   if (hdr.pixelfmt.flags & DDPF_FOURCC)
     {
       /* fourcc is dXt* or rXgb */
@@ -308,6 +298,15 @@ read_dds (gchar    *filename,
   else
     {
       precision = GIMP_PRECISION_U8_GAMMA;
+    }
+
+  /* verify header information is accurate */
+  if (d.bpp < 1 ||
+      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))))
+    {
+      fclose (fp);
+      g_message ("Invalid or corrupted DDS header\n");
+      return GIMP_PDB_EXECUTION_ERROR;
     }
 
   image = gimp_image_new_with_precision (hdr.width, hdr.height, type, precision);
@@ -923,6 +922,13 @@ load_layer (FILE            *fp,
   unsigned int   size = hdr->pitch_or_linsize >> (2 * level);
   unsigned int   layerw;
   int            format = DDS_COMPRESS_NONE;
+  gsize          file_size;
+  gsize          current_position;
+
+  current_position = ftell (fp);
+  fseek (fp, 0L, SEEK_END);
+  file_size = ftell (fp);
+  fseek (fp, current_position, SEEK_SET);
 
   if (width < 1) width = 1;
   if (height < 1) height = 1;
@@ -1027,6 +1033,12 @@ load_layer (FILE            *fp,
         size *= 16;
     }
 
+  if (size > (file_size - current_position))
+    {
+      g_message ("Requested data exceeds size of file.\n");
+      return 0;
+    }
+
   if ((hdr->flags & DDSD_LINEARSIZE) &&
       !fread (buf, size, 1, fp))
     {
@@ -1063,6 +1075,13 @@ load_layer (FILE            *fp,
                                bablfmt, pixels, GEGL_AUTO_ROWSTRIDE);
               n = 0;
               gimp_progress_update ((double)y / (double)hdr->height);
+            }
+
+          current_position = ftell (fp);
+          if ((width * d->bpp) > (file_size - current_position))
+            {
+              g_message ("Requested data exceeds size of file.\n");
+              return 0;
             }
 
           if ((hdr->flags & DDSD_PITCH) &&