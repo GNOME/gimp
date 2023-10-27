GNOME/gimp

commit 6ad54ca3a30fb1a155f902ddd1b44c389cb0f382

plug-ins: Fix DDS vulnerability (ZDI-CAN-22093)
Resolves #10069

Currently, the DDS header information for the width, height, and bytes per scan line
are read in and assumed to be correct. As these values are used for memory allocation
and reading, it would be good to verify they do not exceed the file size.

This patch adds a condition after the header is read in to verify those values. If they exceed
the file size (mins an offset), the file is not read in and an error message is shown.

plug-ins/file-dds/ddsread.c
@@ -124,6 +124,7 @@ read_dds (GFile          *file,
   guint              l = 0;
   guchar            *pixels;
   FILE              *fp;
+  gsize              file_size;
   dds_header_t       hdr;
   dds_header_dx10_t  dx10hdr;
   dds_load_info_t    d;
@@ -156,6 +157,10 @@ read_dds (GFile          *file,
                    gimp_file_get_utf8_name (file), g_strerror (errno));
       return GIMP_PDB_EXECUTION_ERROR;
     }
+
+  fseek (fp, 0L, SEEK_END);
+  file_size = ftell (fp);
+  fseek (fp, 0, SEEK_SET);
 
   gimp_progress_init_printf ("Loading %s:", gimp_file_get_utf8_name (file));
 
@@ -205,6 +210,17 @@ read_dds (GFile          *file,
         {
           hdr.pitch_or_linsize = hdr.height * hdr.width * (hdr.pixelfmt.bpp >> 3);
         }
+    }
+
+  /* verify header information is accurate */
+  if (hdr.depth < 1                                       ||
+      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))) ||
+      (((guint64) hdr.height * hdr.width * hdr.depth) > (file_size - sizeof (hdr))))
+    {
+      fclose (fp);
+      g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errno),
+                   _("Invalid or corrupted DDS header"));
+      return GIMP_PDB_EXECUTION_ERROR;
     }
 
   if (hdr.pixelfmt.flags & DDPF_FOURCC)

commit 8faad92ee9f265a273bfc67d03d51d3159fb8396

plug-ins: Fix DDS import regression from 6ad54ca3
@Wormnest pointed out that compressed files are likely smaller than
width * height * bps, so our check to prevent ZDI-CAN-22093
also caught valid files.
The size check is removed from load_image () and moved to load_layer ()
before the two fread() functions, as we know exactly how much we'll
try to read at that point.

plug-ins/file-dds/ddsread.c
@@ -364,9 +364,8 @@ read_dds (GFile          *file,
     }
 
   /* verify header information is accurate */
-  if (d.bpp < 1                                           ||
-      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))) ||
-      (((guint64) hdr.height * hdr.width * d.gimp_bps) > (file_size - sizeof (hdr))))
+  if (d.bpp < 1 ||
+      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))))
     {
       fclose (fp);
       g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errno),
@@ -1028,6 +1027,13 @@ load_layer (FILE             *fp,
   guint          size = hdr->pitch_or_linsize >> (2 * level);
   guint          layerw;
   gint           format = DDS_COMPRESS_NONE;
+  gsize          file_size;
+  gsize          current_position;
+
+  current_position = ftell (fp);
+  fseek (fp, 0L, SEEK_END);
+  file_size = ftell (fp);
+  fseek (fp, current_position, SEEK_SET);
 
   if (width < 1) width = 1;
   if (height < 1) height = 1;
@@ -1144,6 +1150,13 @@ load_layer (FILE             *fp,
         size *= 16;
     }
 
+  if (size > (file_size - current_position))
+    {
+      g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
+                   _("Requested data exceeds size of file.\n"));
+      return FALSE;
+    }
+
   if ((hdr->flags & DDSD_LINEARSIZE) &&
       !fread (buf, size, 1, fp))
     {
@@ -1181,6 +1194,14 @@ load_layer (FILE             *fp,
                                bablfmt, pixels, GEGL_AUTO_ROWSTRIDE);
               n = 0;
               gimp_progress_update ((double) y / (double) hdr->height);
+            }
+
+          current_position = ftell (fp);
+          if ((width * d->bpp) > (file_size - current_position))
+            {
+              g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
+                           _("Requested data exceeds size of file.\n"));
+              return FALSE;
             }
 
           if ((hdr->flags & DDSD_PITCH) &&