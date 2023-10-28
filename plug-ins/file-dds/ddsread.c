GNOME/gimp

commit 8faad92ee9f265a273bfc67d03d51d3159fb8396

plug-ins: Fix DDS import regression from 6ad54ca3
@Wormnest pointed out that compressed files are likely smaller than
width * height * bps, so our check to prevent ZDI-CAN-22093
also caught valid files.
The size check is removed from load_image () and moved to load_layer ()
before the two fread() functions, as we know exactly how much we'll
try to read at that point.

plug-ins/file-dds/ddsread.c
@@ -364,9 +364,8 @@ read_dds (GFile          *file,
     }
 
   /* verify header information is accurate */
-  if (d.bpp < 1                                           ||
-      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))) ||
-      (((guint64) hdr.height * hdr.width * d.gimp_bps) > (file_size - sizeof (hdr))))
+  if (d.bpp < 1 ||
+      (hdr.pitch_or_linsize > (file_size - sizeof (hdr))))
     {
       fclose (fp);
       g_set_error (error, G_FILE_ERROR, g_file_error_from_errno (errno),
@@ -1028,6 +1027,13 @@ load_layer (FILE             *fp,
   guint          size = hdr->pitch_or_linsize >> (2 * level);
   guint          layerw;
   gint           format = DDS_COMPRESS_NONE;
+  gsize          file_size;
+  gsize          current_position;
+
+  current_position = ftell (fp);
+  fseek (fp, 0L, SEEK_END);
+  file_size = ftell (fp);
+  fseek (fp, current_position, SEEK_SET);
 
   if (width < 1) width = 1;
   if (height < 1) height = 1;
@@ -1144,6 +1150,13 @@ load_layer (FILE             *fp,
         size *= 16;
     }
 
+  if (size > (file_size - current_position))
+    {
+      g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
+                   _("Requested data exceeds size of file.\n"));
+      return FALSE;
+    }
+
   if ((hdr->flags & DDSD_LINEARSIZE) &&
       !fread (buf, size, 1, fp))
     {
@@ -1181,6 +1194,14 @@ load_layer (FILE             *fp,
                                bablfmt, pixels, GEGL_AUTO_ROWSTRIDE);
               n = 0;
               gimp_progress_update ((double) y / (double) hdr->height);
+            }
+
+          current_position = ftell (fp);
+          if ((width * d->bpp) > (file_size - current_position))
+            {
+              g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
+                           _("Requested data exceeds size of file.\n"));
+              return FALSE;
             }
 
           if ((hdr->flags & DDSD_PITCH) &&

commit 83d13c600454b8afd271e0201baf902bdb4c148b

plug-ins: Additional fixes for DDS Import
@Wormnest noted remaining regressions after 8faad92e.
The second fread() only runs if the DDSD_PITCH flag is set,
so the error handling check should also be conditional.
Additionally, the ZDI-CAN-22093 exploit no longer runs but
still could cause a plug-in crash. This patch adds an additional
check to ensure the buffer size was within bounds.

plug-ins/file-dds/ddsread.c
@@ -1150,7 +1150,8 @@ load_layer (FILE             *fp,
         size *= 16;
     }
 
-  if (size > (file_size - current_position))
+  if (size > (file_size - current_position) ||
+      size > hdr->pitch_or_linsize)
     {
       g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
                    _("Requested data exceeds size of file.\n"));
@@ -1197,7 +1198,9 @@ load_layer (FILE             *fp,
             }
 
           current_position = ftell (fp);
-          if ((width * d->bpp) > (file_size - current_position))
+          if ((hdr->flags & DDSD_PITCH)                          &&
+              ((width * d->bpp) > (file_size - current_position) ||
+               (width * d->bpp) > hdr->pitch_or_linsize))
             {
               g_set_error (error, G_FILE_ERROR, G_FILE_ERROR_FAILED,
                            _("Requested data exceeds size of file.\n"));