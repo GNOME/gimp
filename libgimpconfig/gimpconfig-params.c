6cca73516aa79d4e46224305b832ac0ac8b645fc
Jacob Boerema <jgboerema@gmail.com>
2023-11-28 15:38:47 -0500

    libgimpconfig: fix #10278 Script-Fu API display commands are failing
    on input arguments
    
    This is the same issue as #10194 but for GimpDisplay. We fix it in
    the same manner.

--- libgimpconfig/gimpconfig-params.c
+++ libgimpconfig/gimpconfig-params.c
@@ -351,6 +351,7 @@ gimp_config_param_spec_duplicate (GParamSpec *pspec)
            * with type names instead.
            */
           g_strcmp0 (type_name, "GimpImage")     == 0 ||
+          g_strcmp0 (type_name, "GimpDisplay")   == 0 ||
           g_strcmp0 (type_name, "GimpDrawable")  == 0 ||
           g_strcmp0 (type_name, "GimpLayer")     == 0 ||
           g_strcmp0 (type_name, "GimpTextLayer") == 0 ||
