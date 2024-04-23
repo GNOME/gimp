42356db4b49a285974f2acc9d0ab2e0178f2068c
Bruno Lopes <brunolopesdsilv@outlook.com>
2024-04-23 07:47:50 -0300

    gitlab-ci, build: Clarify DWARF debug symbols "extraction"
    Partially reverts af79bbe0 (regarding .debug "extraction" in cross builds)
    
    Now, we call the split debug script from the main bundling script, which makes
    similar to our macOS .app bundling script. This cleans a bit of code in .yml
    and make things clearer to the mere mortals in the scripts.

--- build/windows/gitlab-ci/3_bundle-gimp-uni_sym.sh
+++ build/windows/gitlab-ci/3_bundle-gimp-uni_sym.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Generate .debug
-find . \( -iname '*.dll' -or -iname '*.exe' -or -iname '*.pyd' \) -type f -exec objcopy --only-keep-debug '{}' '{}'.debug \;
-find . \( -iname '*.dll' -or -iname '*.exe' -or -iname '*.pyd' \) -type f -exec objcopy --add-gnu-debuglink='{}'.debug '{}' --strip-unneeded \;
-
-# Copy .debug to .debug folder
-dbgList=$(find . -iname '*.debug') && dbgArray=($dbgList)
-for dbg in "${dbgArray[@]}"; do
-	FP="$dbg" 0> /dev/null
-	NAME="${FP##*/}" 0> /dev/null
-	DIR="${FP%/*}" 0> /dev/null
-	echo "$FP -> $DIR/.debug" 0> /dev/null
-	if [ ! -d "$DIR/.debug" ]; then
+# Copy .debug to corresponding .debug folder
+# (this is done in a separate script to reduce output)
+while [ -n "$1" ]
+do
+	FP="$1"
+	NAME="${FP##*/}"
+	DIR="${FP%/*}"
+	echo "$FP -> $DIR/.debug"
+	if [ ! -d "$DIR/.debug" ]
+	then
 		mkdir "$DIR/.debug"
 	fi
 	mv "$FP" "$DIR/.debug"
+	shift
 done
