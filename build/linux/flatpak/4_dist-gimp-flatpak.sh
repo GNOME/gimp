95d517a135cd229d760192b4d52f7daecc6d2bcc
Bruno <brunvonlope@outlook.com>
2024-07-07 17:15:55 -0300

    Test publishing outside master branch

--- build/linux/flatpak/4_dist-gimp-flatpak.sh
+++ build/linux/flatpak/4_dist-gimp-flatpak.sh
@@ -9,11 +9,6 @@
 # (it is NOT a "real"/full bundle, deps from GNOME runtime are not bundled)
 flatpak build-bundle repo gimp-git.flatpak --runtime-repo=https://nightly.gnome.org/gnome-nightly.flatpakrepo org.gimp.GIMP ${BRANCH}
 
-# Only distribute on GNOME nightly if from 'master' branch
-if [ "$CI_COMMIT_BRANCH" != "$CI_DEFAULT_BRANCH" ]; then
-  exit 0
-fi
-
 # Prepare OSTree repo for GNOME nightly distribution
 tar cf repo.tar repo/
 
