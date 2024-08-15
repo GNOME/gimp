e503436197930ae09e7379cb85d6a70cc67018bd
Jehan <jehan@girinstud.io>
2024-08-15 17:32:34 +0200

    app: "Keep embedded profile" as default profile conversion policy.

--- app/config/gimpdialogconfig.c
+++ app/config/gimpdialogconfig.c
@@ -176,7 +176,7 @@ gimp_dialog_config_class_init (GimpDialogConfigClass *klass)
                          "Color profile policy",
                          COLOR_PROFILE_POLICY_BLURB,
                          GIMP_TYPE_COLOR_PROFILE_POLICY,
-                         GIMP_COLOR_PROFILE_POLICY_ASK,
+                         GIMP_COLOR_PROFILE_POLICY_KEEP,
                          GIMP_PARAM_STATIC_STRINGS);
 
   GIMP_CONFIG_PROP_ENUM (object_class, PROP_METADATA_ROTATION_POLICY,
