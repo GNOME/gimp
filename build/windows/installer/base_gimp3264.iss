GNOME/gimp

commit a8b9bedf3c7a084677be291ca5c6703a9152924e

Issue #5146: Change Installer ProductInfo in revisions
This allows better versioning control by ITs.

build/windows/installer/base_gimp3264.iss
@@ -113,31 +113,38 @@ NotRecognizedMessagesWarning=no
 ;INSTALLER AND APP INFO
 AppName=GIMP
 #if Defined(GIMP_UNSTABLE) && GIMP_UNSTABLE != ""
-DefaultDirName={autopf}\GIMP {#GIMP_APP_VERSION}
+	;Inno installer identifier: https://github.com/jrsoftware/issrc/pull/461
+	SetupMutex=GIMP-{#GIMP_APP_VERSION}
+	;Inno installer (default) install location
+	DefaultDirName={autopf}\GIMP {#GIMP_APP_VERSION}
+	;Inno uninstaller identifier
+	AppID=GIMP-{#GIMP_APP_VERSION}
 #else
-DefaultDirName={autopf}\GIMP {#MAJOR}
-#endif
-#if !defined(REVISION)
-AppVerName=GIMP {#GIMP_VERSION}
-#else
-AppVerName=GIMP {#GIMP_VERSION}-{#REVISION}
-#endif
-
-VersionInfoVersion={#GIMP_VERSION}
-AppVersion={#GIMP_VERSION}
-#if Defined(GIMP_UNSTABLE) && GIMP_UNSTABLE != ""
-;SetupMutex avoids two installers running at the same time but the
-;UX is ultra clunky: https://github.com/jrsoftware/issrc/pull/461
-SetupMutex=GIMP-{#GIMP_APP_VERSION}
-AppID=GIMP-{#GIMP_APP_VERSION}
-#else
-SetupMutex=GIMP-{#MAJOR}
-AppID=GIMP-{#MAJOR}
+	SetupMutex=GIMP-{#MAJOR}
+	DefaultDirName={autopf}\GIMP {#MAJOR}
+	AppID=GIMP-{#MAJOR}
 #endif
 
+#if !defined(REVISION) || REVISION == "0"
+	;Inno installer file version
+	VersionInfoVersion={#GIMP_VERSION}
+	;Inno installer product version and ImmersiveControlPanel 'DisplayVersion'
+	AppVersion={#GIMP_VERSION}.0
+	;ImmersiveControlPanel 'DisplayName'
+	AppVerName=GIMP {#GIMP_VERSION}
+#else
+	VersionInfoVersion={#GIMP_VERSION}.{#REVISION}
+	AppVersion={#GIMP_VERSION}.{#REVISION}
+	AppVerName=GIMP {#GIMP_VERSION}.{#REVISION}
+#endif
+
+;ImmersiveControlPanel 'Publisher'
 AppPublisher=The GIMP Team
+;ControlPanel 'URLInfoAbout'
 AppPublisherURL=https://www.gimp.org/
+;ControlPanel 'HelpLink'
 AppSupportURL=https://www.gimp.org/docs/
+;ControlPanel 'URLUpdateInfo'
 AppUpdatesURL=https://www.gimp.org/
 
 
@@ -193,16 +200,17 @@ LZMANumFastBytes=273
 ;SignedUninstallerDir=_Uninst
 #endif //NOCOMPRESSION
 
-#if !defined(REVISION)
+#if !defined(REVISION) || REVISION == "0"
 OutputBaseFileName=gimp-{#GIMP_VERSION}-setup
 OutputManifestFile=gimp-{#GIMP_VERSION}-setup.txt
 #else
-OutputBaseFileName=gimp-{#GIMP_VERSION}-{#REVISION}-setup
-OutputManifestFile=gimp-{#GIMP_VERSION}-{#REVISION}-setup.txt
+OutputBaseFileName=gimp-{#GIMP_VERSION}.{#REVISION}-setup
+OutputManifestFile=gimp-{#GIMP_VERSION}.{#REVISION}-setup.txt
 #endif
 
 
 ;UNINSTALLER
+;ImmersiveControlPanel 'DisplayIcon'
 UninstallDisplayIcon={app}\bin\gimp-{#GIMP_APP_VERSION}.exe
 UninstallFilesDir={app}\uninst
 

build/windows/gitlab-ci/4_dist-gimp-inno.ps1
@@ -2,6 +2,7 @@
 
 # Parameters
 param ($GIMP_VERSION,
+       $REVISION,
        $GIMP_APP_VERSION,
        $GIMP_API_VERSION,
        $GIMP_BASE = '..\..\..\',
@@ -18,6 +19,11 @@ if (-Not $GIMP_VERSION)
   {
     $GIMP_VERSION = Get-Content -Path "$CONFIG_PATH"                         | Select-String 'GIMP_VERSION'        |
                     Foreach-Object {$_ -replace '#define GIMP_VERSION "',''} | Foreach-Object {$_ -replace '"',''}
+  }
+
+if (-Not $REVISION)
+  {
+    $REVISION = "0"
   }
 
 if (-Not $GIMP_APP_VERSION)
@@ -114,7 +120,7 @@ if (Test-Path -Path $gen_path)
 # Construct now the installer
 Set-Location build/windows/installer
 Set-Alias -Name 'iscc' -Value "${INNOPATH}\iscc.exe"
-iscc -DGIMP_VERSION="$GIMP_VERSION" -DGIMP_APP_VERSION="$GIMP_APP_VERSION" -DGIMP_API_VERSION="$GIMP_API_VERSION" -DGIMP_DIR="$GIMP_BASE" -DDIR32="$GIMP32" -DDIR64="$GIMP64" -DDIRA64="$GIMPA64" -DDEPS_DIR="$GIMP_BASE" -DDDIR32="$GIMP32" -DDDIR64="$GIMP64" -DDDIRA64="$GIMPA64" -DDEBUG_SYMBOLS -DLUA -DPYTHON base_gimp3264.iss
+iscc -DGIMP_VERSION="$GIMP_VERSION" -DREVISION="$REVISION" -DGIMP_APP_VERSION="$GIMP_APP_VERSION" -DGIMP_API_VERSION="$GIMP_API_VERSION" -DGIMP_DIR="$GIMP_BASE" -DDIR32="$GIMP32" -DDIR64="$GIMP64" -DDIRA64="$GIMPA64" -DDEPS_DIR="$GIMP_BASE" -DDDIR32="$GIMP32" -DDDIR64="$GIMP64" -DDDIRA64="$GIMPA64" -DDEBUG_SYMBOLS -DLUA -DPYTHON base_gimp3264.iss
 
 # Test if the installer was created and return success/failure.
 if (Test-Path -Path "_Output/gimp-${GIMP_VERSION}-setup.exe" -PathType Leaf)

commit 19ab03e69f61a289b49aeeda3a1e36c3623822d7

build/windows: Uninstall previous desktop .lnk icon again for RC2 and onwards
A regression introduced by a8b9bedf is that installing GIMP 3.0.0-RC1+git was
keeping GIMP 3.0.0-RC1 icon since an [InstallDelete] line was mislooked by me.

As a side note, to understand how the Windows versioning introduced by a8b9bedf
works, please read the latest base_gimp3264.iss file at 3.2.2 [Setup] section.

build/windows/installer/base_gimp3264.iss
@@ -386,8 +386,8 @@ Type: files; Name: "{app}\bin\gimp-console-?.??.exe"
 ;old ghostscript
 Type: filesandordirs; Name: "{app}\share\ghostscript\*"
 ;get previous GIMP icon name from uninstall name in Registry
-Type: files; Name: "{autoprograms}\GIMP {reg:HKA\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1,DisplayVersion|GIMP {#GIMP_MUTEX_VERSION}}.lnk"; Check: CheckRegValueExists('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1','DisplayVersion')
-Type: files; Name: "{autodesktop}\GIMP {reg:HKA\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1,DisplayVersion|GIMP {#GIMP_MUTEX_VERSION}}.lnk"; Check: CheckRegValueExists('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1','DisplayVersion')
+Type: files; Name: "{autoprograms}\{reg:HKA\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1,DisplayName|GIMP {#GIMP_MUTEX_VERSION}}.lnk"; Check: CheckRegValueExists('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1','DisplayName')
+Type: files; Name: "{autodesktop}\{reg:HKA\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1,DisplayName|GIMP {#GIMP_MUTEX_VERSION}}.lnk"; Check: CheckRegValueExists('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-{#GIMP_MUTEX_VERSION}_is1','DisplayName')
 ;remove old babl and gegl plugins
 Type: filesandordirs; Name: "{app}\lib\babl-0.1"
 Type: filesandordirs; Name: "{app}\lib\gegl-0.4"