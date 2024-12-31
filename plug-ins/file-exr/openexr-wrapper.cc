6390563d7686de698bb0857bf7fd4709e65459e9
Jacob Boerema <jgboerema@gmail.com>
2024-12-30 21:42:07 -0500

    plug-ins, exr: fix #12340 crash on loading unsupported exr type
    When trying to load an exr type that GIMP can't handle we threw an
    exception causing a crash of the plug-in.
    
    This isn't very user friendly, so change this to show a less
    disruptive error message that we can't load the image.

--- plug-ins/file-exr/openexr-wrapper.cc
+++ plug-ins/file-exr/openexr-wrapper.cc
@@ -84,6 +84,8 @@ struct _EXRLoader
   {
     const Channel* chan;
 
+    can_load = true;
+
     if (channels_.findChannel("R") ||
         channels_.findChannel("G") ||
         channels_.findChannel("B"))
@@ -145,7 +147,7 @@ struct _EXRLoader
           }
         else
           {
-            throw;
+            can_load = false;
           }
       }
 
@@ -252,6 +254,10 @@ struct _EXRLoader
 
   int hasAlpha() const {
     return has_alpha_ ? 1 : 0;
+  }
+
+  int canLoad() const {
+    return can_load ? 1 : 0;
   }
 
   GimpColorProfile *getProfile() const {
@@ -416,6 +422,7 @@ struct _EXRLoader
   int bpc_;
   EXRImageType image_type_;
   bool has_alpha_;
+  bool can_load;
   std::string format_string_;
   std::string unknown_channel_name_;
 };
@@ -430,6 +437,12 @@ exr_loader_new (const char *filename)
     {
       Imf::BlobAttribute::registerAttributeType();
       file = new EXRLoader(filename);
+
+      if (file && ! file->canLoad())
+        {
+          exr_loader_unref (file);
+          file = NULL;
+        }
     }
   catch (...)
     {
