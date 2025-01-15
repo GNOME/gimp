135935c448f19a59f90c63c84e2abe8cf77c2c64
Jehan <jehan@girinstud.io>
2025-01-15 01:41:21 +0100

    Revert "Issue #7539: Opening some images lock GIMP."
    This reverts commit 9b94e347bcb39c1f5bed2f0df16cbd7deefa6ff4.
    
    I leave a comment though, because we are not 100% sure that the issue is
    gone. In fact, the upstream report is still opened.
    
    The good point is that on stable code, we only debug crashes by default,
    while the specific hang we had was happening with debugging metadata
    warnings. So hopefully we won't have random people reporting GIMP
    hanging (only people explicitly trying to help debugging GIMP).

--- libgimpbase/gimputils.c
+++ libgimpbase/gimputils.c
@@ -1317,16 +1317,12 @@ gimp_stack_trace_print (const gchar   *prog_name,
       /* Child process. */
       gchar *args[9] = { "gdb", "-batch",
                          "-ex", "info threads",
-                         /* We used to be able to ask for the full
-                          * backtrace of all threads, but a bug,
-                          * possibly in gdb, could lock the whole GIMP
-                          * process. So for now, let's just have a
-                          * backtrace of the main process (most bugs
-                          * happen here anyway).
-                          * See issue #7539.
+                         /* A bug, possibly in gdb, could lock the whole
+                          * GIMP process with a full thread backtrace in
+                          * some conditions. We aren't sure if it still
+                          * exists. See issue #7539.
                           */
-                         /*"-ex", "thread apply all backtrace full",*/
-                         "-ex", "backtrace full",
+                         "-ex", "thread apply all backtrace full",
                          (gchar *) prog_name, NULL, NULL };
 
       if (prog_name == NULL)
