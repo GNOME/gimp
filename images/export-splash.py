4b8a165d5a40b5c42e767abcf103278cf088404c
Andre Klapper <a9016009@gmx.de>
2025-01-18 10:50:30 +0100

    Do not embed file creation date/time in splash PNG file
    Reproducible builds don't like this.
    
    Closes #7

--- images/export-splash.py
+++ images/export-splash.py
@@ -9,6 +9,7 @@ image.scale(1920, 1080)
 drawables = image.get_selected_drawables()
 config.set_property("image", image)
 config.set_property("file", Gio.file_new_for_path("gimp-data/images/gimp-splash.png"))
+config.set_property("time", False)
 retval = procedure.run(config)
 if retval.index(0) != Gimp.PDBStatusType.SUCCESS:
   sys.exit(os.EX_SOFTWARE)
