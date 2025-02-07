6c879b8cb86cc1f2ca2366d98ff6bfad73bdbb17
Bruno <brunvonlope@outlook.com>
2025-02-07 20:50:49 -0300

    build/linux: Only 'Nimbus' and 'StandardSymbols' fonts are needed for WMF
    Following e1294fec
    
    This makes the AppImage's WMF bundling consistent with Windows and macOS.
    See also: https://github.com/caolanm/libwmf/tree/master/fonts

--- build/linux/appimage/3_dist-gimp-goappimage.sh
+++ build/linux/appimage/3_dist-gimp-goappimage.sh
@@ -296,8 +296,8 @@ conf_app LIBTHAI_DICTDIR "share/libthai"
 ### Needed for full CJK and Cyrillic support in file-pdf
 bund_usr "$UNIX_PREFIX" "share/poppler"
 ### file-wmf support
-bund_usr "$UNIX_PREFIX" "share/fonts/type1/urw-base35/*.t1" --dest "share/libwmf/fonts"
-bund_usr "$UNIX_PREFIX" "share/fonts/type1/urw-base35/*.afm" --dest "share/libwmf/fonts"
+bund_usr "$UNIX_PREFIX" "share/fonts/type1/urw-base35/Nimbus*" --dest "share/libwmf/fonts"
+bund_usr "$UNIX_PREFIX" "share/fonts/type1/urw-base35/StandardSymbols*" --dest "share/libwmf/fonts"
 if [ "$GIMP_UNSTABLE" ]; then
   ### Image graph support
   bund_usr "$UNIX_PREFIX" "bin/libgvc*" --rename "bin/dot"
