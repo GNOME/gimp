GNOME/gimp

commit 8c0fdb9cf0e98eeb41764fce97847300e93af6e6

Issue #12825: implement a get_help_id() for GimpGeglProcedure.
This fixes the second part of #12825, which is that the help ID was even
wronger when editing a filter.

app/actions/gimpgeglprocedure.c
@@ -48,6 +48,8 @@
 #include "core/gimpsettings.h"
 #include "core/gimptoolinfo.h"
 
+#include "widgets/gimphelp-ids.h"
+
 #include "tools/gimpoperationtool.h"
 #include "tools/tool_manager.h"
 
@@ -64,6 +66,7 @@ static gint64   gimp_gegl_procedure_get_memsize         (GimpObject     *object,
 static gchar  * gimp_gegl_procedure_get_description     (GimpViewable   *viewable,
                                                          gchar         **tooltip);
 
+static const gchar * gimp_gegl_procedure_get_help_id    (GimpProcedure  *procedure);
 static const gchar * gimp_gegl_procedure_get_menu_label (GimpProcedure  *procedure);
 static gboolean      gimp_gegl_procedure_get_sensitive  (GimpProcedure  *procedure,
                                                          GimpObject     *object,
@@ -103,6 +106,7 @@ gimp_gegl_procedure_class_init (GimpGeglProcedureClass *klass)
   viewable_class->default_icon_name = "gimp-gegl";
   viewable_class->get_description   = gimp_gegl_procedure_get_description;
 
+  proc_class->get_help_id           = gimp_gegl_procedure_get_help_id;
   proc_class->get_menu_label        = gimp_gegl_procedure_get_menu_label;
   proc_class->get_sensitive         = gimp_gegl_procedure_get_sensitive;
   proc_class->execute               = gimp_gegl_procedure_execute;
@@ -151,6 +155,17 @@ gimp_gegl_procedure_get_description (GimpViewable  *viewable,
     *tooltip = g_strdup (gimp_procedure_get_blurb (procedure));
 
   return g_strdup (gimp_procedure_get_label (procedure));
+}
+
+static const gchar *
+gimp_gegl_procedure_get_help_id (GimpProcedure *procedure)
+{
+  GimpGeglProcedure *proc = GIMP_GEGL_PROCEDURE (procedure);
+
+  if (gegl_operation_get_key (proc->operation, "gimp:help-id"))
+    return gegl_operation_get_key (proc->operation, "gimp:help-id");
+
+  return GIMP_HELP_TOOL_GEGL;
 }
 
 static const gchar *

commit 29ba885985b16ad8f613c15b7c41dfcd3052b721

app: fix help ID of hardcoded filters.
Help ID of filters with an explicit help ID got broken with commit
8c0fdb9cf0 (which was fixing help ID for filters added to menus with a
GEGL key).

app/actions/gimpgeglprocedure.c
@@ -162,7 +162,9 @@ gimp_gegl_procedure_get_help_id (GimpProcedure *procedure)
 {
   GimpGeglProcedure *proc = GIMP_GEGL_PROCEDURE (procedure);
 
-  if (gegl_operation_get_key (proc->operation, "gimp:help-id"))
+  if (procedure->help_id)
+    return procedure->help_id;
+  else if (gegl_operation_get_key (proc->operation, "gimp:help-id"))
     return gegl_operation_get_key (proc->operation, "gimp:help-id");
 
   return GIMP_HELP_TOOL_GEGL;