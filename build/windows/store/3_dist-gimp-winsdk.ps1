GNOME/gimp

commit b285cfd310796cbba2240a577d4146a731709141

build/windows: Extend local compatibility layer to .NET and msstore-cli download
The original code from 9eec33ba was wrong because it was not downloading at
CI_PROJECT_DIR. Now, it does, and only downloads on parent dir if locally.

Also, fix .NET Runtime download URL.

build/windows/store/3_dist-gimp-winsdk.ps1
@@ -22,6 +22,8 @@ if (-not $GITLAB_CI)
       {
         Set-Location ..\..\..
       }
+
+    $PARENT_DIR = '..\'
   }
 
 
@@ -53,18 +55,18 @@ if ("$CI_COMMIT_TAG" -eq (git describe --all | Foreach-Object {$_ -replace 'tags
     if (-not (Test-Path "$Env:ProgramFiles\dotnet\shared\Microsoft.NETCore.App\$dotnet_major*\"))
       {
         Write-Output "(INFO): downloading .NET v$dotnet_tag"
-        Invoke-WebRequest https://download.visualstudio.microsoft.com/download/pr/92f9abc6-1e19-40cd-82cf-670be98d3533/46e1346503f4b54418bf9d5f861f1d43/dotnet-runtime-$dotnet_tag-win-x64.zip -OutFile ..\dotnet-runtime.zip
-        Expand-Archive ..\dotnet-runtime.zip ..\dotnet-runtime -Force
-        $env:Path = "$(Resolve-Path $PWD\..\dotnet-runtime);" + $env:Path
-        $env:DOTNET_ROOT = "$(Resolve-Path $PWD\..\dotnet-runtime)"
+        Invoke-WebRequest https://aka.ms/dotnet/$dotnet_major/dotnet-runtime-win-$cpu_arch.zip -OutFile ${PARENT_DIR}dotnet-runtime.zip
+        Expand-Archive ${PARENT_DIR}dotnet-runtime.zip ${PARENT_DIR}dotnet-runtime -Force
+        $env:Path = "$(Resolve-Path $PWD\${PARENT_DIR}dotnet-runtime);" + $env:Path
+        $env:DOTNET_ROOT = "$(Resolve-Path $PWD\${PARENT_DIR}dotnet-runtime)"
       }
 
     if (-not (Test-Path Registry::'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\App Paths\MSStore.exe'))
       {
         Write-Output "(INFO): downloading MSStoreCLI $msstore_tag"
-        Invoke-WebRequest https://github.com/microsoft/msstore-cli/releases/download/$msstore_tag/MSStoreCLI-win-$cpu_arch.zip -OutFile ..\MSStoreCLI.zip
-        Expand-Archive ..\MSStoreCLI.zip ..\MSStoreCLI -Force
-        $env:Path = "$(Resolve-Path $PWD\..\MSStoreCLI);" + $env:Path
+        Invoke-WebRequest https://github.com/microsoft/msstore-cli/releases/download/$msstore_tag/MSStoreCLI-win-$cpu_arch.zip -OutFile ${PARENT_DIR}MSStoreCLI.zip
+        Expand-Archive ${PARENT_DIR}MSStoreCLI.zip ${PARENT_DIR}MSStoreCLI -Force
+        $env:Path = "$(Resolve-Path $PWD\${PARENT_DIR}MSStoreCLI);" + $env:Path
       }
     $msstore_text = " | Installed MSStoreCLI: $(msstore --version)"
   }

commit 2983c0ac613f189ea885cc7a00c26a5fba6fa3d0

build/windows: Fix MSIX script failure at .msixupload making
This was a regression introduced by b285cfd3

build/windows/store/3_dist-gimp-winsdk.ps1
@@ -339,7 +339,7 @@ if (((Test-Path $a64_bundle) -and (Test-Path $x64_bundle)) -and (Get-ChildItem *
       {
         Write-Output "(INFO): creating $MSIXUPLOAD for submission"
         Get-ChildItem *.msixbundle | ForEach-Object { Compress-Archive -Path "$($_.Basename).msixbundle" -DestinationPath "$($_.Basename).zip" }
-        Get-ChildItem *.zip | Rename-Item -NewName $MSIXUPLOAD
+        Get-ChildItem ${IDENTITY_NAME}*.zip | Rename-Item -NewName $MSIXUPLOAD
         #Get-ChildItem *.appxsym | Remove-Item -Recurse -Force
         Get-ChildItem *.msixbundle | Remove-Item -Recurse -Force
       }
@@ -472,7 +472,7 @@ if ("$CI_COMMIT_TAG" -eq (git describe --all | Foreach-Object {$_ -replace 'tags
       }
     else
       {
-        $PRODUCT_ID="9PNSJCLXDZ0V"
+        #$PRODUCT_ID="NONE_YET"
       }
 
     ## Create submission and upload .msixupload file to it