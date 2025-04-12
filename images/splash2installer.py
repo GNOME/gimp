9ecc6c4c72537df4a586e73a316c9228749bcc15
Bruno Lopes <brunvonlope@outlook.com>
2025-04-12 10:03:33 -0300

    images: Make scaled 'installsplash*.bmp'

--- images/splash2installer.py
+++ images/splash2installer.py
@@ -10,50 +10,37 @@ config    = procedure.create_config()
 
 def export_scaled_img(image, target_width, target_height, export_path):
   img        = image.duplicate()
-  w          = img.get_width()
-  h          = img.get_height()
-  new_width  = target_width
-  new_height = target_height
-  offx       = 0
-  offy       = 0
-
-  bg_new_width  = target_width
-  bg_new_height = target_height
-  bg_offx       = 0
-  bg_offy       = 0
   layer = img.flatten()
-  bg_layer = layer.copy()
-  img.insert_layer(bg_layer, None, 1)
-  if w / target_width * target_height < h:
-    new_width = target_height / h * w
-    offx = (target_width - new_width) / 2
+  layer.scale(target_width, target_height, False)
+  img.resize(target_width, target_height, 0, 0)
 
-    bg_new_height = target_width / w * h
-    bg_offy = (target_height - bg_new_height) / 2
-  else:
-    new_height = target_width / w * h
-    offy = (target_height - new_height) / 2
+  config.set_property("image", img)
+  config.set_property("file", Gio.file_new_for_path(export_path))
+  retval = procedure.run(config)
+  if retval.index(0) != Gimp.PDBStatusType.SUCCESS:
+    sys.exit(os.EX_SOFTWARE)
+  img.delete()
 
-    bg_new_width = target_height / h * w
-    bg_offx = (target_width - bg_new_width) / 2
-  layer.scale(new_width, new_height, False)
-  bg_layer.scale(bg_new_width, bg_new_height, False)
-  img.resize(target_width, target_height, offx, offy)
-  bg_layer.set_offsets(bg_offx, bg_offy)
 
-  # See https://gitlab.gnome.org/GNOME/gimp-data/-/issues/5
-  filter = Gimp.DrawableFilter.new(bg_layer, "gegl:gaussian-blur", "")
+# See https://gitlab.gnome.org/GNOME/gimp-data/-/issues/5
+def export_blurred_img(image, target_width, target_height, export_path):
+  img        = image.duplicate()
+  layer = img.flatten()
+  layer.scale(target_width, target_height, False)
+  img.resize(target_width, target_height, 0, 0)
+
+  filter = Gimp.DrawableFilter.new(layer, "gegl:gaussian-blur", "")
   filter_config = filter.get_config()
   filter_config.set_property('std-dev-x', 27.5)
   filter_config.set_property('std-dev-y', 27.5)
-  bg_layer.merge_filter(filter)
+  layer.merge_filter(filter)
 
-  filter = Gimp.DrawableFilter.new(bg_layer, "gegl:noise-hsv", "")
+  filter = Gimp.DrawableFilter.new(layer, "gegl:noise-hsv", "")
   filter_config = filter.get_config()
   filter_config.set_property('hue-distance', 2.0)
   filter_config.set_property('saturation-distance', 0.002)
   filter_config.set_property('value-distance', 0.015)
-  bg_layer.merge_filter(filter)
+  layer.merge_filter(filter)
 
   config.set_property("image", img)
   config.set_property("file", Gio.file_new_for_path(export_path))
@@ -85,11 +72,15 @@ def export_cropped_img(image, target_width, target_height, export_path):
   img.delete()
 
 
-# These sizes are pretty much hardcoded, and in particular the ratio matters in
-# InnoSetup. Or so am I told. XXX
-export_scaled_img(image, 994, 692, 'build/windows/installer/installsplash.bmp')
-export_scaled_img(image, 497, 360, 'build/windows/installer/installsplash_small.bmp')
-export_scaled_img(image, 1160, 803, 'build/windows/installer/installsplash_big.bmp')
+# https://gitlab.gnome.org/GNOME/gimp/-/issues/11677 
+export_scaled_img(image, 558, 314, 'build/windows/installer/installsplash_top.scale-100.bmp')
+export_scaled_img(image, 686, 386, 'build/windows/installer/installsplash_top.scale-125.bmp')
+export_scaled_img(image, 816, 459, 'build/windows/installer/installsplash_top.scale-150.bmp')
+export_scaled_img(image, 988, 556, 'build/windows/installer/installsplash_top.scale-175.bmp')
+export_scaled_img(image, 1074, 604, 'build/windows/installer/installsplash_top.scale-200.bmp')
+export_scaled_img(image, 1244, 700, 'build/windows/installer/installsplash_top.scale-225.bmp')
+export_scaled_img(image, 1417, 797, 'build/windows/installer/installsplash_top.scale-250.bmp')
+export_blurred_img(image, 558, 314, 'build/windows/installer/installsplash_bottom.bmp')
 
 # Avoid the images being re-generated at each build.
 pathlib.Path('gimp-data/images/stamp-installsplash.bmp').touch()
