a2aedc789bbaa8b2c113fbf7753e36d9cbcc40ef
Bruno Lopes <brunvonlope@outlook.com>
2025-04-26 22:11:48 -0300

    build/linux: Improve a bit the comments of Flatpak dist script

--- build/linux/flatpak/3_dist-gimp-flatpak.sh
+++ build/linux/flatpak/3_dist-gimp-flatpak.sh
@@ -8,15 +8,18 @@ esac
 set -e
 
 
+# GLOBAL INFO
 APP_ID='org.gimp.GIMP.Nightly'
 
 
+# GIMP FILES AS REPO
 if [ "$GITLAB_CI" ]; then
   # Extract previously exported OSTree repo/
   tar xf repo.tar --warning=no-timestamp
 fi
 
 
+# CONSTRUCT .FLATPAK
 # Generate a Flatpak "bundle" to be tested with GNOME runtime installed
 # (it is NOT a real/full bundle, deps from GNOME runtime are not bundled)
 echo -e "\e[0Ksection_start:`date +%s`:flat_making[collapsed=true]\r\e[0KPackaging repo as ${APP_ID}.flatpak"
@@ -24,7 +27,7 @@ flatpak build-bundle repo ${APP_ID}.flatpak --runtime-repo=https://nightly.gnome
 echo -e "\e[0Ksection_end:`date +%s`:flat_making\r\e[0K"
 
 
-# Generate shasums for .flatpak
+# GENERATE SHASUMS FOR .FLATPAK
 echo -e "\e[0Ksection_start:`date +%s`:flat_trust[collapsed=true]\r\e[0KChecksumming ${APP_ID}.flatpak"
 echo "(INFO): ${APP_ID}.flatpak SHA-256: $(sha256sum ${APP_ID}.flatpak | cut -d ' ' -f 1)"
 echo "(INFO): ${APP_ID}.flatpak SHA-512: $(sha512sum ${APP_ID}.flatpak | cut -d ' ' -f 1)"
@@ -38,7 +41,7 @@ if [ "$GITLAB_CI" ]; then
 fi
 
 
-# Publish GIMP repo in GNOME nightly
+# PUBLISH GIMP REPO IN GNOME NIGHTLY
 # We take the commands from 'flatpak_ci_initiative.yml'
 if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
   echo -e "\e[0Ksection_start:`date +%s`:flat_publish[collapsed=true]\r\e[0KPublishing repo to GNOME nightly"
