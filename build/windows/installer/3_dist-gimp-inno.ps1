GNOME/gimp

commit c346731e1b1f88d6753707fb331e8c98e3a56243

build/windows: Fixes to make the installer script work locally again

build/windows/installer/3_dist-gimp-inno.ps1
@@ -23,6 +23,8 @@ if (-not $GITLAB_CI)
       {
         Set-Location ..\..\..
       }
+
+    $PARENT_DIR = '..\'
   }
 
 
@@ -31,6 +33,7 @@ if (-not $GITLAB_CI)
 #if (-not (Get-Command "python" -ErrorAction SilentlyContinue) -or "$(Get-Command "python" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Source)" -like '*WindowsApps*')
 #  {
     Invoke-Expression ((Get-Content build\windows\1_build-deps-msys2.ps1 | Select-String 'MSYS_ROOT\)' -Context 0,12) -replace '> ','')
+    $env:PATH = "$MSYS_ROOT/usr/bin;" + $env:PATH
     Invoke-Expression ((Get-Content .gitlab-ci.yml | Select-String 'win_environ\[' -Context 0,7) -replace '> ','' -replace '- ','')
 #  }
 
@@ -150,15 +153,15 @@ function download_langs ([array]$langsArray)
       if ($langfile -ne '' -and -not (Test-Path "$langfilePath" -Type Leaf))
         {
           Write-Output "(INFO): temporarily installing $($langfilePath -replace '\\\\','\')"
-          Copy-Item "issrc\Files\$langfile" "$langfilePath" -Force
+          Copy-Item "${PARENT_DIR}issrc\Files\$langfile" "$langfilePath" -Force
         }
     }
 }
-git clone --depth 1 https://github.com/jrsoftware/issrc.git
+git clone --depth 1 https://github.com/jrsoftware/issrc.git "${PARENT_DIR}issrc"
 download_langs $langsArray_Official
 New-Item "$INNO_PATH\Languages\Unofficial" -ItemType Directory -Force | Out-Null
 download_langs $langsArray_unofficial
-Remove-Item "issrc" -Recurse -Force
+Remove-Item "${PARENT_DIR}issrc" -Recurse -Force
 ### Patch 'AppVer*' against Inno pervasive behavior: https://groups.google.com/g/innosetup/c/w0sebw5YAeg
 function fix_msg ([array]$langsArray, [string]$AppVer)
 {

commit b25e489f6b07ee718a54e2d1c0674d418ebdb71a

build/windows: Only MSYS_ROOT env is needed for fix_msg.py
Fix regression introduced by c346731e, avoiding CYGWIN bins on PATH.

build/windows/installer/3_dist-gimp-inno.ps1
@@ -33,8 +33,10 @@ if (-not $GITLAB_CI)
 #if (-not (Get-Command "python" -ErrorAction SilentlyContinue) -or "$(Get-Command "python" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Source)" -like '*WindowsApps*')
 #  {
     Invoke-Expression ((Get-Content build\windows\1_build-deps-msys2.ps1 | Select-String 'MSYS_ROOT\)' -Context 0,12) -replace '> ','')
-    $env:PATH = "$MSYS_ROOT/usr/bin;" + $env:PATH
-    Invoke-Expression ((Get-Content .gitlab-ci.yml | Select-String 'win_environ\[' -Context 0,7) -replace '> ','' -replace '- ','')
+    #Need to install python modules (see lang\fix_msg.py)
+    $env:MSYS_ROOT = "$MSYS_ROOT"
+    #Needed to run python
+    $env:PATH = "$MSYS_ROOT/$MSYSTEM_PREFIX/bin;" + $env:PATH
 #  }
 
 

build/windows/installer/lang/fix_msg.py
@@ -13,7 +13,7 @@ except ImportError:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'charset_normalizer'])
   else:
     MINGW_PACKAGE_PREFIX = "mingw-w64-clang-aarch64" if platform.machine() == "ARM64" else "mingw-w64-clang-x86_64"
-    subprocess.check_call(['powershell', 'pacman', '--noconfirm', '-S', '--needed', f"{MINGW_PACKAGE_PREFIX}-python-charset-normalizer"])
+    subprocess.check_call(['powershell', os.getenv("MSYS_ROOT") + '/usr/bin/pacman', '--noconfirm', '-S', '--needed', f"{MINGW_PACKAGE_PREFIX}-python-charset-normalizer"])
 finally:
   from charset_normalizer import detect
 