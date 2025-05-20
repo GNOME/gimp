b8a39a49116cc8fb427007518d90d473df672450
Jehan <jehan@girinstud.io>
2025-05-20 16:49:31 +0200

    app: make sure KEEP_ABOVE windows are also set with UTILITY hint.

--- app/widgets/gimpwidgets-utils.c
+++ app/widgets/gimpwidgets-utils.c
@@ -915,6 +915,7 @@ gimp_window_set_hint (GtkWindow      *window,
       break;
 
     case GIMP_WINDOW_HINT_KEEP_ABOVE:
+      gtk_window_set_type_hint (window, GDK_WINDOW_TYPE_HINT_UTILITY);
       gtk_window_set_keep_above (window, TRUE);
       break;
     }
