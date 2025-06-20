6c7ec3a697a4cf142ea161c3e05fbe864cab63a0
Bruno Lopes <brunvonlope@outlook.com>
2025-06-20 18:50:16 -0300

    gitlab: Update coding style URL pointing to gimp-web-devel
    (cherry picked from commit 35b3cdf31c659d74dbfa412afd82c43886764a60)

--- .gitlab/run_style_check_diff.sh
+++ .gitlab/run_style_check_diff.sh
@@ -4,7 +4,7 @@ ancestor_horizon=28  # days (4 weeks)
 
 echo ""
 echo "This script may be wrong. You may disregard it if it conflicts with"
-echo "https://gitlab.gnome.org/GNOME/gimp/-/blob/master/CODING_STYLE.md"
+echo "https://developer.gimp.org/core/coding_style/"
 
 clang-format --version
 
