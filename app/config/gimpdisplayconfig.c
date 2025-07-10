a4a5b2e300ee5f797e0b3e8b1240da53d6fbe473
Jehan <jehan@girinstud.io>
2025-07-10 23:12:10 +0200

    app: (temporarily) redefine g_set_str() to avoid bumping GLib requirement.

--- app/config/gimpdisplayconfig.c
+++ app/config/gimpdisplayconfig.c
@@ -28,6 +28,8 @@
 #include "libgimpcolor/gimpcolor.h"
 #include "libgimpconfig/gimpconfig.h"
 
+#include "core/core-types.h" /* Temporary g_set_str() redefined */
+
 #include "config-types.h"
 
 #include "gimprc-blurbs.h"
