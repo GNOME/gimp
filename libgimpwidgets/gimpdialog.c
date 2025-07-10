6669c8de07a25f98dbae57dd9ef912399e0e8d49
Michael Natterer <mitch@gimp.org>
2025-07-10 23:27:54 +0200

    libgimp*: revert using g_set_str() outside app/
    (cherry picked from commit 50ce759eca180200175916edd961fe331eeb1904)

--- libgimpwidgets/gimpdialog.c
+++ libgimpwidgets/gimpdialog.c
@@ -248,7 +248,8 @@ gimp_dialog_set_property (GObject      *object,
       break;
 
     case PROP_HELP_ID:
-      g_set_str (&private->help_id, g_value_get_string (value));
+      g_free (private->help_id);
+      private->help_id = g_value_dup_string (value);
       gimp_help_set_help_data (GTK_WIDGET (object), NULL, private->help_id);
       break;
 
