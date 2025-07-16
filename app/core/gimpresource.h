7f9a7b26b7d2e980b3a087887fd64c9eb58eba4a
Michael Natterer <mitch@gimp.org>
2025-07-16 17:11:15 +0200

    app: use G_DECLARE_DERIVABLE_TYPE() in GimpResource
    (cherry picked from commit b32ab62822bb0d1c671c0df3319ea1ee4abc181e)

--- app/core/gimpresource.h
+++ app/core/gimpresource.h
@@ -22,20 +22,12 @@
 #include "gimpviewable.h"
 
 
-#define GIMP_TYPE_RESOURCE            (gimp_resource_get_type ())
-#define GIMP_RESOURCE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_RESOURCE, GimpResource))
-#define GIMP_RESOURCE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GIMP_TYPE_RESOURCE, GimpResourceClass))
-#define GIMP_IS_RESOURCE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIMP_TYPE_RESOURCE))
-#define GIMP_IS_RESOURCE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GIMP_TYPE_RESOURCE))
-#define GIMP_RESOURCE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GIMP_TYPE_RESOURCE, GimpResourceClass))
+#define GIMP_TYPE_RESOURCE (gimp_resource_get_type ())
+G_DECLARE_DERIVABLE_TYPE (GimpResource,
+                          gimp_resource,
+                          GIMP, RESOURCE,
+                          GimpViewable)
 
-
-typedef struct _GimpResourceClass GimpResourceClass;
-
-struct _GimpResource
-{
-  GimpViewable parent_instance;
-};
 
 struct _GimpResourceClass
 {
