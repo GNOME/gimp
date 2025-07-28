d7687ee7e6d1f18ae46edba493d4eeabddde7740
Bruno Lopes <brunvonlope@outlook.com>
2025-07-27 21:43:15 -0300

    build/linux: Complete 2aee80b7

--- build/linux/flatpak/2_build-gimp-flatpakbuilder.sh
+++ build/linux/flatpak/2_build-gimp-flatpakbuilder.sh
@@ -45,7 +45,7 @@ fi
 printf "\e[0Ksection_start:`date +%s`:gimp_build[collapsed=true]\r\e[0KBuilding GIMP\n"
 eval $FLATPAK_BUILDER --force-clean $BUILDER_ARGS --keep-build-dirs --build-only --disable-download \
                       "$GIMP_PREFIX" build/linux/flatpak/org.gimp.GIMP-nightly.json > gimp-flatpak-builder.log 2>&1 || { cat gimp-flatpak-builder.log; exit 1; }
-if [ "$GITLAB_CI"  ]; then
+if [ "$GITLAB_CI" ]; then
   tar cf gimp-meson-log.tar .flatpak-builder/build/gimp-1/_flatpak_build/meson-logs/meson-log.txt
 fi
 printf "\e[0Ksection_end:`date +%s`:gimp_build\r\e[0K\n"
@@ -53,9 +53,9 @@ printf "\e[0Ksection_end:`date +%s`:gimp_build\r\e[0K\n"
 ## Cleanup GIMP_PREFIX (not working) and export it to OSTree repo
 ## https://github.com/flatpak/flatpak-builder/issues/14
 printf "\e[0Ksection_start:`date +%s`:gimp_bundle[collapsed=true]\r\e[0KCreating OSTree repo\n"
-eval $FLATPAK_BUILDER --user --disable-rofiles-fuse --finish-only --repo=repo \
+eval $FLATPAK_BUILDER $BUILDER_ARGS --finish-only --repo=repo \
                       "$GIMP_PREFIX" build/linux/flatpak/org.gimp.GIMP-nightly.json
-if [ "$GITLAB_CI"  ]; then
+if [ "$GITLAB_CI" ]; then
   tar cf repo.tar repo/
 fi
 printf "\e[0Ksection_end:`date +%s`:gimp_bundle\r\e[0K\n"
