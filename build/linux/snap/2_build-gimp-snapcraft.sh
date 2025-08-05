1dd58f6084f0700972c3c2372f28e9afa6a18d70
Bruno Lopes <brunvonlope@outlook.com>
2025-08-05 19:21:44 -0300

    build/linux: Better support for local Snap builds
    (cherry picked from commit a65d7a64a295cc244f0f70c8cec1a6c7e70c452a)

--- build/linux/snap/2_build-gimp-snapcraft.sh
+++ build/linux/snap/2_build-gimp-snapcraft.sh
@@ -20,29 +20,32 @@ fi
 
 # Prepare env
 printf "\e[0Ksection_start:`date +%s`:snap_environ[collapsed=true]\r\e[0KPreparing build environment\n"
-## Option 1: no container environment (recommended)
+## portable environment which works on CI (unlike lxd) and on VMs (unlike multipass)
 export SNAPCRAFT_BUILD_ENVIRONMENT=host
-## Option 2: containerized environment by lxd
-#sudo snap install lxd
-#sudo lxd init --auto
-#sudo usermod -a -G lxd $(whoami)
-#sudo newgrp lxd
+build_environment_option='--destructive-mode'
+## (snapcraft does not allow to freely set the .yaml path, so let's just temporarely copy it)
+cp build/linux/snap/snapcraft.yaml .
 printf "\e[0Ksection_end:`date +%s`:snap_environ\r\e[0K\n"
 
 
-# Build babl, gegl and GIMP
-printf "\e[0Ksection_start:`date +%s`:gimp_build[collapsed=true]\r\e[0KBuilding babl, gegl and GIMP\n"
-## (snapcraft does not allow to freely set the .yaml path, so let's just temporarely copy it)
-cp build/linux/snap/snapcraft.yaml .
-## (snapcraft does not allow building in parts like flatpak-builder, so we need to build everything in one go)
+# Build (babl, gegl and) GIMP
+printf "\e[0Ksection_start:`date +%s`:gimp_build[collapsed=true]\r\e[0KBuilding (babl, gegl and) GIMP\n"
 if [ -z "$GITLAB_CI" ]; then
-  sudo snapcraft --build-for=$DPKG_ARCH --verbosity=verbose
+  sudo snapcraft stage gimp $build_environment_option --build-for=${DPKG_ARCH:-$(dpkg --print-architecture)} --verbosity=verbose
 else
+  ## (snapcraft remote-build does not allow building in parts, so we need to build and pack everything in one go)
   snapcraft remote-build --launchpad-accept-public-upload --build-for=$DPKG_ARCH --launchpad-timeout=7200 --verbosity=verbose
 fi
-rm snapcraft.yaml
 printf "\e[0Ksection_end:`date +%s`:gimp_build\r\e[0K\n"
 
+# Bundle
+printf "\e[0Ksection_start:`date +%s`:gimp_bundle[collapsed=true]\r\e[0KBundling GIMP\n"
+if [ -z "$GITLAB_CI" ]; then
+  sudo snapcraft prime $build_environment_option --build-for=${DPKG_ARCH:-$(dpkg --print-architecture)} --verbosity=verbose
+else
+  # Rename .snap bundle file to avoid confusion (the distribution of the .snap is done only in 3_dist-gimp-snapcraft.sh)
+  mv *.snap temp_$(echo *.snap)
+fi
+printf "\e[0Ksection_end:`date +%s`:gimp_bundle\r\e[0K\n"
 
-# Rename .snap bundle file to avoid confusion (the distribution of the .snap is done only in 3_dist-gimp-snapcraft.sh)
-mv *.snap temp_$(echo *.snap)
+rm snapcraft.yaml
