e262f7f07fc08e4b52b3f40c53d3652550c0f9c0
Michael Natterer <mitch@gimp.org>
2025-08-11 12:06:00 +0200

    app: GimpRowDeviceInfo: add missing dot in comment
    (cherry picked from commit 2f87f8e51e8657f98fad27f8a42321f16e1881c5)

--- app/widgets/gimprowdeviceinfo.c
+++ app/widgets/gimprowdeviceinfo.c
@@ -1,7 +1,7 @@
 /* GIMP - The GNU Image Manipulation Program
  * Copyright (C) 1995 Spencer Kimball and Peter Mattis
  *
- * gimprowdeviceinfoc
+ * gimprowdeviceinfo.c
  * Copyright (C) 2025 Michael Natterer <mitch@gimp.org>
  *
  * This program is free software: you can redistribute it and/or modify
