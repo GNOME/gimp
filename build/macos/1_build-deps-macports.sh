7accd52ce122b195d69ac466d562b7917ba96df7
Bruno Lopes <brunvonlope@outlook.com>
2025-08-21 15:18:02 -0300

    build, gitlab-ci, meson: Add macOS builds to our CI
    This adds macOS support on our CI aiming to MacPorts + macOS SDK 11. But since
    the GNOME runner have short timeout and no cache we fallback to Homebrew for
    now (which inelegantly links to X11 and follows runner's very high macOS 14).
    
    There is also still work to do: .dmg creation, multi-arch and notarization.
    
    Even so, this is a huge improvement, because macOS devs can now easily test MRs
    inhouse and non-macOS devs can contribute for macOS inhouse too. With the word
    "inhouse" I mean from this official gimp repo, without needing gimp-macos-build.
    
    For making that possible, I ported various concepts from gimp-macos-build:
    - .typelib regeneration for relocatability
    - LC_RPATH, LC_LOAD_DYLIB and LC_ID_DYLIB patching for relocatability
    - LC_BUILD_VERSION (minos) check for compatibility
    - GdkPixBuf loaders.cache patching
    - NEW: immodules.cache patching
    - Python.framework complex handling
    - Ad-hoc code signing on binaries (do not confuse with notarization)
    - .dSYM generation for babl, gegl and gimp
    - etc
    All these concepts into few scripts, without depending on gtk-mac-bundler.
    
    Since it was designed to work with MacPorts, we could make use of it on
    gimp-macos-build, but things are so endlessly complicated there into the
    9999 scripts and CircleCI is very slow, so I'm not sure if gimp-macos-build
    have any future. The ideal would be official macOS 11 builds on GIMP repo.

---
+++ build/macos/1_build-deps-macports.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+# Ensure the script work properly
+case $(readlink /proc/$$/exe) in
+  *bash)
+    set -o posix
+    ;;
+esac
+set -e
+if [ "$0" != 'build/macos/1_build-deps-macports.sh' ] && [ $(basename "$PWD") != 'macos' ]; then
+  printf '\033[31m(ERROR)\033[0m: Script called from wrong dir. Please, call this script from the root of gimp git dir/\n'
+  exit 1
+elif [ $(basename "$PWD") = 'macos' ]; then
+  cd ../../..
+fi
+if [ -z "$GITLAB_CI" ]; then
+  GIT_DEPTH='1'
+
+  PARENT_DIR='/..'
+fi
+
+
+# Install part of the deps
+if [ -z "$OPT_PREFIX" ]; then
+  export OPT_PREFIX=$(which port | sed 's|/bin/port||' || brew --prefix)
+  if echo "$OPT_PREFIX" | grep -q 'not found'; then
+    printf '\033[31m(ERROR)\033[0m: MacPorts installation not found. Please, install it on: https://www.macports.org/install.php\n'
+    exit 1
+  fi
+fi
+if [ "$OPT_PREFIX" != '/opt/local' ] && [ "$OPT_PREFIX" != '/opt/homebrew' ]; then
+  sed -i .bak "s/^#build_arch.*/build_arch $(uname -m)/" "${OPT_PREFIX}/etc/macports/macports.conf" >/dev/null 2>&1 || true
+fi
+export MACOSX_DEPLOYMENT_TARGET=$(awk '/LSMinimumSystemVersion/{found=1} found && /<string>/{gsub(/.*<string>|<\/string>.*/, ""); print; exit}' build/macos/Info.plist) #End of config
+
+printf "\e[0Ksection_start:`date +%s`:deps_install[collapsed=true]\r\e[0KInstalling dependencies provided by $( [ -f "$OPT_PREFIX/bin/port" ] && echo MacPorts || echo Homebrew )\n"
+if [ -f "$OPT_PREFIX/bin/port" ]; then
+  eval $( [ "$OPT_PREFIX"=/opt/local ] && echo sudo ) port sync && eval $( [ "$OPT_PREFIX"=/opt/local ] && echo sudo ) port upgrade outdated
+  eval $( [ "$OPT_PREFIX"=/opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | xargs)
+  git apply -v build/macos/patches/0001-meson-Patch-python-version.patch || true
+else
+  brew upgrade --quiet
+  brew install --quiet $(tr '\\' '\n' < build/macos/all-deps-uni.txt | grep -v '#' | sed -n 's/.*|homebrew://p' | awk '{print $1}' | xargs)
+  git apply -v build/macos/patches/0001-build-macos-Do-not-require-gexiv2-0.14-on-homebrew.patch || true
+fi
+git apply -v build/macos/patches/0001-app-libgimpwidgets-meson-plug-ins-Patch-macOS-bundle.patch || true
+printf "\e[0Ksection_end:`date +%s`:deps_install\r\e[0K\n"
+
+
+# Prepare env (only GIMP_PREFIX is needed for flatpak)
+GIMP_DIR="$PWD"
+cd ${GIMP_DIR}${PARENT_DIR}
+
+if [ -z "$GIMP_PREFIX" ]; then
+  export GIMP_PREFIX="$PWD/_install"
+fi
+eval "$(sed -n -e 's/- //' -e '/macos_environ\[/,/macos_environ/p' $GIMP_DIR/.gitlab-ci.yml)"
+
+
+# Build some deps (including babl and GEGL)
+self_build()
+{
+  dep=$(basename "$1" .git)
+  printf "\e[0Ksection_start:`date +%s`:${dep}_build[collapsed=true]\r\e[0KBuilding $dep\n"
+  if [ ! -d "$dep" ]; then
+    git clone --depth $GIT_DEPTH --branch ${2:-master} $1
+  fi
+  cd $dep
+  git pull
+
+  # Configure and build
+  if [ ! -f "_build-$(uname -m)/build.ninja" ]; then
+    meson setup _build-$(uname -m) -Dprefix="$GIMP_PREFIX" $PKGCONF_RELOCATABLE_OPTION \
+                -Dbuildtype=debugoptimized \
+                -Dc_args="-I${OPT_PREFIX}/include" -Dcpp_args="-I${OPT_PREFIX}/include" -Dc_link_args="-L${OPT_PREFIX}/lib" -Dcpp_link_args="-L${OPT_PREFIX}/lib"
+  fi
+  cd _build-$(uname -m)
+  ninja
+  ninja install
+  cd ../..
+  printf "\e[0Ksection_end:`date +%s`:${dep}_build\r\e[0K\n"
+}
+
+self_build https://gitlab.gnome.org/GNOME/babl
+self_build https://gitlab.gnome.org/GNOME/gegl
+
+cd $GIMP_DIR
