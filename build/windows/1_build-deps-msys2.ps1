a2d106e3fae89b749ffc72804cb022c08b6a52cc
Bruno Lopes <brunvonlope@outlook.com>
2025-09-14 07:16:05 -0300

    build/windows: Build exiv2 with setlocale patch

--- build/windows/1_build-deps-msys2.ps1
+++ build/windows/1_build-deps-msys2.ps1
@@ -127,6 +127,7 @@ function self_build ([string]$repo, [array]$branch, [array]$patches, [array]$opt
 
 self_build babl
 self_build gegl @('build/windows/patches/0001-meson-only-generate-CodeView-.pdb-symbols-on-Windows.patch') @('-Dworkshop=true')
+self_build https://github.com/Exiv2/exiv2 'v0.28.7' @('https://github.com/Exiv2/exiv2/pull/3361.patch') @('-DEXIV2_ENABLE_VIDEO=OFF', '-DEXIV2_BUILD_EXIV2_COMMAND=OFF')
 
 Set-Location $GIMP_DIR
 
