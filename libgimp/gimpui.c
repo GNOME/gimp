2cb2b0deda22edcd808612896d0dd7ff5eabf5f5
Jehan <jehan@girinstud.io>
2025-09-21 19:18:30 +0200

    libgimp: avoiding a build warning on Windows.
    Seen in the CI:
    
    > ../libgimp/gimpui.c:406:1: warning: 'gimp_ui_get_foreign_window' defined but not used [-Wunused-function]
    
    (cherry picked from commit 70bf795dfb9e7ea03aef76da41b68d4e05558e8d)

--- libgimp/gimpui.c
+++ libgimp/gimpui.c
@@ -402,6 +402,10 @@ gimp_osx_focus_window (gpointer user_data)
 }
 #endif
 
+/* Currently broken on Win32 so avoiding a "defined but not used"
+ * warning when building on Windows.
+ */
+#ifndef GDK_WINDOWING_WIN32
 static GdkWindow *
 gimp_ui_get_foreign_window (gpointer window)
 {
@@ -418,6 +422,7 @@ gimp_ui_get_foreign_window (gpointer window)
 
   return NULL;
 }
+#endif
 
 static gboolean
 gimp_window_transient_on_mapped (GtkWidget   *window,
