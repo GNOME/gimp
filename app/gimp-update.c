3493f4929259b1643157254f7c9d74900d1512a8
Bruno Lopes <brunvonlope@outlook.com>
2025-09-27 09:22:32 -0300

    app: Use 'g_message' (like the help plug-in) when GVFSd-HTTP is not present
    Closes #13553
    
    Such issue tends to affect only AppImage users (e.g. when running it in
    KDE installations). This is because AppImage files are not ran by
    any host helper (e.g. snapd or flatpak) to connect to GVFSd and is
    not possible to bundle GVFSd due to its own nature (it is daemon).
    
    Since GVFS is listed in the INSTALL file as a required dependency, it is
    fair enough to be more clear when it is not present, otherwise users will
    have outdated GIMP without knowing (like happened in macOS in the past).
    
    We are already doing this with GIMP help plug-in by the way. The
    difference is that, at GIMP start, such techinical error message can
    be surprising, but even so it is needed as feedback for fail on the
    update check button in the end of day. Let's follow GIMP help on this.
    
    (cherry picked from commit 46d9a0969852230997dd743c14a9ada8da8b7021)

--- app/gimp-update.c
+++ app/gimp-update.c
@@ -422,8 +422,22 @@ gimp_check_updates_callback (GObject      *source,
     {
       gchar *uri = g_file_get_uri (G_FILE (source));
 
+#ifndef GIMP_CONSOLE_COMPILATION
+#ifndef G_OS_WIN32
+      g_message ("%s: loading of %s failed: %s\n\n%s",
+                 G_STRFUNC, uri, error->message,
+                 _("Perhaps you are missing GIO backends and need "
+                   "to install GVFS?"));
+#else
+      g_message ("%s: loading of %s failed: %s\n\n%s",
+                 G_STRFUNC, uri, error->message,
+                 _("Perhaps you are missing GIO backends."));
+#endif /* G_OS_WIN32 */
+    
+#else
       g_printerr ("%s: loading of %s failed: %s\n", G_STRFUNC,
                   uri, error->message);
+#endif /* GIMP_CONSOLE_COMPILATION */
 
       g_free (uri);
       g_clear_error (&error);
