dcb71470f28b9fa2f045ab012cdbae0d9bed901d
Jacob Boerema <jgboerema@gmail.com>
2026-01-12 09:23:53 -0500

    plug-ins: Do not forget to free our json data in psd-load

--- plug-ins/file-psd/psd-load.c
+++ plug-ins/file-psd/psd-load.c
@@ -2830,6 +2830,8 @@ add_layers (GimpImage     *image,
       g_free (lyr_a[lidx]->chn_info);
       g_free (lyr_a[lidx]->name);
       g_free (lyr_a[lidx]->layer_styles);
+      g_free (lyr_a[lidx]->layer_effects.effects);
+      g_free (lyr_a[lidx]->smart_object.smart_object_data);
       g_free (lyr_a[lidx]);
     }
   g_free (lyr_a);
