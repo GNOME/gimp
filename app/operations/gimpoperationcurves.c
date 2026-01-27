b85e4db100e9c3d9eb965452e77558df731b9486
Jehan <jehan@girinstud.io>
2026-01-27 20:24:31 +0100

    app: typo fix.
    Not localizing it because anyway it doesn't look this arg is ever shown
    in UI (there is another "trc" arg inside the config objects). But at
    least fixing this typo because it's ugly.

--- app/operations/gimpoperationcurves.c
+++ app/operations/gimpoperationcurves.c
@@ -73,7 +73,7 @@ gimp_operation_curves_class_init (GimpOperationCurvesClass *klass)
   g_object_class_install_property (object_class,
                                    GIMP_OPERATION_POINT_FILTER_PROP_TRC,
                                    g_param_spec_enum ("trc",
-                                                      "Linear/Percptual",
+                                                      "Linear/Perceptual",
                                                       "What TRC to operate on",
                                                       GIMP_TYPE_TRC_TYPE,
                                                       GIMP_TRC_NON_LINEAR,
