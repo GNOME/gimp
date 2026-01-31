c38abe4ce84ee9779550f7d496b68fa8d62d34cc
Bruno Lopes <brunvonlope@outlook.com>
2026-01-30 21:55:44 -0300

    Test pango without libthai

--- build/windows/1_build-deps-msys2.ps1
+++ build/windows/1_build-deps-msys2.ps1
@@ -170,6 +170,7 @@ if ($env:VCPKG_ROOT)
   }
 self_build https://gitlab.gnome.org/GNOME/babl
 self_build https://gitlab.gnome.org/GNOME/gegl
+self_build https://gitlab.gnome.org/GNOME/pango main @('-Dlibthai=disabled')
 if ("$env:MSYSTEM_PREFIX" -ne 'MINGW32')
   {
     self_build https://github.com/Exiv2/exiv2 "v0.28.7" @('https://github.com/Exiv2/exiv2/pull/3361.patch') @('-DCMAKE_DLL_NAME_WITH_SOVERSION=ON', '-DEXIV2_BUILD_EXIV2_COMMAND=OFF', '-DEXIV2_ENABLE_VIDEO=OFF')
