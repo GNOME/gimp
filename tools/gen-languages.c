GNOME/gimp

commit 88261f8b4cab65350cd858e9221a91da4c7f86db

Issue #13774: languages list is not localized on AppImage.
Seems like our CI runners are configured with POSIX locale. Both C and
POSIX locales are basically overriding any attempt to localize during
the build (setting LANGUAGE doesn't work) and unfortunately we are now
self-localizing the list of languages during build.

So let's add a bogus en_US.UTF-8 locale before setting LANGUAGE. This
should fix the issue. It would probably also fix the same issue for
various third-party packages if distributions' build machines are set
similarly!

tools/gen-languages.c
@@ -280,6 +280,26 @@ gimp_language_store_parser_init (GError **error)
       goto cleanup;
     }
 
+  locale = setlocale (LC_ALL, NULL);
+  if (g_strcmp0 (locale, "C") == 0 || g_strcmp0 (locale, "POSIX") == 0)
+    {
+      /* Note: we do not care about what we set, except not C or POSIX.
+       * It may be any valid locale. This needs to be set so that this
+       * script works even on build systems with C or POSIX locale. In
+       * these 2 cases only, the LANGUAGE is ignored and our whole
+       * self-localizing code below would be basically moot.
+       */
+      g_setenv ("LC_ALL", "en_US.UTF-8", TRUE);
+      if (setlocale (LC_ALL, "") == NULL)
+        {
+          g_printerr ("ERROR: %s: setlocale() failed. "
+                      "The build system needs to be set up for localization. "
+                      "Please install at least \"en_US.UTF-8\" or set a locale other than C or POSIX.\n",
+                      __FILE__);
+          exit (EXIT_FAILURE);
+        }
+    }
+
   /* Generate the localized language names. */
   g_hash_table_iter_init (&lang_iter, l10n_lang_list);
   gimp_bind_text_domain ("iso_639_3", ISOCODES_LOCALEDIR);

.gitlab-ci.yml
@@ -219,11 +219,14 @@ deps-debian-nonreloc:
             libwmf-dev
             libxmu-dev
             libxpm-dev
+            locales
             mypaint-brushes
             poppler-data
             python3
             python3-gi
             python3-gi-cairo" >> Dockerfile
+    - echo "RUN echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen" >> Dockerfile
+    - echo "RUN locale-gen" >> Dockerfile
     # Prepare environ
     - echo "FROM $CI_REGISTRY_IMAGE:build-debian-${DEB_VERSION}-${RUNNER}" > Dockerfile2;
     - echo "RUN printf \"\e[0Ksection_start:\`date +%s\`:environ[collapsed=true]\r\e[0KPreparing build environment\n\"" >> Dockerfile2;

commit cae3be98df77b27ebaa6b9a07a5d64f375f18e28

tools/gen-languages: query user locale before falling back to en_US
setlocale(LC_ALL, "") queries the user locale.

setlocale(LC_ALL, NULL) also queries the locale, but not the user
locale. It actually queries the programs locale, which by default is C.

Otherwise the user would need to have en_US.UTF-8 available no matter
what.

Check that derivates UTF8 of C locale aren't used as well.

Bug: https://bugs.gentoo.org/969468
Fixes: 88261f8b4cab65350cd858e9221a91da4c7f86db
Signed-off-by: Alfred Wingate <parona@protonmail.com>

tools/gen-languages.c
@@ -280,8 +280,8 @@ gimp_language_store_parser_init (GError **error)
       goto cleanup;
     }
 
-  locale = setlocale (LC_ALL, NULL);
-  if (g_strcmp0 (locale, "C") == 0 || g_strcmp0 (locale, "POSIX") == 0)
+  locale = setlocale (LC_ALL, "");
+  if (g_strcmp0 (locale, "C") == 0 || g_strcmp0 (locale, "POSIX") == 0 || g_str_has_prefix (locale, "C."))
     {
       /* Note: we do not care about what we set, except not C or POSIX.
        * It may be any valid locale. This needs to be set so that this