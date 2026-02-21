94685fcfe9bb1ec896ceb3b04d7155566df0e536
Bruno Lopes <brunvonlope@outlook.com>
2026-02-21 12:16:21 -0300

    Test

--- build/macos/1_build-deps-macports.sh
+++ build/macos/1_build-deps-macports.sh
@@ -39,26 +39,30 @@ printf "\e[0Ksection_start:`date +%s`:deps_install[collapsed=true]\r\e[0KInstall
 if [ -f "$OPT_PREFIX/bin/port" ]; then
   eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port sync && eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port upgrade outdated
   if [ "$CI_JOB_NAME" ] && ls -d macports* 2>/dev/null | grep -q .; then
-    if echo "$CI_JOB_NAME" | grep -q 'part1' && [ -d 'macports-cached' ]; then
-      cp -fa macports-cached/* $OPT_PREFIX/var/macports || true
-    elif [ -d 'macports' ]; then
-      cp -fa macports/* $OPT_PREFIX/var/macports || true
+    if echo "$CI_JOB_NAME" | grep -q 'part1' && [ -d "macports-cached-$(uname -m)" ]; then
+      cp -fa macports-cached-$(uname -m)/* $OPT_PREFIX/var/macports || true
+    elif [ -d "macports-$(uname -m)" ]; then
+      cp -fa macports-$(uname -m)/* $OPT_PREFIX/var/macports || true
     fi
     eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port deactivate -fN installed
   fi
   if echo "$CI_JOB_NAME" | grep -q 'part1'; then
-    eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | awk '{print} /vala/{exit}' | xargs)
+    eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -vN $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | awk '{print} /gettext/{exit}' | xargs)
   elif echo "$CI_JOB_NAME" | grep -q 'part2'; then
+    eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -vN $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | awk '{print} /gi-docgen/{exit}' | xargs)
+  elif echo "$CI_JOB_NAME" | grep -q 'part3'; then
+    eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | awk '{print} /vala/{exit}' | xargs)
+  elif echo "$CI_JOB_NAME" | grep -q 'part4'; then
     eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | awk '{print} /appstream/{exit}' | xargs)
   else
     eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | xargs)
   fi
   if echo "$CI_JOB_NAME" | grep -q 'deps'; then
-    cp -fa $OPT_PREFIX/var/macports . || true
+    cp -fa $OPT_PREFIX/var/macports macports-$(uname -m) || true
     if echo "$CI_JOB_NAME" | grep -q 'part'; then
       exit 0
     elif [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
-      cp -fa macports macports-cached || true
+      cp -fa macports--$(uname -m) macports-cached-$(uname -m) || true
     fi
   fi
   git apply -v build/macos/patches/0001-meson-Patch-python-version.patch || true
