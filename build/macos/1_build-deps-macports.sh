GNOME/gimp

commit f5fa988a54712b09fd18b4ddcf0a847f4220ce18

gitlab-ci, build/macos: Reset GitLab CI cache preparing for multi-arch support

build/macos/1_build-deps-macports.sh
@@ -39,10 +39,10 @@ printf "\e[0Ksection_start:`date +%s`:deps_install[collapsed=true]\r\e[0KInstall
 if [ -f "$OPT_PREFIX/bin/port" ]; then
   eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port sync && eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port upgrade outdated
   if [ "$CI_JOB_NAME" ] && ls -d macports* 2>/dev/null | grep -q .; then
-    if echo "$CI_JOB_NAME" | grep -q 'part1' && [ -d 'macports-cached' ]; then
-      cp -fa macports-cached/* $OPT_PREFIX/var/macports || true
-    elif [ -d 'macports' ]; then
-      cp -fa macports/* $OPT_PREFIX/var/macports || true
+    if echo "$CI_JOB_NAME" | grep -q 'part1' && [ -d "macports-cached-$(uname -m)" ]; then
+      cp -fa macports-cached-$(uname -m)/* $OPT_PREFIX/var/macports || true
+    elif [ -d "macports-$(uname -m)" ]; then
+      cp -fa macports-$(uname -m)/* $OPT_PREFIX/var/macports || true
     fi
     eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port deactivate -fN installed
   fi
@@ -54,11 +54,11 @@ if [ -f "$OPT_PREFIX/bin/port" ]; then
     eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | xargs)
   fi
   if echo "$CI_JOB_NAME" | grep -q 'deps'; then
-    cp -fa $OPT_PREFIX/var/macports . || true
+    cp -fa $OPT_PREFIX/var/macports macports-$(uname -m) || true
     if echo "$CI_JOB_NAME" | grep -q 'part'; then
       exit 0
     elif [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
-      cp -fa macports macports-cached || true
+      cp -fa macports-$(uname -m) macports-cached-$(uname -m) || true
     fi
   fi
   git apply -v build/macos/patches/0001-meson-Patch-python-version.patch || true

.gitlab-ci.yml
@@ -666,6 +666,7 @@ gimp-win-eol:
   tags:
     - macos
   variables:
+    ARCH: arm64
     ORKA_RUNNER: tahoe-1
     PKGCONF_RELOCATABLE_OPTION: '-Dpkgconfig.relocatable=true'
   before_script:
@@ -688,17 +689,17 @@ deps-macos-inhouse-part1:
   extends: .macos-inhouse
   stage: dependencies
   cache:
-    - key: $CI_PROJECT_NAMESPACE
+    - key: $CI_PROJECT_NAMESPACE-$ARCH
       unprotect: true
       paths:
-        - macports-cached
+        - macports-cached-$ARCH
       policy: pull
   script:
     # Do not call sh since it is old bash on macOS, not the default zsh
     - zsh build/macos/1_build-deps-macports.sh --part1
   artifacts:
     paths:
-      - macports
+      - macports-$ARCH
     expire_in: 2 hours
 
 deps-macos-inhouse-part2:
@@ -711,7 +712,7 @@ deps-macos-inhouse-part2:
     - zsh build/macos/1_build-deps-macports.sh --part2
   artifacts:
     paths:
-      - macports
+      - macports-$ARCH
     expire_in: 2 hours
 
 deps-macos-inhouse:
@@ -722,17 +723,17 @@ deps-macos-inhouse:
       optional: true
   stage: dependencies
   cache:
-    - key: $CI_PROJECT_NAMESPACE
+    - key: $CI_PROJECT_NAMESPACE-$ARCH
       unprotect: true
       paths:
-        - macports-cached
+        - macports-cached-$ARCH
       policy: push
   script:
     # Do not call sh since it is old bash on macOS, not the default zsh
     - zsh build/macos/1_build-deps-macports.sh
   artifacts:
     paths:
-      - macports
+      - macports-$ARCH
       - _install-*
       # The full build dir is needed due to its .o for .dSYM creation on next job
       - babl/_build-*

commit 59fe682fe1630507d4ce08013218c955693fcc86

build/macos: Copy macports contents, not macports dir
Ths fixes a bug introduced by f5fa988a

build/macos/1_build-deps-macports.sh
@@ -54,7 +54,7 @@ if [ -f "$OPT_PREFIX/bin/port" ]; then
     eval $( [ "$OPT_PREFIX" = /opt/local ] && echo sudo ) port install -N $(grep -v '^#' build/macos/all-deps-uni.txt | sed 's/|homebrew:[^ ]*//g' | tr -d '\' | xargs)
   fi
   if echo "$CI_JOB_NAME" | grep -q 'deps'; then
-    cp -fa $OPT_PREFIX/var/macports macports-$(uname -m) || true
+    cp -fa $OPT_PREFIX/var/macports/* macports-$(uname -m) || true
     if echo "$CI_JOB_NAME" | grep -q 'part'; then
       exit 0
     elif [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then