50627a445c12fdf95af5b6c2a32e28671725b6a6
Bruno Lopes <brunvonlope@outlook.com>
2026-03-17 18:58:45 -0300

    meson: Restore virtual support to install .pdb generated by GCC
    We will not use it since GCC is still broken (59d0e828)
    but it is always good to have a complete script.

--- tools/meson_install_win_debug.py
+++ tools/meson_install_win_debug.py
@@ -18,7 +18,11 @@ if os.getenv("MESON_BUILD_ROOT", False):
     build_installed = json.load(f)
   for build_bin, installed_bin in build_installed.items():
     if build_bin.endswith((".dll", ".exe")):
+      #Clang generates the .pdb on binary build dir
       pdb_debug = os.path.splitext(build_bin)[0] + ".pdb"
+      #GCC generates the .pdb on build root dir
+      if not os.path.isfile(pdb_debug):
+        pdb_debug = os.path.join(os.getenv("MESON_BUILD_ROOT"), os.path.splitext(os.path.basename(build_bin))[0] + ".pdb")
       install_dir = os.path.dirname(installed_bin)
       if os.path.isfile(pdb_debug):
         if not os.getenv("MESON_INSTALL_DRY_RUN"):
