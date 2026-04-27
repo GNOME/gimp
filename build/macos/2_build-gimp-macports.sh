3b3db5ce720c0f4b8ebf4cabc9c17d6999d16dce
Bruno Lopes <brunvonlope@outlook.com>
2026-04-27 07:19:45 +0200

    build/macos: Try to fix lack of gimp symlinks on macOS .app
    (cherry picked from commit 24131f144f82daa947b0f035ee13f9314199c1f0)

--- build/macos/2_build-gimp-macports.sh
+++ build/macos/2_build-gimp-macports.sh
@@ -40,7 +40,7 @@ printf "\e[0Ksection_start:`date +%s`:gimp_build[collapsed=true]\r\e[0KBuilding 
 if [ ! -f "_build-$(uname -m)/build.ninja" ]; then
   meson setup _build-$(uname -m) -Dprefix="$GIMP_PREFIX" $NON_RELOCATABLE_OPTION $WARN_AS_ERROR_ON_CI \
               $PKGCONF_RELOCATABLE_OPTION $DMG_OPTION \
-              -Dbuild-id=org.gimp.GIMP_official \
+              -Denable-default-bin=enabled -Dbuild-id=org.gimp.GIMP_official \
               -Dc_args="-I${OPT_PREFIX}/include" -Dcpp_args="-I${OPT_PREFIX}/include" -Dc_link_args="-L${OPT_PREFIX}/lib" -Dcpp_link_args="-L${OPT_PREFIX}/lib"
 fi
 cd _build-$(uname -m)
