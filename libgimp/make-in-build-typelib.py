9a7533e614f4a2cff0a5d3673e82173bff4c71d0
Bruno Lopes <brunvonlope@outlook.com>
2026-05-13 14:38:03 -0300

    libgimp: Fix installed .typelib on macOS builds
    We were already fixing the in-build .typelib as a temp file.
    Let's fix the installed one too to allow running GIMP locally.

--- libgimp/make-in-build-typelib.py
+++ libgimp/make-in-build-typelib.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-# macOS-only script to generate temporary .gir then .typelib files only to be
-# used during build, pointing to the non-installed libgimp* .dylib libraries.
-# This is needed because .typelib have path references to .dylib so we patch in
-# a similar way we path GIMP_TEMP_UPDATE_RPATH binaries from tools/in-build-gimp.py
+# macOS-only script to generate/patch .typelib files.
+# Modes:
+# - 'in-build': generates a temporary .typelib pointing to non-installed libs.
+# - 'nonreloc': overwrites the .typelib prepending the install prefix.
+# - 'reloc': overwrites the .typelib prepending '@rpath/'.
 from datetime import datetime
 import os
 from pathlib import Path
@@ -12,32 +13,43 @@ import subprocess
 import sys
 
 # Arguments to this script:
-gir_name = os.path.basename(sys.argv[1])
-typelib_name = os.path.basename(sys.argv[2])
+gir_path = Path(sys.argv[1])
+gir_name = gir_path.name
+typelib_path = Path(sys.argv[2])
+typelib_name = typelib_path.name
 builddir = sys.argv[3]
 prefix = sys.argv[4]
+compiler = sys.argv[5]
+mode = sys.argv[6] if len(sys.argv) > 6 else "in-build"
 
-print("PWD:", os.getcwd())
 print("ARGS:", " ".join(sys.argv[1:]))
 
 # This is only for macOS (see the rationale in the comment above).
-tmp_gir_dir = Path(builddir) / "tmp"
+tmp_gir_dir = Path(builddir) if mode not in ["in-build"] else Path(builddir) / "tmp"
 tmp_gir_dir.mkdir(parents=True, exist_ok=True)
 tmp_gir_path = Path(f"{tmp_gir_dir}/{gir_name}")
-shutil.copy2(Path(f"{builddir}/{gir_name}"), tmp_gir_path)
+if mode in ["in-build"]:
+  shutil.copy2(gir_path, tmp_gir_path)
 
 def get_lib_path(p):
   name = os.path.basename(p)
-  folder = name.split('-')[0]
-  #Exception: libgimpui is built inside the 'libgimp' directory
-  if folder == "libgimpui":
-    folder = "libgimp"
-  return f"{folder}/{name}"
+  if mode == "nonreloc":
+    return f"{prefix}/lib/{name}"
+  elif mode == "reloc":
+    return f"@rpath/{name}"
+  else: # "in-build"
+    folder = name.split('-')[0]
+    #Exception: libgimpui is built inside the 'libgimp' directory
+    if folder == "libgimpui":
+      folder = "libgimp"
+    return f"{folder}/{name}"
 
 text = Path(tmp_gir_path).read_text()
 text = re.sub(r'shared-library="([^"]+)"', lambda m: 'shared-library="' + ",".join(get_lib_path(p) for p in m.group(1).split(",")) + '"', text)
 Path(tmp_gir_path).write_text(text)
-subprocess.run(["g-ir-compiler", f"--includedir={tmp_gir_dir}", f"--includedir={prefix}/share/gir-1.0/", str(tmp_gir_path), "-o", tmp_gir_dir / typelib_name], check=True)
 
-stamp_path = Path(f"{builddir}/{gir_name}-macos-typelib.stamp")
+output_typelib = typelib_path if mode not in ["in-build"] else tmp_gir_dir / typelib_name
+subprocess.run([compiler, f"--includedir={tmp_gir_dir}", f"--includedir={prefix}/share/gir-1.0/", str(tmp_gir_path), "-o", str(output_typelib)], check=True)
+
+stamp_path = Path(f"{builddir}/{gir_name}-macos-typelib-{mode}.stamp")
 stamp_path.write_text(f"/* Generated on {datetime.now().strftime('%c')}. */\n")
