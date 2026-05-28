cbb88f51d5e0d900e7aabe760431e9cc50368dc8
Alx Sa <cmyk.student@gmail.com>
2026-05-28 12:48:57 +0000

    xcf: Store vector data before freeing
    Resolves #16428
    Previously, we checked if vdata->modified was
    TRUE before rasterizing a vector layer on load.
    As @Aruius noticed however, the prior call to
    gimp_layer_from_layer () freed the associated layer,
    which triggered a callback function to free vdata.
    
    This patch stores the value of vdata->modified beforehand,
    so that we can get an accurate check on the modified state
    before deciding to rasterize.

--- app/xcf/xcf-load.c
+++ app/xcf/xcf-load.c
@@ -1318,6 +1318,7 @@ xcf_load_image (Gimp     *gimp,
           gboolean                floating;
           gchar                  *name;
           gint                    position = 0;
+          gboolean                modified;
 
           selected = g_list_find (info->selected_layers, layer);
           linked   = g_list_find (info->linked_layers, layer);
@@ -1362,6 +1363,7 @@ xcf_load_image (Gimp     *gimp,
           /* Store current layer position so we can add it after the fact */
           parent   = gimp_layer_get_parent (layer);
           position = gimp_item_get_index (GIMP_ITEM (layer));
+          modified = vdata->modified;
 
           vlayer = gimp_layer_from_layer (layer, FALSE, GIMP_TYPE_VECTOR_LAYER,
                                           "image",                image,
@@ -1369,7 +1371,7 @@ xcf_load_image (Gimp     *gimp,
                                           NULL);
           g_object_unref (options);
 
-          if (vdata->modified)
+          if (modified)
             gimp_rasterizable_rasterize (GIMP_RASTERIZABLE (vlayer), FALSE);
           gimp_image_add_layer (image, vlayer, parent, position, FALSE);
 
