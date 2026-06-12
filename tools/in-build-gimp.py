6b314bd3fd888dcb34b8928ef716312dc0abc2cd
Bruno Lopes <brunvonlope@outlook.com>
2026-06-12 13:14:42 -0300

    tools: Make in-build-gimp.py easier to understand
    While me and Jehan can understand it, not sure
    about the other developers. Let's make it clearer.
    
    (cherry picked from commit dabd65554cf7189bc864b10a21ff52aa556738c7)

--- tools/in-build-gimp.py
+++ tools/in-build-gimp.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 try:
   # fcntl only exists on UNIX-like platforms. On particular, it won't
   # exist on Windows. But it's fine, because we don't need to
@@ -21,37 +20,37 @@ from pathlib import Path
 
 # In some case, this script may not be run concurrently, in particular
 # on macOS where we need to set rpath and install_name_tool doesn't like
-# when a RPATH already exists.
+# when a LC_RPATH already exists (see GIMP_TEMP_UPDATE_RPATH section below).
 lock = None
-
 def cleanup(lock):
   if lock is not None:
     fcntl.flock(lock, fcntl.LOCK_UN)
     os.close(lock)
 
-def is_script_file(filepath):
-  try:
-    with open(filepath, 'rb') as f:
-      return f.read(2) == b'#!'
-  except IOError:
-    return False
-
 try:
+  # Get needed env vars
   if os.environ.get('GIMP_TESTING_PLUGINDIRS') is not None:
     sys.stderr.write(f"ERROR: do not set $GIMP_TESTING_PLUGINDIRS. This script will take care of it.\n")
     sys.stderr.write(f"       Set $GIMP_TESTING_PLUG_INS instead.\n")
     sys.exit(1)
-
   GIMP_GLOBAL_BUILD_ROOT  = os.environ.get("GIMP_GLOBAL_BUILD_ROOT", ".")
   GIMP_GLOBAL_SOURCE_ROOT = os.environ.get("GIMP_GLOBAL_SOURCE_ROOT")
   GIMP_TESTING_PLUG_INS   = os.environ.get("GIMP_TESTING_PLUG_INS")
 
+  # Create the temporary config directory
   suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
   GIMP3_DIRECTORY = os.path.join(GIMP_GLOBAL_BUILD_ROOT, f".GIMP3-build-config-{suffix}")
   os.makedirs(GIMP3_DIRECTORY, mode=0o700, exist_ok=False)
   os.environ["GIMP3_DIRECTORY"] = GIMP3_DIRECTORY
   sys.stderr.write(f"INFO: temporary GIMP configuration directory: {GIMP3_DIRECTORY}\n")
 
+  # Copy just the strictly needed plug-ins by the script or test (seee issue #11385)
+  def is_script_file(filepath):
+    try:
+      with open(filepath, 'rb') as f:
+        return f.read(2) == b'#!'
+    except IOError:
+      return False
   if GIMP_TESTING_PLUG_INS is not None:
     processed_plugins = []
     plugins_dir = os.path.join(GIMP3_DIRECTORY, 'plug-ins')
@@ -84,6 +83,7 @@ try:
       if not is_script_file(dst):
         processed_plugins.append(dst)
 
+  # Copy the non-installed data for running the script or test (e.g. Palette unit-test)
   data_types = [ 'palettes', 'brushes', 'patterns', 'dynamics', 'gradients' ]
   for dtype in data_types:
     env_var = os.environ.get("GIMP_TESTING_" + dtype.upper())
@@ -96,27 +96,23 @@ try:
         dst = os.path.join(ddir, os.path.basename(data))
         shutil.copyfile(src, dst)
 
-  # Earlier code used to set DYLD_LIBRARY_PATH environment variable instead, but
-  # it didn't work on contributor's builds because of System Integrity
-  # Protection (SIP), though it did work in the CI which had older macOS.
-  # So, we just set LC_RPATH on binaries, but this restrict us to only one
-  # target at a time. See: #14236 and gimp-data/images/logo/meson.build
-  rpath_array = [f"{GIMP_GLOBAL_BUILD_ROOT}/libgimp",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpbase",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpcolor",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpconfig",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpmath",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpmodule",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpthumb",
-                 f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpwidgets"]
-
   if "GIMP_TEMP_UPDATE_RPATH" in os.environ:
+    # Earlier code used to set DYLD_LIBRARY_PATH environment variable instead, but
+    # it didn't work on contributor's builds because of System Integrity
+    # Protection (SIP), though it did work in the CI which had older macOS.
+    # So, we change LC_RPATH, but by only one in-build-gimp.py at a time (see #14236).
+    rpath_array = [f"{GIMP_GLOBAL_BUILD_ROOT}/libgimp",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpbase",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpcolor",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpconfig",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpmath",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpmodule",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpthumb",
+                  f"{GIMP_GLOBAL_BUILD_ROOT}/libgimpwidgets"]
     lock = os.open(__file__, os.O_RDONLY)
     fcntl.flock(lock, fcntl.LOCK_EX)
-
     binaries_to_patch = os.environ.get("GIMP_TEMP_UPDATE_RPATH", "").split(":")
     binaries_to_patch.extend(processed_plugins)
-
     for binary in binaries_to_patch:
       result = subprocess.run(['otool', '-l', binary], stdout=subprocess.PIPE)
       out = result.stdout.decode('utf-8', errors='replace')
@@ -125,9 +121,9 @@ try:
         if not new_rpath in regex:
           subprocess.run(["install_name_tool", "-add_rpath", new_rpath, binary], check=True)
 
-  #Ensure the same python from meson.build (GIMP_PYTHON_WITH_GI) is used by plugins
-  #This is needed because GIMP_PYTHON_WITH_GI can not coincide with python3 from shebang
-  #(on MacPorts, there is no python3 symlink, so we would misuse Xcode python3 without GI)
+  # Ensure the same python from meson.build (GIMP_PYTHON_WITH_GI) is used by plugins
+  # This is needed because GIMP_PYTHON_WITH_GI can not coincide with python3 from shebang
+  # (on MacPorts, there is no python3 symlink, so we would misuse Xcode python3 without GI)
   different_python=False
   if sys.platform not in ['win32', 'cygwin'] and os.environ.get("GIMP_PYTHON_WITH_GI"):
     python_symlink = shutil.which("python3")
@@ -144,6 +140,7 @@ try:
         os.symlink(os.environ.get("GIMP_PYTHON_WITH_GI"), tmp_symlink)
       os.environ["PATH"] = tmp_path + os.pathsep + os.environ.get("PATH", "")
 
+  # Finally, run gimp!
   if "GIMP_DEBUG_SELF_WRAPPER" in os.environ and shutil.which("gdb"):
     sys.stderr.write(f"RUNNING: {os.environ['GIMP_DEBUG_SELF_WRAPPER']} --batch -x {os.environ['GIMP_GLOBAL_SOURCE_ROOT']}/tools/debug-in-build-gimp.py --args {os.environ['GIMP_SELF_IN_BUILD']} {' '.join(sys.argv[1:])}\n")
     subprocess.run([os.environ["GIMP_DEBUG_SELF_WRAPPER"],"--return-child-result","--batch","-x",f"{os.environ['GIMP_GLOBAL_SOURCE_ROOT']}/tools/debug-in-build-gimp.py","--args", os.environ["GIMP_SELF_IN_BUILD"]] + sys.argv[1:], stdin=sys.stdin, check=True)
@@ -151,10 +148,12 @@ try:
     sys.stderr.write(f"RUNNING: {os.environ['GIMP_SELF_IN_BUILD']} {' '.join(sys.argv[1:])}\n")
     subprocess.run([os.environ["GIMP_SELF_IN_BUILD"]] + sys.argv[1:],stdin=sys.stdin, check=True)
 
+  # Undo GIMP_PYTHON_WITH_GI trick
   if sys.platform not in ['win32', 'cygwin'] and different_python:
     os.environ["PATH"] = os.pathsep.join([p for p in os.environ["PATH"].split(os.pathsep) if p != tmp_path])
     shutil.rmtree(tmp_path, ignore_errors=True)
 
+  # Undo GIMP_TEMP_UPDATE_RPATH trick
   if "GIMP_TEMP_UPDATE_RPATH" in os.environ:
     for binary in binaries_to_patch:
       result = subprocess.run(['otool', '-l', binary], stdout=subprocess.PIPE)
