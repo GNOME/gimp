GNOME/gimp

commit a98a90397662b8ea979a0d5540058264d25f143a

app: get rid of gimp_action_(g|s)et_accel_path().

app/actions/windows-actions.c
@@ -455,19 +455,15 @@ windows_actions_update_display_accels (GimpActionGroup *group)
 
       if (action)
         {
-          const gchar *accel_path;
-          guint        accel_key;
-
-          accel_path = gimp_action_get_accel_path (action);
+          gchar *accel;
 
           if (i < 9)
-            accel_key = GDK_KEY_1 + i;
+            accel = gtk_accelerator_name (GDK_KEY_1 + i, GDK_MOD1_MASK);
           else
-            accel_key = GDK_KEY_0;
+            accel = gtk_accelerator_name (GDK_KEY_0 + i, GDK_MOD1_MASK);
 
-          gtk_accel_map_change_entry (accel_path,
-                                      accel_key, GDK_MOD1_MASK,
-                                      TRUE);
+          gimp_action_set_accels (action, (const gchar*[]) { accel, NULL });
+          g_free (accel);
         }
     }
 }

app/actions/edit-actions.c
@@ -235,7 +235,6 @@ edit_actions_setup (GimpActionGroup *group)
   GimpContext *context = gimp_get_user_context (group->gimp);
   GimpRGB      color;
   GimpPattern *pattern;
-  GimpAction  *action;
 
   gimp_action_group_add_actions (group, "edit-action",
                                  edit_actions,
@@ -250,11 +249,6 @@ edit_actions_setup (GimpActionGroup *group)
                                       edit_fill_actions,
                                       G_N_ELEMENTS (edit_fill_actions),
                                       edit_fill_cmd_callback);
-
-  action = gimp_action_group_get_action (group,
-                                         "edit-paste-as-new-image-short");
-  gimp_action_set_accel_path (action,
-                              "<Actions>/edit/edit-paste-as-new-image");
 
   g_signal_connect_object (context, "foreground-changed",
                            G_CALLBACK (edit_actions_foreground_changed),

app/actions/tools-actions.c
@@ -660,8 +660,7 @@ static const GimpEnumActionEntry tools_object_2_actions[] =
 void
 tools_actions_setup (GimpActionGroup *group)
 {
-  GimpAction *action;
-  GList      *list;
+  GList *list;
 
   gimp_action_group_add_actions (group, "tools-action",
                                  tools_actions,
@@ -671,10 +670,6 @@ tools_actions_setup (GimpActionGroup *group)
                                         tools_alternative_actions,
                                         G_N_ELEMENTS (tools_alternative_actions),
                                         tools_select_cmd_callback);
-
-  action = gimp_action_group_get_action (group,
-                                         "tools-by-color-select-short");
-  gimp_action_set_accel_path (action, "<Actions>/tools/tools-by-color-select");
 
   gimp_action_group_add_enum_actions (group, NULL,
                                       tools_color_average_radius_actions,

app/widgets/gimpaction.c
@@ -386,19 +386,6 @@ gimp_action_is_sensitive (GimpAction   *action,
 }
 
 void
-gimp_action_set_accel_path (GimpAction  *action,
-                            const gchar *accel_path)
-{
-  gtk_action_set_accel_path ((GtkAction *) action, accel_path);
-}
-
-const gchar *
-gimp_action_get_accel_path (GimpAction *action)
-{
-  return gtk_action_get_accel_path ((GtkAction *) action);
-}
-
-void
 gimp_action_set_accel_group (GimpAction  *action,
                              GtkAccelGroup *accel_group)
 {

app/widgets/gimpaction.h
@@ -103,10 +103,6 @@ gboolean      gimp_action_get_sensitive       (GimpAction    *action,
 gboolean      gimp_action_is_sensitive        (GimpAction    *action,
                                                const gchar  **reason);
 
-void          gimp_action_set_accel_path      (GimpAction    *action,
-                                               const gchar   *accel_path);
-const gchar * gimp_action_get_accel_path      (GimpAction    *action);
-
 void          gimp_action_set_accel_group     (GimpAction    *action,
                                                GtkAccelGroup *accel_group);
 void          gimp_action_connect_accelerator (GimpAction    *action);

commit 8cb5e6a2e4a77f90b704ed2c8a8af61ed6fd72c8

app: Restore Alt+0 accel for 10th image
10th image originally used accelerator key Alt+0. Rewrite in
a98a903976 accidentally add also + i (copy paste error from `if` clause
for images <1, 9>), therefore it was also Alt+9 like the previous 9th
image (duplicity). Restore it back to Alt+0.

Fixes: a98a903976 ("app: get rid of gimp_action_(g|s)et_accel_path().")
Fixes: #16554
Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

app/actions/windows-actions.c
@@ -461,7 +461,7 @@ windows_actions_update_display_accels (GimpActionGroup *group)
           if (i < 9)
             accel = gtk_accelerator_name (GDK_KEY_1 + i, GDK_MOD1_MASK);
           else
-            accel = gtk_accelerator_name (GDK_KEY_0 + i, GDK_MOD1_MASK);
+            accel = gtk_accelerator_name (GDK_KEY_0, GDK_MOD1_MASK);
 
           gimp_action_set_accels (action, (const gchar*[]) { accel, NULL });
           g_free (accel);