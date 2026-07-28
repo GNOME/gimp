6cd33d28ddbd2451b283b1c78deb72b56465ef88
Alx Sa <cmyk.student@gmail.com>
2026-07-28 16:21:38 +0000

    core, xcf: Save channel filters in XCF
    When live filters were enabled for channels, XCF was not
    updated to support saving and loading them.
    This patch adds the layer effect saving and loading code
    to channels, and bumps the XCF version to 26.
    
    
    (cherry picked from commit f8794344ed0f264b2555d82a5a4545a90b036d07)
    
    94ad15ac core, xcf: Save channel filters in XCF
    a5698593 xcf: Load layer mask filters too
    18c7e5c9 core: Add specific 3.2 micro version
    28acd7aa core: Move mask check outside group check
    
    Co-authored-by: Alx Sa <cmyk.student@gmail.com>

--- app/core/gimpimage.c
+++ app/core/gimpimage.c
@@ -2959,6 +2959,19 @@ gimp_image_get_xcf_version (GimpImage    *image,
           break;
         }
 
+      if (gimp_layer_get_mask (layer))
+        {
+          GimpLayerMask *mask = gimp_layer_get_mask (layer);
+
+          filters = gimp_drawable_get_filters (GIMP_DRAWABLE (mask));
+          if (gimp_container_get_n_children (filters) > 0)
+            {
+              ADD_REASON (g_strdup_printf (_("Channel effects were saved "
+                                             "in %s"), "GIMP 3.2.6"));
+              version = MAX (26, version);
+            }
+        }
+
       /* need version 3 for layer trees */
       if (gimp_viewable_get_children (GIMP_VIEWABLE (layer)))
         {
@@ -3054,13 +3067,22 @@ gimp_image_get_xcf_version (GimpImage    *image,
   items = gimp_image_get_channel_list (image);
   for (list = items; list; list = g_list_next (list))
     {
-      GimpChannel *channel = GIMP_CHANNEL (list->data);
+      GimpChannel   *channel = GIMP_CHANNEL (list->data);
+      GimpContainer *filters;
 
       if (gimp_item_get_lock_visibility (GIMP_ITEM (channel)))
         {
           ADD_REASON (g_strdup_printf (_("Visibility locks were added in %s"),
                                        "GIMP 3.0"));
           version = MAX (17, version);
+        }
+
+      filters = gimp_drawable_get_filters (GIMP_DRAWABLE (channel));
+      if (gimp_container_get_n_children (filters) > 0)
+        {
+          ADD_REASON (g_strdup_printf (_("Channel effects were saved in %s"),
+                                         "GIMP 3.2.6"));
+          version = MAX (26, version);
         }
     }
   g_list_free (items);
