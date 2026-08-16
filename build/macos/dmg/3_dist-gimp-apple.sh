4db583469e48e3e23ae7bafe542b25247247aa3b
Bruno Lopes <brunvonlope@outlook.com>
2026-08-16 17:13:00 -0300

    build: Update link to gimp-web on IMPORTANT.txt file
    (cherry picked from commit 71ab82c973d06d4e87251c57262b8229a3a322b4)

--- build/macos/dmg/3_dist-gimp-apple.sh
+++ build/macos/dmg/3_dist-gimp-apple.sh
@@ -406,7 +406,7 @@ if [ "$GITLAB_CI" ]; then
   mkdir -p $output_dir
   mv -f ${DMG_ARTIFACT}* $output_dir
   if [ "$CI_COMMIT_BRANCH" != "$CI_DEFAULT_BRANCH" ]; then
-    echo "See the instructions on how install at the 'Automatic Development Builds' section of https://www.gimp.org/downloads/devel/" > $output_dir/IMPORTANT.txt
+    echo "See the instructions on how install at the 'Automatic Development Builds' section of https://www.gimp.org/downloads/devel/#automatic-builds" > $output_dir/IMPORTANT.txt
   fi
 fi
 done
