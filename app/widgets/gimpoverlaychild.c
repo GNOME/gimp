GNOME/gimp

commit 7dc252ea7f7d94866c873640ce1d03bb1c0a2733

app/widgets: Set cursor for GdkWindow using own API
Implements a preparatory step ahead of a potential GTK 4 port. Reasoning
from the documentation [0]:

GTK 4 removes the GDK_WA_CURSOR flag. Instead, just use
gdk_window_set_cursor() to set a cursor on the window after creating it.
... (excerpt)

[0] https://docs.gtk.org/gtk4/migrating-3to4.html#review-your-window-creation-flags

app/widgets/gimpoverlaychild.c
@@ -172,12 +172,10 @@ gimp_overlay_child_realize (GimpOverlayBox   *box,
   attributes.visual      = gtk_widget_get_visual (child->widget);
   attributes.event_mask  = (gtk_widget_get_events (widget) |
                             GDK_EXPOSURE_MASK);
-  attributes.cursor      = gdk_cursor_new_for_display (display, GDK_LEFT_PTR);
 
   attributes_mask = (GDK_WA_X        |
                      GDK_WA_Y        |
-                     GDK_WA_VISUAL   |
-                     GDK_WA_CURSOR);
+                     GDK_WA_VISUAL);
 
   child->window = gdk_window_new (gdk_screen_get_root_window (screen),
                                   &attributes, attributes_mask);
@@ -185,6 +183,7 @@ gimp_overlay_child_realize (GimpOverlayBox   *box,
   gtk_widget_set_parent_window (child->widget, child->window);
   gdk_offscreen_window_set_embedder (child->window,
                                      gtk_widget_get_window (widget));
+  gdk_window_set_cursor (child->window, gdk_cursor_new_for_display (display, GDK_LEFT_PTR));
 
   g_object_unref (attributes.cursor);
 

commit 502d174c3d90ba8612e04312ae0acff7998d38f0

app/widgets: Remove forgotten g_object_unref
Fallout from commit 7dc252ea7f7d94866c873640ce1d03bb1c0a2733.

app/widgets/gimpoverlaychild.c
@@ -185,8 +185,6 @@ gimp_overlay_child_realize (GimpOverlayBox   *box,
                                      gtk_widget_get_window (widget));
   gdk_window_set_cursor (child->window, gdk_cursor_new_for_display (display, GDK_LEFT_PTR));
 
-  g_object_unref (attributes.cursor);
-
   g_signal_connect (child->window, "from-embedder",
                     G_CALLBACK (gimp_overlay_child_from_embedder),
                     child);