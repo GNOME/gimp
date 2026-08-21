GNOME/gimp

commit e90dbe28256a534d9b4f8eb74c62dc808d837e11

app,libgimp,plug-ins: Use GMainContext instead of GtkMain
Implements a preparatory step ahead of a potential GTK 4 port. Reasoning
from the documentation [0]:

GTK 4 removes the gtk_main_* family of APIs. The recommended replacement
is GtkApplication, but you can also iterate the GLib main loop directly,
using GMainContext APIs. The replacement for gtk_events_pending() is
g_main_context_pending(), the replacement for gtk_main_iteration() is
g_main_context_iteration().

This commit does not touch the use of gtk_main_quit() and gtk_main_do_event().

[0] https://docs.gtk.org/gtk4/migrating-3to4.html#stop-using-gtk_main-and-related-apis

app/gui/splash.c
@@ -403,8 +403,8 @@ splash_update (const gchar *text1,
   if (splash->timer)
     splash_timer_elapsed ();
 
-  if (gtk_events_pending ())
-    gtk_main_iteration ();
+  while (g_main_context_pending (NULL))
+    g_main_context_iteration(NULL, TRUE);
 }
 
 

libgimp/gimpprogressbar.c
@@ -126,8 +126,8 @@ gimp_progress_bar_start (const gchar *message,
   gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (bar), 0.0);
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
+    while (g_main_context_pending (NULL))
+      g_main_context_iteration(NULL, TRUE);
 }
 
 static void
@@ -139,8 +139,8 @@ gimp_progress_bar_end (gpointer user_data)
   gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (bar), 0.0);
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
+    while (g_main_context_pending (NULL))
+      g_main_context_iteration(NULL, TRUE);
 }
 
 static void
@@ -152,8 +152,8 @@ gimp_progress_bar_set_text (const gchar *message,
   gtk_progress_bar_set_text (GTK_PROGRESS_BAR (bar), message ? message : " ");
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
+    while (g_main_context_pending (NULL))
+      g_main_context_iteration(NULL, TRUE);
 }
 
 static void
@@ -168,8 +168,8 @@ gimp_progress_bar_set_value (gdouble  percentage,
     gtk_progress_bar_pulse (GTK_PROGRESS_BAR (bar));
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
+    while (g_main_context_pending (NULL))
+      g_main_context_iteration(NULL, TRUE);
 }
 
 static void
@@ -180,8 +180,8 @@ gimp_progress_bar_pulse (gpointer user_data)
   gtk_progress_bar_pulse (GTK_PROGRESS_BAR (bar));
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
-    while (gtk_events_pending ())
-      gtk_main_iteration ();
+    while (g_main_context_pending (NULL))
+      g_main_context_iteration(NULL, TRUE);
 }
 
 static GBytes *

libgimpwidgets/gimpbrowser.c
@@ -399,8 +399,8 @@ gimp_browser_show_message (GimpBrowser *browser,
       gimp_browser_set_widget (browser, label);
     }
 
-  while (gtk_events_pending ())
-    gtk_main_iteration ();
+  while (g_main_context_pending (NULL))
+    g_main_context_iteration(NULL, TRUE);
 }
 
 

plug-ins/gimpressionist/repaint.c
@@ -857,8 +857,8 @@ repaint (ppm_t *p, ppm_t *a)
                           "%.1f %%", 100 * (1.0 - ((double)i / max_progress)));
               preview_set_button_label (tmps);
 
-              while(gtk_events_pending())
-                gtk_main_iteration();
+              while (g_main_context_pending (NULL))
+                g_main_context_iteration(NULL, TRUE);
             }
         }
 

plug-ins/script-fu/libscriptfu/scheme-wrapper.c
@@ -1935,8 +1935,8 @@ script_fu_marshal_procedure_call (scheme   *sc,
 
 #ifdef GDK_WINDOWING_WIN32
   /* This seems to help a lot on Windoze. */
-  while (gtk_events_pending ())
-    gtk_main_iteration ();
+  while (g_main_context_pending (NULL))
+    g_main_context_iteration(NULL, TRUE);
 #endif
 
   return return_val;

commit 6b9867a034d7d634c33251806060591b50803f00

app, libgimp, plug-ins: Fix styling
Fallout from commit e90dbe28256a534d9b4f8eb74c62dc808d837e11.

app/gui/splash.c
@@ -404,7 +404,7 @@ splash_update (const gchar *text1,
     splash_timer_elapsed ();
 
   while (g_main_context_pending (NULL))
-    g_main_context_iteration(NULL, TRUE);
+    g_main_context_iteration (NULL, TRUE);
 }
 
 

libgimp/gimpprogressbar.c
@@ -127,7 +127,7 @@ gimp_progress_bar_start (const gchar *message,
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
     while (g_main_context_pending (NULL))
-      g_main_context_iteration(NULL, TRUE);
+      g_main_context_iteration (NULL, TRUE);
 }
 
 static void
@@ -140,7 +140,7 @@ gimp_progress_bar_end (gpointer user_data)
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
     while (g_main_context_pending (NULL))
-      g_main_context_iteration(NULL, TRUE);
+      g_main_context_iteration (NULL, TRUE);
 }
 
 static void
@@ -153,7 +153,7 @@ gimp_progress_bar_set_text (const gchar *message,
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
     while (g_main_context_pending (NULL))
-      g_main_context_iteration(NULL, TRUE);
+      g_main_context_iteration (NULL, TRUE);
 }
 
 static void
@@ -169,7 +169,7 @@ gimp_progress_bar_set_value (gdouble  percentage,
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
     while (g_main_context_pending (NULL))
-      g_main_context_iteration(NULL, TRUE);
+      g_main_context_iteration (NULL, TRUE);
 }
 
 static void
@@ -181,7 +181,7 @@ gimp_progress_bar_pulse (gpointer user_data)
 
   if (gtk_widget_is_drawable (GTK_WIDGET (bar)))
     while (g_main_context_pending (NULL))
-      g_main_context_iteration(NULL, TRUE);
+      g_main_context_iteration (NULL, TRUE);
 }
 
 static GBytes *

libgimpwidgets/gimpbrowser.c
@@ -400,7 +400,7 @@ gimp_browser_show_message (GimpBrowser *browser,
     }
 
   while (g_main_context_pending (NULL))
-    g_main_context_iteration(NULL, TRUE);
+    g_main_context_iteration (NULL, TRUE);
 }
 
 

plug-ins/gimpressionist/repaint.c
@@ -858,7 +858,7 @@ repaint (ppm_t *p, ppm_t *a)
               preview_set_button_label (tmps);
 
               while (g_main_context_pending (NULL))
-                g_main_context_iteration(NULL, TRUE);
+                g_main_context_iteration (NULL, TRUE);
             }
         }
 

plug-ins/script-fu/libscriptfu/scheme-wrapper.c
@@ -1936,7 +1936,7 @@ script_fu_marshal_procedure_call (scheme   *sc,
 #ifdef GDK_WINDOWING_WIN32
   /* This seems to help a lot on Windoze. */
   while (g_main_context_pending (NULL))
-    g_main_context_iteration(NULL, TRUE);
+    g_main_context_iteration (NULL, TRUE);
 #endif
 
   return return_val;