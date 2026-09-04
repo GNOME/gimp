bbdf36ac98956c311c6bce036c3e0a1136ddda13
Manu Cornet <m@ma.nu>
2026-09-04 17:33:01 +0900

    app/dialogs: Clean up unused fields in QuitDialog
    (cherry picked from commit 62a299d45b8143b2fc8751bd154d17f8a19a08c3)

--- app/dialogs/quit-dialog.c
+++ app/dialogs/quit-dialog.c
@@ -68,8 +68,6 @@ struct _QuitDialog
   gboolean               do_quit;
 
   GtkWidget             *dialog;
-  GimpContainerTreeView *tree_view;
-  GtkTreeViewColumn     *save_column;
   GtkWidget             *ok_button;
   GimpMessageBox        *box;
   GtkWidget             *lost_label;
