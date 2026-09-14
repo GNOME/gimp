GNOME/gimp

commit 4348a7d5d9d572f4598da1742974b072a751c576

build/macos: Use $CI_COMMIT_REF_PROTECTED instead of $CI_DEFAULT_BRANCH
Otherwise, the .dmg would not be notarized on 3.2.6 testing builds and release.

build/macos/dmg/3_dist-gimp-apple.sh
@@ -243,7 +243,7 @@ printf "\e[0Ksection_end:`date +%s`:${ARCH}_source\r\e[0K\n"
 # 5. FINISH .DMG AS COMPRESSED READ-ONLY
 DMG_ARTIFACT="gimp-${CUSTOM_GIMP_VERSION}-${ARCH}.dmg"
 printf "\e[0Ksection_start:`date +%s`:${ARCH}_making[collapsed=true]\r\e[0KCompressing %s\n" ${DMG_ARTIFACT}
-if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
+if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ]; then
   #Prepare certs to be stored on cert_container
   security delete-keychain cert_container 2>/dev/null || true
   security create-keychain -p "" cert_container
@@ -343,7 +343,7 @@ fi
 hdiutil detach "$DMG_MOUNT"
 hdiutil convert -verbose "temp_$ARCH.dmg" -format ULFO -o "$DMG_ARTIFACT"
 rm "temp_$ARCH.dmg"
-if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
+if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ]; then
   printf '(INFO): signing .dmg\n'
   codesign -s "${codesign_subject}" "$DMG_ARTIFACT"
 fi
@@ -351,7 +351,7 @@ printf "\e[0Ksection_end:`date +%s`:${ARCH}_making\r\e[0K\n"
 
 
 # 6.A NOTARIZE .DMG
-if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
+if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ]; then
   printf "\e[0Ksection_start:`date +%s`:${ARCH}_trust[collapsed=true]\r\e[0KNotarizing ${DMG_ARTIFACT}\n"
   printf "(INFO): submitting to Apple servers\n"
   NOTARY_OUT="$(xcrun notarytool submit ${DMG_ARTIFACT} --apple-id ${notarization_login} --team-id ${notarization_teamid} --password ${notarization_password} --wait 2>&1)"
@@ -395,7 +395,7 @@ printf "(INFO): ${DMG_ARTIFACT} SHA-512: $(echo $sha512 | cut -d ' ' -f 1)\n"
 if [ "$GIMP_RELEASE" ] && [ -z "$GIMP_IS_RC_GIT" ]; then
   echo $sha512 > ${DMG_ARTIFACT}.SHA512SUMS
 fi
-if [ -z "$GITLAB_CI" ] || [ "$CI_COMMIT_BRANCH" != "$CI_DEFAULT_BRANCH" ]; then
+if [ -z "$GITLAB_CI" ] || [ -z "$CI_COMMIT_REF_PROTECTED" ]; then
   printf "(INFO): Suceeded. To test this build, whitelist it from the artifact with: \033[32mxattr -r -d com.apple.quarantine ./${BUNDLE_NAME}\033[0m\n"
 fi
 printf "\e[0Ksection_end:`date +%s`:${ARCH}_trust\r\e[0K\n"
@@ -405,7 +405,7 @@ if [ "$GITLAB_CI" ]; then
   output_dir='build/macos/dmg/_Output'
   mkdir -p $output_dir
   mv -f ${DMG_ARTIFACT}* $output_dir
-  if [ "$CI_COMMIT_BRANCH" != "$CI_DEFAULT_BRANCH" ]; then
+  if [ -z "$CI_COMMIT_REF_PROTECTED" ]; then
     echo "See the instructions on how install at the 'Automatic Development Builds' section of https://www.gimp.org/downloads/devel/#automatic-builds" > $output_dir/IMPORTANT.txt
   fi
 fi

commit ba33e0618429be8ecd2adadff01377f4d7760100

build/macos: Codesigning/notarization should not run on MRs
This fixed a regression introduced by 4348a7d5

build/macos/dmg/3_dist-gimp-apple.sh
@@ -256,7 +256,7 @@ printf "\e[0Ksection_end:`date +%s`:${ARCH}_source\r\e[0K\n"
 # 5. FINISH .DMG AS COMPRESSED READ-ONLY
 DMG_ARTIFACT="gimp-${CUSTOM_GIMP_VERSION}-${ARCH}.dmg"
 printf "\e[0Ksection_start:`date +%s`:${ARCH}_making[collapsed=true]\r\e[0KCompressing %s\n" ${DMG_ARTIFACT}
-if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ]; then
+if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ] && [ "$CI_PIPELINE_SOURCE" != 'merge_request_event' ]; then
   #Prepare certs to be stored on cert_container
   security delete-keychain cert_container 2>/dev/null || true
   security create-keychain -p "" cert_container
@@ -356,7 +356,7 @@ fi
 hdiutil detach "$DMG_MOUNT"
 hdiutil convert -verbose "temp_$ARCH.dmg" -format ULFO -o "$DMG_ARTIFACT"
 rm "temp_$ARCH.dmg"
-if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ]; then
+if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ] && [ "$CI_PIPELINE_SOURCE" != 'merge_request_event' ]; then
   printf '(INFO): signing .dmg\n'
   codesign -s "${codesign_subject}" "$DMG_ARTIFACT"
 fi
@@ -364,7 +364,7 @@ printf "\e[0Ksection_end:`date +%s`:${ARCH}_making\r\e[0K\n"
 
 
 # 6.A NOTARIZE .DMG
-if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ]; then
+if [ "$GITLAB_CI" ] && [ "$CI_COMMIT_REF_PROTECTED" ] && [ "$CI_PIPELINE_SOURCE" != 'merge_request_event' ]; then
   printf "\e[0Ksection_start:`date +%s`:${ARCH}_trust[collapsed=true]\r\e[0KNotarizing ${DMG_ARTIFACT}\n"
   printf "(INFO): submitting to Apple servers\n"
   NOTARY_OUT="$(xcrun notarytool submit ${DMG_ARTIFACT} --apple-id ${notarization_login} --team-id ${notarization_teamid} --password ${notarization_password} --wait 2>&1)"
@@ -408,7 +408,7 @@ printf "(INFO): ${DMG_ARTIFACT} SHA-512: $(echo $sha512 | cut -d ' ' -f 1)\n"
 if [ "$GIMP_RELEASE" ] && [ -z "$GIMP_IS_RC_GIT" ]; then
   echo $sha512 > ${DMG_ARTIFACT}.SHA512SUMS
 fi
-if [ -z "$GITLAB_CI" ] || [ -z "$CI_COMMIT_REF_PROTECTED" ]; then
+if [ -z "$GITLAB_CI" ] || [ -z "$CI_COMMIT_REF_PROTECTED" ] || [ "$CI_PIPELINE_SOURCE" = 'merge_request_event' ]; then
   printf "(INFO): Suceeded. To test this build, whitelist it from the artifact with: \033[32mxattr -r -d com.apple.quarantine ./${BUNDLE_NAME}\033[0m\n"
 fi
 printf "\e[0Ksection_end:`date +%s`:${ARCH}_trust\r\e[0K\n"
@@ -418,7 +418,7 @@ if [ "$GITLAB_CI" ]; then
   output_dir='build/macos/dmg/_Output'
   mkdir -p $output_dir
   mv -f ${DMG_ARTIFACT}* $output_dir
-  if [ -z "$CI_COMMIT_REF_PROTECTED" ]; then
+  if [ -z "$CI_COMMIT_REF_PROTECTED" ] || [ "$CI_PIPELINE_SOURCE" = 'merge_request_event' ]; then
     echo "See the instructions on how install at the 'Automatic Development Builds' section of https://www.gimp.org/downloads/devel/#automatic-builds" > $output_dir/IMPORTANT.txt
   fi
 fi